d0edb159e735939c72480237b0565789510452de
Bastien Le Querrec <bastien@nuolezio.org>
2015-03-19 22:38:32 +0100

    add a tool to import a patch from upstream

---
+++ import-patch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Import a patch from TextSecure and adapt it for SecuredText
+# ../TextSecure must be an updated git tree
+# usage: ./import-patch.sh ID
+
+set -eo pipefail
+
+if [[ $1 == "" ]]; then
+    echo "usage: ./import-patch.sh ID"
+    exit 1
+fi
+
+pushd ../TextSecure > /dev/null
+git checkout $1 2> /dev/null
+git format-patch -1 --minimal 2> /dev/null
+git checkout master > /dev/null 2>&1
+popd > /dev/null
+mv ../TextSecure/*.patch .
+
+./fix-patch.sh
