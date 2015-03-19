SamSprung/Signal-Android

add borderless thumbnails, "bubble" refactor
Closes #2430

// FREEBIE

src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -96,6 +96,7 @@ import org.thoughtcrime.securesms.util.DynamicTheme;
 import org.thoughtcrime.securesms.util.Emoji;
 import org.thoughtcrime.securesms.util.GroupUtil;
 import org.thoughtcrime.securesms.util.MemoryCleaner;
+import org.thoughtcrime.securesms.util.ResUtil;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libaxolotl.InvalidMessageException;
@@ -358,7 +359,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private void handleAbortSecureSession() {
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(R.string.ConversationActivity_abort_secure_session_confirmation);
-    builder.setIcon(Dialogs.resolveIcon(this, R.attr.dialog_alert_icon));
+    builder.setIcon(ResUtil.getDrawable(this, R.attr.dialog_alert_icon));
     builder.setCancelable(true);
     builder.setMessage(R.string.ConversationActivity_are_you_sure_that_you_want_to_abort_this_secure_session_question);
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -405,7 +406,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(getString(R.string.ConversationActivity_leave_group));
-    builder.setIcon(Dialogs.resolveIcon(this, R.attr.dialog_info_icon));
+    builder.setIcon(ResUtil.getDrawable(this, R.attr.dialog_info_icon));
     builder.setCancelable(true);
     builder.setMessage(getString(R.string.ConversationActivity_are_you_sure_you_want_to_leave_this_group));
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -498,7 +499,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private void handleDeleteThread() {
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(R.string.ConversationActivity_delete_thread_confirmation);
-    builder.setIcon(Dialogs.resolveIcon(this, R.attr.dialog_alert_icon));
+    builder.setIcon(ResUtil.getDrawable(this, R.attr.dialog_alert_icon));
     builder.setCancelable(true);
     builder.setMessage(R.string.ConversationActivity_are_you_sure_that_you_want_to_permanently_delete_this_conversation_question);
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {

src/org/thoughtcrime/securesms/ImageMediaAdapter.java
@@ -19,24 +19,25 @@ package org.thoughtcrime.securesms;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import org.thoughtcrime.securesms.ImageMediaAdapter.ViewHolder;
+import org.thoughtcrime.securesms.components.ForegroundImageView;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.CursorRecyclerViewAdapter;
 import org.thoughtcrime.securesms.database.PartDatabase.ImageRecord;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
-import org.thoughtcrime.securesms.recipients.RecipientFormattingException;
 import org.thoughtcrime.securesms.recipients.Recipients;
+import org.thoughtcrime.securesms.util.FutureTaskListener;
 import org.thoughtcrime.securesms.util.MediaUtil;
 
 import ws.com.google.android.mms.pdu.PduPart;
@@ -48,11 +49,11 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
   private final int          gridSize;
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
-    public ImageView   imageView;
+    public ForegroundImageView imageView;
 
     public ViewHolder(View v) {
       super(v);
-      imageView = (ImageView) v.findViewById(R.id.image);
+      imageView = (ForegroundImageView) v.findViewById(R.id.image);
     }
   }
 
@@ -70,8 +71,8 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
 
   @Override
   public void onBindViewHolder(final ViewHolder viewHolder, final Cursor cursor) {
-    final ImageView   imageView   = viewHolder.imageView;
-    final ImageRecord imageRecord = ImageRecord.from(cursor);
+    final ForegroundImageView imageView   = viewHolder.imageView;
+    final ImageRecord         imageRecord = ImageRecord.from(cursor);
 
     PduPart part = new PduPart();
 
@@ -79,8 +80,26 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
     part.setContentType(imageRecord.getContentType().getBytes());
     part.setId(imageRecord.getPartId());
 
+    imageView.setVisibility(View.INVISIBLE);
     Slide slide = MediaUtil.getSlideForPart(getContext(), masterSecret, part, imageRecord.getContentType());
-    if (slide != null) slide.setThumbnailOn(getContext(), imageView, gridSize, gridSize, new ColorDrawable(0x11ffffff));
+    if (slide != null) {
+      slide.getThumbnail(getContext()).addListener(new FutureTaskListener<Pair<Drawable, Boolean>>() {
+        @Override
+        public void onSuccess(final Pair<Drawable, Boolean> result) {
+          imageView.post(new Runnable() {
+            @Override
+            public void run() {
+              imageView.show(result.first, false);
+            }
+          });
+        }
+
+        @Override
+        public void onFailure(Throwable error) {
+          Log.w(TAG, error);
+        }
+      });
+    }
 
     imageView.setOnClickListener(new OnMediaClickListener(imageRecord));
   }

src/org/thoughtcrime/securesms/MediaPreviewActivity.java
@@ -20,6 +20,7 @@ import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.opengl.GLES20;
 import android.os.AsyncTask;
@@ -31,12 +32,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.Recipient.RecipientModifiedListener;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
@@ -66,7 +69,6 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
   private MasterSecret masterSecret;
   private boolean      paused;
 
-  private View              loadingView;
   private TextView          errorText;
   private Bitmap            bitmap;
   private ImageView         image;
@@ -82,6 +84,7 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
     dynamicLanguage.onCreate(this);
 
     super.onCreate(bundle);
+
     setFullscreenIfPossible();
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                          WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -146,7 +149,6 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
   }
 
   private void initializeViews() {
-    loadingView   =             findViewById(R.id.loading_indicator);
     errorText     = (TextView)  findViewById(R.id.error);
     image         = (ImageView) findViewById(R.id.image);
     imageAttacher = new PhotoViewAttacher(image);
@@ -191,6 +193,13 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
   }
 
   private void displayImage() {
+    try {
+      image.setImageBitmap(BitmapFactory.decodeStream(PartAuthority.getThumbnail(this, masterSecret, mediaUri)));
+      image.setVisibility(View.VISIBLE);
+    } catch (IOException fnfe) {
+      Log.w(TAG, "tried to render thumbnail, but it wasn't found. carrying on.");
+    }
+
     new AsyncTask<Void,Void,Bitmap>() {
       @Override
       protected Bitmap doInBackground(Void... params) {
@@ -207,18 +216,12 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
       }
 
       @Override
-      protected void onPreExecute() {
-        loadingView.setVisibility(View.VISIBLE);
-      }
-
-      @Override
       protected void onPostExecute(Bitmap bitmap) {
         if (paused) {
           if (bitmap != null) bitmap.recycle();
           return;
         }
 
-        loadingView.setVisibility(View.GONE);
         if (bitmap == null) {
           errorText.setText(R.string.MediaPreviewActivity_cant_display);
           errorText.setVisibility(View.VISIBLE);

src/org/thoughtcrime/securesms/components/IncomingBubbleContainer.java
@@ -1,0 +1,87 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms.components;
+
+import android.content.Context;
+import android.support.annotation.DrawableRes;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.util.ResUtil;
+
+public class IncomingBubbleContainer extends BubbleContainer {
+  private static final String TAG = IncomingBubbleContainer.class.getSimpleName();
+
+  private static final boolean[] CORNERS_MESSAGE_CAPTIONED = new boolean[]{false, true, true, true };
+  private static final boolean[] CORNERS_MEDIA_CAPTIONED   = new boolean[]{true,  true, true, false};
+  private static final boolean[] CORNERS_ROUNDED           = new boolean[]{true,  true, true, true };
+
+  private int foregroundColor;
+  private int triangleTickRes;
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context) {
+    super(context);
+  }
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    super(context, attrs, defStyleAttr, defStyleRes);
+  }
+
+  @Override
+  protected void onCreateView() {
+    Log.w(TAG, "onCreateView()");
+    LayoutInflater inflater = LayoutInflater.from(getContext());
+    inflater.inflate(R.layout.conversation_bubble_incoming, this, true);
+
+    this.foregroundColor = ResUtil.getColor(getContext(), R.attr.conversation_item_received_background);
+    this.triangleTickRes = ResUtil.getDrawableRes(getContext(), R.attr.triangle_tick_incoming);
+  }
+
+  @Override
+  protected int getForegroundColor(@TransportState int transportState) {
+    return foregroundColor;
+  }
+
+  @Override
+  protected boolean[] getMessageCorners(@MediaState int mediaState) {
+    return mediaState == MEDIA_STATE_CAPTIONED ? CORNERS_MESSAGE_CAPTIONED : CORNERS_ROUNDED;
+  }
+
+  @Override
+  protected boolean[] getMediaCorners(@MediaState int mediaState) {
+    return mediaState == MEDIA_STATE_CAPTIONED ? CORNERS_MEDIA_CAPTIONED : CORNERS_ROUNDED;
+  }
+
+  @Override
+  protected int getTriangleTickRes(@TransportState int transportState) {
+    return triangleTickRes;
+  }
+}

src/org/thoughtcrime/securesms/preferences/AppProtectionPreferenceFragment.java
@@ -23,6 +23,7 @@ import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.crypto.MasterSecretUtil;
 import org.thoughtcrime.securesms.service.KeyCachingService;
 import org.thoughtcrime.securesms.util.Dialogs;
+import org.thoughtcrime.securesms.util.ResUtil;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 
 import java.util.concurrent.TimeUnit;
@@ -123,7 +124,7 @@ public class AppProtectionPreferenceFragment extends PreferenceFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.ApplicationPreferencesActivity_disable_storage_encryption);
         builder.setMessage(R.string.ApplicationPreferencesActivity_warning_this_will_disable_storage_encryption_for_all_messages);
-        builder.setIcon(Dialogs.resolveIcon(getActivity(), R.attr.dialog_alert_icon));
+        builder.setIcon(ResUtil.getDrawable(getActivity(), R.attr.dialog_alert_icon));
         builder.setPositiveButton(R.string.ApplicationPreferencesActivity_disable, new DialogInterface.OnClickListener() {
           @Override
           public void onClick(DialogInterface dialog, int which) {

src/org/thoughtcrime/securesms/preferences/LedBlinkPatternListPreference.java
@@ -33,6 +33,7 @@ import android.widget.Toast;
 
 import org.thoughtcrime.securesms.ApplicationPreferencesActivity;
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.util.ResUtil;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Dialogs;
 
@@ -82,7 +83,7 @@ public class LedBlinkPatternListPreference extends ListPreference implements OnS
 
   private void initializeDialog(View view) {
     AlertDialog.Builder builder = new AlertDialog.Builder(context);
-    builder.setIcon(Dialogs.resolveIcon(context, R.attr.dialog_info_icon));
+    builder.setIcon(ResUtil.getDrawable(context, R.attr.dialog_info_icon));
     builder.setTitle(R.string.preferences__pref_led_blink_custom_pattern_title);
     builder.setView(view);
     builder.setOnCancelListener(new CustomDialogCancelListener());

typ-AhmedSleem/Silence-Android

src/org/SecuredText/SecuredText/ConversationActivity.java
@@ -99,6 +99,7 @@ import org.SecuredText.SecuredText.util.DynamicTheme;
 import org.SecuredText.SecuredText.util.Emoji;
 import org.SecuredText.SecuredText.util.GroupUtil;
 import org.SecuredText.SecuredText.util.MemoryCleaner;
+import org.SecuredText.SecuredText.util.ResUtil;
 import org.SecuredText.SecuredText.util.SecuredTextPreferences;
 import org.SecuredText.SecuredText.util.Util;
 import org.whispersystems.libaxolotl.InvalidMessageException;
@@ -392,7 +393,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private void handleAbortSecureSession() {
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(R.string.ConversationActivity_abort_secure_session_confirmation);
-    builder.setIcon(Dialogs.resolveIcon(this, R.attr.dialog_alert_icon));
+    builder.setIcon(ResUtil.getDrawable(this, R.attr.dialog_alert_icon));
     builder.setCancelable(true);
     builder.setMessage(R.string.ConversationActivity_are_you_sure_that_you_want_to_abort_this_secure_session_question);
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -439,7 +440,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(getString(R.string.ConversationActivity_leave_group));
-    builder.setIcon(Dialogs.resolveIcon(this, R.attr.dialog_info_icon));
+    builder.setIcon(ResUtil.getDrawable(this, R.attr.dialog_info_icon));
     builder.setCancelable(true);
     builder.setMessage(getString(R.string.ConversationActivity_are_you_sure_you_want_to_leave_this_group));
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -532,7 +533,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private void handleDeleteThread() {
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(R.string.ConversationActivity_delete_thread_confirmation);
-    builder.setIcon(Dialogs.resolveIcon(this, R.attr.dialog_alert_icon));
+    builder.setIcon(ResUtil.getDrawable(this, R.attr.dialog_alert_icon));
     builder.setCancelable(true);
     builder.setMessage(R.string.ConversationActivity_are_you_sure_that_you_want_to_permanently_delete_this_conversation_question);
     builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {

src/org/SecuredText/SecuredText/ImageMediaAdapter.java
@@ -19,24 +19,25 @@ package org.SecuredText.SecuredText;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import org.SecuredText.SecuredText.ImageMediaAdapter.ViewHolder;
+import org.SecuredText.SecuredText.components.ForegroundImageView;
 import org.SecuredText.SecuredText.crypto.MasterSecret;
 import org.SecuredText.SecuredText.database.CursorRecyclerViewAdapter;
 import org.SecuredText.SecuredText.database.PartDatabase.ImageRecord;
 import org.SecuredText.SecuredText.mms.Slide;
 import org.SecuredText.SecuredText.recipients.RecipientFactory;
-import org.SecuredText.SecuredText.recipients.RecipientFormattingException;
 import org.SecuredText.SecuredText.recipients.Recipients;
+import org.SecuredText.SecuredText.util.FutureTaskListener;
 import org.SecuredText.SecuredText.util.MediaUtil;
 
 import ws.com.google.android.mms.pdu.PduPart;
@@ -48,11 +49,11 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
   private final int          gridSize;
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
-    public ImageView   imageView;
+    public ForegroundImageView imageView;
 
     public ViewHolder(View v) {
       super(v);
-      imageView = (ImageView) v.findViewById(R.id.image);
+      imageView = (ForegroundImageView) v.findViewById(R.id.image);
     }
   }
 
@@ -70,8 +71,8 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
 
   @Override
   public void onBindViewHolder(final ViewHolder viewHolder, final Cursor cursor) {
-    final ImageView   imageView   = viewHolder.imageView;
-    final ImageRecord imageRecord = ImageRecord.from(cursor);
+    final ForegroundImageView imageView   = viewHolder.imageView;
+    final ImageRecord         imageRecord = ImageRecord.from(cursor);
 
     PduPart part = new PduPart();
 
@@ -79,8 +80,26 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
     part.setContentType(imageRecord.getContentType().getBytes());
     part.setId(imageRecord.getPartId());
 
+    imageView.setVisibility(View.INVISIBLE);
     Slide slide = MediaUtil.getSlideForPart(getContext(), masterSecret, part, imageRecord.getContentType());
-    if (slide != null) slide.setThumbnailOn(getContext(), imageView, gridSize, gridSize, new ColorDrawable(0x11ffffff));
+    if (slide != null) {
+      slide.getThumbnail(getContext()).addListener(new FutureTaskListener<Pair<Drawable, Boolean>>() {
+        @Override
+        public void onSuccess(final Pair<Drawable, Boolean> result) {
+          imageView.post(new Runnable() {
+            @Override
+            public void run() {
+              imageView.show(result.first, false);
+            }
+          });
+        }
+
+        @Override
+        public void onFailure(Throwable error) {
+          Log.w(TAG, error);
+        }
+      });
+    }
 
     imageView.setOnClickListener(new OnMediaClickListener(imageRecord));
   }

src/org/SecuredText/SecuredText/MediaPreviewActivity.java
@@ -20,6 +20,7 @@ import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.opengl.GLES20;
 import android.os.AsyncTask;
@@ -31,12 +32,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.SecuredText.SecuredText.crypto.MasterSecret;
+import org.SecuredText.SecuredText.mms.PartAuthority;
 import org.SecuredText.SecuredText.recipients.Recipient;
 import org.SecuredText.SecuredText.recipients.Recipient.RecipientModifiedListener;
 import org.SecuredText.SecuredText.recipients.RecipientFactory;
@@ -66,7 +69,6 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
   private MasterSecret masterSecret;
   private boolean      paused;
 
-  private View              loadingView;
   private TextView          errorText;
   private Bitmap            bitmap;
   private ImageView         image;
@@ -82,6 +84,7 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
     dynamicLanguage.onCreate(this);
 
     super.onCreate(bundle);
+
     setFullscreenIfPossible();
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                          WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -146,7 +149,6 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
   }
 
   private void initializeViews() {
-    loadingView   =             findViewById(R.id.loading_indicator);
     errorText     = (TextView)  findViewById(R.id.error);
     image         = (ImageView) findViewById(R.id.image);
     imageAttacher = new PhotoViewAttacher(image);
@@ -191,6 +193,13 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
   }
 
   private void displayImage() {
+    try {
+      image.setImageBitmap(BitmapFactory.decodeStream(PartAuthority.getThumbnail(this, masterSecret, mediaUri)));
+      image.setVisibility(View.VISIBLE);
+    } catch (IOException fnfe) {
+      Log.w(TAG, "tried to render thumbnail, but it wasn't found. carrying on.");
+    }
+
     new AsyncTask<Void,Void,Bitmap>() {
       @Override
       protected Bitmap doInBackground(Void... params) {
@@ -207,18 +216,12 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
       }
 
       @Override
-      protected void onPreExecute() {
-        loadingView.setVisibility(View.VISIBLE);
-      }
-
-      @Override
       protected void onPostExecute(Bitmap bitmap) {
         if (paused) {
           if (bitmap != null) bitmap.recycle();
           return;
         }
 
-        loadingView.setVisibility(View.GONE);
         if (bitmap == null) {
           errorText.setText(R.string.MediaPreviewActivity_cant_display);
           errorText.setVisibility(View.VISIBLE);

src/org/SecuredText/SecuredText/components/IncomingBubbleContainer.java
@@ -1,0 +1,87 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.SecuredText.SecuredText.components;
+
+import android.content.Context;
+import android.support.annotation.DrawableRes;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import org.SecuredText.SecuredText.R;
+import org.SecuredText.SecuredText.util.ResUtil;
+
+public class IncomingBubbleContainer extends BubbleContainer {
+  private static final String TAG = IncomingBubbleContainer.class.getSimpleName();
+
+  private static final boolean[] CORNERS_MESSAGE_CAPTIONED = new boolean[]{false, true, true, true };
+  private static final boolean[] CORNERS_MEDIA_CAPTIONED   = new boolean[]{true,  true, true, false};
+  private static final boolean[] CORNERS_ROUNDED           = new boolean[]{true,  true, true, true };
+
+  private int foregroundColor;
+  private int triangleTickRes;
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context) {
+    super(context);
+  }
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @SuppressWarnings("UnusedDeclaration")
+  public IncomingBubbleContainer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    super(context, attrs, defStyleAttr, defStyleRes);
+  }
+
+  @Override
+  protected void onCreateView() {
+    Log.w(TAG, "onCreateView()");
+    LayoutInflater inflater = LayoutInflater.from(getContext());
+    inflater.inflate(R.layout.conversation_bubble_incoming, this, true);
+
+    this.foregroundColor = ResUtil.getColor(getContext(), R.attr.conversation_item_received_background);
+    this.triangleTickRes = ResUtil.getDrawableRes(getContext(), R.attr.triangle_tick_incoming);
+  }
+
+  @Override
+  protected int getForegroundColor(@TransportState int transportState) {
+    return foregroundColor;
+  }
+
+  @Override
+  protected boolean[] getMessageCorners(@MediaState int mediaState) {
+    return mediaState == MEDIA_STATE_CAPTIONED ? CORNERS_MESSAGE_CAPTIONED : CORNERS_ROUNDED;
+  }
+
+  @Override
+  protected boolean[] getMediaCorners(@MediaState int mediaState) {
+    return mediaState == MEDIA_STATE_CAPTIONED ? CORNERS_MEDIA_CAPTIONED : CORNERS_ROUNDED;
+  }
+
+  @Override
+  protected int getTriangleTickRes(@TransportState int transportState) {
+    return triangleTickRes;
+  }
+}

src/org/SecuredText/SecuredText/preferences/AppProtectionPreferenceFragment.java
@@ -23,6 +23,7 @@ import org.SecuredText.SecuredText.crypto.MasterSecret;
 import org.SecuredText.SecuredText.crypto.MasterSecretUtil;
 import org.SecuredText.SecuredText.service.KeyCachingService;
 import org.SecuredText.SecuredText.util.Dialogs;
+import org.SecuredText.SecuredText.util.ResUtil;
 import org.SecuredText.SecuredText.util.SecuredTextPreferences;
 
 import java.util.concurrent.TimeUnit;
@@ -123,7 +124,7 @@ public class AppProtectionPreferenceFragment extends PreferenceFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.ApplicationPreferencesActivity_disable_storage_encryption);
         builder.setMessage(R.string.ApplicationPreferencesActivity_warning_this_will_disable_storage_encryption_for_all_messages);
-        builder.setIcon(Dialogs.resolveIcon(getActivity(), R.attr.dialog_alert_icon));
+        builder.setIcon(ResUtil.getDrawable(getActivity(), R.attr.dialog_alert_icon));
         builder.setPositiveButton(R.string.ApplicationPreferencesActivity_disable, new DialogInterface.OnClickListener() {
           @Override
           public void onClick(DialogInterface dialog, int which) {

src/org/SecuredText/SecuredText/preferences/LedBlinkPatternListPreference.java
@@ -33,6 +33,7 @@ import android.widget.Toast;
 
 import org.SecuredText.SecuredText.ApplicationPreferencesActivity;
 import org.SecuredText.SecuredText.R;
+import org.SecuredText.SecuredText.util.ResUtil;
 import org.SecuredText.SecuredText.util.SecuredTextPreferences;
 import org.SecuredText.SecuredText.util.Dialogs;
 
@@ -82,7 +83,7 @@ public class LedBlinkPatternListPreference extends ListPreference implements OnS
 
   private void initializeDialog(View view) {
     AlertDialog.Builder builder = new AlertDialog.Builder(context);
-    builder.setIcon(Dialogs.resolveIcon(context, R.attr.dialog_info_icon));
+    builder.setIcon(ResUtil.getDrawable(context, R.attr.dialog_info_icon));
     builder.setTitle(R.string.preferences__pref_led_blink_custom_pattern_title);
     builder.setView(view);
     builder.setOnCancelListener(new CustomDialogCancelListener());