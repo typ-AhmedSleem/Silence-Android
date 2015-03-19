SamSprung/Signal-Android

Fix SMIL regression
Closes #2654

// FREEBIE

src/org/thoughtcrime/securesms/jobs/MmsSendJob.java
@@ -22,6 +22,7 @@ import org.thoughtcrime.securesms.transport.UndeliverableMessageException;
 import org.thoughtcrime.securesms.util.Hex;
 import org.thoughtcrime.securesms.util.NumberUtil;
 import org.thoughtcrime.securesms.util.TelephonyUtil;
+import org.thoughtcrime.securesms.util.SmilUtil;
 import org.whispersystems.jobqueue.JobParameters;
 import org.whispersystems.jobqueue.requirements.NetworkRequirement;
 
@@ -98,6 +99,7 @@ public class MmsSendJob extends SendJob {
     MmsRadio radio = MmsRadio.getInstance(context);
 
     try {
+      prepareMessageMedia(masterSecret, message, MediaConstraints.MMS_CONSTRAINTS, true);
       if (isCdmaDevice()) {
         Log.w(TAG, "Sending MMS directly without radio change...");
         try {
@@ -129,9 +131,9 @@ public class MmsSendJob extends SendJob {
         radio.disconnect();
       }
 
-    } catch (MmsRadioException mre) {
-      Log.w(TAG, mre);
-      throw new UndeliverableMessageException(mre);
+    } catch (MmsRadioException | IOException e) {
+      Log.w(TAG, e);
+      throw new UndeliverableMessageException(e);
     }
   }
 
@@ -140,8 +142,6 @@ public class MmsSendJob extends SendJob {
       throws IOException, UndeliverableMessageException, InsecureFallbackApprovalException
   {
     String number = TelephonyUtil.getManager(context).getLine1Number();
-
-    prepareMessageMedia(masterSecret, message, MediaConstraints.MMS_CONSTRAINTS, true);
 
     if (MmsDatabase.Types.isSecureType(message.getDatabaseMessageBox())) {
       throw new UndeliverableMessageException("Attempt to send encrypted MMS?");
@@ -227,6 +227,12 @@ public class MmsSendJob extends SendJob {
     }
   }
 
-
+  @Override
+  protected void prepareMessageMedia(MasterSecret masterSecret, SendReq message,
+                                     MediaConstraints constraints, boolean toMemory)
+      throws IOException, UndeliverableMessageException {
+    super.prepareMessageMedia(masterSecret, message, constraints, toMemory);
+    message.setBody(SmilUtil.getSmilBody(message.getBody()));
+  }
 
 }

src/org/thoughtcrime/securesms/util/SmilUtil.java
@@ -3,6 +3,8 @@ package org.thoughtcrime.securesms.util;
 import android.util.Log;
 
 import org.thoughtcrime.securesms.dom.smil.SmilDocumentImpl;
+import org.thoughtcrime.securesms.dom.smil.parser.SmilXmlSerializer;
+import org.thoughtcrime.securesms.mms.PartParser;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.mms.SlideDeck;
 import org.w3c.dom.smil.SMILDocument;
@@ -14,14 +16,34 @@ import org.w3c.dom.smil.SMILRegionElement;
 import org.w3c.dom.smil.SMILRegionMediaElement;
 import org.w3c.dom.smil.SMILRootLayoutElement;
 
+import java.io.ByteArrayOutputStream;
+
+import ws.com.google.android.mms.ContentType;
+import ws.com.google.android.mms.pdu.PduBody;
+import ws.com.google.android.mms.pdu.PduParser;
+import ws.com.google.android.mms.pdu.PduPart;
+
 public class SmilUtil {
   private static final String TAG = SmilUtil.class.getSimpleName();
 
   public static final int ROOT_HEIGHT = 1024;
   public static final int ROOT_WIDTH  = 1024;
 
-  public static SMILDocument createSmilDocument(SlideDeck deck) {
-    Log.w(TAG, "Creating SMIL document from SlideDeck.");
+  public static PduBody getSmilBody(PduBody body) {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    SmilXmlSerializer.serialize(SmilUtil.createSmilDocument(body), out);
+    PduPart smilPart = new PduPart();
+    smilPart.setContentId("smil".getBytes());
+    smilPart.setContentLocation("smil.xml".getBytes());
+    smilPart.setContentType(ContentType.APP_SMIL.getBytes());
+    smilPart.setData(out.toByteArray());
+    body.addPart(0, smilPart);
+
+    return body;
+  }
+
+  private static SMILDocument createSmilDocument(PduBody body) {
+    Log.w(TAG, "Creating SMIL document from PduBody.");
 
     SMILDocument document = new SmilDocumentImpl();
 
@@ -45,9 +67,10 @@ public class SmilUtil {
     SMILParElement par = (SMILParElement) document.createElement("par");
     bodyElement.appendChild(par);
 
-    for (Slide slide : deck.getSlides()) {
-      SMILRegionElement regionElement = slide.getSmilRegion(document);
-      SMILMediaElement  mediaElement  = slide.getMediaElement(document);
+    for (int i=0; i<body.getPartsNum(); i++) {
+      PduPart part = body.getPart(i);
+      SMILRegionElement regionElement = getRegion(document, part);
+      SMILMediaElement  mediaElement  = getMediaElement(document, part);
 
       if (regionElement != null) {
         ((SMILRegionMediaElement)mediaElement).setRegion(regionElement);
@@ -59,7 +82,42 @@ public class SmilUtil {
     return document;
   }
 
-  public static SMILMediaElement createMediaElement(String tag, SMILDocument document, String src) {
+  private static SMILRegionElement getRegion(SMILDocument document, PduPart part) {
+    if (PartParser.isAudio(part)) return null;
+
+    SMILRegionElement region = (SMILRegionElement) document.createElement("region");
+    if (PartParser.isText(part)) {
+      region.setId("Text");
+      region.setTop(SmilUtil.ROOT_HEIGHT);
+      region.setHeight(50);
+    } else {
+      region.setId("Image");
+      region.setTop(0);
+      region.setHeight(SmilUtil.ROOT_HEIGHT);
+    }
+    region.setLeft(0);
+    region.setWidth(SmilUtil.ROOT_WIDTH);
+    region.setFit("meet");
+    return region;
+  }
+
+  private static SMILMediaElement getMediaElement(SMILDocument document, PduPart part) {
+    final String tag;
+    if (PartParser.isImage(part)) {
+      tag = "img";
+    } else if (PartParser.isAudio(part)) {
+      tag = "audio";
+    } else if (PartParser.isVideo(part)) {
+      tag = "video";
+    } else if (PartParser.isText(part)) {
+      tag = "text";
+    } else {
+      tag = "ref";
+    }
+    return createMediaElement(tag, document, new String(part.getName()));
+  }
+
+  private static SMILMediaElement createMediaElement(String tag, SMILDocument document, String src) {
     SMILMediaElement mediaElement = (SMILMediaElement) document.createElement(tag);
     mediaElement.setSrc(escapeXML(src));
     return mediaElement;

typ-AhmedSleem/Silence-Android

src/org/SecuredText/SecuredText/jobs/MmsSendJob.java
@@ -25,6 +25,7 @@ import org.SecuredText.SecuredText.transport.UndeliverableMessageException;
 import org.SecuredText.SecuredText.util.Hex;
 import org.SecuredText.SecuredText.util.NumberUtil;
 import org.SecuredText.SecuredText.util.TelephonyUtil;
+import org.SecuredText.SecuredText.util.SmilUtil;
 import org.whispersystems.jobqueue.JobParameters;
 import org.whispersystems.jobqueue.requirements.NetworkRequirement;
 import org.whispersystems.libaxolotl.NoSessionException;
@@ -106,6 +107,7 @@ public class MmsSendJob extends SendJob {
     MmsRadio radio = MmsRadio.getInstance(context);
 
     try {
+      prepareMessageMedia(masterSecret, message, MediaConstraints.MMS_CONSTRAINTS, true);
       if (isCdmaDevice()) {
         Log.w(TAG, "Sending MMS directly without radio change...");
         try {
@@ -137,9 +139,9 @@ public class MmsSendJob extends SendJob {
         radio.disconnect();
       }
 
-    } catch (MmsRadioException mre) {
-      Log.w(TAG, mre);
-      throw new UndeliverableMessageException(mre);
+    } catch (MmsRadioException | IOException e) {
+      Log.w(TAG, e);
+      throw new UndeliverableMessageException(e);
     }
   }
 
@@ -149,8 +151,6 @@ public class MmsSendJob extends SendJob {
   {
     String  number         = TelephonyUtil.getManager(context).getLine1Number();
     boolean upgradedSecure = false;
-
-    prepareMessageMedia(masterSecret, message, MediaConstraints.MMS_CONSTRAINTS, true);
 
     if (MmsDatabase.Types.isSecureType(message.getDatabaseMessageBox())) {
       message        = getEncryptedMessage(masterSecret, message);
@@ -250,6 +250,12 @@ public class MmsSendJob extends SendJob {
     }
   }
 
-
+  @Override
+  protected void prepareMessageMedia(MasterSecret masterSecret, SendReq message,
+                                     MediaConstraints constraints, boolean toMemory)
+      throws IOException, UndeliverableMessageException {
+    super.prepareMessageMedia(masterSecret, message, constraints, toMemory);
+    message.setBody(SmilUtil.getSmilBody(message.getBody()));
+  }
 
 }

src/org/SecuredText/SecuredText/util/SmilUtil.java
@@ -3,6 +3,8 @@ package org.SecuredText.SecuredText.util;
 import android.util.Log;
 
 import org.SecuredText.SecuredText.dom.smil.SmilDocumentImpl;
+import org.SecuredText.SecuredText.dom.smil.parser.SmilXmlSerializer;
+import org.SecuredText.SecuredText.mms.PartParser;
 import org.SecuredText.SecuredText.mms.Slide;
 import org.SecuredText.SecuredText.mms.SlideDeck;
 import org.w3c.dom.smil.SMILDocument;
@@ -14,14 +16,34 @@ import org.w3c.dom.smil.SMILRegionElement;
 import org.w3c.dom.smil.SMILRegionMediaElement;
 import org.w3c.dom.smil.SMILRootLayoutElement;
 
+import java.io.ByteArrayOutputStream;
+
+import ws.com.google.android.mms.ContentType;
+import ws.com.google.android.mms.pdu.PduBody;
+import ws.com.google.android.mms.pdu.PduParser;
+import ws.com.google.android.mms.pdu.PduPart;
+
 public class SmilUtil {
   private static final String TAG = SmilUtil.class.getSimpleName();
 
   public static final int ROOT_HEIGHT = 1024;
   public static final int ROOT_WIDTH  = 1024;
 
-  public static SMILDocument createSmilDocument(SlideDeck deck) {
-    Log.w(TAG, "Creating SMIL document from SlideDeck.");
+  public static PduBody getSmilBody(PduBody body) {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    SmilXmlSerializer.serialize(SmilUtil.createSmilDocument(body), out);
+    PduPart smilPart = new PduPart();
+    smilPart.setContentId("smil".getBytes());
+    smilPart.setContentLocation("smil.xml".getBytes());
+    smilPart.setContentType(ContentType.APP_SMIL.getBytes());
+    smilPart.setData(out.toByteArray());
+    body.addPart(0, smilPart);
+
+    return body;
+  }
+
+  private static SMILDocument createSmilDocument(PduBody body) {
+    Log.w(TAG, "Creating SMIL document from PduBody.");
 
     SMILDocument document = new SmilDocumentImpl();
 
@@ -45,9 +67,10 @@ public class SmilUtil {
     SMILParElement par = (SMILParElement) document.createElement("par");
     bodyElement.appendChild(par);
 
-    for (Slide slide : deck.getSlides()) {
-      SMILRegionElement regionElement = slide.getSmilRegion(document);
-      SMILMediaElement  mediaElement  = slide.getMediaElement(document);
+    for (int i=0; i<body.getPartsNum(); i++) {
+      PduPart part = body.getPart(i);
+      SMILRegionElement regionElement = getRegion(document, part);
+      SMILMediaElement  mediaElement  = getMediaElement(document, part);
 
       if (regionElement != null) {
         ((SMILRegionMediaElement)mediaElement).setRegion(regionElement);
@@ -59,7 +82,42 @@ public class SmilUtil {
     return document;
   }
 
-  public static SMILMediaElement createMediaElement(String tag, SMILDocument document, String src) {
+  private static SMILRegionElement getRegion(SMILDocument document, PduPart part) {
+    if (PartParser.isAudio(part)) return null;
+
+    SMILRegionElement region = (SMILRegionElement) document.createElement("region");
+    if (PartParser.isText(part)) {
+      region.setId("Text");
+      region.setTop(SmilUtil.ROOT_HEIGHT);
+      region.setHeight(50);
+    } else {
+      region.setId("Image");
+      region.setTop(0);
+      region.setHeight(SmilUtil.ROOT_HEIGHT);
+    }
+    region.setLeft(0);
+    region.setWidth(SmilUtil.ROOT_WIDTH);
+    region.setFit("meet");
+    return region;
+  }
+
+  private static SMILMediaElement getMediaElement(SMILDocument document, PduPart part) {
+    final String tag;
+    if (PartParser.isImage(part)) {
+      tag = "img";
+    } else if (PartParser.isAudio(part)) {
+      tag = "audio";
+    } else if (PartParser.isVideo(part)) {
+      tag = "video";
+    } else if (PartParser.isText(part)) {
+      tag = "text";
+    } else {
+      tag = "ref";
+    }
+    return createMediaElement(tag, document, new String(part.getName()));
+  }
+
+  private static SMILMediaElement createMediaElement(String tag, SMILDocument document, String src) {
     SMILMediaElement mediaElement = (SMILMediaElement) document.createElement(tag);
     mediaElement.setSrc(escapeXML(src));
     return mediaElement;