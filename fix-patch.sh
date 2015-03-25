e032bf8166d177659a9526d5985bf1bc870773f5
Bastien Le Querrec <bastien@nuolezio.org>
2015-03-25 22:36:44 +0100

    improve tool to import upstream patches

--- fix-patch.sh
+++ fix-patch.sh
@@ -3,3 +3,4 @@
 find . -name "*.patch" -print | xargs sed -i 's/thoughtcrime\/securesms/smssecure\/smssecure/g'
 find . -name "*.patch" -print | xargs sed -i 's/org.thoughtcrime.securesms/org.smssecure.smssecure/g'
 find . -name "*.patch" -print | xargs sed -i 's/org.smssecure.smssecure.util.TextSecure/org.smssecure.smssecure.util.SMSSecure/g'
+find . -name "*.patch" -print | xargs sed -i 's/TextSecurePreferences/SMSSecurePreferences/g'
