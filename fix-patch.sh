c5262cc8668e0ad4363aa138f29268f6510058ac
Bastien Le Querrec <bastien@nuolezio.org>
2015-03-26 17:32:41 +0100

    update issues references in patches

--- fix-patch.sh
+++ fix-patch.sh
@@ -4,3 +4,5 @@ find . -name "*.patch" -print | xargs sed -i 's/thoughtcrime\/securesms/smssecur
 find . -name "*.patch" -print | xargs sed -i 's/org.thoughtcrime.securesms/org.smssecure.smssecure/g'
 find . -name "*.patch" -print | xargs sed -i 's/org.smssecure.smssecure.util.TextSecure/org.smssecure.smssecure.util.SMSSecure/g'
 find . -name "*.patch" -print | xargs sed -i 's/TextSecurePreferences/SMSSecurePreferences/g'
+find . -name "*.patch" -print | xargs sed -i 's/^[Ff]ixes #/Fixes https:\/\/github.com\/WhisperSystems\/TextSecure\/issues\//g'
+find . -name "*.patch" -print | xargs sed -i 's/^[Cc]loses #/Closes https:\/\/github.com\/WhisperSystems\/TextSecure\/pull\//g'
