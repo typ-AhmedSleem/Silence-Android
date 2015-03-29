0d6c52e6539eb5bb2ee01048272883573f8b5b4d
Bastien Le Querrec <bastien@nuolezio.org>
2015-03-29 14:42:01 +0200

    add a block with the version number in preferences

--- src/org/smssecure/smssecure/ApplicationPreferencesActivity.java
+++ src/org/smssecure/smssecure/ApplicationPreferencesActivity.java
@@ -71,6 +71,7 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
   private static final String PREFERENCE_CATEGORY_APPEARANCE     = "preference_category_appearance";
   private static final String PREFERENCE_CATEGORY_STORAGE        = "preference_category_storage";
   private static final String PREFERENCE_CATEGORY_ADVANCED       = "preference_category_advanced";
+  private static final String PREFERENCE_ABOUT                   = "preference_about";
 
   private static final String PUSH_MESSAGING_PREF = "pref_toggle_push_messaging";
 
@@ -179,6 +180,12 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
           .setSummary(AppearancePreferenceFragment.getSummary(getActivity()));
       this.findPreference(PREFERENCE_CATEGORY_STORAGE)
           .setSummary(StoragePreferenceFragment.getSummary(getActivity()));
+
+      String version = String.format(this.getString(R.string.preferences__about_version), BuildConfig.VERSION_NAME);
+      String build   = String.format(this.getString(R.string.preferences__about_build),   BuildConfig.VERSION_CODE);
+
+      this.findPreference(PREFERENCE_ABOUT)
+          .setSummary(version + "\n" + build);
     }
 
     private class CategoryClickListener implements Preference.OnPreferenceClickListener {
