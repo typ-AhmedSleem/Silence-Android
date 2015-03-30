fb89d438c28405fa4999205ac7f34ea78d9bfcbc
Bastien Le Querrec <bastien@nuolezio.org>
2015-03-30 16:46:27 +0200

    change "Git commit" with "Build ID"

--- src/org/smssecure/smssecure/ApplicationPreferencesActivity.java
+++ src/org/smssecure/smssecure/ApplicationPreferencesActivity.java
@@ -181,11 +181,11 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
       this.findPreference(PREFERENCE_CATEGORY_STORAGE)
           .setSummary(StoragePreferenceFragment.getSummary(getActivity()));
 
-      String version        = String.format(this.getString(R.string.preferences__about_version),          BuildConfig.VERSION_NAME);
-      String buildGitCommit = String.format(this.getString(R.string.preferences__about_build_git_commit), BuildConfig.BUILD_GIT_COMMIT);
+      String version = String.format(this.getString(R.string.preferences__about_version),  BuildConfig.VERSION_NAME);
+      String buildID = String.format(this.getString(R.string.preferences__about_build_id), BuildConfig.BUILD_GIT_COMMIT);
 
       this.findPreference(PREFERENCE_ABOUT)
-          .setSummary(version + "\n" + buildGitCommit);
+          .setSummary(version + "\n" + buildID);
     }
 
     private class CategoryClickListener implements Preference.OnPreferenceClickListener {
