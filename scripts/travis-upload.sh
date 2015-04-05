b19e7ff48408147cff2912dc3fe960321fb2fc1d
Bastien Le Querrec <bastien@nuolezio.org>
2015-04-05 15:09:16 +0200

    add upload from travis

---
+++ scripts/travis-upload.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Upload a build to files.smssecure.org
+# usage: ./travis-upload <key>
+
+set -eo pipefail
+
+if [ "$#" -lt 1 ]; then
+    echo "usage: ./travis-upload <key>"
+    exit 1
+fi
+
+if [ `basename $(pwd)` = "scripts" ]; then
+    cd ..
+fi
+
+if [ ! -f "./build/outputs/apk/SMSSecure-debug.apk" ]; then
+    echo "APK not found. Did you run `./gradlew build`?"
+    exit 1
+fi
+
+COMMIT=$(git rev-parse --short HEAD)
+curl --form "fileupload=@./build/outputs/apk/SMSSecure-debug.apk;filename=SMSSecure-debug-$COMMIT.apk" -H "Authorization: KEY $1" https://files.smssecure.org/receive_apk
