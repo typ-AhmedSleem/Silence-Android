bbee1f9e14b5f4cbc5ae836d151a76282f981b77
Carey Metcalfe <carey@cmetcalfe.ca>
2015-05-28 13:30:16 -0400

    Fix PartDatabase tests
    Broken as of 2b28a150ccbd56e270ef8273f523896db9f2d6c0

--- androidTest/java/org/smssecure/smssecure/database/PartDatabaseTest.java
+++ androidTest/java/org/smssecure/smssecure/database/PartDatabaseTest.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 
 import org.smssecure.smssecure.SMSSecureTestCase;
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.database.PartDatabase.PartId;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -12,7 +13,6 @@ import ws.com.google.android.mms.pdu.PduPart;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyFloat;
-import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class PartDatabaseTest extends SMSSecureTestCase {
-  private static final long PART_ID = 1L;
+  private static final PartId PART_ID = new PartId(1L, 1L);
 
   private PartDatabase database;
 
@@ -33,18 +33,18 @@ public class PartDatabaseTest extends SMSSecureTestCase {
   }
 
   public void testTaskNotRunWhenThumbnailExists() throws Exception {
-    when(database.getPart(eq(PART_ID))).thenReturn(getPduPartSkeleton("x/x"));
-    doReturn(mock(InputStream.class)).when(database).getDataStream(any(MasterSecret.class), anyLong(), eq("thumbnail"));
+    when(database.getPart(PART_ID)).thenReturn(getPduPartSkeleton("x/x"));
+    doReturn(mock(InputStream.class)).when(database).getDataStream(any(MasterSecret.class), any(PartId.class), eq("thumbnail"));
 
     database.getThumbnailStream(null, PART_ID);
 
-    verify(database, never()).updatePartThumbnail(any(MasterSecret.class), anyLong(), any(PduPart.class), any(InputStream.class), anyFloat());
+    verify(database, never()).updatePartThumbnail(any(MasterSecret.class), any(PartId.class), any(PduPart.class), any(InputStream.class), anyFloat());
   }
 
   public void testTaskRunWhenThumbnailMissing() throws Exception {
-    when(database.getPart(eq(PART_ID))).thenReturn(getPduPartSkeleton("image/png"));
-    doReturn(null).when(database).getDataStream(any(MasterSecret.class), anyLong(), eq("thumbnail"));
-    doNothing().when(database).updatePartThumbnail(any(MasterSecret.class), anyLong(), any(PduPart.class), any(InputStream.class), anyFloat());
+    when(database.getPart(PART_ID)).thenReturn(getPduPartSkeleton("image/png"));
+    doReturn(null).when(database).getDataStream(any(MasterSecret.class), any(PartId.class), eq("thumbnail"));
+    doNothing().when(database).updatePartThumbnail(any(MasterSecret.class), any(PartId.class), any(PduPart.class), any(InputStream.class), anyFloat());
 
     try {
       database.new ThumbnailFetchCallable(mock(MasterSecret.class), PART_ID).call();
