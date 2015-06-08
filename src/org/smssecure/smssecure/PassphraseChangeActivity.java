03b96bdbd668fe45bafbe0a6a34a23670f6b9dcf
McLoo <McLoo@gmx.com>
2015-06-09 01:34:24 +0200

    add dark theme to PassphraseChangeActivity also adding DynamicLanguage to fix ActionBar language changes
    Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/245c0adeea9352fa2a5be0580de2b3d412c12955

--- src/org/smssecure/smssecure/PassphraseChangeActivity.java
+++ src/org/smssecure/smssecure/PassphraseChangeActivity.java
@@ -31,6 +31,8 @@ import android.widget.Toast;
 import org.smssecure.smssecure.crypto.InvalidPassphraseException;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.crypto.MasterSecretUtil;
+import org.smssecure.smssecure.util.DynamicLanguage;
+import org.smssecure.smssecure.util.DynamicTheme;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
 
 /**
@@ -41,6 +43,9 @@ import org.smssecure.smssecure.util.SMSSecurePreferences;
 
 public class PassphraseChangeActivity extends PassphraseActivity {
 
+  private DynamicTheme    dynamicTheme    = new DynamicTheme();
+  private DynamicLanguage dynamicLanguage = new DynamicLanguage();
+
   private EditText originalPassphrase;
   private EditText newPassphrase;
   private EditText repeatPassphrase;
@@ -50,11 +55,20 @@ public class PassphraseChangeActivity extends PassphraseActivity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
+    dynamicTheme.onCreate(this);
+    dynamicLanguage.onCreate(this);
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.change_passphrase_activity);
 
     initializeResources();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    dynamicTheme.onResume(this);
+    dynamicLanguage.onResume(this);
   }
 
   private void initializeResources() {
