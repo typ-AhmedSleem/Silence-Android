1b94143f2bd902ed69ad72474bb660779f3ea47a
Jake McGinty <me@jake.su>
2015-06-12 15:31:12 -0700

    upgrade glide and RoundedImageView
    Closes https://github.com/WhisperSystems/TextSecure/pull/3417
    // FREEBIE

--- src/org/smssecure/smssecure/contacts/ContactPhotoFactory.java
+++ src/org/smssecure/smssecure/contacts/ContactPhotoFactory.java
@@ -15,7 +15,7 @@ import android.widget.ImageView;
 
 import com.amulyakhare.textdrawable.TextDrawable;
 import com.amulyakhare.textdrawable.util.ColorGenerator;
-import com.makeramen.RoundedDrawable;
+import com.makeramen.roundedimageview.RoundedDrawable;
 
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.recipients.Recipient;
