signalapp/Signal-Android

Colorize conversations.
// FREEBIE

src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -16,14 +16,17 @@
  */
 package org.thoughtcrime.securesms;
 
+import android.annotation.TargetApi;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -40,6 +43,7 @@ import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnKeyListener;
 import android.view.ViewStub;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -49,6 +53,7 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 import com.google.protobuf.ByteString;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import org.thoughtcrime.securesms.TransportOptions.OnTransportChangedListener;
 import org.thoughtcrime.securesms.components.AnimatingToggle;
@@ -58,6 +63,7 @@ import org.thoughtcrime.securesms.components.emoji.EmojiDrawer;
 import org.thoughtcrime.securesms.components.emoji.EmojiToggle;
 import org.thoughtcrime.securesms.contacts.ContactAccessor;
 import org.thoughtcrime.securesms.contacts.ContactAccessor.ContactData;
+import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.crypto.MasterCipher;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.crypto.SecurityEvent;
@@ -165,6 +171,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private DynamicTheme    dynamicTheme    = new DynamicTheme();
   private DynamicLanguage dynamicLanguage = new DynamicLanguage();
 
+  @TargetApi(Build.VERSION_CODES.KITKAT)
   @Override
   protected void onPreCreate() {
     dynamicTheme.onCreate(this);
@@ -219,6 +226,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     initializeIme();
 
     titleView.setTitle(recipients);
+    setActionBarColor(recipients.getColor());
     setBlockedUserState(recipients);
     calculateCharactersRemaining();
 
@@ -797,11 +805,13 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
   @Override
   public void onModified(final Recipients recipients) {
+    Log.w(TAG, "onModified()");
     titleView.post(new Runnable() {
       @Override
       public void run() {
         titleView.setTitle(recipients);
         setBlockedUserState(recipients);
+        setActionBarColor(recipients.getColor());
       }
     });
   }
@@ -977,6 +987,18 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
         return null;
       }
     }.execute(thisThreadId);
+  }
+
+  private void setActionBarColor(Optional<Integer> color) {
+    int colorPrimary     = getResources().getColor(R.color.textsecure_primary);
+    int colorPrimaryDark = getResources().getColor(R.color.textsecure_primary_dark);
+
+    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color.or(colorPrimary)));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      if (color.isPresent()) getWindow().setStatusBarColor(ContactColors.getStatusTinted(color.get()).or(colorPrimaryDark));
+      else                   getWindow().setStatusBarColor(colorPrimaryDark);
+    }
   }
 
   private void setBlockedUserState(Recipients recipients) {

src/org/thoughtcrime/securesms/components/AvatarImageView.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 
+import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
@@ -27,10 +28,11 @@ public class AvatarImageView extends ImageView {
 
   public void setAvatar(@Nullable Recipients recipients, boolean quickContactEnabled) {
     if (recipients != null) {
-      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext()));
+      int backgroundColor = recipients.getColor().or(ContactColors.UNKNOWN_COLOR);
+      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext(), backgroundColor));
       setAvatarClickHandler(recipients, quickContactEnabled);
     } else {
-      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext()));
+      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext(), ContactColors.UNKNOWN_COLOR));
       setOnClickListener(null);
     }
   }

src/org/thoughtcrime/securesms/contacts/avatars/ContactColors.java
@@ -1,0 +1,107 @@
+package org.thoughtcrime.securesms.contacts.avatars;
+
+import android.support.annotation.NonNull;
+import android.util.SparseIntArray;
+
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
+import org.whispersystems.libaxolotl.util.guava.Optional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactColors {
+
+  public static final  int UNKNOWN_COLOR   = 0xff9E9E9E;
+
+  private static final int RED_300         = 0xffE57373;
+  private static final int RED_700         = 0xFFD32F2F;
+  private static final int PINK_300        = 0xffF06292;
+  private static final int PINK_700        = 0xFFC2185B;
+  private static final int PURPLE_300      = 0xffBA68C8;
+  private static final int PURPLE_700      = 0xFF7B1FA2;
+  private static final int DEEP_PURPLE_300 = 0xff9575CD;
+  private static final int DEEP_PURPLE_700 = 0xFF512DA8;
+  private static final int INDIGO_300      = 0xff7986CB;
+  private static final int INDIGO_700      = 0xFF303F9F;
+  private static final int BLUE_300        = 0xff64B5F6;
+  private static final int BLUE_700        = 0xFF1976D2;
+  private static final int LIGHT_BLUE_300  = 0xff4FC3F7;
+  private static final int LIGHT_BLUE_700  = 0xFF0288D1;
+  private static final int CYAN_300        = 0xff4DD0E1;
+  private static final int CYAN_700        = 0xFF0097A7;
+  private static final int TEAL_300        = 0xFF4DB6AC;
+  private static final int TEAL_700        = 0xFF00796B;
+  private static final int GREEN_300       = 0xFF81C784;
+  private static final int GREEN_700       = 0xFF388E3C;
+  private static final int LIGHT_GREEN_300 = 0xFFAED581;
+  private static final int LIGHT_GREEN_700 = 0xFF689F38;
+  private static final int LIME_300        = 0xFFDCE775;
+  private static final int LIME_700        = 0xFFAFB42B;
+  private static final int YELLOW_300      = 0xFFFFF176;
+  private static final int YELLOW_700      = 0xFFFBC02D;
+  private static final int AMBER_300       = 0xFFFFD54F;
+  private static final int AMBER_700       = 0xFFFFA000;
+  private static final int ORANGE_300      = 0xFFFFB74D;
+  private static final int ORANGE_700      = 0xFFF57C00;
+  private static final int DEEP_ORANGE_300 = 0xFFFF8A65;
+  private static final int DEEP_ORANGE_700 = 0xFFE64A19;
+  private static final int BROWN_300       = 0xFFA1887F;
+  private static final int BROWN_700       = 0xFF5D4037;
+  private static final int BLUE_GREY_300   = 0xFF90A4AE;
+  private static final int BLUE_GREY_700   = 0xFF455A64;
+
+  private static final List<Integer> MATERIAL_300 = new ArrayList<>(Arrays.asList(
+      RED_300,
+      PINK_300,
+      PURPLE_300,
+      DEEP_PURPLE_300,
+      INDIGO_300,
+      BLUE_300,
+      LIGHT_BLUE_300,
+      CYAN_300,
+      TEAL_300,
+      GREEN_300,
+      LIGHT_GREEN_300,
+      LIME_300,
+      AMBER_300,
+      ORANGE_300,
+      DEEP_ORANGE_300,
+      BROWN_300,
+      BLUE_GREY_300)
+  );
+
+  private static final SparseIntArray MATERIAL_300_TO_700 = new SparseIntArray() {{
+    put(RED_300, RED_700);
+    put(PINK_300, PINK_700);
+    put(PURPLE_300, PURPLE_700);
+    put(DEEP_PURPLE_300, DEEP_PURPLE_700);
+    put(INDIGO_300, INDIGO_700);
+    put(BLUE_300, BLUE_700);
+    put(LIGHT_BLUE_300, LIGHT_BLUE_700);
+    put(CYAN_300, CYAN_700);
+    put(TEAL_300, TEAL_700);
+    put(GREEN_300, GREEN_700);
+    put(LIGHT_GREEN_300, LIGHT_GREEN_700);
+    put(LIME_300, LIME_700);
+    put(AMBER_300, AMBER_700);
+    put(ORANGE_300, ORANGE_700);
+    put(DEEP_ORANGE_300, DEEP_ORANGE_700);
+    put(BROWN_300, BROWN_700);
+    put(BLUE_GREY_300, BLUE_GREY_700);
+  }};
+
+
+  private static final ColorGenerator MATERIAL_GENERATOR = ColorGenerator.create(MATERIAL_300);
+
+  public static int generateFor(@NonNull String name) {
+    return MATERIAL_GENERATOR.getColor(name);
+  }
+
+  public static Optional<Integer> getStatusTinted(int color) {
+    int statusTinted = MATERIAL_300_TO_700.get(color, -1);
+    return statusTinted == -1 ? Optional.<Integer>absent() : Optional.of(statusTinted);
+  }
+
+}

src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
@@ -46,6 +46,7 @@ import android.util.Log;
 import org.thoughtcrime.securesms.ConversationActivity;
 import org.thoughtcrime.securesms.ConversationListActivity;
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.MmsSmsDatabase;
@@ -193,8 +194,10 @@ public class MessageNotifier {
     List<NotificationItem>     notifications       = notificationState.getNotifications();
     NotificationCompat.Builder builder             = new NotificationCompat.Builder(context);
     Recipient                  recipient           = notifications.get(0).getIndividualRecipient();
-    Drawable                   recipientPhoto      = recipient.getContactPhoto().asDrawable(context);
+    Recipients                 recipients          = notifications.get(0).getRecipients();
     int                        largeIconTargetSize = context.getResources().getDimensionPixelSize(R.dimen.contact_photo_target_size);
+    Drawable                   recipientPhoto      = recipient.getContactPhoto().asDrawable(context, recipients == null ? ContactColors.UNKNOWN_COLOR :
+                                                                                                     recipients.getColor().or(ContactColors.UNKNOWN_COLOR));
 
     if (recipientPhoto != null) {
       Bitmap recipientPhotoBitmap = BitmapUtil.createFromDrawable(recipientPhoto, largeIconTargetSize, largeIconTargetSize);

src/org/thoughtcrime/securesms/recipients/Recipients.java
@@ -22,6 +22,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.util.Patterns;
 
+import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhoto;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
 import org.thoughtcrime.securesms.database.RecipientPreferenceDatabase.RecipientsPreferences;
@@ -32,6 +33,7 @@ import org.thoughtcrime.securesms.util.GroupUtil;
 import org.thoughtcrime.securesms.util.ListenableFutureTask;
 import org.thoughtcrime.securesms.util.NumberUtil;
 import org.thoughtcrime.securesms.util.Util;
+import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -49,10 +51,11 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
   private final Set<RecipientsModifiedListener> listeners = Collections.newSetFromMap(new WeakHashMap<RecipientsModifiedListener, Boolean>());
   private final List<Recipient> recipients;
 
-  private Uri          ringtone          = null;
-  private long         mutedUntil        = 0;
-  private boolean      blocked           = false;
-  private VibrateState vibrate           = VibrateState.DEFAULT;
+  private Uri               ringtone   = null;
+  private long              mutedUntil = 0;
+  private boolean           blocked    = false;
+  private VibrateState      vibrate    = VibrateState.DEFAULT;
+  private Optional<Integer> color      = Optional.absent();
 
   Recipients() {
     this(new LinkedList<Recipient>(), (RecipientsPreferences)null);
@@ -66,6 +69,7 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
       mutedUntil = preferences.getMuteUntil();
       vibrate    = preferences.getVibrateState();
       blocked    = preferences.isBlocked();
+      color      = preferences.getColor();
     }
   }
 
@@ -84,6 +88,7 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
             mutedUntil = result.getMuteUntil();
             vibrate    = result.getVibrateState();
             blocked    = result.isBlocked();
+            color      = result.getColor();
 
             localListeners = new HashSet<>(listeners);
           }
@@ -99,6 +104,21 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
         Log.w(TAG, error);
       }
     });
+  }
+
+  public synchronized Optional<Integer> getColor() {
+    if      (color.isPresent())                   return color;
+    else if (isGroupRecipient())                  return Optional.absent();
+    else if (recipients.get(0).getName() == null) return Optional.absent();
+    else    return Optional.of(ContactColors.generateFor(recipients.get(0).getName()));
+  }
+
+  public void setColor(Optional<Integer> color) {
+    synchronized (this) {
+      this.color = color;
+    }
+
+    notifyListeners();
   }
 
   public synchronized @Nullable Uri getRingtone() {

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/ConversationActivity.java
@@ -16,14 +16,17 @@
  */
 package org.smssecure.smssecure;
 
+import android.annotation.TargetApi;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -40,6 +43,7 @@ import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnKeyListener;
 import android.view.ViewStub;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -49,6 +53,7 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 import com.google.protobuf.ByteString;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import org.smssecure.smssecure.TransportOptions.OnTransportChangedListener;
 import org.smssecure.smssecure.components.AnimatingToggle;
@@ -59,6 +64,7 @@ import org.smssecure.smssecure.components.emoji.EmojiToggle;
 import org.smssecure.smssecure.contacts.ContactAccessor;
 import org.smssecure.smssecure.contacts.ContactAccessor.ContactData;
 import org.smssecure.smssecure.crypto.KeyExchangeInitiator;
+import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.crypto.MasterCipher;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.crypto.SecurityEvent;
@@ -165,9 +171,10 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private boolean    isMmsEnabled = true;
   private boolean    isCharactersLeftViewEnabled;
 
-  private DynamicTheme        dynamicTheme        = new DynamicTheme();
-  private DynamicLanguage     dynamicLanguage     = new DynamicLanguage();
+  private DynamicTheme    dynamicTheme    = new DynamicTheme();
+  private DynamicLanguage dynamicLanguage = new DynamicLanguage();
 
+  @TargetApi(Build.VERSION_CODES.KITKAT)
   @Override
   protected void onPreCreate() {
     dynamicTheme.onCreate(this);
@@ -221,6 +228,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     initializeIme();
 
     titleView.setTitle(recipients);
+    setActionBarColor(recipients.getColor());
     setBlockedUserState(recipients);
     calculateCharactersRemaining();
 
@@ -784,11 +792,13 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
   @Override
   public void onModified(final Recipients recipients) {
+    Log.w(TAG, "onModified()");
     titleView.post(new Runnable() {
       @Override
       public void run() {
         titleView.setTitle(recipients);
         setBlockedUserState(recipients);
+        setActionBarColor(recipients.getColor());
       }
     });
   }
@@ -978,6 +988,18 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }.execute(thisThreadId);
 
     return future;
+  }
+
+  private void setActionBarColor(Optional<Integer> color) {
+    int colorPrimary     = getResources().getColor(R.color.textsecure_primary);
+    int colorPrimaryDark = getResources().getColor(R.color.textsecure_primary_dark);
+
+    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color.or(colorPrimary)));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      if (color.isPresent()) getWindow().setStatusBarColor(ContactColors.getStatusTinted(color.get()).or(colorPrimaryDark));
+      else                   getWindow().setStatusBarColor(colorPrimaryDark);
+    }
   }
 
   private void setBlockedUserState(Recipients recipients) {

src/org/smssecure/smssecure/components/AvatarImageView.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 
+import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
@@ -27,10 +28,11 @@ public class AvatarImageView extends ImageView {
 
   public void setAvatar(@Nullable Recipients recipients, boolean quickContactEnabled) {
     if (recipients != null) {
-      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext()));
+      int backgroundColor = recipients.getColor().or(ContactColors.UNKNOWN_COLOR);
+      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext(), backgroundColor));
       setAvatarClickHandler(recipients, quickContactEnabled);
     } else {
-      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext()));
+      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext(), ContactColors.UNKNOWN_COLOR));
       setOnClickListener(null);
     }
   }

src/org/smssecure/smssecure/contacts/avatars/ContactColors.java
@@ -1,0 +1,107 @@
+package org.smssecure.smssecure.contacts.avatars;
+
+import android.support.annotation.NonNull;
+import android.util.SparseIntArray;
+
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
+import org.whispersystems.libaxolotl.util.guava.Optional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactColors {
+
+  public static final  int UNKNOWN_COLOR   = 0xff9E9E9E;
+
+  private static final int RED_300         = 0xffE57373;
+  private static final int RED_700         = 0xFFD32F2F;
+  private static final int PINK_300        = 0xffF06292;
+  private static final int PINK_700        = 0xFFC2185B;
+  private static final int PURPLE_300      = 0xffBA68C8;
+  private static final int PURPLE_700      = 0xFF7B1FA2;
+  private static final int DEEP_PURPLE_300 = 0xff9575CD;
+  private static final int DEEP_PURPLE_700 = 0xFF512DA8;
+  private static final int INDIGO_300      = 0xff7986CB;
+  private static final int INDIGO_700      = 0xFF303F9F;
+  private static final int BLUE_300        = 0xff64B5F6;
+  private static final int BLUE_700        = 0xFF1976D2;
+  private static final int LIGHT_BLUE_300  = 0xff4FC3F7;
+  private static final int LIGHT_BLUE_700  = 0xFF0288D1;
+  private static final int CYAN_300        = 0xff4DD0E1;
+  private static final int CYAN_700        = 0xFF0097A7;
+  private static final int TEAL_300        = 0xFF4DB6AC;
+  private static final int TEAL_700        = 0xFF00796B;
+  private static final int GREEN_300       = 0xFF81C784;
+  private static final int GREEN_700       = 0xFF388E3C;
+  private static final int LIGHT_GREEN_300 = 0xFFAED581;
+  private static final int LIGHT_GREEN_700 = 0xFF689F38;
+  private static final int LIME_300        = 0xFFDCE775;
+  private static final int LIME_700        = 0xFFAFB42B;
+  private static final int YELLOW_300      = 0xFFFFF176;
+  private static final int YELLOW_700      = 0xFFFBC02D;
+  private static final int AMBER_300       = 0xFFFFD54F;
+  private static final int AMBER_700       = 0xFFFFA000;
+  private static final int ORANGE_300      = 0xFFFFB74D;
+  private static final int ORANGE_700      = 0xFFF57C00;
+  private static final int DEEP_ORANGE_300 = 0xFFFF8A65;
+  private static final int DEEP_ORANGE_700 = 0xFFE64A19;
+  private static final int BROWN_300       = 0xFFA1887F;
+  private static final int BROWN_700       = 0xFF5D4037;
+  private static final int BLUE_GREY_300   = 0xFF90A4AE;
+  private static final int BLUE_GREY_700   = 0xFF455A64;
+
+  private static final List<Integer> MATERIAL_300 = new ArrayList<>(Arrays.asList(
+      RED_300,
+      PINK_300,
+      PURPLE_300,
+      DEEP_PURPLE_300,
+      INDIGO_300,
+      BLUE_300,
+      LIGHT_BLUE_300,
+      CYAN_300,
+      TEAL_300,
+      GREEN_300,
+      LIGHT_GREEN_300,
+      LIME_300,
+      AMBER_300,
+      ORANGE_300,
+      DEEP_ORANGE_300,
+      BROWN_300,
+      BLUE_GREY_300)
+  );
+
+  private static final SparseIntArray MATERIAL_300_TO_700 = new SparseIntArray() {{
+    put(RED_300, RED_700);
+    put(PINK_300, PINK_700);
+    put(PURPLE_300, PURPLE_700);
+    put(DEEP_PURPLE_300, DEEP_PURPLE_700);
+    put(INDIGO_300, INDIGO_700);
+    put(BLUE_300, BLUE_700);
+    put(LIGHT_BLUE_300, LIGHT_BLUE_700);
+    put(CYAN_300, CYAN_700);
+    put(TEAL_300, TEAL_700);
+    put(GREEN_300, GREEN_700);
+    put(LIGHT_GREEN_300, LIGHT_GREEN_700);
+    put(LIME_300, LIME_700);
+    put(AMBER_300, AMBER_700);
+    put(ORANGE_300, ORANGE_700);
+    put(DEEP_ORANGE_300, DEEP_ORANGE_700);
+    put(BROWN_300, BROWN_700);
+    put(BLUE_GREY_300, BLUE_GREY_700);
+  }};
+
+
+  private static final ColorGenerator MATERIAL_GENERATOR = ColorGenerator.create(MATERIAL_300);
+
+  public static int generateFor(@NonNull String name) {
+    return MATERIAL_GENERATOR.getColor(name);
+  }
+
+  public static Optional<Integer> getStatusTinted(int color) {
+    int statusTinted = MATERIAL_300_TO_700.get(color, -1);
+    return statusTinted == -1 ? Optional.<Integer>absent() : Optional.of(statusTinted);
+  }
+
+}

src/org/smssecure/smssecure/notifications/MessageNotifier.java
@@ -46,6 +46,7 @@ import android.util.Log;
 import org.smssecure.smssecure.ConversationActivity;
 import org.smssecure.smssecure.ConversationListActivity;
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.database.MmsSmsDatabase;
@@ -182,8 +183,10 @@ public class MessageNotifier {
     List<NotificationItem>     notifications       = notificationState.getNotifications();
     NotificationCompat.Builder builder             = new NotificationCompat.Builder(context);
     Recipient                  recipient           = notifications.get(0).getIndividualRecipient();
-    Drawable                   recipientPhoto      = recipient.getContactPhoto().asDrawable(context);
+    Recipients                 recipients          = notifications.get(0).getRecipients();
     int                        largeIconTargetSize = context.getResources().getDimensionPixelSize(R.dimen.contact_photo_target_size);
+    Drawable                   recipientPhoto      = recipient.getContactPhoto().asDrawable(context, recipients == null ? ContactColors.UNKNOWN_COLOR :
+                                                                                                     recipients.getColor().or(ContactColors.UNKNOWN_COLOR));
 
     if (recipientPhoto != null) {
       Bitmap recipientPhotoBitmap = BitmapUtil.createFromDrawable(recipientPhoto, largeIconTargetSize, largeIconTargetSize);

src/org/smssecure/smssecure/recipients/Recipients.java
@@ -22,6 +22,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.util.Patterns;
 
+import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.contacts.avatars.ContactPhoto;
 import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory;
 import org.smssecure.smssecure.database.RecipientPreferenceDatabase.RecipientsPreferences;
@@ -32,6 +33,7 @@ import org.smssecure.smssecure.util.GroupUtil;
 import org.smssecure.smssecure.util.ListenableFutureTask;
 import org.smssecure.smssecure.util.NumberUtil;
 import org.smssecure.smssecure.util.Util;
+import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -49,10 +51,11 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
   private final Set<RecipientsModifiedListener> listeners = Collections.newSetFromMap(new WeakHashMap<RecipientsModifiedListener, Boolean>());
   private final List<Recipient> recipients;
 
-  private Uri          ringtone          = null;
-  private long         mutedUntil        = 0;
-  private boolean      blocked           = false;
-  private VibrateState vibrate           = VibrateState.DEFAULT;
+  private Uri               ringtone   = null;
+  private long              mutedUntil = 0;
+  private boolean           blocked    = false;
+  private VibrateState      vibrate    = VibrateState.DEFAULT;
+  private Optional<Integer> color      = Optional.absent();
 
   Recipients() {
     this(new LinkedList<Recipient>(), (RecipientsPreferences)null);
@@ -66,6 +69,7 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
       mutedUntil = preferences.getMuteUntil();
       vibrate    = preferences.getVibrateState();
       blocked    = preferences.isBlocked();
+      color      = preferences.getColor();
     }
   }
 
@@ -84,6 +88,7 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
             mutedUntil = result.getMuteUntil();
             vibrate    = result.getVibrateState();
             blocked    = result.isBlocked();
+            color      = result.getColor();
 
             localListeners = new HashSet<>(listeners);
           }
@@ -99,6 +104,21 @@ public class Recipients implements Iterable<Recipient>, RecipientModifiedListene
         Log.w(TAG, error);
       }
     });
+  }
+
+  public synchronized Optional<Integer> getColor() {
+    if      (color.isPresent())                   return color;
+    else if (isGroupRecipient())                  return Optional.absent();
+    else if (recipients.get(0).getName() == null) return Optional.absent();
+    else    return Optional.of(ContactColors.generateFor(recipients.get(0).getName()));
+  }
+
+  public void setColor(Optional<Integer> color) {
+    synchronized (this) {
+      this.color = color;
+    }
+
+    notifyListeners();
   }
 
   public synchronized @Nullable Uri getRingtone() {