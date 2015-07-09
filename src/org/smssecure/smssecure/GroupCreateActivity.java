SamSprung/Signal-Android

Support for multi-device contact sync.
// FREEBIE

src/org/thoughtcrime/securesms/GroupCreateActivity.java
@@ -44,10 +44,12 @@ import com.google.protobuf.ByteString;
 import com.soundcloud.android.crop.Crop;
 
 import org.thoughtcrime.securesms.components.PushRecipientsPanel;
-import org.thoughtcrime.securesms.contacts.ContactAccessor;
 import org.thoughtcrime.securesms.contacts.RecipientsEditor;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.GroupDatabase;
+import org.thoughtcrime.securesms.database.NotInDirectoryException;
+import org.thoughtcrime.securesms.database.TextSecureDirectory;
 import org.thoughtcrime.securesms.database.ThreadDatabase;
 import org.thoughtcrime.securesms.mms.OutgoingGroupMediaMessage;
 import org.thoughtcrime.securesms.recipients.Recipient;
@@ -63,10 +65,8 @@ import org.thoughtcrime.securesms.util.ProgressDialogAsyncTask;
 import org.thoughtcrime.securesms.util.SelectedRecipientsAdapter;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
-import org.thoughtcrime.securesms.crypto.MasterSecret;
-import org.thoughtcrime.securesms.database.TextSecureDirectory;
-import org.thoughtcrime.securesms.database.NotInDirectoryException;
 import org.whispersystems.textsecure.api.util.InvalidNumberException;
+import org.whispersystems.textsecure.internal.push.TextSecureProtos.GroupContext;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -80,8 +80,6 @@ import java.util.Set;
 
 import ws.com.google.android.mms.MmsException;
 
-import static org.thoughtcrime.securesms.contacts.ContactAccessor.ContactData;
-import static org.whispersystems.textsecure.internal.push.PushMessageProtos.PushMessageContent.GroupContext;
 
 /**
  * Activity to create and update groups

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/GroupCreateActivity.java
@@ -33,9 +33,11 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import org.smssecure.smssecure.components.PushRecipientsPanel;
-import org.smssecure.smssecure.contacts.ContactAccessor;
 import org.smssecure.smssecure.contacts.RecipientsEditor;
+import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
+import org.smssecure.smssecure.database.NotInDirectoryException;
+import org.smssecure.smssecure.database.SMSSecureDirectory;
 import org.smssecure.smssecure.database.ThreadDatabase;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
@@ -44,8 +46,8 @@ import org.smssecure.smssecure.util.DynamicLanguage;
 import org.smssecure.smssecure.util.DynamicTheme;
 import org.smssecure.smssecure.util.SelectedRecipientsAdapter;
 import org.smssecure.smssecure.util.Util;
-import org.smssecure.smssecure.crypto.MasterSecret;
 import org.whispersystems.textsecure.api.util.InvalidNumberException;
+import org.whispersystems.textsecure.internal.push.TextSecureProtos.GroupContext;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -55,7 +57,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-import static org.smssecure.smssecure.contacts.ContactAccessor.ContactData;
 
 /**
  * Activity to create and update groups