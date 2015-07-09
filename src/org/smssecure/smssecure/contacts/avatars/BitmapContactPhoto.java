SamSprung/Signal-Android

Refactor contact photo logic to not reuse drawables.
// FREEBIE

src/org/thoughtcrime/securesms/contacts/avatars/BitmapContactPhoto.java
@@ -1,0 +1,24 @@
+package org.thoughtcrime.securesms.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.makeramen.roundedimageview.RoundedDrawable;
+
+public class BitmapContactPhoto implements ContactPhoto {
+
+  private final Bitmap bitmap;
+
+  BitmapContactPhoto(Bitmap bitmap) {
+    this.bitmap = bitmap;
+  }
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    return RoundedDrawable.fromBitmap(bitmap)
+                          .setScaleType(ImageView.ScaleType.CENTER_CROP)
+                          .setOval(true);
+  }
+}

src/org/thoughtcrime/securesms/contacts/avatars/GeneratedContactPhoto.java
@@ -1,0 +1,37 @@
+package org.thoughtcrime.securesms.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
+import org.thoughtcrime.securesms.R;
+
+public class GeneratedContactPhoto implements ContactPhoto {
+
+  private final String name;
+  private final int    color;
+
+  GeneratedContactPhoto(@NonNull String name) {
+    this(name, ColorGenerator.MATERIAL.getColor(name));
+  }
+
+  GeneratedContactPhoto(@NonNull String name, int color) {
+    this.name  = name;
+    this.color = color;
+  }
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    int targetSize = context.getResources().getDimensionPixelSize(R.dimen.contact_photo_target_size);
+
+    return TextDrawable.builder()
+                       .beginConfig()
+                       .width(targetSize)
+                       .height(targetSize)
+                       .endConfig()
+                       .buildRound(String.valueOf(name.charAt(0)), color);
+  }
+}

src/org/thoughtcrime/securesms/contacts/avatars/ResourceContactPhoto.java
@@ -1,0 +1,46 @@
+package org.thoughtcrime.securesms.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.widget.ImageView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.makeramen.roundedimageview.RoundedDrawable;
+
+public class ResourceContactPhoto implements ContactPhoto {
+
+  private final int resourceId;
+  private final int color;
+
+  ResourceContactPhoto(int resourceId, int color) {
+    this.resourceId = resourceId;
+    this.color      = color;
+  }
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    Drawable        background = TextDrawable.builder().buildRound(" ", color);
+    RoundedDrawable foreground = (RoundedDrawable) RoundedDrawable.fromDrawable(context.getResources().getDrawable(resourceId));
+    foreground.setScaleType(ImageView.ScaleType.CENTER);
+
+    return new ExpandingLayerDrawable(new Drawable[] {background, foreground});
+  }
+
+  private static class ExpandingLayerDrawable extends LayerDrawable {
+    public ExpandingLayerDrawable(Drawable[] layers) {
+      super(layers);
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+      return -1;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+      return -1;
+    }
+  }
+
+}

src/org/thoughtcrime/securesms/contacts/avatars/TransparentContactPhoto.java
@@ -1,0 +1,16 @@
+package org.thoughtcrime.securesms.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.makeramen.roundedimageview.RoundedDrawable;
+
+public class TransparentContactPhoto implements ContactPhoto {
+
+  TransparentContactPhoto() {}
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    return RoundedDrawable.fromDrawable(context.getResources().getDrawable(android.R.color.transparent));
+  }
+}

src/org/thoughtcrime/securesms/recipients/RecipientFactory.java
@@ -20,7 +20,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
-import org.thoughtcrime.securesms.contacts.ContactPhotoFactory;
+import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
 import org.thoughtcrime.securesms.database.CanonicalAddressDatabase;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
@@ -120,7 +120,6 @@ public class RecipientFactory {
   }
 
   public static void clearCache() {
-    ContactPhotoFactory.clearCache();
     provider.clearCache();
   }
 

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/contacts/avatars/BitmapContactPhoto.java
@@ -1,0 +1,24 @@
+package org.smssecure.smssecure.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.makeramen.roundedimageview.RoundedDrawable;
+
+public class BitmapContactPhoto implements ContactPhoto {
+
+  private final Bitmap bitmap;
+
+  BitmapContactPhoto(Bitmap bitmap) {
+    this.bitmap = bitmap;
+  }
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    return RoundedDrawable.fromBitmap(bitmap)
+                          .setScaleType(ImageView.ScaleType.CENTER_CROP)
+                          .setOval(true);
+  }
+}

src/org/smssecure/smssecure/contacts/avatars/GeneratedContactPhoto.java
@@ -1,0 +1,37 @@
+package org.smssecure.smssecure.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
+import org.smssecure.smssecure.R;
+
+public class GeneratedContactPhoto implements ContactPhoto {
+
+  private final String name;
+  private final int    color;
+
+  GeneratedContactPhoto(@NonNull String name) {
+    this(name, ColorGenerator.MATERIAL.getColor(name));
+  }
+
+  GeneratedContactPhoto(@NonNull String name, int color) {
+    this.name  = name;
+    this.color = color;
+  }
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    int targetSize = context.getResources().getDimensionPixelSize(R.dimen.contact_photo_target_size);
+
+    return TextDrawable.builder()
+                       .beginConfig()
+                       .width(targetSize)
+                       .height(targetSize)
+                       .endConfig()
+                       .buildRound(String.valueOf(name.charAt(0)), color);
+  }
+}

src/org/smssecure/smssecure/contacts/avatars/ResourceContactPhoto.java
@@ -1,0 +1,46 @@
+package org.smssecure.smssecure.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.widget.ImageView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.makeramen.roundedimageview.RoundedDrawable;
+
+public class ResourceContactPhoto implements ContactPhoto {
+
+  private final int resourceId;
+  private final int color;
+
+  ResourceContactPhoto(int resourceId, int color) {
+    this.resourceId = resourceId;
+    this.color      = color;
+  }
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    Drawable        background = TextDrawable.builder().buildRound(" ", color);
+    RoundedDrawable foreground = (RoundedDrawable) RoundedDrawable.fromDrawable(context.getResources().getDrawable(resourceId));
+    foreground.setScaleType(ImageView.ScaleType.CENTER);
+
+    return new ExpandingLayerDrawable(new Drawable[] {background, foreground});
+  }
+
+  private static class ExpandingLayerDrawable extends LayerDrawable {
+    public ExpandingLayerDrawable(Drawable[] layers) {
+      super(layers);
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+      return -1;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+      return -1;
+    }
+  }
+
+}

src/org/smssecure/smssecure/contacts/avatars/TransparentContactPhoto.java
@@ -1,0 +1,16 @@
+package org.smssecure.smssecure.contacts.avatars;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.makeramen.roundedimageview.RoundedDrawable;
+
+public class TransparentContactPhoto implements ContactPhoto {
+
+  TransparentContactPhoto() {}
+
+  @Override
+  public Drawable asDrawable(Context context) {
+    return RoundedDrawable.fromDrawable(context.getResources().getDrawable(android.R.color.transparent));
+  }
+}

src/org/smssecure/smssecure/recipients/RecipientFactory.java
@@ -20,7 +20,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
-import org.smssecure.smssecure.contacts.ContactPhotoFactory;
+import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory;
 import org.smssecure.smssecure.database.CanonicalAddressDatabase;
 import org.smssecure.smssecure.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
@@ -120,7 +120,6 @@ public class RecipientFactory {
   }
 
   public static void clearCache() {
-    ContactPhotoFactory.clearCache();
     provider.clearCache();
   }
 