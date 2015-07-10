SamSprung/Signal-Android

Refactor colorization to support dark theme.
// FREEBIE

src/org/thoughtcrime/securesms/ConversationItem.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -39,6 +40,7 @@ import android.widget.Toast;
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 
 import org.thoughtcrime.securesms.ConversationFragment.SelectionClickListener;
+import org.thoughtcrime.securesms.color.ThemeType;
 import org.thoughtcrime.securesms.components.AvatarImageView;
 import org.thoughtcrime.securesms.components.ThumbnailView;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
@@ -192,14 +194,14 @@ public class ConversationItem extends LinearLayout {
   private void setBubbleState(MessageRecord messageRecord) {
     int[]      attributes   = new int[]{R.attr.conversation_item_bubble_background};
     TypedArray colors       = context.obtainStyledAttributes(attributes);
-    int        defaultColor = colors.getColor(0, 0xFFFFFF);
+    int        defaultColor = colors.getColor(0, Color.WHITE);
 
     if (messageRecord.isOutgoing()) {
       bodyBubble.getBackground().setColorFilter(defaultColor, PorterDuff.Mode.MULTIPLY);
     } else {
       bodyBubble.getBackground().setColorFilter(messageRecord.getIndividualRecipient()
                                                              .getColor()
-                                                             .or(defaultColor),
+                                                             .toConversationColor(ThemeType.getCurrent(context)),
                                                 PorterDuff.Mode.MULTIPLY);
     }
 

src/org/thoughtcrime/securesms/color/BlueGreyMaterialColor.java
@@ -1,0 +1,28 @@
+package org.thoughtcrime.securesms.color;
+
+import java.util.HashMap;
+
+class BlueGreyMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "blue_grey";
+
+  BlueGreyMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFECEFF1);
+      put("100", 0xFFCFD8DC);
+      put("200", 0xFFB0BEC5);
+      put("300", 0xFF90A4AE);
+      put("400", 0xFF78909C);
+      put("500", 0xFF607D8B);
+      put("600", 0xFF546E7A);
+      put("700", 0xFF455A64);
+      put("800", 0xFF37474F);
+      put("900", 0xFF263238);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/thoughtcrime/securesms/color/CyanMaterialColor.java
@@ -1,0 +1,32 @@
+package org.thoughtcrime.securesms.color;
+
+import java.util.HashMap;
+
+class CyanMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "cyan";
+
+  CyanMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFE0F7FA);
+      put("100", 0xFFB2EBF2);
+      put("200", 0xFF80DEEA);
+      put("300", 0xFF4DD0E1);
+      put("400", 0xFF26C6DA);
+      put("500", 0xFF00BCD4);
+      put("600", 0xFF00ACC1);
+      put("700", 0xFF0097A7);
+      put("800", 0xFF00838F);
+      put("900", 0xFF006064);
+      put("A100", 0xFF84FFFF);
+      put("A200", 0xFF18FFFF);
+      put("A400", 0xFF00E5FF);
+      put("A700", 0xFF00B8D4);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/thoughtcrime/securesms/color/LightGreenMaterialColor.java
@@ -1,0 +1,32 @@
+package org.thoughtcrime.securesms.color;
+
+import java.util.HashMap;
+
+class LightGreenMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "light_green";
+
+  LightGreenMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFF1F8E9);
+      put("100", 0xFFDCEDC8);
+      put("200", 0xFFC5E1A5);
+      put("300", 0xFFAED581);
+      put("400", 0xFF9CCC65);
+      put("500", 0xFF8BC34A);
+      put("600", 0xFF7CB342);
+      put("700", 0xFF689F38);
+      put("800", 0xFF558B2F);
+      put("900", 0xFF33691E);
+      put("A100", 0xFFCCFF90);
+      put("A200", 0xFFB2FF59);
+      put("A400", 0xFF76FF03);
+      put("A700", 0xFF64DD17);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/thoughtcrime/securesms/color/PinkMaterialColor.java
@@ -1,0 +1,32 @@
+package org.thoughtcrime.securesms.color;
+
+import java.util.HashMap;
+
+public class PinkMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "pink";
+
+  PinkMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFFCE4EC);
+      put("100", 0xFFF8BBD0);
+      put("200", 0xFFF48FB1);
+      put("300", 0xFFF06292);
+      put("400", 0xFFEC407A);
+      put("500", 0xFFE91E63);
+      put("600", 0xFFD81B60);
+      put("700", 0xFFC2185B);
+      put("800", 0xFFAD1457);
+      put("900", 0xFF880E4F);
+      put("A100", 0xFFFF80AB);
+      put("A200", 0xFFFF4081);
+      put("A400", 0xFFF50057);
+      put("A700", 0xFFC51162);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/thoughtcrime/securesms/color/PurpleMaterialColor.java
@@ -1,0 +1,32 @@
+package org.thoughtcrime.securesms.color;
+
+import java.util.HashMap;
+
+class PurpleMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "purple";
+
+  PurpleMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFF3E5F5);
+      put("100", 0xFFE1BEE7);
+      put("200", 0xFFCE93D8);
+      put("300", 0xFFBA68C8);
+      put("400", 0xFFAB47BC);
+      put("500", 0xFF9C27B0);
+      put("600", 0xFF8E24AA);
+      put("700", 0xFF7B1FA2);
+      put("800", 0xFF6A1B9A);
+      put("900", 0xFF4A148C);
+      put("A100", 0xFFEA80FC);
+      put("A200", 0xFFE040FB);
+      put("A400", 0xFFD500F9);
+      put("A700", 0xFFAA00FF);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/thoughtcrime/securesms/color/TealMaterialColor.java
@@ -1,0 +1,32 @@
+package org.thoughtcrime.securesms.color;
+
+import java.util.HashMap;
+
+class TealMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "teal";
+
+  TealMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFE0F2F1);
+      put("100", 0xFFB2DFDB);
+      put("200", 0xFF80CBC4);
+      put("300", 0xFF4DB6AC);
+      put("400", 0xFF26A69A);
+      put("500", 0xFF009688);
+      put("600", 0xFF00897B);
+      put("700", 0xFF00796B);
+      put("800", 0xFF00695C);
+      put("900", 0xFF004D40);
+      put("A100", 0xFFA7FFEB);
+      put("A200", 0xFF64FFDA);
+      put("A400", 0xFF1DE9B6);
+      put("A700", 0xFF00BFA5);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/thoughtcrime/securesms/recipients/Recipient.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import org.thoughtcrime.securesms.color.MaterialColor;
 import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhoto;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
@@ -28,7 +29,6 @@ import org.thoughtcrime.securesms.recipients.RecipientProvider.RecipientDetails;
 import org.thoughtcrime.securesms.util.FutureTaskListener;
 import org.thoughtcrime.securesms.util.GroupUtil;
 import org.thoughtcrime.securesms.util.ListenableFutureTask;
-import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -46,16 +46,17 @@ public class Recipient {
   private String number;
   private String name;
 
-  private ContactPhoto      contactPhoto;
-  private Uri               contactUri;
-  private Optional<Integer> color;
+  private ContactPhoto contactPhoto;
+  private Uri          contactUri;
+
+  @Nullable private MaterialColor color;
 
   Recipient(long recipientId, String number, ListenableFutureTask<RecipientDetails> future)
   {
     this.recipientId  = recipientId;
     this.number       = number;
     this.contactPhoto = ContactPhotoFactory.getLoadingPhoto();
-    this.color        = Optional.absent();
+    this.color        = null;
 
     future.addListener(new FutureTaskListener<RecipientDetails>() {
       @Override
@@ -97,13 +98,13 @@ public class Recipient {
     return this.name;
   }
 
-  public synchronized @NonNull Optional<Integer> getColor() {
-    if      (color.isPresent()) return color;
-    else if (name != null)      return Optional.of(ContactColors.generateFor(name));
-    else                        return Optional.of(ContactColors.UNKNOWN_COLOR);
+  public synchronized @NonNull MaterialColor getColor() {
+    if      (color != null) return color;
+    else if (name != null)  return ContactColors.generateFor(name);
+    else                    return ContactColors.UNKNOWN_COLOR;
   }
 
-  public void setColor(Optional<Integer> color) {
+  public void setColor(@NonNull MaterialColor color) {
     synchronized (this) {
       this.color = color;
     }
@@ -141,8 +142,7 @@ public class Recipient {
 
   public static Recipient getUnknownRecipient() {
     return new Recipient(-1, new RecipientDetails("Unknown", "Unknown", null,
-                                                  ContactPhotoFactory.getDefaultContactPhoto("Unknown"),
-                                                  Optional.<Integer>absent()));
+                                                  ContactPhotoFactory.getDefaultContactPhoto("Unknown"), null));
   }
 
   @Override

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/ConversationItem.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -39,6 +40,7 @@ import android.widget.Toast;
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 
 import org.smssecure.smssecure.ConversationFragment.SelectionClickListener;
+import org.smssecure.smssecure.color.ThemeType;
 import org.smssecure.smssecure.components.AvatarImageView;
 import org.smssecure.smssecure.components.ThumbnailView;
 import org.smssecure.smssecure.crypto.KeyExchangeInitiator;
@@ -193,14 +195,14 @@ public class ConversationItem extends LinearLayout {
   private void setBubbleState(MessageRecord messageRecord) {
     int[]      attributes   = new int[]{R.attr.conversation_item_bubble_background};
     TypedArray colors       = context.obtainStyledAttributes(attributes);
-    int        defaultColor = colors.getColor(0, 0xFFFFFF);
+    int        defaultColor = colors.getColor(0, Color.WHITE);
 
     if (messageRecord.isOutgoing()) {
       bodyBubble.getBackground().setColorFilter(defaultColor, PorterDuff.Mode.MULTIPLY);
     } else {
       bodyBubble.getBackground().setColorFilter(messageRecord.getIndividualRecipient()
                                                              .getColor()
-                                                             .or(defaultColor),
+                                                             .toConversationColor(ThemeType.getCurrent(context)),
                                                 PorterDuff.Mode.MULTIPLY);
     }
 

src/org/smssecure/smssecure/color/BlueGreyMaterialColor.java
@@ -1,0 +1,28 @@
+package org.smssecure.smssecure.color;
+
+import java.util.HashMap;
+
+class BlueGreyMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "blue_grey";
+
+  BlueGreyMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFECEFF1);
+      put("100", 0xFFCFD8DC);
+      put("200", 0xFFB0BEC5);
+      put("300", 0xFF90A4AE);
+      put("400", 0xFF78909C);
+      put("500", 0xFF607D8B);
+      put("600", 0xFF546E7A);
+      put("700", 0xFF455A64);
+      put("800", 0xFF37474F);
+      put("900", 0xFF263238);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/smssecure/smssecure/color/CyanMaterialColor.java
@@ -1,0 +1,32 @@
+package org.smssecure.smssecure.color;
+
+import java.util.HashMap;
+
+class CyanMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "cyan";
+
+  CyanMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFE0F7FA);
+      put("100", 0xFFB2EBF2);
+      put("200", 0xFF80DEEA);
+      put("300", 0xFF4DD0E1);
+      put("400", 0xFF26C6DA);
+      put("500", 0xFF00BCD4);
+      put("600", 0xFF00ACC1);
+      put("700", 0xFF0097A7);
+      put("800", 0xFF00838F);
+      put("900", 0xFF006064);
+      put("A100", 0xFF84FFFF);
+      put("A200", 0xFF18FFFF);
+      put("A400", 0xFF00E5FF);
+      put("A700", 0xFF00B8D4);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/smssecure/smssecure/color/LightGreenMaterialColor.java
@@ -1,0 +1,32 @@
+package org.smssecure.smssecure.color;
+
+import java.util.HashMap;
+
+class LightGreenMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "light_green";
+
+  LightGreenMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFF1F8E9);
+      put("100", 0xFFDCEDC8);
+      put("200", 0xFFC5E1A5);
+      put("300", 0xFFAED581);
+      put("400", 0xFF9CCC65);
+      put("500", 0xFF8BC34A);
+      put("600", 0xFF7CB342);
+      put("700", 0xFF689F38);
+      put("800", 0xFF558B2F);
+      put("900", 0xFF33691E);
+      put("A100", 0xFFCCFF90);
+      put("A200", 0xFFB2FF59);
+      put("A400", 0xFF76FF03);
+      put("A700", 0xFF64DD17);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/smssecure/smssecure/color/PinkMaterialColor.java
@@ -1,0 +1,32 @@
+package org.smssecure.smssecure.color;
+
+import java.util.HashMap;
+
+public class PinkMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "pink";
+
+  PinkMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFFCE4EC);
+      put("100", 0xFFF8BBD0);
+      put("200", 0xFFF48FB1);
+      put("300", 0xFFF06292);
+      put("400", 0xFFEC407A);
+      put("500", 0xFFE91E63);
+      put("600", 0xFFD81B60);
+      put("700", 0xFFC2185B);
+      put("800", 0xFFAD1457);
+      put("900", 0xFF880E4F);
+      put("A100", 0xFFFF80AB);
+      put("A200", 0xFFFF4081);
+      put("A400", 0xFFF50057);
+      put("A700", 0xFFC51162);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/smssecure/smssecure/color/PurpleMaterialColor.java
@@ -1,0 +1,32 @@
+package org.smssecure.smssecure.color;
+
+import java.util.HashMap;
+
+class PurpleMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "purple";
+
+  PurpleMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFF3E5F5);
+      put("100", 0xFFE1BEE7);
+      put("200", 0xFFCE93D8);
+      put("300", 0xFFBA68C8);
+      put("400", 0xFFAB47BC);
+      put("500", 0xFF9C27B0);
+      put("600", 0xFF8E24AA);
+      put("700", 0xFF7B1FA2);
+      put("800", 0xFF6A1B9A);
+      put("900", 0xFF4A148C);
+      put("A100", 0xFFEA80FC);
+      put("A200", 0xFFE040FB);
+      put("A400", 0xFFD500F9);
+      put("A700", 0xFFAA00FF);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/smssecure/smssecure/color/TealMaterialColor.java
@@ -1,0 +1,32 @@
+package org.smssecure.smssecure.color;
+
+import java.util.HashMap;
+
+class TealMaterialColor extends MaterialColor {
+
+  static final String SERIALIZED_NAME = "teal";
+
+  TealMaterialColor() {
+    super(new HashMap<String, Integer>() {{
+      put("50", 0xFFE0F2F1);
+      put("100", 0xFFB2DFDB);
+      put("200", 0xFF80CBC4);
+      put("300", 0xFF4DB6AC);
+      put("400", 0xFF26A69A);
+      put("500", 0xFF009688);
+      put("600", 0xFF00897B);
+      put("700", 0xFF00796B);
+      put("800", 0xFF00695C);
+      put("900", 0xFF004D40);
+      put("A100", 0xFFA7FFEB);
+      put("A200", 0xFF64FFDA);
+      put("A400", 0xFF1DE9B6);
+      put("A700", 0xFF00BFA5);
+    }});
+  }
+
+  @Override
+  public String serialize() {
+    return SERIALIZED_NAME;
+  }
+}

src/org/smssecure/smssecure/recipients/Recipient.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import org.smssecure.smssecure.color.MaterialColor;
 import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.contacts.avatars.ContactPhoto;
 import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory;
@@ -28,7 +29,6 @@ import org.smssecure.smssecure.recipients.RecipientProvider.RecipientDetails;
 import org.smssecure.smssecure.util.FutureTaskListener;
 import org.smssecure.smssecure.util.GroupUtil;
 import org.smssecure.smssecure.util.ListenableFutureTask;
-import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -46,16 +46,17 @@ public class Recipient {
   private String number;
   private String name;
 
-  private ContactPhoto      contactPhoto;
-  private Uri               contactUri;
-  private Optional<Integer> color;
+  private ContactPhoto contactPhoto;
+  private Uri          contactUri;
+
+  @Nullable private MaterialColor color;
 
   Recipient(long recipientId, String number, ListenableFutureTask<RecipientDetails> future)
   {
     this.recipientId  = recipientId;
     this.number       = number;
     this.contactPhoto = ContactPhotoFactory.getLoadingPhoto();
-    this.color        = Optional.absent();
+    this.color        = null;
 
     future.addListener(new FutureTaskListener<RecipientDetails>() {
       @Override
@@ -97,13 +98,13 @@ public class Recipient {
     return this.name;
   }
 
-  public synchronized @NonNull Optional<Integer> getColor() {
-    if      (color.isPresent()) return color;
-    else if (name != null)      return Optional.of(ContactColors.generateFor(name));
-    else                        return Optional.of(ContactColors.UNKNOWN_COLOR);
+  public synchronized @NonNull MaterialColor getColor() {
+    if      (color != null) return color;
+    else if (name != null)  return ContactColors.generateFor(name);
+    else                    return ContactColors.UNKNOWN_COLOR;
   }
 
-  public void setColor(Optional<Integer> color) {
+  public void setColor(@NonNull MaterialColor color) {
     synchronized (this) {
       this.color = color;
     }
@@ -141,8 +142,7 @@ public class Recipient {
 
   public static Recipient getUnknownRecipient() {
     return new Recipient(-1, new RecipientDetails("Unknown", "Unknown", null,
-                                                  ContactPhotoFactory.getDefaultContactPhoto("Unknown"),
-                                                  Optional.<Integer>absent()));
+                                                  ContactPhotoFactory.getDefaultContactPhoto("Unknown"), null));
   }
 
   @Override