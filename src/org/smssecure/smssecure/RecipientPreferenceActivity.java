SamSprung/Signal-Android

Fully colorize conversations.
1. Switch from 300 to 500 colors.

2. Colorize incoming conversation bubbles.

3. Colorize recipeint preference activity toolbar.

4. Support inverted colors in avatars.

5. Make status bar icons tint according to secondary color.

// FREEBIE

src/org/thoughtcrime/securesms/RecipientPreferenceActivity.java
@@ -6,6 +6,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.CheckBoxPreference;
@@ -18,7 +19,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.preference.PreferenceFragment;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -26,6 +26,7 @@ import android.widget.TextView;
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 
 import org.thoughtcrime.securesms.components.AvatarImageView;
+import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.RecipientPreferenceDatabase.VibrateState;
@@ -53,6 +54,7 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
   private final DynamicLanguage dynamicLanguage = new DynamicLanguage();
 
   private AvatarImageView avatar;
+  private Toolbar         toolbar;
   private TextView        title;
   private TextView        blockedIndicator;
 
@@ -103,7 +105,7 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
   }
 
   private void initializeToolbar() {
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    this.toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -115,8 +117,18 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
   }
 
   private void setHeader(Recipients recipients) {
+    Optional<Integer> color = recipients.getColor();
+
     this.avatar.setAvatar(recipients, true);
     this.title.setText(recipients.toShortString());
+    this.toolbar.setBackgroundColor(color.or(getResources().getColor(R.color.textsecure_primary)));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      int primaryDark = getResources().getColor(R.color.textsecure_primary_dark);
+
+      if (color.isPresent()) getWindow().setStatusBarColor(ContactColors.getStatusTinted(color.get()).or(primaryDark));
+      else                   getWindow().setStatusBarColor(primaryDark);
+    }
 
     if (recipients.isBlocked()) this.blockedIndicator.setVisibility(View.VISIBLE);
     else                        this.blockedIndicator.setVisibility(View.GONE);
@@ -207,11 +219,11 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
       }
 
       if (recipients.getColor().isPresent()) {
-        colorPreference.setValue(recipients.getColor().get());
         colorPreference.setEnabled(true);
+        colorPreference.setValue(recipients.getColor().get());
       } else {
-        colorPreference.setValue(getResources().getColor(R.color.textsecure_primary));
         colorPreference.setEnabled(false);
+        colorPreference.setValue(getResources().getColor(R.color.textsecure_primary));
       }
 
       if (!recipients.isSingleRecipient() || recipients.isGroupRecipient()) {
@@ -288,7 +300,7 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
       public boolean onPreferenceChange(Preference preference, Object newValue) {
         final int value = (Integer)newValue;
 
-        if (value != recipients.getColor().get()) {
+        if (preference.isEnabled() && value != recipients.getColor().get()) {
           recipients.setColor(Optional.of(value));
 
           new AsyncTask<Void, Void, Void>() {

src/org/thoughtcrime/securesms/components/AvatarImageView.java
@@ -2,12 +2,14 @@ package org.thoughtcrime.securesms.components;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 
+import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
 import org.thoughtcrime.securesms.recipients.Recipient;
@@ -15,6 +17,8 @@ import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.Recipients;
 
 public class AvatarImageView extends ImageView {
+
+  private boolean inverted;
 
   public AvatarImageView(Context context) {
     super(context);
@@ -24,15 +28,21 @@ public class AvatarImageView extends ImageView {
   public AvatarImageView(Context context, AttributeSet attrs) {
     super(context, attrs);
     setScaleType(ScaleType.CENTER_CROP);
+
+    if (attrs != null) {
+      TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.AvatarImageView, 0, 0);
+      inverted = typedArray.getBoolean(0, false);
+      typedArray.recycle();
+    }
   }
 
   public void setAvatar(@Nullable Recipients recipients, boolean quickContactEnabled) {
     if (recipients != null) {
       int backgroundColor = recipients.getColor().or(ContactColors.UNKNOWN_COLOR);
-      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext(), backgroundColor));
+      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext(), backgroundColor, inverted));
       setAvatarClickHandler(recipients, quickContactEnabled);
     } else {
-      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext(), ContactColors.UNKNOWN_COLOR));
+      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext(), ContactColors.UNKNOWN_COLOR, inverted));
       setOnClickListener(null);
     }
   }

src/org/thoughtcrime/securesms/recipients/Recipient.java
@@ -21,12 +21,14 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import org.thoughtcrime.securesms.contacts.avatars.ContactColors;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhoto;
 import org.thoughtcrime.securesms.contacts.avatars.ContactPhotoFactory;
 import org.thoughtcrime.securesms.recipients.RecipientProvider.RecipientDetails;
 import org.thoughtcrime.securesms.util.FutureTaskListener;
 import org.thoughtcrime.securesms.util.GroupUtil;
 import org.thoughtcrime.securesms.util.ListenableFutureTask;
+import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -44,33 +46,30 @@ public class Recipient {
   private String number;
   private String name;
 
-  private ContactPhoto contactPhoto;
-  private Uri          contactUri;
+  private ContactPhoto      contactPhoto;
+  private Uri               contactUri;
+  private Optional<Integer> color;
 
   Recipient(long recipientId, String number, ListenableFutureTask<RecipientDetails> future)
   {
     this.recipientId  = recipientId;
     this.number       = number;
     this.contactPhoto = ContactPhotoFactory.getLoadingPhoto();
+    this.color        = Optional.absent();
 
     future.addListener(new FutureTaskListener<RecipientDetails>() {
       @Override
       public void onSuccess(RecipientDetails result) {
         if (result != null) {
-          Set<RecipientModifiedListener> localListeners;
-
           synchronized (Recipient.this) {
             Recipient.this.name         = result.name;
             Recipient.this.number       = result.number;
             Recipient.this.contactUri   = result.contactUri;
             Recipient.this.contactPhoto = result.avatar;
-
-            localListeners              = new HashSet<>(listeners);
-            listeners.clear();
+            Recipient.this.color        = result.color;
           }
 
-          for (RecipientModifiedListener listener : localListeners)
-            listener.onModified(Recipient.this);
+          notifyListeners();
         }
       }
 
@@ -87,6 +86,7 @@ public class Recipient {
     this.contactUri   = details.contactUri;
     this.name         = details.name;
     this.contactPhoto = details.avatar;
+    this.color        = details.color;
   }
 
   public synchronized Uri getContactUri() {
@@ -95,6 +95,20 @@ public class Recipient {
 
   public synchronized @Nullable String getName() {
     return this.name;
+  }
+
+  public synchronized @NonNull Optional<Integer> getColor() {
+    if      (color.isPresent()) return color;
+    else if (name != null)      return Optional.of(ContactColors.generateFor(name));
+    else                        return Optional.of(ContactColors.UNKNOWN_COLOR);
+  }
+
+  public void setColor(Optional<Integer> color) {
+    synchronized (this) {
+      this.color = color;
+    }
+
+    notifyListeners();
   }
 
   public String getNumber() {
@@ -126,7 +140,9 @@ public class Recipient {
   }
 
   public static Recipient getUnknownRecipient() {
-    return new Recipient(-1, new RecipientDetails("Unknown", "Unknown", null, ContactPhotoFactory.getDefaultContactPhoto("Unknown")));
+    return new Recipient(-1, new RecipientDetails("Unknown", "Unknown", null,
+                                                  ContactPhotoFactory.getDefaultContactPhoto("Unknown"),
+                                                  Optional.<Integer>absent()));
   }
 
   @Override
@@ -142,6 +158,17 @@ public class Recipient {
   @Override
   public int hashCode() {
     return 31 + (int)this.recipientId;
+  }
+
+  private void notifyListeners() {
+    Set<RecipientModifiedListener> localListeners;
+
+    synchronized (this) {
+      localListeners = new HashSet<>(listeners);
+    }
+
+    for (RecipientModifiedListener listener : localListeners)
+      listener.onModified(Recipient.this);
   }
 
   public interface RecipientModifiedListener {

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/RecipientPreferenceActivity.java
@@ -6,6 +6,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.CheckBoxPreference;
@@ -18,7 +19,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.preference.PreferenceFragment;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -26,6 +26,7 @@ import android.widget.TextView;
 import com.afollestad.materialdialogs.AlertDialogWrapper;
 
 import org.smssecure.smssecure.components.AvatarImageView;
+import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.database.RecipientPreferenceDatabase.VibrateState;
@@ -53,6 +54,7 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
   private final DynamicLanguage dynamicLanguage = new DynamicLanguage();
 
   private AvatarImageView avatar;
+  private Toolbar         toolbar;
   private TextView        title;
   private TextView        blockedIndicator;
 
@@ -103,7 +105,7 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
   }
 
   private void initializeToolbar() {
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    this.toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -115,8 +117,18 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
   }
 
   private void setHeader(Recipients recipients) {
+    Optional<Integer> color = recipients.getColor();
+
     this.avatar.setAvatar(recipients, true);
     this.title.setText(recipients.toShortString());
+    this.toolbar.setBackgroundColor(color.or(getResources().getColor(R.color.textsecure_primary)));
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      int primaryDark = getResources().getColor(R.color.textsecure_primary_dark);
+
+      if (color.isPresent()) getWindow().setStatusBarColor(ContactColors.getStatusTinted(color.get()).or(primaryDark));
+      else                   getWindow().setStatusBarColor(primaryDark);
+    }
 
     if (recipients.isBlocked()) this.blockedIndicator.setVisibility(View.VISIBLE);
     else                        this.blockedIndicator.setVisibility(View.GONE);
@@ -207,11 +219,11 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
       }
 
       if (recipients.getColor().isPresent()) {
-        colorPreference.setValue(recipients.getColor().get());
         colorPreference.setEnabled(true);
+        colorPreference.setValue(recipients.getColor().get());
       } else {
-        colorPreference.setValue(getResources().getColor(R.color.textsecure_primary));
         colorPreference.setEnabled(false);
+        colorPreference.setValue(getResources().getColor(R.color.textsecure_primary));
       }
 
       if (!recipients.isSingleRecipient() || recipients.isGroupRecipient()) {
@@ -288,7 +300,7 @@ public class RecipientPreferenceActivity extends PassphraseRequiredActionBarActi
       public boolean onPreferenceChange(Preference preference, Object newValue) {
         final int value = (Integer)newValue;
 
-        if (value != recipients.getColor().get()) {
+        if (preference.isEnabled() && value != recipients.getColor().get()) {
           recipients.setColor(Optional.of(value));
 
           new AsyncTask<Void, Void, Void>() {

src/org/smssecure/smssecure/components/AvatarImageView.java
@@ -2,12 +2,14 @@ package org.smssecure.smssecure.components;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 
+import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory;
 import org.smssecure.smssecure.recipients.Recipient;
@@ -15,6 +17,8 @@ import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.Recipients;
 
 public class AvatarImageView extends ImageView {
+
+  private boolean inverted;
 
   public AvatarImageView(Context context) {
     super(context);
@@ -24,15 +28,21 @@ public class AvatarImageView extends ImageView {
   public AvatarImageView(Context context, AttributeSet attrs) {
     super(context, attrs);
     setScaleType(ScaleType.CENTER_CROP);
+
+    if (attrs != null) {
+      TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.AvatarImageView, 0, 0);
+      inverted = typedArray.getBoolean(0, false);
+      typedArray.recycle();
+    }
   }
 
   public void setAvatar(@Nullable Recipients recipients, boolean quickContactEnabled) {
     if (recipients != null) {
       int backgroundColor = recipients.getColor().or(ContactColors.UNKNOWN_COLOR);
-      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext(), backgroundColor));
+      setImageDrawable(recipients.getContactPhoto().asDrawable(getContext(), backgroundColor, inverted));
       setAvatarClickHandler(recipients, quickContactEnabled);
     } else {
-      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext(), ContactColors.UNKNOWN_COLOR));
+      setImageDrawable(ContactPhotoFactory.getDefaultContactPhoto(null).asDrawable(getContext(), ContactColors.UNKNOWN_COLOR, inverted));
       setOnClickListener(null);
     }
   }

src/org/smssecure/smssecure/recipients/Recipient.java
@@ -21,12 +21,14 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import org.smssecure.smssecure.contacts.avatars.ContactColors;
 import org.smssecure.smssecure.contacts.avatars.ContactPhoto;
 import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory;
 import org.smssecure.smssecure.recipients.RecipientProvider.RecipientDetails;
 import org.smssecure.smssecure.util.FutureTaskListener;
 import org.smssecure.smssecure.util.GroupUtil;
 import org.smssecure.smssecure.util.ListenableFutureTask;
+import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -44,33 +46,30 @@ public class Recipient {
   private String number;
   private String name;
 
-  private ContactPhoto contactPhoto;
-  private Uri          contactUri;
+  private ContactPhoto      contactPhoto;
+  private Uri               contactUri;
+  private Optional<Integer> color;
 
   Recipient(long recipientId, String number, ListenableFutureTask<RecipientDetails> future)
   {
     this.recipientId  = recipientId;
     this.number       = number;
     this.contactPhoto = ContactPhotoFactory.getLoadingPhoto();
+    this.color        = Optional.absent();
 
     future.addListener(new FutureTaskListener<RecipientDetails>() {
       @Override
       public void onSuccess(RecipientDetails result) {
         if (result != null) {
-          Set<RecipientModifiedListener> localListeners;
-
           synchronized (Recipient.this) {
             Recipient.this.name         = result.name;
             Recipient.this.number       = result.number;
             Recipient.this.contactUri   = result.contactUri;
             Recipient.this.contactPhoto = result.avatar;
-
-            localListeners              = new HashSet<>(listeners);
-            listeners.clear();
+            Recipient.this.color        = result.color;
           }
 
-          for (RecipientModifiedListener listener : localListeners)
-            listener.onModified(Recipient.this);
+          notifyListeners();
         }
       }
 
@@ -87,6 +86,7 @@ public class Recipient {
     this.contactUri   = details.contactUri;
     this.name         = details.name;
     this.contactPhoto = details.avatar;
+    this.color        = details.color;
   }
 
   public synchronized Uri getContactUri() {
@@ -95,6 +95,20 @@ public class Recipient {
 
   public synchronized @Nullable String getName() {
     return this.name;
+  }
+
+  public synchronized @NonNull Optional<Integer> getColor() {
+    if      (color.isPresent()) return color;
+    else if (name != null)      return Optional.of(ContactColors.generateFor(name));
+    else                        return Optional.of(ContactColors.UNKNOWN_COLOR);
+  }
+
+  public void setColor(Optional<Integer> color) {
+    synchronized (this) {
+      this.color = color;
+    }
+
+    notifyListeners();
   }
 
   public String getNumber() {
@@ -126,7 +140,9 @@ public class Recipient {
   }
 
   public static Recipient getUnknownRecipient() {
-    return new Recipient(-1, new RecipientDetails("Unknown", "Unknown", null, ContactPhotoFactory.getDefaultContactPhoto("Unknown")));
+    return new Recipient(-1, new RecipientDetails("Unknown", "Unknown", null,
+                                                  ContactPhotoFactory.getDefaultContactPhoto("Unknown"),
+                                                  Optional.<Integer>absent()));
   }
 
   @Override
@@ -142,6 +158,17 @@ public class Recipient {
   @Override
   public int hashCode() {
     return 31 + (int)this.recipientId;
+  }
+
+  private void notifyListeners() {
+    Set<RecipientModifiedListener> localListeners;
+
+    synchronized (this) {
+      localListeners = new HashSet<>(listeners);
+    }
+
+    for (RecipientModifiedListener listener : localListeners)
+      listener.onModified(Recipient.this);
   }
 
   public interface RecipientModifiedListener {