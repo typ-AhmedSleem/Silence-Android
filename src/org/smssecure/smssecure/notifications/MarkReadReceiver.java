SamSprung/Signal-Android

Fix up wearable reply stuff a little.
1) Don't include MasterSecret in PendingIntents.

2) Correctly reply to non-push group threads, rather than
   just an individual in that group.

// FREEBIE

src/org/thoughtcrime/securesms/notifications/MarkReadReceiver.java
@@ -5,34 +5,38 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 
-public class MarkReadReceiver extends BroadcastReceiver {
+public class MarkReadReceiver extends MasterSecretBroadcastReceiver {
 
-  public static final String CLEAR_ACTION = "org.thoughtcrime.securesms.notifications.CLEAR";
+  private static final String TAG              = MarkReadReceiver.class.getSimpleName();
+  public static final  String CLEAR_ACTION     = "org.thoughtcrime.securesms.notifications.CLEAR";
+  public static final  String THREAD_IDS_EXTRA = "thread_ids";
 
   @Override
-  public void onReceive(final Context context, Intent intent) {
-    if (!intent.getAction().equals(CLEAR_ACTION))
+  protected void onReceive(final Context context, Intent intent,
+                           @Nullable final MasterSecret masterSecret)
+  {
+    if (!CLEAR_ACTION.equals(intent.getAction()))
       return;
 
-    final long[]       threadIds    = intent.getLongArrayExtra("thread_ids");
-    final MasterSecret masterSecret = intent.getParcelableExtra("master_secret");
+    final long[] threadIds = intent.getLongArrayExtra(THREAD_IDS_EXTRA);
 
-    if (threadIds != null && masterSecret != null) {
-      Log.w("MarkReadReceiver", "threadIds length: " + threadIds.length);
+    if (threadIds != null) {
+      Log.w("TAG", "threadIds length: " + threadIds.length);
 
       ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE))
-          .cancel(MessageNotifier.NOTIFICATION_ID);
+                                   .cancel(MessageNotifier.NOTIFICATION_ID);
 
       new AsyncTask<Void, Void, Void>() {
         @Override
         protected Void doInBackground(Void... params) {
           for (long threadId : threadIds) {
-            Log.w("MarkReadReceiver", "Marking as read: " + threadId);
+            Log.w(TAG, "Marking as read: " + threadId);
             DatabaseFactory.getThreadDatabase(context).setRead(threadId);
           }
 

src/org/thoughtcrime/securesms/notifications/MasterSecretBroadcastReceiver.java
@@ -1,0 +1,19 @@
+package org.thoughtcrime.securesms.notifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.service.KeyCachingService;
+
+public abstract class MasterSecretBroadcastReceiver extends BroadcastReceiver {
+
+  @Override
+  public final void onReceive(Context context, Intent intent) {
+    onReceive(context, intent, KeyCachingService.getMasterSecret(context));
+  }
+
+  protected abstract void onReceive(Context context, Intent intent, @Nullable MasterSecret masterSecret);
+}

src/org/thoughtcrime/securesms/notifications/NotificationState.java
@@ -4,11 +4,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.ConversationActivity;
+import org.thoughtcrime.securesms.ConversationPopupActivity;
 import org.thoughtcrime.securesms.database.RecipientPreferenceDatabase.VibrateState;
 import org.thoughtcrime.securesms.recipients.Recipients;
 
@@ -70,39 +70,48 @@ public class NotificationState {
     return notifications;
   }
 
-  public PendingIntent getMarkAsReadIntent(Context context, MasterSecret masterSecret) {
-    Bundle extras = new Bundle();
-    extras.putParcelable("master_secret", masterSecret);
-    return craftIntent(context, MarkReadReceiver.CLEAR_ACTION, extras);
-  }
-
-  public PendingIntent getReplyIntent(Context context, MasterSecret masterSecret, long recipientId) {
-    Bundle extras = new Bundle();
-    extras.putParcelable("master_secret", masterSecret);
-    extras.putLong("recipient_id", recipientId);
-    return craftIntent(context, WearReplyReceiver.REPLY_ACTION, extras);
-  }
-
-  private PendingIntent craftIntent(Context context, String intentAction, Bundle extras) {
+  public PendingIntent getMarkAsReadIntent(Context context) {
     long[] threadArray = new long[threads.size()];
-    int index          = 0;
+    int    index       = 0;
 
     for (long thread : threads) {
       Log.w("NotificationState", "Added thread: " + thread);
       threadArray[index++] = thread;
     }
 
-    Intent intent = new Intent(intentAction);
-    intent.putExtra("thread_ids", threadArray);
-    intent.putExtras(extras);
+    Intent intent = new Intent(MarkReadReceiver.CLEAR_ACTION);
+    intent.putExtra(MarkReadReceiver.THREAD_IDS_EXTRA, threadArray);
     intent.setPackage(context.getPackageName());
 
     // XXX : This is an Android bug.  If we don't pull off the extra
     // once before handing off the PendingIntent, the array will be
     // truncated to one element when the PendingIntent fires.  Thanks guys!
     Log.w("NotificationState", "Pending array off intent length: " +
-            intent.getLongArrayExtra("thread_ids").length);
+        intent.getLongArrayExtra("thread_ids").length);
 
     return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
   }
+
+  public PendingIntent getWearableReplyIntent(Context context, Recipients recipients) {
+    if (threads.size() != 1) throw new AssertionError("We only support replies to single thread notifications!");
+
+    Intent intent = new Intent(WearReplyReceiver.REPLY_ACTION);
+    intent.putExtra(WearReplyReceiver.RECIPIENT_IDS_EXTRA, recipients.getIds());
+    intent.setPackage(context.getPackageName());
+
+    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+  }
+
+  public PendingIntent getQuickReplyIntent(Context context, Recipients recipients) {
+    if (threads.size() != 1) throw new AssertionError("We only support replies to single thread notifications!");
+
+    Intent     intent           = new Intent(context, ConversationPopupActivity.class);
+    intent.putExtra(ConversationActivity.RECIPIENTS_EXTRA, recipients.getIds());
+    intent.putExtra(ConversationActivity.THREAD_ID_EXTRA, (long)threads.toArray()[0]);
+    intent.setData((Uri.parse("custom://"+System.currentTimeMillis())));
+
+    return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+  }
+
+
 }

src/org/thoughtcrime/securesms/notifications/WearReplyReceiver.java
@@ -17,70 +17,67 @@
 
 package org.thoughtcrime.securesms.notifications;
 
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.RemoteInput;
-import android.util.Log;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
-import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.mms.OutgoingMediaMessage;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
-import org.thoughtcrime.securesms.recipients.RecipientProvider;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.sms.MessageSender;
 import org.thoughtcrime.securesms.sms.OutgoingTextMessage;
 
+import ws.com.google.android.mms.pdu.PduBody;
+
 /**
  * Get the response text from the Wearable Device and sends an message as a reply
- *
- * @author Alix Ducros (Ported to TextSecure-Codebase by Christoph Haefner)
  */
-public class WearReplyReceiver extends BroadcastReceiver {
+public class WearReplyReceiver extends MasterSecretBroadcastReceiver {
 
-  public static final String TAG = WearReplyReceiver.class.getSimpleName();
-  public static final String REPLY_ACTION = "org.thoughtcrime.securesms.notifications.WEAR_REPLY";
+  public static final String TAG                 = WearReplyReceiver.class.getSimpleName();
+  public static final String REPLY_ACTION        = "org.thoughtcrime.securesms.notifications.WEAR_REPLY";
+  public static final String RECIPIENT_IDS_EXTRA = "recipient_ids";
 
   @Override
-  public void onReceive(final Context context, Intent intent) {
-    if (!intent.getAction().equals(REPLY_ACTION))
-      return;
+  protected void onReceive(final Context context, Intent intent,
+                           final @Nullable MasterSecret masterSecret)
+  {
+    if (!REPLY_ACTION.equals(intent.getAction())) return;
 
     Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
-    if (remoteInput == null)
-      return;
 
-    final long[] threadIds = intent.getLongArrayExtra("thread_ids");
-    final MasterSecret masterSecret = intent.getParcelableExtra("master_secret");
-    final long recipientId = intent.getLongExtra("recipient_id", -1);
+    if (remoteInput == null) return;
+
+    final long[]       recipientIds = intent.getLongArrayExtra(RECIPIENT_IDS_EXTRA);
     final CharSequence responseText = remoteInput.getCharSequence(MessageNotifier.EXTRA_VOICE_REPLY);
+    final Recipients   recipients   = RecipientFactory.getRecipientsForIds(context, recipientIds, false);
 
-    final Recipients recipients = RecipientFactory.getRecipientsForIds(context, new long[]{recipientId}, false);
-
-    if (threadIds != null && masterSecret != null) {
-
-      ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
-              .cancel(MessageNotifier.NOTIFICATION_ID);
-
+    if (masterSecret != null && responseText != null) {
       new AsyncTask<Void, Void, Void>() {
         @Override
         protected Void doInBackground(Void... params) {
-          for (long threadId : threadIds) {
-            Log.w(TAG, "Marking as read: " + threadId);
-            DatabaseFactory.getThreadDatabase(context).setRead(threadId);
+          long threadId;
+
+          if (recipients.isGroupRecipient()) {
+            OutgoingMediaMessage reply = new OutgoingMediaMessage(context, recipients, new PduBody(), responseText.toString(), 0);
+            threadId = MessageSender.send(context, masterSecret, reply, -1, false);
+          } else {
+            OutgoingTextMessage reply = new OutgoingTextMessage(recipients, responseText.toString());
+            threadId = MessageSender.send(context, masterSecret, reply, -1, false);
           }
 
-          OutgoingTextMessage reply = new OutgoingTextMessage(recipients, responseText.toString());
-          MessageSender.send(context, masterSecret, reply, threadIds[0], false);
-
+          DatabaseFactory.getThreadDatabase(context).setRead(threadId);
           MessageNotifier.updateNotification(context, masterSecret);
+
           return null;
         }
       }.execute();
     }
+
   }
 }

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/notifications/MarkReadReceiver.java
@@ -5,34 +5,38 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
 
-public class MarkReadReceiver extends BroadcastReceiver {
+public class MarkReadReceiver extends MasterSecretBroadcastReceiver {
 
-  public static final String CLEAR_ACTION = "org.smssecure.smssecure.notifications.CLEAR";
+  private static final String TAG              = MarkReadReceiver.class.getSimpleName();
+  public static final  String CLEAR_ACTION     = "org.smssecure.smssecure.notifications.CLEAR";
+  public static final  String THREAD_IDS_EXTRA = "thread_ids";
 
   @Override
-  public void onReceive(final Context context, Intent intent) {
-    if (!intent.getAction().equals(CLEAR_ACTION))
+  protected void onReceive(final Context context, Intent intent,
+                           @Nullable final MasterSecret masterSecret)
+  {
+    if (!CLEAR_ACTION.equals(intent.getAction()))
       return;
 
-    final long[]       threadIds    = intent.getLongArrayExtra("thread_ids");
-    final MasterSecret masterSecret = intent.getParcelableExtra("master_secret");
+    final long[] threadIds = intent.getLongArrayExtra(THREAD_IDS_EXTRA);
 
-    if (threadIds != null && masterSecret != null) {
-      Log.w("MarkReadReceiver", "threadIds length: " + threadIds.length);
+    if (threadIds != null) {
+      Log.w("TAG", "threadIds length: " + threadIds.length);
 
       ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE))
-          .cancel(MessageNotifier.NOTIFICATION_ID);
+                                   .cancel(MessageNotifier.NOTIFICATION_ID);
 
       new AsyncTask<Void, Void, Void>() {
         @Override
         protected Void doInBackground(Void... params) {
           for (long threadId : threadIds) {
-            Log.w("MarkReadReceiver", "Marking as read: " + threadId);
+            Log.w(TAG, "Marking as read: " + threadId);
             DatabaseFactory.getThreadDatabase(context).setRead(threadId);
           }
 

src/org/smssecure/smssecure/notifications/MasterSecretBroadcastReceiver.java
@@ -1,0 +1,19 @@
+package org.smssecure.smssecure.notifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.service.KeyCachingService;
+
+public abstract class MasterSecretBroadcastReceiver extends BroadcastReceiver {
+
+  @Override
+  public final void onReceive(Context context, Intent intent) {
+    onReceive(context, intent, KeyCachingService.getMasterSecret(context));
+  }
+
+  protected abstract void onReceive(Context context, Intent intent, @Nullable MasterSecret masterSecret);
+}

src/org/smssecure/smssecure/notifications/NotificationState.java
@@ -4,11 +4,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.ConversationActivity;
+import org.smssecure.smssecure.ConversationPopupActivity;
 import org.smssecure.smssecure.database.RecipientPreferenceDatabase.VibrateState;
 import org.smssecure.smssecure.recipients.Recipients;
 
@@ -70,39 +70,48 @@ public class NotificationState {
     return notifications;
   }
 
-  public PendingIntent getMarkAsReadIntent(Context context, MasterSecret masterSecret) {
-    Bundle extras = new Bundle();
-    extras.putParcelable("master_secret", masterSecret);
-    return craftIntent(context, MarkReadReceiver.CLEAR_ACTION, extras);
-  }
-
-  public PendingIntent getReplyIntent(Context context, MasterSecret masterSecret, long recipientId) {
-    Bundle extras = new Bundle();
-    extras.putParcelable("master_secret", masterSecret);
-    extras.putLong("recipient_id", recipientId);
-    return craftIntent(context, WearReplyReceiver.REPLY_ACTION, extras);
-  }
-
-  private PendingIntent craftIntent(Context context, String intentAction, Bundle extras) {
+  public PendingIntent getMarkAsReadIntent(Context context) {
     long[] threadArray = new long[threads.size()];
-    int index          = 0;
+    int    index       = 0;
 
     for (long thread : threads) {
       Log.w("NotificationState", "Added thread: " + thread);
       threadArray[index++] = thread;
     }
 
-    Intent intent = new Intent(intentAction);
-    intent.putExtra("thread_ids", threadArray);
-    intent.putExtras(extras);
+    Intent intent = new Intent(MarkReadReceiver.CLEAR_ACTION);
+    intent.putExtra(MarkReadReceiver.THREAD_IDS_EXTRA, threadArray);
     intent.setPackage(context.getPackageName());
 
     // XXX : This is an Android bug.  If we don't pull off the extra
     // once before handing off the PendingIntent, the array will be
     // truncated to one element when the PendingIntent fires.  Thanks guys!
     Log.w("NotificationState", "Pending array off intent length: " +
-            intent.getLongArrayExtra("thread_ids").length);
+        intent.getLongArrayExtra("thread_ids").length);
 
     return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
   }
+
+  public PendingIntent getWearableReplyIntent(Context context, Recipients recipients) {
+    if (threads.size() != 1) throw new AssertionError("We only support replies to single thread notifications!");
+
+    Intent intent = new Intent(WearReplyReceiver.REPLY_ACTION);
+    intent.putExtra(WearReplyReceiver.RECIPIENT_IDS_EXTRA, recipients.getIds());
+    intent.setPackage(context.getPackageName());
+
+    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+  }
+
+  public PendingIntent getQuickReplyIntent(Context context, Recipients recipients) {
+    if (threads.size() != 1) throw new AssertionError("We only support replies to single thread notifications!");
+
+    Intent     intent           = new Intent(context, ConversationPopupActivity.class);
+    intent.putExtra(ConversationActivity.RECIPIENTS_EXTRA, recipients.getIds());
+    intent.putExtra(ConversationActivity.THREAD_ID_EXTRA, (long)threads.toArray()[0]);
+    intent.setData((Uri.parse("custom://"+System.currentTimeMillis())));
+
+    return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+  }
+
+
 }

src/org/smssecure/smssecure/notifications/WearReplyReceiver.java
@@ -17,70 +17,67 @@
 
 package org.smssecure.smssecure.notifications;
 
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.RemoteInput;
-import android.util.Log;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
-import org.smssecure.smssecure.recipients.Recipient;
+import org.smssecure.smssecure.mms.OutgoingMediaMessage;
 import org.smssecure.smssecure.recipients.RecipientFactory;
-import org.smssecure.smssecure.recipients.RecipientProvider;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.sms.MessageSender;
 import org.smssecure.smssecure.sms.OutgoingTextMessage;
 
+import ws.com.google.android.mms.pdu.PduBody;
+
 /**
  * Get the response text from the Wearable Device and sends an message as a reply
- *
- * @author Alix Ducros (Ported to TextSecure-Codebase by Christoph Haefner)
  */
-public class WearReplyReceiver extends BroadcastReceiver {
+public class WearReplyReceiver extends MasterSecretBroadcastReceiver {
 
-  public static final String TAG = WearReplyReceiver.class.getSimpleName();
-  public static final String REPLY_ACTION = "org.smssecure.smssecure.notifications.WEAR_REPLY";
+  public static final String TAG                 = WearReplyReceiver.class.getSimpleName();
+  public static final String REPLY_ACTION        = "org.smssecure.smssecure.notifications.WEAR_REPLY";
+  public static final String RECIPIENT_IDS_EXTRA = "recipient_ids";
 
   @Override
-  public void onReceive(final Context context, Intent intent) {
-    if (!intent.getAction().equals(REPLY_ACTION))
-      return;
+  protected void onReceive(final Context context, Intent intent,
+                           final @Nullable MasterSecret masterSecret)
+  {
+    if (!REPLY_ACTION.equals(intent.getAction())) return;
 
     Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
-    if (remoteInput == null)
-      return;
 
-    final long[] threadIds = intent.getLongArrayExtra("thread_ids");
-    final MasterSecret masterSecret = intent.getParcelableExtra("master_secret");
-    final long recipientId = intent.getLongExtra("recipient_id", -1);
+    if (remoteInput == null) return;
+
+    final long[]       recipientIds = intent.getLongArrayExtra(RECIPIENT_IDS_EXTRA);
     final CharSequence responseText = remoteInput.getCharSequence(MessageNotifier.EXTRA_VOICE_REPLY);
+    final Recipients   recipients   = RecipientFactory.getRecipientsForIds(context, recipientIds, false);
 
-    final Recipients recipients = RecipientFactory.getRecipientsForIds(context, new long[]{recipientId}, false);
-
-    if (threadIds != null && masterSecret != null) {
-
-      ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
-              .cancel(MessageNotifier.NOTIFICATION_ID);
-
+    if (masterSecret != null && responseText != null) {
       new AsyncTask<Void, Void, Void>() {
         @Override
         protected Void doInBackground(Void... params) {
-          for (long threadId : threadIds) {
-            Log.w(TAG, "Marking as read: " + threadId);
-            DatabaseFactory.getThreadDatabase(context).setRead(threadId);
+          long threadId;
+
+          if (recipients.isGroupRecipient()) {
+            OutgoingMediaMessage reply = new OutgoingMediaMessage(context, recipients, new PduBody(), responseText.toString(), 0);
+            threadId = MessageSender.send(context, masterSecret, reply, -1, false);
+          } else {
+            OutgoingTextMessage reply = new OutgoingTextMessage(recipients, responseText.toString());
+            threadId = MessageSender.send(context, masterSecret, reply, -1, false);
           }
 
-          OutgoingTextMessage reply = new OutgoingTextMessage(recipients, responseText.toString());
-          MessageSender.send(context, masterSecret, reply, threadIds[0], false);
-
+          DatabaseFactory.getThreadDatabase(context).setRead(threadId);
           MessageNotifier.updateNotification(context, masterSecret);
+
           return null;
         }
       }.execute();
     }
+
   }
 }