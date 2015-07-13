SamSprung/Signal-Android

Add camera preview to message composition

src/org/thoughtcrime/securesms/components/SurfacePreviewStrategy.java
@@ -1,0 +1,72 @@
+/***
+ Copyright (c) 2013 CommonsWare, LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package org.thoughtcrime.securesms.components;
+
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import com.commonsware.cwac.camera.PreviewStrategy;
+
+import java.io.IOException;
+
+class SurfacePreviewStrategy implements PreviewStrategy,
+    SurfaceHolder.Callback {
+  private final CameraView cameraView;
+  private SurfaceView preview=null;
+  private SurfaceHolder previewHolder=null;
+
+  @SuppressWarnings("deprecation")
+  SurfacePreviewStrategy(CameraView cameraView) {
+    this.cameraView=cameraView;
+    preview=new SurfaceView(cameraView.getContext());
+    previewHolder=preview.getHolder();
+    previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    previewHolder.addCallback(this);
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+    cameraView.previewCreated();
+  }
+
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format,
+                             int width, int height) {
+    cameraView.initPreview(width, height);
+  }
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    cameraView.previewDestroyed();
+  }
+
+  @Override
+  public void attach(Camera camera) throws IOException {
+    camera.setPreviewDisplay(previewHolder);
+  }
+
+  @Override
+  public void attach(MediaRecorder recorder) {
+    recorder.setPreviewDisplay(previewHolder.getSurface());
+  }
+
+  @Override
+  public View getWidget() {
+    return(preview);
+  }
+}

src/org/thoughtcrime/securesms/components/TexturePreviewStrategy.java
@@ -1,0 +1,88 @@
+package org.thoughtcrime.securesms.components;
+/***
+ Copyright (c) 2013 CommonsWare, LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import android.annotation.TargetApi;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.view.TextureView;
+import android.view.View;
+
+import com.commonsware.cwac.camera.PreviewStrategy;
+
+import java.io.IOException;
+
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+class TexturePreviewStrategy implements PreviewStrategy,
+    TextureView.SurfaceTextureListener {
+  private final CameraView cameraView;
+  private TextureView widget=null;
+  private SurfaceTexture surface=null;
+
+  TexturePreviewStrategy(CameraView cameraView) {
+    this.cameraView=cameraView;
+    widget=new TextureView(cameraView.getContext());
+    widget.setSurfaceTextureListener(this);
+  }
+
+  @Override
+  public void onSurfaceTextureAvailable(SurfaceTexture surface,
+                                        int width, int height) {
+    this.surface=surface;
+
+    cameraView.previewCreated();
+    cameraView.initPreview(width, height);
+  }
+
+  @Override
+  public void onSurfaceTextureSizeChanged(SurfaceTexture surface,
+                                          int width, int height) {
+    cameraView.previewReset(width, height);
+  }
+
+  @Override
+  public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+    cameraView.previewDestroyed();
+
+    return(true);
+  }
+
+  @Override
+  public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    // no-op
+  }
+
+  @Override
+  public void attach(Camera camera) throws IOException {
+    camera.setPreviewTexture(surface);
+  }
+
+  @Override
+  public void attach(MediaRecorder recorder) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+      // no-op
+    }
+    else {
+      throw new IllegalStateException(
+          "Cannot use TextureView with MediaRecorder");
+    }
+  }
+
+  @Override
+  public View getWidget() {
+    return(widget);
+  }
+}

src/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -24,6 +24,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -36,6 +37,7 @@ import android.widget.Toast;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.components.ThumbnailView;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.util.BitmapDecodingException;
 
 import java.io.File;
@@ -106,11 +108,24 @@ public class AttachmentManager {
     setMedia(new AudioSlide(context, audio));
   }
 
+  public void setEncryptedImage(Uri uri, MasterSecret masterSecret) throws IOException, BitmapDecodingException {
+    setMedia(new ImageSlide(context, masterSecret, uri), masterSecret);
+  }
+
   public void setMedia(final Slide slide) {
+    setMedia(slide, null);
+  }
+
+  public void setMedia(final Slide slide, @Nullable MasterSecret masterSecret) {
+    Slide thumbnailSlide = slideDeck.getThumbnailSlide(context);
+    if (thumbnailSlide != null && thumbnailSlide.isEncrypted()) {
+      Uri dataUri = slideDeck.getThumbnailSlide(context).getPart().getDataUri();
+      new File(dataUri.getPath()).delete();
+    }
     slideDeck.clear();
     slideDeck.addSlide(slide);
     attachmentView.setVisibility(View.VISIBLE);
-    thumbnail.setImageResource(slide);
+    thumbnail.setImageResource(slide, masterSecret);
     attachmentListener.onAttachmentChanged();
   }
 

src/org/thoughtcrime/securesms/mms/ImageSlide.java
@@ -20,25 +20,36 @@ import android.content.Context;
 import android.content.res.Resources.Theme;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.util.BitmapDecodingException;
+import org.thoughtcrime.securesms.util.Util;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import ws.com.google.android.mms.ContentType;
 import ws.com.google.android.mms.pdu.PduPart;
 
 public class ImageSlide extends Slide {
   private static final String TAG = ImageSlide.class.getSimpleName();
+  private boolean encrypted = false;
 
   public ImageSlide(Context context, MasterSecret masterSecret, PduPart part) {
     super(context, masterSecret, part);
   }
 
   public ImageSlide(Context context, Uri uri) throws IOException, BitmapDecodingException {
-    super(context, constructPartFromUri(uri));
+    this(context, null, uri);
+  }
+
+  public ImageSlide(Context context, MasterSecret masterSecret, Uri uri) throws IOException, BitmapDecodingException {
+    super(context, masterSecret, constructPartFromByteArrayAndUri(uri, decryptContent(uri, masterSecret), masterSecret != null));
+    encrypted = masterSecret != null;
   }
 
   @Override
@@ -62,12 +73,32 @@ public class ImageSlide extends Slide {
     return true;
   }
 
-  private static PduPart constructPartFromUri(Uri uri)
+  @Override
+  public boolean isEncrypted() {
+    return encrypted;
+  }
+
+  private static byte[] decryptContent(Uri uri, MasterSecret masterSecret) {
+    try {
+      if (masterSecret != null) {
+        InputStream inputStream = new DecryptingPartInputStream(new File(uri.getPath()), masterSecret);
+        return Util.readFully(inputStream);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private static PduPart constructPartFromByteArrayAndUri(Uri uri, @Nullable byte[] data, boolean encrypted)
       throws IOException, BitmapDecodingException
   {
     PduPart part = new PduPart();
 
     part.setDataUri(uri);
+    if (data != null)
+      part.setData(data);
+    part.setEncrypted(encrypted);
     part.setContentType(ContentType.IMAGE_JPEG.getBytes());
     part.setContentId((System.currentTimeMillis()+"").getBytes());
     part.setName(("Image" + System.currentTimeMillis()).getBytes());

src/org/thoughtcrime/securesms/mms/PartAuthority.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
 
+import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.PartDatabase;
 import org.thoughtcrime.securesms.providers.PartProvider;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -46,7 +48,11 @@ public class PartAuthority {
         partUri = new PartUriParser(uri);
         return partDatabase.getThumbnailStream(masterSecret, partUri.getPartId());
       default:
-        return context.getContentResolver().openInputStream(uri);
+        String tempMediaDir = context.getDir("media", Context.MODE_PRIVATE).getPath();
+        if (uri.getPath().startsWith(tempMediaDir))
+          return  new DecryptingPartInputStream(new File(uri.getPath()), masterSecret);
+        else
+          return context.getContentResolver().openInputStream(uri);
       }
     } catch (SecurityException se) {
       throw new IOException(se);

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/components/SurfacePreviewStrategy.java
@@ -1,0 +1,72 @@
+/***
+ Copyright (c) 2013 CommonsWare, LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package org.smssecure.smssecure.components;
+
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import com.commonsware.cwac.camera.PreviewStrategy;
+
+import java.io.IOException;
+
+class SurfacePreviewStrategy implements PreviewStrategy,
+    SurfaceHolder.Callback {
+  private final CameraView cameraView;
+  private SurfaceView preview=null;
+  private SurfaceHolder previewHolder=null;
+
+  @SuppressWarnings("deprecation")
+  SurfacePreviewStrategy(CameraView cameraView) {
+    this.cameraView=cameraView;
+    preview=new SurfaceView(cameraView.getContext());
+    previewHolder=preview.getHolder();
+    previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    previewHolder.addCallback(this);
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+    cameraView.previewCreated();
+  }
+
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format,
+                             int width, int height) {
+    cameraView.initPreview(width, height);
+  }
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    cameraView.previewDestroyed();
+  }
+
+  @Override
+  public void attach(Camera camera) throws IOException {
+    camera.setPreviewDisplay(previewHolder);
+  }
+
+  @Override
+  public void attach(MediaRecorder recorder) {
+    recorder.setPreviewDisplay(previewHolder.getSurface());
+  }
+
+  @Override
+  public View getWidget() {
+    return(preview);
+  }
+}

src/org/smssecure/smssecure/components/TexturePreviewStrategy.java
@@ -1,0 +1,88 @@
+package org.smssecure.smssecure.components;
+/***
+ Copyright (c) 2013 CommonsWare, LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import android.annotation.TargetApi;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.view.TextureView;
+import android.view.View;
+
+import com.commonsware.cwac.camera.PreviewStrategy;
+
+import java.io.IOException;
+
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+class TexturePreviewStrategy implements PreviewStrategy,
+    TextureView.SurfaceTextureListener {
+  private final CameraView cameraView;
+  private TextureView widget=null;
+  private SurfaceTexture surface=null;
+
+  TexturePreviewStrategy(CameraView cameraView) {
+    this.cameraView=cameraView;
+    widget=new TextureView(cameraView.getContext());
+    widget.setSurfaceTextureListener(this);
+  }
+
+  @Override
+  public void onSurfaceTextureAvailable(SurfaceTexture surface,
+                                        int width, int height) {
+    this.surface=surface;
+
+    cameraView.previewCreated();
+    cameraView.initPreview(width, height);
+  }
+
+  @Override
+  public void onSurfaceTextureSizeChanged(SurfaceTexture surface,
+                                          int width, int height) {
+    cameraView.previewReset(width, height);
+  }
+
+  @Override
+  public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+    cameraView.previewDestroyed();
+
+    return(true);
+  }
+
+  @Override
+  public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    // no-op
+  }
+
+  @Override
+  public void attach(Camera camera) throws IOException {
+    camera.setPreviewTexture(surface);
+  }
+
+  @Override
+  public void attach(MediaRecorder recorder) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+      // no-op
+    }
+    else {
+      throw new IllegalStateException(
+          "Cannot use TextureView with MediaRecorder");
+    }
+  }
+
+  @Override
+  public View getWidget() {
+    return(widget);
+  }
+}

src/org/smssecure/smssecure/mms/AttachmentManager.java
@@ -24,6 +24,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -36,6 +37,7 @@ import android.widget.Toast;
 
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.components.ThumbnailView;
+import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.util.BitmapDecodingException;
 
 import java.io.File;
@@ -106,11 +108,24 @@ public class AttachmentManager {
     setMedia(new AudioSlide(context, audio));
   }
 
+  public void setEncryptedImage(Uri uri, MasterSecret masterSecret) throws IOException, BitmapDecodingException {
+    setMedia(new ImageSlide(context, masterSecret, uri), masterSecret);
+  }
+
   public void setMedia(final Slide slide) {
+    setMedia(slide, null);
+  }
+
+  public void setMedia(final Slide slide, @Nullable MasterSecret masterSecret) {
+    Slide thumbnailSlide = slideDeck.getThumbnailSlide(context);
+    if (thumbnailSlide != null && thumbnailSlide.isEncrypted()) {
+      Uri dataUri = slideDeck.getThumbnailSlide(context).getPart().getDataUri();
+      new File(dataUri.getPath()).delete();
+    }
     slideDeck.clear();
     slideDeck.addSlide(slide);
     attachmentView.setVisibility(View.VISIBLE);
-    thumbnail.setImageResource(slide);
+    thumbnail.setImageResource(slide, masterSecret);
     attachmentListener.onAttachmentChanged();
   }
 

src/org/smssecure/smssecure/mms/ImageSlide.java
@@ -20,25 +20,36 @@ import android.content.Context;
 import android.content.res.Resources.Theme;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
 
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.util.BitmapDecodingException;
+import org.smssecure.smssecure.util.Util;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import ws.com.google.android.mms.ContentType;
 import ws.com.google.android.mms.pdu.PduPart;
 
 public class ImageSlide extends Slide {
   private static final String TAG = ImageSlide.class.getSimpleName();
+  private boolean encrypted = false;
 
   public ImageSlide(Context context, MasterSecret masterSecret, PduPart part) {
     super(context, masterSecret, part);
   }
 
   public ImageSlide(Context context, Uri uri) throws IOException, BitmapDecodingException {
-    super(context, constructPartFromUri(uri));
+    this(context, null, uri);
+  }
+
+  public ImageSlide(Context context, MasterSecret masterSecret, Uri uri) throws IOException, BitmapDecodingException {
+    super(context, masterSecret, constructPartFromByteArrayAndUri(uri, decryptContent(uri, masterSecret), masterSecret != null));
+    encrypted = masterSecret != null;
   }
 
   @Override
@@ -62,12 +73,32 @@ public class ImageSlide extends Slide {
     return true;
   }
 
-  private static PduPart constructPartFromUri(Uri uri)
+  @Override
+  public boolean isEncrypted() {
+    return encrypted;
+  }
+
+  private static byte[] decryptContent(Uri uri, MasterSecret masterSecret) {
+    try {
+      if (masterSecret != null) {
+        InputStream inputStream = new DecryptingPartInputStream(new File(uri.getPath()), masterSecret);
+        return Util.readFully(inputStream);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private static PduPart constructPartFromByteArrayAndUri(Uri uri, @Nullable byte[] data, boolean encrypted)
       throws IOException, BitmapDecodingException
   {
     PduPart part = new PduPart();
 
     part.setDataUri(uri);
+    if (data != null)
+      part.setData(data);
+    part.setEncrypted(encrypted);
     part.setContentType(ContentType.IMAGE_JPEG.getBytes());
     part.setContentId((System.currentTimeMillis()+"").getBytes());
     part.setName(("Image" + System.currentTimeMillis()).getBytes());

src/org/smssecure/smssecure/mms/PartAuthority.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
 
+import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.database.PartDatabase;
 import org.smssecure.smssecure.providers.PartProvider;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -46,7 +48,11 @@ public class PartAuthority {
         partUri = new PartUriParser(uri);
         return partDatabase.getThumbnailStream(masterSecret, partUri.getPartId());
       default:
-        return context.getContentResolver().openInputStream(uri);
+        String tempMediaDir = context.getDir("media", Context.MODE_PRIVATE).getPath();
+        if (uri.getPath().startsWith(tempMediaDir))
+          return  new DecryptingPartInputStream(new File(uri.getPath()), masterSecret);
+        else
+          return context.getContentResolver().openInputStream(uri);
       }
     } catch (SecurityException se) {
       throw new IOException(se);