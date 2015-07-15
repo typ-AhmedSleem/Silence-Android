4b6756c3fbb22645f1263e99ef37681b05e68af1
Carey Metcalfe <carey@cmetcalfe.ca>
2015-07-15 00:19:04 -0400

    Fix sed syntax for GNU/Linux, add missing fixes

--- scripts/fix-patch.sh
+++ scripts/fix-patch.sh
@@ -6,13 +6,20 @@ if [ "$#" -lt 1 ]; then
 fi
 
 for file in "$@"; do
-    sed -i '' -e 's/thoughtcrime\/securesms/smssecure\/smssecure/g' \
-              -e 's/org.thoughtcrime.securesms/org.smssecure.smssecure/g' \
-              -e 's/org.thoughtcrime.provider.securesms/org.smssecure.provider.smssecure/g' \
-              -e 's/org.smssecure.smssecure.util.TextSecure/org.smssecure.smssecure.util.SMSSecure/g' \
-              -e 's/TextSecurePreferences/SMSSecurePreferences/g' \
-              -e 's/TextSecureTestCase/SMSSecureTestCase/g' \
-              -e 's/^[Ff]ixes #/Fixes https:\/\/github.com\/WhisperSystems\/TextSecure\/issues\//g' \
-              -e 's/^[Cc]loses #/Closes https:\/\/github.com\/WhisperSystems\/TextSecure\/pull\//g' \
-              "$file"
+    sed -i -e 's/thoughtcrime\/securesms/smssecure\/smssecure/g' \
+           -e 's/org.thoughtcrime.securesms/org.smssecure.smssecure/g' \
+           -e 's/org.thoughtcrime.provider.securesms/org.smssecure.provider.smssecure/g' \
+           -e 's/org.smssecure.smssecure.util.TextSecure/org.smssecure.smssecure.util.SMSSecure/g' \
+           -e 's/TextSecurePreferences/SMSSecurePreferences/g' \
+           -e 's/TextSecureTestCase/SMSSecureTestCase/g' \
+           -e 's/TextSecure.LightNoActionBar/SMSSecure.LightNoActionBar/g' \
+           -e 's/TextSecure.LightActionBar/SMSSecure.LightActionBar/g' \
+           -e 's/TextSecure.DarkNoActionBar/SMSSecure.DarkNoActionBar/g' \
+           -e 's/TextSecure.DarkActionBar/SMSSecure.DarkActionBar/g' \
+           -e 's/TextSecure.LightTheme/SMSSecure.LightTheme/g' \
+           -e 's/TextSecure.DarkTheme/SMSSecure.DarkTheme/g' \
+           -e 's/TextSecure.TitleTextStyle/SMSSecure.TitleTextStyle/g' \
+           -e 's/^[Ff]ixes #/Fixes https:\/\/github.com\/WhisperSystems\/TextSecure\/issues\//g' \
+           -e 's/^[Cc]loses #/Closes https:\/\/github.com\/WhisperSystems\/TextSecure\/pull\//g' \
+           "$file"
 done
