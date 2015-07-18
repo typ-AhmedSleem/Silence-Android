SamSprung/Signal-Android

Clean up contact queries.
// FREEBIE

src/org/thoughtcrime/securesms/PushContactSelectionListFragment.java
@@ -32,9 +32,10 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.thoughtcrime.securesms.contacts.ContactAccessor;
 import org.thoughtcrime.securesms.contacts.ContactSelectionListAdapter;
 import org.thoughtcrime.securesms.contacts.ContactSelectionListItem;
+import org.thoughtcrime.securesms.contacts.ContactsCursorLoader;
+import org.thoughtcrime.securesms.util.TextSecurePreferences;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -140,11 +141,10 @@ public class PushContactSelectionListFragment extends    Fragment
 
   @Override
   public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-    if (getActivity().getIntent().getBooleanExtra(PushContactSelectionActivity.PUSH_ONLY_EXTRA, false)) {
-      return ContactAccessor.getInstance().getCursorLoaderForPushContacts(getActivity(), cursorFilter);
-    } else {
-      return ContactAccessor.getInstance().getCursorLoaderForContacts(getActivity(), cursorFilter);
-    }
+    boolean pushOnly    = getActivity().getIntent().getBooleanExtra(PushContactSelectionActivity.PUSH_ONLY_EXTRA, false);
+    boolean supportsSms = TextSecurePreferences.isSmsEnabled(getActivity());
+
+    return new ContactsCursorLoader(getActivity(), !pushOnly && supportsSms, cursorFilter);
   }
 
   @Override

src/org/thoughtcrime/securesms/contacts/ContactsCursorLoader.java
@@ -18,12 +18,15 @@ package org.thoughtcrime.securesms.contacts;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MergeCursor;
 import android.support.v4.content.CursorLoader;
+import android.text.TextUtils;
 import android.util.Log;
 
-import junit.framework.Assert;
+import org.thoughtcrime.securesms.database.DatabaseFactory;
+import org.thoughtcrime.securesms.util.NumberUtil;
 
-import java.util.concurrent.Semaphore;
+import java.util.ArrayList;
 
 /**
  * CursorLoader that initializes a ContactsDatabase instance
@@ -31,47 +34,34 @@ import java.util.concurrent.Semaphore;
  * @author Jake McGinty
  */
 public class ContactsCursorLoader extends CursorLoader {
-  private static final String TAG        = ContactsCursorLoader.class.getSimpleName();
-  private static final int    DB_PERMITS = 100;
 
-  private final Context          context;
-  private final String           filter;
-  private final boolean          pushOnly;
-  private final Semaphore        dbSemaphore = new Semaphore(DB_PERMITS);
-  private       ContactsDatabase db;
+  private static final String TAG = ContactsCursorLoader.class.getSimpleName();
 
-  public ContactsCursorLoader(Context context, String filter, boolean pushOnly) {
+  private final String  filter;
+  private       boolean includeSmsContacts;
+
+  public ContactsCursorLoader(Context context, boolean includeSmsContacts, String filter) {
     super(context);
-    this.context  = context;
+
     this.filter   = filter;
-    this.pushOnly = pushOnly;
-    this.db       = new ContactsDatabase(context);
+    this.includeSmsContacts = includeSmsContacts;
   }
 
   @Override
   public Cursor loadInBackground() {
-    try {
-      dbSemaphore.acquire();
-      return db.query(filter, pushOnly);
-    } catch (InterruptedException ie) {
-      throw new AssertionError(ie);
-    } finally {
-      dbSemaphore.release();
-    }
-  }
+    ContactsDatabase  contactsDatabase = DatabaseFactory.getContactsDatabase(getContext());
+    ArrayList<Cursor> cursorList       = new ArrayList<>(3);
 
-  @Override
-  public void onReset() {
-    Log.w(TAG, "onReset()");
-    try {
-      dbSemaphore.acquire(DB_PERMITS);
-      db.close();
-      db = new ContactsDatabase(context);
-    } catch (InterruptedException ie) {
-      throw new AssertionError(ie);
-    } finally {
-      dbSemaphore.release(DB_PERMITS);
+    cursorList.add(contactsDatabase.queryTextSecureContacts(filter));
+
+    if (includeSmsContacts) {
+      cursorList.add(contactsDatabase.querySystemContacts(filter));
     }
-    super.onReset();
+
+    if (!TextUtils.isEmpty(filter) && NumberUtil.isValidSmsOrEmail(filter)) {
+      cursorList.add(contactsDatabase.getNewNumberCursor(filter));
+    }
+
+    return new MergeCursor(cursorList.toArray(new Cursor[0]));
   }
 }

src/org/thoughtcrime/securesms/database/DatabaseFactory.java
@@ -26,6 +26,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import org.thoughtcrime.securesms.DatabaseUpgradeActivity;
+import org.thoughtcrime.securesms.contacts.ContactsDatabase;
 import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
 import org.thoughtcrime.securesms.crypto.MasterCipher;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
@@ -86,6 +87,7 @@ public class DatabaseFactory {
   private final PushDatabase pushDatabase;
   private final GroupDatabase groupDatabase;
   private final RecipientPreferenceDatabase recipientPreferenceDatabase;
+  private final ContactsDatabase contactsDatabase;
 
   public static DatabaseFactory getInstance(Context context) {
     synchronized (lock) {
@@ -148,6 +150,10 @@ public class DatabaseFactory {
     return getInstance(context).recipientPreferenceDatabase;
   }
 
+  public static ContactsDatabase getContactsDatabase(Context context) {
+    return getInstance(context).contactsDatabase;
+  }
+
   private DatabaseFactory(Context context) {
     this.databaseHelper              = new DatabaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
     this.sms                         = new SmsDatabase(context, databaseHelper);
@@ -163,6 +169,7 @@ public class DatabaseFactory {
     this.pushDatabase                = new PushDatabase(context, databaseHelper);
     this.groupDatabase               = new GroupDatabase(context, databaseHelper);
     this.recipientPreferenceDatabase = new RecipientPreferenceDatabase(context, databaseHelper);
+    this.contactsDatabase            = new ContactsDatabase(context);
   }
 
   public void reset(Context context) {

typ-AhmedSleem/Silence-Android

src/org/smssecure/smssecure/PushContactSelectionListFragment.java
@@ -32,9 +32,10 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.smssecure.smssecure.contacts.ContactAccessor;
 import org.smssecure.smssecure.contacts.ContactSelectionListAdapter;
 import org.smssecure.smssecure.contacts.ContactSelectionListItem;
+import org.smssecure.smssecure.contacts.ContactsCursorLoader;
+import org.smssecure.smssecure.util.SMSSecurePreferences;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -140,7 +141,7 @@ public class PushContactSelectionListFragment extends    Fragment
 
   @Override
   public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-    return ContactAccessor.getInstance().getCursorLoaderForContacts(getActivity(), cursorFilter);
+    return new ContactsCursorLoader(getActivity(), true, cursorFilter);
   }
 
   @Override

src/org/smssecure/smssecure/contacts/ContactsCursorLoader.java
@@ -18,12 +18,15 @@ package org.smssecure.smssecure.contacts;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MergeCursor;
 import android.support.v4.content.CursorLoader;
+import android.text.TextUtils;
 import android.util.Log;
 
-import junit.framework.Assert;
+import org.smssecure.smssecure.database.DatabaseFactory;
+import org.smssecure.smssecure.util.NumberUtil;
 
-import java.util.concurrent.Semaphore;
+import java.util.ArrayList;
 
 /**
  * CursorLoader that initializes a ContactsDatabase instance
@@ -31,47 +34,34 @@ import java.util.concurrent.Semaphore;
  * @author Jake McGinty
  */
 public class ContactsCursorLoader extends CursorLoader {
-  private static final String TAG        = ContactsCursorLoader.class.getSimpleName();
-  private static final int    DB_PERMITS = 100;
 
-  private final Context          context;
-  private final String           filter;
-  private final boolean          pushOnly;
-  private final Semaphore        dbSemaphore = new Semaphore(DB_PERMITS);
-  private       ContactsDatabase db;
+  private static final String TAG = ContactsCursorLoader.class.getSimpleName();
 
-  public ContactsCursorLoader(Context context, String filter, boolean pushOnly) {
+  private final String  filter;
+  private       boolean includeSmsContacts;
+
+  public ContactsCursorLoader(Context context, boolean includeSmsContacts, String filter) {
     super(context);
-    this.context  = context;
+
     this.filter   = filter;
-    this.pushOnly = pushOnly;
-    this.db       = new ContactsDatabase(context);
+    this.includeSmsContacts = includeSmsContacts;
   }
 
   @Override
   public Cursor loadInBackground() {
-    try {
-      dbSemaphore.acquire();
-      return db.query(filter, pushOnly);
-    } catch (InterruptedException ie) {
-      throw new AssertionError(ie);
-    } finally {
-      dbSemaphore.release();
-    }
-  }
+    ContactsDatabase  contactsDatabase = DatabaseFactory.getContactsDatabase(getContext());
+    ArrayList<Cursor> cursorList       = new ArrayList<>(3);
 
-  @Override
-  public void onReset() {
-    Log.w(TAG, "onReset()");
-    try {
-      dbSemaphore.acquire(DB_PERMITS);
-      db.close();
-      db = new ContactsDatabase(context);
-    } catch (InterruptedException ie) {
-      throw new AssertionError(ie);
-    } finally {
-      dbSemaphore.release(DB_PERMITS);
+    cursorList.add(contactsDatabase.querySMSSecureContacts(filter));
+
+    if (includeSmsContacts) {
+      cursorList.add(contactsDatabase.querySystemContacts(filter));
     }
-    super.onReset();
+
+    if (!TextUtils.isEmpty(filter) && NumberUtil.isValidSmsOrEmail(filter)) {
+      cursorList.add(contactsDatabase.getNewNumberCursor(filter));
+    }
+
+    return new MergeCursor(cursorList.toArray(new Cursor[0]));
   }
 }

src/org/smssecure/smssecure/database/DatabaseFactory.java
@@ -26,6 +26,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import org.smssecure.smssecure.DatabaseUpgradeActivity;
+import org.smssecure.smssecure.contacts.ContactsDatabase;
 import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
 import org.smssecure.smssecure.crypto.MasterCipher;
 import org.smssecure.smssecure.crypto.MasterSecret;
@@ -83,6 +84,7 @@ public class DatabaseFactory {
   private final DraftDatabase draftDatabase;
   private final GroupDatabase groupDatabase;
   private final RecipientPreferenceDatabase recipientPreferenceDatabase;
+  private final ContactsDatabase contactsDatabase;
 
   public static DatabaseFactory getInstance(Context context) {
     synchronized (lock) {
@@ -141,6 +143,10 @@ public class DatabaseFactory {
     return getInstance(context).recipientPreferenceDatabase;
   }
 
+  public static ContactsDatabase getContactsDatabase(Context context) {
+    return getInstance(context).contactsDatabase;
+  }
+
   private DatabaseFactory(Context context) {
     this.databaseHelper              = new DatabaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
     this.sms                         = new SmsDatabase(context, databaseHelper);
@@ -155,6 +161,7 @@ public class DatabaseFactory {
     this.draftDatabase               = new DraftDatabase(context, databaseHelper);
     this.groupDatabase               = new GroupDatabase(context, databaseHelper);
     this.recipientPreferenceDatabase = new RecipientPreferenceDatabase(context, databaseHelper);
+    this.contactsDatabase            = new ContactsDatabase(context);
   }
 
   public void reset(Context context) {