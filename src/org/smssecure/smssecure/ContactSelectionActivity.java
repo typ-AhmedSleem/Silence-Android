b29f18b8458abddc51cc00c4a9ecded193e90b4d
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-07-22 16:25:02 -0700

    Expand tap target of contact selection keyboard toggles.
    Fixes https://github.com/WhisperSystems/TextSecure/issues/3750
    // FREEBIE
    
    Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/39ac2b50424d3f8e51dfef8517319b5cf0c91fca

--- src/org/smssecure/smssecure/ContactSelectionActivity.java
+++ src/org/smssecure/smssecure/ContactSelectionActivity.java
@@ -16,14 +16,15 @@
  */
 package org.smssecure.smssecure;
 
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.TouchDelegate;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -69,7 +70,7 @@ public abstract class ContactSelectionActivity extends PassphraseRequiredActionB
   }
 
   @Override
-  protected void onCreate(Bundle icicle, @NonNull MasterSecret masterSecret) {
+  protected void onCreate(Bundle icicle, MasterSecret masterSecret) {
     setContentView(R.layout.contact_selection_activity);
 
     initializeToolbar();
@@ -108,7 +109,7 @@ public abstract class ContactSelectionActivity extends PassphraseRequiredActionB
       public void onClick(View v) {
         searchText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
         ServiceUtil.getInputMethodManager(ContactSelectionActivity.this).showSoftInput(searchText, 0);
-        toggle.display(dialpadToggle);
+        displayTogglingView(dialpadToggle);
       }
     });
 
@@ -117,7 +118,7 @@ public abstract class ContactSelectionActivity extends PassphraseRequiredActionB
       public void onClick(View v) {
         searchText.setInputType(InputType.TYPE_CLASS_PHONE);
         ServiceUtil.getInputMethodManager(ContactSelectionActivity.this).showSoftInput(searchText, 0);
-        toggle.display(keyboardToggle);
+        displayTogglingView(keyboardToggle);
       }
     });
 
@@ -126,10 +127,12 @@ public abstract class ContactSelectionActivity extends PassphraseRequiredActionB
       public void onClick(View v) {
         searchText.setText("");
 
-        if (SearchUtil.isTextInput(searchText)) toggle.display(dialpadToggle);
-        else                                    toggle.display(keyboardToggle);
+        if (SearchUtil.isTextInput(searchText)) displayTogglingView(dialpadToggle);
+        else                                    displayTogglingView(keyboardToggle);
       }
     });
+
+    expandTapArea(toolbar, dialpadToggle, 500);
   }
 
   private void initializeSearch() {
@@ -146,9 +149,9 @@ public abstract class ContactSelectionActivity extends PassphraseRequiredActionB
 
       @Override
       public void afterTextChanged(Editable s) {
-        if      (!SearchUtil.isEmpty(searchText))     toggle.display(clearToggle);
-        else if (SearchUtil.isTextInput(searchText))  toggle.display(dialpadToggle);
-        else if (SearchUtil.isPhoneInput(searchText)) toggle.display(keyboardToggle);
+        if      (!SearchUtil.isEmpty(searchText))     displayTogglingView(clearToggle);
+        else if (SearchUtil.isTextInput(searchText))  displayTogglingView(dialpadToggle);
+        else if (SearchUtil.isPhoneInput(searchText)) displayTogglingView(keyboardToggle);
 
         contactsFragment.setQueryFilter(searchText.getText().toString());
       }
@@ -157,6 +160,28 @@ public abstract class ContactSelectionActivity extends PassphraseRequiredActionB
 
   @Override
   public void onContactSelected(String number) {}
+
+  private void displayTogglingView(View view) {
+    toggle.display(view);
+    expandTapArea(toolbar, view, 500);
+  }
+
+  private void expandTapArea(final View container, final View child, final int padding) {
+    container.post(new Runnable() {
+      @Override
+      public void run() {
+        Rect rect = new Rect();
+        child.getHitRect(rect);
+
+        rect.top    -= padding;
+        rect.left   -= padding;
+        rect.right  += padding;
+        rect.bottom += padding;
+
+        container.setTouchDelegate(new TouchDelegate(rect, child));
+      }
+    });
+  }
 
   private static class SearchUtil {
 
