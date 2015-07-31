SamSprung/Signal-Android

Smooth transition for custom keyboards and softkey
Closes #3786
Fixes #3780
// FREEBIE

src/org/thoughtcrime/securesms/components/InputAwareLayout.java
@@ -1,0 +1,94 @@
+package org.thoughtcrime.securesms.components;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import org.thoughtcrime.securesms.components.KeyboardAwareLinearLayout.OnKeyboardShownListener;
+import org.thoughtcrime.securesms.util.ServiceUtil;
+
+public class InputAwareLayout extends KeyboardAwareLinearLayout implements OnKeyboardShownListener {
+  private InputView current;
+
+  public InputAwareLayout(Context context) {
+    this(context, null);
+  }
+
+  public InputAwareLayout(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public InputAwareLayout(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    addOnKeyboardShownListener(this);
+  }
+
+  @Override public void onKeyboardShown() {
+    hideAttachedInput();
+  }
+
+  public void show(@NonNull final EditText imeTarget, @NonNull final InputView input) {
+    if (isKeyboardOpen()) {
+      hideSoftkey(imeTarget, new Runnable() {
+        @Override public void run() {
+          input.show(getKeyboardHeight(), true);
+        }
+      });
+    } else if (current != null && current.isShowing()) {
+      current.hide(true);
+      input.show(getKeyboardHeight(), true);
+    } else {
+      input.show(getKeyboardHeight(), false);
+    }
+
+    current = input;
+  }
+
+  public InputView getCurrentInput() {
+    return current;
+  }
+
+  public void hideCurrentInput(EditText imeTarget) {
+    if (isKeyboardOpen()) hideSoftkey(imeTarget, null);
+    else                  hideAttachedInput();
+  }
+
+  public void hideAttachedInput() {
+    if (current != null) current.hide(true);
+    current = null;
+  }
+
+  public boolean isInputOpen() {
+    return (isKeyboardOpen() || (current != null && current.isShowing()));
+  }
+
+  public void showSoftkey(final EditText inputTarget) {
+    postOnKeyboardOpen(new Runnable() {
+      @Override public void run() {
+        hideAttachedInput();
+      }
+    });
+    inputTarget.post(new Runnable() {
+      @Override public void run() {
+        inputTarget.requestFocus();
+        ServiceUtil.getInputMethodManager(inputTarget.getContext()).showSoftInput(inputTarget, 0);
+      }
+    });
+  }
+
+  private void hideSoftkey(final EditText inputTarget, @Nullable Runnable runAfterClose) {
+    if (runAfterClose != null) postOnKeyboardClose(runAfterClose);
+
+    ServiceUtil.getInputMethodManager(inputTarget.getContext())
+               .hideSoftInputFromWindow(inputTarget.getWindowToken(), 0);
+  }
+
+  public interface InputView {
+    void show(int height, boolean immediate);
+    void hide(boolean immediate);
+    boolean isShowing();
+  }
+}
+

src/org/thoughtcrime/securesms/components/emoji/EmojiDrawer.java
@@ -1,6 +1,7 @@
 package org.thoughtcrime.securesms.components.emoji;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
@@ -18,7 +19,7 @@ import android.widget.LinearLayout;
 import com.astuetz.PagerSlidingTabStrip;
 
 import org.thoughtcrime.securesms.R;
-import org.thoughtcrime.securesms.components.KeyboardAwareLinearLayout;
+import org.thoughtcrime.securesms.components.InputManager.InputView;
 import org.thoughtcrime.securesms.components.RepeatableImageKey;
 import org.thoughtcrime.securesms.components.RepeatableImageKey.KeyEventListener;
 import org.thoughtcrime.securesms.components.emoji.EmojiPageView.EmojiSelectionListener;
@@ -27,7 +28,7 @@ import org.thoughtcrime.securesms.util.ResUtil;
 import java.util.LinkedList;
 import java.util.List;
 
-public class EmojiDrawer extends LinearLayout {
+public class EmojiDrawer extends LinearLayout implements InputView {
   private static final KeyEvent DELETE_KEY_EVENT = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL);
 
   private ViewPager            pager;
@@ -35,6 +36,7 @@ public class EmojiDrawer extends LinearLayout {
   private PagerSlidingTabStrip strip;
   private RecentEmojiPageModel recentModel;
   private EmojiEventListener   listener;
+  private EmojiDrawerListener  drawerListener;
 
   public EmojiDrawer(Context context) {
     this(context, null);
@@ -43,6 +45,9 @@ public class EmojiDrawer extends LinearLayout {
   public EmojiDrawer(Context context, AttributeSet attrs) {
     super(context, attrs);
     setOrientation(VERTICAL);
+  }
+
+  private void initView() {
     final View v = LayoutInflater.from(getContext()).inflate(R.layout.emoji_drawer, this, true);
     initializeResources(v);
     initializePageModels();
@@ -51,6 +56,10 @@ public class EmojiDrawer extends LinearLayout {
 
   public void setEmojiEventListener(EmojiEventListener listener) {
     this.listener = listener;
+  }
+
+  public void setDrawerListener(EmojiDrawerListener listener) {
+    this.drawerListener = listener;
   }
 
   private void initializeResources(View v) {
@@ -66,20 +75,27 @@ public class EmojiDrawer extends LinearLayout {
     });
   }
 
+  @Override
   public boolean isShowing() {
     return getVisibility() == VISIBLE;
   }
 
-  public void show(KeyboardAwareLinearLayout container) {
+  @Override
+  public void show(int height, boolean immediate) {
+    if (this.pager == null) initView();
     ViewGroup.LayoutParams params = getLayoutParams();
-    params.height = container.getKeyboardHeight();
+    params.height = height;
     Log.w("EmojiDrawer", "showing emoji drawer with height " + params.height);
     setLayoutParams(params);
     setVisibility(VISIBLE);
+    if (drawerListener != null) drawerListener.onShown();
   }
 
-  public void dismiss() {
+  @Override
+  public void hide(boolean immediate) {
     setVisibility(GONE);
+    if (drawerListener != null) drawerListener.onHidden();
+    Log.w("EmojiDrawer", "hide()");
   }
 
   private void initializeEmojiGrid() {
@@ -160,5 +176,10 @@ public class EmojiDrawer extends LinearLayout {
 
   public interface EmojiEventListener extends EmojiSelectionListener {
     void onKeyEvent(KeyEvent keyEvent);
+  }
+
+  public interface EmojiDrawerListener {
+    void onShown();
+    void onHidden();
   }
 }

src/org/thoughtcrime/securesms/components/emoji/EmojiToggle.java
@@ -8,8 +8,9 @@ import android.util.AttributeSet;
 import android.widget.ImageButton;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.components.emoji.EmojiDrawer.EmojiDrawerListener;
 
-public class EmojiToggle extends ImageButton {
+public class EmojiToggle extends ImageButton implements EmojiDrawerListener {
 
   private Drawable emojiToggle;
   private Drawable imeToggle;
@@ -46,6 +47,18 @@ public class EmojiToggle extends ImageButton {
     this.imeToggle       = drawables.getDrawable(1);
 
     drawables.recycle();
+    setToEmoji();
+  }
+
+  public void attach(EmojiDrawer drawer) {
+    drawer.setDrawerListener(this);
+  }
+
+  @Override public void onShown() {
+    setToIme();
+  }
+
+  @Override public void onHidden() {
     setToEmoji();
   }
 }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/3786
Fixes https://github.com/WhisperSystems/TextSecure/issues/3780
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/13bad6dfed6d7fc329cbc7ce54d93ced3f4f42e6

src/org/smssecure/smssecure/components/InputAwareLayout.java
@@ -1,0 +1,94 @@
+package org.smssecure.smssecure.components;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import org.smssecure.smssecure.components.KeyboardAwareLinearLayout.OnKeyboardShownListener;
+import org.smssecure.smssecure.util.ServiceUtil;
+
+public class InputAwareLayout extends KeyboardAwareLinearLayout implements OnKeyboardShownListener {
+  private InputView current;
+
+  public InputAwareLayout(Context context) {
+    this(context, null);
+  }
+
+  public InputAwareLayout(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public InputAwareLayout(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    addOnKeyboardShownListener(this);
+  }
+
+  @Override public void onKeyboardShown() {
+    hideAttachedInput();
+  }
+
+  public void show(@NonNull final EditText imeTarget, @NonNull final InputView input) {
+    if (isKeyboardOpen()) {
+      hideSoftkey(imeTarget, new Runnable() {
+        @Override public void run() {
+          input.show(getKeyboardHeight(), true);
+        }
+      });
+    } else if (current != null && current.isShowing()) {
+      current.hide(true);
+      input.show(getKeyboardHeight(), true);
+    } else {
+      input.show(getKeyboardHeight(), false);
+    }
+
+    current = input;
+  }
+
+  public InputView getCurrentInput() {
+    return current;
+  }
+
+  public void hideCurrentInput(EditText imeTarget) {
+    if (isKeyboardOpen()) hideSoftkey(imeTarget, null);
+    else                  hideAttachedInput();
+  }
+
+  public void hideAttachedInput() {
+    if (current != null) current.hide(true);
+    current = null;
+  }
+
+  public boolean isInputOpen() {
+    return (isKeyboardOpen() || (current != null && current.isShowing()));
+  }
+
+  public void showSoftkey(final EditText inputTarget) {
+    postOnKeyboardOpen(new Runnable() {
+      @Override public void run() {
+        hideAttachedInput();
+      }
+    });
+    inputTarget.post(new Runnable() {
+      @Override public void run() {
+        inputTarget.requestFocus();
+        ServiceUtil.getInputMethodManager(inputTarget.getContext()).showSoftInput(inputTarget, 0);
+      }
+    });
+  }
+
+  private void hideSoftkey(final EditText inputTarget, @Nullable Runnable runAfterClose) {
+    if (runAfterClose != null) postOnKeyboardClose(runAfterClose);
+
+    ServiceUtil.getInputMethodManager(inputTarget.getContext())
+               .hideSoftInputFromWindow(inputTarget.getWindowToken(), 0);
+  }
+
+  public interface InputView {
+    void show(int height, boolean immediate);
+    void hide(boolean immediate);
+    boolean isShowing();
+  }
+}
+

src/org/smssecure/smssecure/components/emoji/EmojiDrawer.java
@@ -1,6 +1,7 @@
 package org.smssecure.smssecure.components.emoji;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
@@ -18,7 +19,7 @@ import android.widget.LinearLayout;
 import com.astuetz.PagerSlidingTabStrip;
 
 import org.smssecure.smssecure.R;
-import org.smssecure.smssecure.components.KeyboardAwareLinearLayout;
+import org.smssecure.smssecure.components.InputManager.InputView;
 import org.smssecure.smssecure.components.RepeatableImageKey;
 import org.smssecure.smssecure.components.RepeatableImageKey.KeyEventListener;
 import org.smssecure.smssecure.components.emoji.EmojiPageView.EmojiSelectionListener;
@@ -27,7 +28,7 @@ import org.smssecure.smssecure.util.ResUtil;
 import java.util.LinkedList;
 import java.util.List;
 
-public class EmojiDrawer extends LinearLayout {
+public class EmojiDrawer extends LinearLayout implements InputView {
   private static final KeyEvent DELETE_KEY_EVENT = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL);
 
   private ViewPager            pager;
@@ -35,6 +36,7 @@ public class EmojiDrawer extends LinearLayout {
   private PagerSlidingTabStrip strip;
   private RecentEmojiPageModel recentModel;
   private EmojiEventListener   listener;
+  private EmojiDrawerListener  drawerListener;
 
   public EmojiDrawer(Context context) {
     this(context, null);
@@ -43,6 +45,9 @@ public class EmojiDrawer extends LinearLayout {
   public EmojiDrawer(Context context, AttributeSet attrs) {
     super(context, attrs);
     setOrientation(VERTICAL);
+  }
+
+  private void initView() {
     final View v = LayoutInflater.from(getContext()).inflate(R.layout.emoji_drawer, this, true);
     initializeResources(v);
     initializePageModels();
@@ -51,6 +56,10 @@ public class EmojiDrawer extends LinearLayout {
 
   public void setEmojiEventListener(EmojiEventListener listener) {
     this.listener = listener;
+  }
+
+  public void setDrawerListener(EmojiDrawerListener listener) {
+    this.drawerListener = listener;
   }
 
   private void initializeResources(View v) {
@@ -66,20 +75,27 @@ public class EmojiDrawer extends LinearLayout {
     });
   }
 
+  @Override
   public boolean isShowing() {
     return getVisibility() == VISIBLE;
   }
 
-  public void show(KeyboardAwareLinearLayout container) {
+  @Override
+  public void show(int height, boolean immediate) {
+    if (this.pager == null) initView();
     ViewGroup.LayoutParams params = getLayoutParams();
-    params.height = container.getKeyboardHeight();
+    params.height = height;
     Log.w("EmojiDrawer", "showing emoji drawer with height " + params.height);
     setLayoutParams(params);
     setVisibility(VISIBLE);
+    if (drawerListener != null) drawerListener.onShown();
   }
 
-  public void dismiss() {
+  @Override
+  public void hide(boolean immediate) {
     setVisibility(GONE);
+    if (drawerListener != null) drawerListener.onHidden();
+    Log.w("EmojiDrawer", "hide()");
   }
 
   private void initializeEmojiGrid() {
@@ -160,5 +176,10 @@ public class EmojiDrawer extends LinearLayout {
 
   public interface EmojiEventListener extends EmojiSelectionListener {
     void onKeyEvent(KeyEvent keyEvent);
+  }
+
+  public interface EmojiDrawerListener {
+    void onShown();
+    void onHidden();
   }
 }

src/org/smssecure/smssecure/components/emoji/EmojiToggle.java
@@ -8,8 +8,9 @@ import android.util.AttributeSet;
 import android.widget.ImageButton;
 
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.components.emoji.EmojiDrawer.EmojiDrawerListener;
 
-public class EmojiToggle extends ImageButton {
+public class EmojiToggle extends ImageButton implements EmojiDrawerListener {
 
   private Drawable emojiToggle;
   private Drawable imeToggle;
@@ -46,6 +47,18 @@ public class EmojiToggle extends ImageButton {
     this.imeToggle       = drawables.getDrawable(1);
 
     drawables.recycle();
+    setToEmoji();
+  }
+
+  public void attach(EmojiDrawer drawer) {
+    drawer.setDrawerListener(this);
+  }
+
+  @Override public void onShown() {
+    setToIme();
+  }
+
+  @Override public void onHidden() {
     setToEmoji();
   }
 }