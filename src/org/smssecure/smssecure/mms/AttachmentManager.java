SamSprung/Signal-Android

ditch RoundedImageView, make animated gifs work
// FREEBIE

src/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -38,6 +38,7 @@ import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.providers.CaptureProvider;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.BitmapDecodingException;
+import org.thoughtcrime.securesms.util.MediaUtil;
 
 import java.io.IOException;
 
@@ -68,22 +69,14 @@ public class AttachmentManager {
     AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
     animation.setDuration(200);
     animation.setAnimationListener(new Animation.AnimationListener() {
-      @Override
-      public void onAnimationStart(Animation animation) {
-
-      }
-
-      @Override
-      public void onAnimationEnd(Animation animation) {
+      @Override public void onAnimationStart(Animation animation) {}
+      @Override public void onAnimationRepeat(Animation animation) {}
+      @Override public void onAnimationEnd(Animation animation) {
         slideDeck.clear();
         attachmentView.setVisibility(View.GONE);
         attachmentListener.onAttachmentChanged();
       }
 
-      @Override
-      public void onAnimationRepeat(Animation animation) {
-
-      }
     });
 
     attachmentView.startAnimation(animation);
@@ -95,7 +88,11 @@ public class AttachmentManager {
   }
 
   public void setImage(MasterSecret masterSecret, Uri image) throws IOException, BitmapDecodingException {
-    setMedia(new ImageSlide(context, masterSecret, image), masterSecret);
+    if (MediaUtil.getMimeType(context, image).startsWith("image/gif")) {
+      setMedia(new GifSlide(context, masterSecret, image), masterSecret);
+    } else {
+      setMedia(new ImageSlide(context, masterSecret, image), masterSecret);
+    }
   }
 
   public void setVideo(Uri video) throws IOException, MediaTooLargeException {
@@ -147,7 +144,6 @@ public class AttachmentManager {
   public Uri getCaptureUri() {
     return captureUri;
   }
-
 
   public void setCaptureUri(Uri captureUri) {
     this.captureUri = captureUri;

src/org/thoughtcrime/securesms/mms/GifSlide.java
@@ -1,0 +1,27 @@
+package org.thoughtcrime.securesms.mms;
+
+import android.content.Context;
+import android.net.Uri;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.util.BitmapDecodingException;
+
+import java.io.IOException;
+
+import ws.com.google.android.mms.pdu.PduPart;
+
+public class GifSlide extends ImageSlide {
+  public GifSlide(Context context, MasterSecret masterSecret, PduPart part) {
+    super(context, masterSecret, part);
+  }
+
+  public GifSlide(Context context, MasterSecret masterSecret, Uri uri)
+      throws IOException, BitmapDecodingException
+  {
+    super(context, masterSecret, uri);
+  }
+
+  @Override public Uri getThumbnailUri() {
+    return getPart().getDataUri();
+  }
+}

src/org/thoughtcrime/securesms/mms/RoundedCorners.java
@@ -1,0 +1,88 @@
+package org.thoughtcrime.securesms.mms;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader.TileMode;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils;
+
+public class RoundedCorners extends BitmapTransformation {
+  private final boolean crop;
+  private final int     radius;
+  private final int     colorHint;
+
+  public RoundedCorners(@NonNull Context context, boolean crop, int radius, int colorHint) {
+    super(context);
+    this.crop      = crop;
+    this.radius    = radius;
+    this.colorHint = colorHint;
+  }
+
+  @Override protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth,
+                                       int outHeight)
+  {
+    final Bitmap toRound = crop ? centerCrop(pool, toTransform, outWidth, outHeight)
+                                : fitCenter(pool, toTransform, outWidth, outHeight);
+    return round(pool, toRound);
+  }
+
+  private Bitmap centerCrop(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+    final Bitmap toReuse     = pool.get(outWidth, outHeight, getSafeConfig(toTransform));
+    final Bitmap transformed = TransformationUtils.centerCrop(toReuse, toTransform, outWidth, outHeight);
+    if (toReuse != null && toReuse != transformed && !pool.put(toReuse)) {
+      toReuse.recycle();
+    }
+    return transformed;
+  }
+
+  private Bitmap fitCenter(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+    return TransformationUtils.fitCenter(toTransform, pool, outWidth, outHeight);
+  }
+
+  private Bitmap round(@NonNull BitmapPool pool, @Nullable Bitmap toRound) {
+    if (toRound == null) {
+      return null;
+    }
+
+    final Bitmap result;
+    final Bitmap toReuse = pool.get(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
+    if (toReuse != null) {
+      result = toReuse;
+    } else {
+      result = Bitmap.createBitmap(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
+    }
+
+    final Canvas canvas      = new Canvas(result);
+    final Paint  cornerPaint = new Paint();
+    final Paint  shaderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    shaderPaint.setShader(new BitmapShader(toRound, TileMode.CLAMP, TileMode.CLAMP));
+    cornerPaint.setColor(colorHint);
+    if (Config.RGB_565.equals(result.getConfig())) {
+      canvas.drawRect(0, 0, radius, radius, cornerPaint);
+      canvas.drawRect(0, toRound.getHeight() - radius, radius, toRound.getHeight(), cornerPaint);
+      canvas.drawRect(toRound.getWidth() - radius, 0, toRound.getWidth(), radius, cornerPaint);
+      canvas.drawRect(toRound.getWidth() - radius, toRound.getHeight() - radius, toRound.getWidth(), toRound.getHeight(), cornerPaint);
+    }
+    canvas.drawRoundRect(new RectF(0, 0, toRound.getWidth(), toRound.getHeight()), radius, radius, shaderPaint);
+//    Log.w("RoundedCorners", "in was " + toRound.getWidth() + "x" + toRound.getHeight() + ", out to " + result.getWidth() + "x" + result.getHeight());
+    return result;
+  }
+
+  private static Bitmap.Config getSafeConfig(Bitmap bitmap) {
+    return bitmap.getConfig() != null ? bitmap.getConfig() : Bitmap.Config.ARGB_8888;
+  }
+
+  @Override public String getId() {
+    return RoundedCorners.class.getCanonicalName();
+  }
+}

src/org/thoughtcrime/securesms/util/MediaUtil.java
@@ -3,11 +3,14 @@ package org.thoughtcrime.securesms.util;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.mms.AudioSlide;
+import org.thoughtcrime.securesms.mms.GifSlide;
 import org.thoughtcrime.securesms.mms.ImageSlide;
 import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.mms.Slide;
@@ -59,7 +62,9 @@ public class MediaUtil {
 
   public static Slide getSlideForPart(Context context, MasterSecret masterSecret, PduPart part, String contentType) {
     Slide slide = null;
-    if (ContentType.isImageType(contentType)) {
+    if (isGif(contentType)) {
+      slide = new GifSlide(context, masterSecret, part);
+    } else if (ContentType.isImageType(contentType)) {
       slide = new ImageSlide(context, masterSecret, part);
     } else if (ContentType.isVideoType(contentType)) {
       slide = new VideoSlide(context, masterSecret, part);
@@ -68,6 +73,23 @@ public class MediaUtil {
     }
 
     return slide;
+  }
+
+  public static String getMimeType(Context context, Uri uri) {
+    String type = context.getContentResolver().getType(uri);
+    if (type == null) {
+      final String extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+    }
+    return type;
+  }
+
+  private static boolean isGif(String contentType) {
+    return !TextUtils.isEmpty(contentType) && contentType.trim().equals("image/gif");
+  }
+
+  public static boolean isGif(PduPart part) {
+    return isGif(Util.toIsoString(part.getContentType()));
   }
 
   public static boolean isImage(PduPart part) {

typ-AhmedSleem/Silence-Android

Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/f13ad54ba11868115fb1630a7ac31d0a306a41c1

src/org/smssecure/smssecure/mms/AttachmentManager.java
@@ -38,6 +38,7 @@ import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.providers.CaptureProvider;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.BitmapDecodingException;
+import org.smssecure.smssecure.util.MediaUtil;
 
 import java.io.IOException;
 
@@ -68,22 +69,14 @@ public class AttachmentManager {
     AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
     animation.setDuration(200);
     animation.setAnimationListener(new Animation.AnimationListener() {
-      @Override
-      public void onAnimationStart(Animation animation) {
-
-      }
-
-      @Override
-      public void onAnimationEnd(Animation animation) {
+      @Override public void onAnimationStart(Animation animation) {}
+      @Override public void onAnimationRepeat(Animation animation) {}
+      @Override public void onAnimationEnd(Animation animation) {
         slideDeck.clear();
         attachmentView.setVisibility(View.GONE);
         attachmentListener.onAttachmentChanged();
       }
 
-      @Override
-      public void onAnimationRepeat(Animation animation) {
-
-      }
     });
 
     attachmentView.startAnimation(animation);
@@ -95,7 +88,11 @@ public class AttachmentManager {
   }
 
   public void setImage(MasterSecret masterSecret, Uri image) throws IOException, BitmapDecodingException {
-    setMedia(new ImageSlide(context, masterSecret, image), masterSecret);
+    if (MediaUtil.getMimeType(context, image).startsWith("image/gif")) {
+      setMedia(new GifSlide(context, masterSecret, image), masterSecret);
+    } else {
+      setMedia(new ImageSlide(context, masterSecret, image), masterSecret);
+    }
   }
 
   public void setVideo(Uri video) throws IOException, MediaTooLargeException {
@@ -147,7 +144,6 @@ public class AttachmentManager {
   public Uri getCaptureUri() {
     return captureUri;
   }
-
 
   public void setCaptureUri(Uri captureUri) {
     this.captureUri = captureUri;

src/org/smssecure/smssecure/mms/GifSlide.java
@@ -1,0 +1,27 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.Context;
+import android.net.Uri;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.BitmapDecodingException;
+
+import java.io.IOException;
+
+import ws.com.google.android.mms.pdu.PduPart;
+
+public class GifSlide extends ImageSlide {
+  public GifSlide(Context context, MasterSecret masterSecret, PduPart part) {
+    super(context, masterSecret, part);
+  }
+
+  public GifSlide(Context context, MasterSecret masterSecret, Uri uri)
+      throws IOException, BitmapDecodingException
+  {
+    super(context, masterSecret, uri);
+  }
+
+  @Override public Uri getThumbnailUri() {
+    return getPart().getDataUri();
+  }
+}

src/org/smssecure/smssecure/mms/RoundedCorners.java
@@ -1,0 +1,88 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader.TileMode;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils;
+
+public class RoundedCorners extends BitmapTransformation {
+  private final boolean crop;
+  private final int     radius;
+  private final int     colorHint;
+
+  public RoundedCorners(@NonNull Context context, boolean crop, int radius, int colorHint) {
+    super(context);
+    this.crop      = crop;
+    this.radius    = radius;
+    this.colorHint = colorHint;
+  }
+
+  @Override protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth,
+                                       int outHeight)
+  {
+    final Bitmap toRound = crop ? centerCrop(pool, toTransform, outWidth, outHeight)
+                                : fitCenter(pool, toTransform, outWidth, outHeight);
+    return round(pool, toRound);
+  }
+
+  private Bitmap centerCrop(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+    final Bitmap toReuse     = pool.get(outWidth, outHeight, getSafeConfig(toTransform));
+    final Bitmap transformed = TransformationUtils.centerCrop(toReuse, toTransform, outWidth, outHeight);
+    if (toReuse != null && toReuse != transformed && !pool.put(toReuse)) {
+      toReuse.recycle();
+    }
+    return transformed;
+  }
+
+  private Bitmap fitCenter(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+    return TransformationUtils.fitCenter(toTransform, pool, outWidth, outHeight);
+  }
+
+  private Bitmap round(@NonNull BitmapPool pool, @Nullable Bitmap toRound) {
+    if (toRound == null) {
+      return null;
+    }
+
+    final Bitmap result;
+    final Bitmap toReuse = pool.get(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
+    if (toReuse != null) {
+      result = toReuse;
+    } else {
+      result = Bitmap.createBitmap(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
+    }
+
+    final Canvas canvas      = new Canvas(result);
+    final Paint  cornerPaint = new Paint();
+    final Paint  shaderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    shaderPaint.setShader(new BitmapShader(toRound, TileMode.CLAMP, TileMode.CLAMP));
+    cornerPaint.setColor(colorHint);
+    if (Config.RGB_565.equals(result.getConfig())) {
+      canvas.drawRect(0, 0, radius, radius, cornerPaint);
+      canvas.drawRect(0, toRound.getHeight() - radius, radius, toRound.getHeight(), cornerPaint);
+      canvas.drawRect(toRound.getWidth() - radius, 0, toRound.getWidth(), radius, cornerPaint);
+      canvas.drawRect(toRound.getWidth() - radius, toRound.getHeight() - radius, toRound.getWidth(), toRound.getHeight(), cornerPaint);
+    }
+    canvas.drawRoundRect(new RectF(0, 0, toRound.getWidth(), toRound.getHeight()), radius, radius, shaderPaint);
+//    Log.w("RoundedCorners", "in was " + toRound.getWidth() + "x" + toRound.getHeight() + ", out to " + result.getWidth() + "x" + result.getHeight());
+    return result;
+  }
+
+  private static Bitmap.Config getSafeConfig(Bitmap bitmap) {
+    return bitmap.getConfig() != null ? bitmap.getConfig() : Bitmap.Config.ARGB_8888;
+  }
+
+  @Override public String getId() {
+    return RoundedCorners.class.getCanonicalName();
+  }
+}

src/org/smssecure/smssecure/util/MediaUtil.java
@@ -3,11 +3,14 @@ package org.smssecure.smssecure.util;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.mms.AudioSlide;
+import org.smssecure.smssecure.mms.GifSlide;
 import org.smssecure.smssecure.mms.ImageSlide;
 import org.smssecure.smssecure.mms.PartAuthority;
 import org.smssecure.smssecure.mms.Slide;
@@ -59,7 +62,9 @@ public class MediaUtil {
 
   public static Slide getSlideForPart(Context context, MasterSecret masterSecret, PduPart part, String contentType) {
     Slide slide = null;
-    if (ContentType.isImageType(contentType)) {
+    if (isGif(contentType)) {
+      slide = new GifSlide(context, masterSecret, part);
+    } else if (ContentType.isImageType(contentType)) {
       slide = new ImageSlide(context, masterSecret, part);
     } else if (ContentType.isVideoType(contentType)) {
       slide = new VideoSlide(context, masterSecret, part);
@@ -68,6 +73,23 @@ public class MediaUtil {
     }
 
     return slide;
+  }
+
+  public static String getMimeType(Context context, Uri uri) {
+    String type = context.getContentResolver().getType(uri);
+    if (type == null) {
+      final String extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+    }
+    return type;
+  }
+
+  private static boolean isGif(String contentType) {
+    return !TextUtils.isEmpty(contentType) && contentType.trim().equals("image/gif");
+  }
+
+  public static boolean isGif(PduPart part) {
+    return isGif(Util.toIsoString(part.getContentType()));
   }
 
   public static boolean isImage(PduPart part) {