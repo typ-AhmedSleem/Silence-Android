SamSprung/Signal-Android

asyncify reminderView
Closes #3762
// FREEBIE

src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -17,6 +17,7 @@
 package org.thoughtcrime.securesms;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
@@ -51,6 +52,7 @@ import org.thoughtcrime.securesms.components.DefaultSmsReminder;
 import org.thoughtcrime.securesms.components.DividerItemDecoration;
 import org.thoughtcrime.securesms.components.ExpiredBuildReminder;
 import org.thoughtcrime.securesms.components.PushRegistrationReminder;
+import org.thoughtcrime.securesms.components.Reminder;
 import org.thoughtcrime.securesms.components.ReminderView;
 import org.thoughtcrime.securesms.components.SystemSmsImportReminder;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
@@ -58,6 +60,7 @@ import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.loaders.ConversationListLoader;
 import org.thoughtcrime.securesms.notifications.MessageNotifier;
 import org.thoughtcrime.securesms.recipients.Recipients;
+import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Locale;
 import java.util.Set;
@@ -131,17 +134,29 @@ public class ConversationListFragment extends Fragment
   }
 
   private void initializeReminders() {
-    if (ExpiredBuildReminder.isEligible(getActivity())) {
-      reminderView.showReminder(new ExpiredBuildReminder());
-    } else if (DefaultSmsReminder.isEligible(getActivity())) {
-      reminderView.showReminder(new DefaultSmsReminder(getActivity()));
-    } else if (SystemSmsImportReminder.isEligible(getActivity())) {
-      reminderView.showReminder(new SystemSmsImportReminder(getActivity(), masterSecret));
-    } else if (PushRegistrationReminder.isEligible(getActivity())) {
-      reminderView.showReminder(new PushRegistrationReminder(getActivity(), masterSecret));
-    } else {
-      reminderView.hide();
-    }
+    reminderView.hide();
+    new AsyncTask<Context, Void, Optional<? extends Reminder>>() {
+      @Override protected Optional<? extends Reminder> doInBackground(Context... params) {
+        final Context context = params[0];
+        if (ExpiredBuildReminder.isEligible(context)) {
+          return Optional.of(new ExpiredBuildReminder());
+        } else if (DefaultSmsReminder.isEligible(context)) {
+          return Optional.of(new DefaultSmsReminder(context));
+        } else if (SystemSmsImportReminder.isEligible(context)) {
+          return Optional.of((new SystemSmsImportReminder(context, masterSecret)));
+        } else if (PushRegistrationReminder.isEligible(context)) {
+          return Optional.of((new PushRegistrationReminder(context, masterSecret)));
+        } else {
+          return Optional.absent();
+        }
+      }
+
+      @Override protected void onPostExecute(Optional<? extends Reminder> reminder) {
+        if (reminder.isPresent() && getActivity() != null && !isRemoving()) {
+          reminderView.showReminder(reminder.get());
+        }
+      }
+    }.execute(getActivity());
   }
 
   private void initializeListAdapter() {

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/3762
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/454c604b8626ee97712d603847f63e179241f6c0

src/org/smssecure/smssecure/ConversationListFragment.java
@@ -17,6 +17,7 @@
 package org.smssecure.smssecure;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
@@ -50,6 +51,7 @@ import com.melnykov.fab.FloatingActionButton;
 import org.smssecure.smssecure.ConversationListAdapter.ItemClickListener;
 import org.smssecure.smssecure.components.DefaultSmsReminder;
 import org.smssecure.smssecure.components.DividerItemDecoration;
+import org.smssecure.smssecure.components.Reminder;
 import org.smssecure.smssecure.components.ReminderView;
 import org.smssecure.smssecure.components.StoreRatingReminder;
 import org.smssecure.smssecure.components.SystemSmsImportReminder;
@@ -58,6 +60,7 @@ import org.smssecure.smssecure.database.loaders.ConversationListLoader;
 import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Locale;
 import java.util.Set;
@@ -131,15 +134,27 @@ public class ConversationListFragment extends Fragment
   }
 
   private void initializeReminders() {
-    if (DefaultSmsReminder.isEligible(getActivity())) {
-      reminderView.showReminder(new DefaultSmsReminder(getActivity()));
-    } else if (SystemSmsImportReminder.isEligible(getActivity())) {
-      reminderView.showReminder(new SystemSmsImportReminder(getActivity(), masterSecret));
-    } else if (StoreRatingReminder.isEligible(getActivity())){
-      reminderView.showReminder(new StoreRatingReminder(getActivity()));
-    } else {
-      reminderView.hide();
-    }
+    reminderView.hide();
+    new AsyncTask<Context, Void, Optional<? extends Reminder>>() {
+      @Override protected Optional<? extends Reminder> doInBackground(Context... params) {
+        final Context context = params[0];
+         if (DefaultSmsReminder.isEligible(context)) {
+          return Optional.of(new DefaultSmsReminder(context));
+        } else if (SystemSmsImportReminder.isEligible(context)) {
+          return Optional.of((new SystemSmsImportReminder(context, masterSecret)));
+        } else if (StoreRatingReminder.isEligible(context)) {
+          return Optional.of((new StoreRatingReminder(context)));
+        } else {
+          return Optional.absent();
+        }
+      }
+
+      @Override protected void onPostExecute(Optional<? extends Reminder> reminder) {
+        if (reminder.isPresent() && getActivity() != null && !isRemoving()) {
+          reminderView.showReminder(reminder.get());
+        }
+      }
+    }.execute(getActivity());
   }
 
   private void initializeListAdapter() {