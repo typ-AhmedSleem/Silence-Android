SamSprung/Signal-Android

Support for notification privacy settings.
// FREEBIE

src/org/thoughtcrime/securesms/notifications/AbstractNotificationBuilder.java
@@ -1,0 +1,70 @@
+package org.thoughtcrime.securesms.notifications;
+
+import android.app.Notification;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+
+import org.thoughtcrime.securesms.database.RecipientPreferenceDatabase;
+import org.thoughtcrime.securesms.preferences.NotificationPrivacyPreference;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.util.TextSecurePreferences;
+import org.thoughtcrime.securesms.util.Util;
+
+public abstract class AbstractNotificationBuilder extends NotificationCompat.Builder {
+
+  protected Context                       context;
+  protected NotificationPrivacyPreference privacy;
+
+  public AbstractNotificationBuilder(Context context, NotificationPrivacyPreference privacy) {
+    super(context);
+
+    this.context = context;
+    this.privacy = privacy;
+  }
+
+  protected CharSequence getStyledMessage(@NonNull Recipient recipient, @Nullable CharSequence message) {
+    SpannableStringBuilder builder = new SpannableStringBuilder();
+    builder.append(Util.getBoldedString(recipient.toShortString()));
+    builder.append(": ");
+    builder.append(message == null ? "" : message);
+
+    return builder;
+  }
+
+  public void setAlarms(@Nullable Uri ringtone, RecipientPreferenceDatabase.VibrateState vibrate) {
+    String defaultRingtoneName   = TextSecurePreferences.getNotificationRingtone(context);
+    boolean defaultVibrate       = TextSecurePreferences.isNotificationVibrateEnabled(context);
+    String ledColor              = TextSecurePreferences.getNotificationLedColor(context);
+    String ledBlinkPattern       = TextSecurePreferences.getNotificationLedPattern(context);
+    String ledBlinkPatternCustom = TextSecurePreferences.getNotificationLedPatternCustom(context);
+    String[] blinkPatternArray   = parseBlinkPattern(ledBlinkPattern, ledBlinkPatternCustom);
+
+    if      (ringtone != null)                       setSound(ringtone);
+    else if (TextUtils.isEmpty(defaultRingtoneName)) setSound(Uri.parse(defaultRingtoneName));
+
+    if (vibrate == RecipientPreferenceDatabase.VibrateState.ENABLED ||
+        (vibrate == RecipientPreferenceDatabase.VibrateState.DEFAULT && defaultVibrate))
+    {
+      setDefaults(Notification.DEFAULT_VIBRATE);
+    }
+
+    if (!ledColor.equals("none")) {
+      setLights(Color.parseColor(ledColor),
+                Integer.parseInt(blinkPatternArray[0]),
+                Integer.parseInt(blinkPatternArray[1]));
+    }
+  }
+
+  private String[] parseBlinkPattern(String blinkPattern, String blinkPatternCustom) {
+    if (blinkPattern.equals("custom"))
+      blinkPattern = blinkPatternCustom;
+
+    return blinkPattern.split(",");
+  }
+}

src/org/thoughtcrime/securesms/notifications/FailedNotificationBuilder.java
@@ -1,0 +1,30 @@
+package org.thoughtcrime.securesms.notifications;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.database.RecipientPreferenceDatabase;
+import org.thoughtcrime.securesms.preferences.NotificationPrivacyPreference;
+
+public class FailedNotificationBuilder extends AbstractNotificationBuilder {
+
+  public FailedNotificationBuilder(Context context, NotificationPrivacyPreference privacy, Intent intent) {
+    super(context, privacy);
+
+    setSmallIcon(R.drawable.icon_notification);
+    setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                                              R.drawable.ic_action_warning_red));
+    setContentTitle(context.getString(R.string.MessageNotifier_message_delivery_failed));
+    setContentText(context.getString(R.string.MessageNotifier_failed_to_deliver_message));
+    setTicker(context.getString(R.string.MessageNotifier_error_delivering_message));
+    setContentIntent(PendingIntent.getActivity(context, 0, intent, 0));
+    setAutoCancel(true);
+    setAlarms(null, RecipientPreferenceDatabase.VibrateState.DEFAULT);
+  }
+
+
+
+}

src/org/thoughtcrime/securesms/notifications/MultipleRecipientNotificationBuilder.java
@@ -1,0 +1,84 @@
+package org.thoughtcrime.securesms.notifications;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import org.thoughtcrime.securesms.ConversationListActivity;
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.preferences.NotificationPrivacyPreference;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.util.Util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MultipleRecipientNotificationBuilder extends AbstractNotificationBuilder {
+
+  private final List<CharSequence> messageBodies = new LinkedList<>();
+
+  public MultipleRecipientNotificationBuilder(Context context, NotificationPrivacyPreference privacy) {
+    super(context, privacy);
+
+    setColor(context.getResources().getColor(R.color.textsecure_primary));
+    setSmallIcon(R.drawable.icon_notification);
+    setContentTitle(context.getString(R.string.app_name));
+    setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, ConversationListActivity.class), 0));
+    setCategory(NotificationCompat.CATEGORY_MESSAGE);
+    setPriority(NotificationCompat.PRIORITY_HIGH);
+    setDeleteIntent(PendingIntent.getBroadcast(context, 0, new Intent(MessageNotifier.DeleteReceiver.DELETE_REMINDER_ACTION), 0));
+  }
+
+  public void setMessageCount(int messageCount, int threadCount) {
+    setSubText(context.getString(R.string.MessageNotifier_d_new_messages_in_d_conversations,
+                                 messageCount, threadCount));
+    setContentInfo(String.valueOf(messageCount));
+    setNumber(messageCount);
+  }
+
+  public void setMostRecentSender(Recipient recipient) {
+    if (privacy.isDisplayContact()) {
+      setContentText(context.getString(R.string.MessageNotifier_most_recent_from_s,
+                                       recipient.toShortString()));
+    }
+  }
+
+  public void addActions(PendingIntent markAsReadIntent) {
+    NotificationCompat.Action markAllAsReadAction = new NotificationCompat.Action(R.drawable.check,
+                                            context.getString(R.string.MessageNotifier_mark_all_as_read),
+                                            markAsReadIntent);
+    addAction(markAllAsReadAction);
+    extend(new NotificationCompat.WearableExtender().addAction(markAllAsReadAction));
+  }
+
+  public void addMessageBody(@NonNull Recipient sender, @Nullable CharSequence body) {
+    if (privacy.isDisplayMessage()) {
+      messageBodies.add(getStyledMessage(sender, body));
+    } else if (privacy.isDisplayContact()) {
+      messageBodies.add(Util.getBoldedString(sender.toShortString()));
+    }
+
+    if (privacy.isDisplayContact() && sender.getContactUri() != null) {
+      addPerson(sender.getContactUri().toString());
+    }
+  }
+
+  @Override
+  public Notification build() {
+    if (privacy.isDisplayMessage() || privacy.isDisplayContact()) {
+      NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle();
+
+      for (CharSequence body : messageBodies) {
+        style.addLine(body);
+      }
+
+      setStyle(style);
+    }
+
+    return super.build();
+  }
+}

src/org/thoughtcrime/securesms/preferences/NotificationPrivacyPreference.java
@@ -1,0 +1,19 @@
+package org.thoughtcrime.securesms.preferences;
+
+public class NotificationPrivacyPreference {
+
+  private final String preference;
+
+  public NotificationPrivacyPreference(String preference) {
+    this.preference = preference;
+  }
+
+  public boolean isDisplayContact() {
+    return "all".equals(preference) || "contact".equals(preference);
+  }
+
+  public boolean isDisplayMessage() {
+    return "all".equals(preference);
+  }
+
+}

src/org/thoughtcrime/securesms/preferences/NotificationsPreferenceFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -14,13 +15,18 @@ import android.text.TextUtils;
 
 import org.thoughtcrime.securesms.ApplicationPreferencesActivity;
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.notifications.MessageNotifier;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 
 public class NotificationsPreferenceFragment extends ListSummaryPreferenceFragment {
 
+  private MasterSecret masterSecret;
+
   @Override
   public void onCreate(Bundle paramBundle) {
     super.onCreate(paramBundle);
+    masterSecret = getArguments().getParcelable("master_secret");
     addPreferencesFromResource(R.xml.preferences_notifications);
 
     this.findPreference(TextSecurePreferences.LED_COLOR_PREF)
@@ -31,10 +37,13 @@ public class NotificationsPreferenceFragment extends ListSummaryPreferenceFragme
         .setOnPreferenceChangeListener(new RingtoneSummaryListener());
     this.findPreference(TextSecurePreferences.REPEAT_ALERTS_PREF)
         .setOnPreferenceChangeListener(new ListSummaryListener());
+    this.findPreference(TextSecurePreferences.NOTIFICATION_PRIVACY_PREF)
+        .setOnPreferenceChangeListener(new NotificationPrivacyListener());
 
     initializeListSummary((ListPreference) findPreference(TextSecurePreferences.LED_COLOR_PREF));
     initializeListSummary((ListPreference) findPreference(TextSecurePreferences.LED_BLINK_PREF));
     initializeListSummary((ListPreference) findPreference(TextSecurePreferences.REPEAT_ALERTS_PREF));
+    initializeListSummary((ListPreference) findPreference(TextSecurePreferences.NOTIFICATION_PRIVACY_PREF));
     initializeRingtoneSummary((RingtonePreference) findPreference(TextSecurePreferences.RINGTONE_PREF));
   }
 
@@ -75,5 +84,21 @@ public class NotificationsPreferenceFragment extends ListSummaryPreferenceFragme
     final int offCapsResId  = R.string.ApplicationPreferencesActivity_Off;
 
     return context.getString(TextSecurePreferences.isNotificationsEnabled(context) ? onCapsResId : offCapsResId);
+  }
+
+  private class NotificationPrivacyListener extends ListSummaryListener {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+      new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void... params) {
+          MessageNotifier.updateNotification(getActivity(), masterSecret);
+          return null;
+        }
+      }.execute();
+
+      return super.onPreferenceChange(preference, value);
+    }
+
   }
 }

typ-AhmedSleem/Silence-Android

Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/120cde9917d9eaa8e94ad1494efce5d30bcc51a5

src/org/smssecure/smssecure/notifications/AbstractNotificationBuilder.java
@@ -1,0 +1,70 @@
+package org.smssecure.smssecure.notifications;
+
+import android.app.Notification;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+
+import org.smssecure.smssecure.database.RecipientPreferenceDatabase;
+import org.smssecure.smssecure.preferences.NotificationPrivacyPreference;
+import org.smssecure.smssecure.recipients.Recipient;
+import org.smssecure.smssecure.util.SMSSecurePreferences;
+import org.smssecure.smssecure.util.Util;
+
+public abstract class AbstractNotificationBuilder extends NotificationCompat.Builder {
+
+  protected Context                       context;
+  protected NotificationPrivacyPreference privacy;
+
+  public AbstractNotificationBuilder(Context context, NotificationPrivacyPreference privacy) {
+    super(context);
+
+    this.context = context;
+    this.privacy = privacy;
+  }
+
+  protected CharSequence getStyledMessage(@NonNull Recipient recipient, @Nullable CharSequence message) {
+    SpannableStringBuilder builder = new SpannableStringBuilder();
+    builder.append(Util.getBoldedString(recipient.toShortString()));
+    builder.append(": ");
+    builder.append(message == null ? "" : message);
+
+    return builder;
+  }
+
+  public void setAlarms(@Nullable Uri ringtone, RecipientPreferenceDatabase.VibrateState vibrate) {
+    String defaultRingtoneName   = SMSSecurePreferences.getNotificationRingtone(context);
+    boolean defaultVibrate       = SMSSecurePreferences.isNotificationVibrateEnabled(context);
+    String ledColor              = SMSSecurePreferences.getNotificationLedColor(context);
+    String ledBlinkPattern       = SMSSecurePreferences.getNotificationLedPattern(context);
+    String ledBlinkPatternCustom = SMSSecurePreferences.getNotificationLedPatternCustom(context);
+    String[] blinkPatternArray   = parseBlinkPattern(ledBlinkPattern, ledBlinkPatternCustom);
+
+    if      (ringtone != null)                       setSound(ringtone);
+    else if (TextUtils.isEmpty(defaultRingtoneName)) setSound(Uri.parse(defaultRingtoneName));
+
+    if (vibrate == RecipientPreferenceDatabase.VibrateState.ENABLED ||
+        (vibrate == RecipientPreferenceDatabase.VibrateState.DEFAULT && defaultVibrate))
+    {
+      setDefaults(Notification.DEFAULT_VIBRATE);
+    }
+
+    if (!ledColor.equals("none")) {
+      setLights(Color.parseColor(ledColor),
+                Integer.parseInt(blinkPatternArray[0]),
+                Integer.parseInt(blinkPatternArray[1]));
+    }
+  }
+
+  private String[] parseBlinkPattern(String blinkPattern, String blinkPatternCustom) {
+    if (blinkPattern.equals("custom"))
+      blinkPattern = blinkPatternCustom;
+
+    return blinkPattern.split(",");
+  }
+}

src/org/smssecure/smssecure/notifications/FailedNotificationBuilder.java
@@ -1,0 +1,30 @@
+package org.smssecure.smssecure.notifications;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+
+import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.database.RecipientPreferenceDatabase;
+import org.smssecure.smssecure.preferences.NotificationPrivacyPreference;
+
+public class FailedNotificationBuilder extends AbstractNotificationBuilder {
+
+  public FailedNotificationBuilder(Context context, NotificationPrivacyPreference privacy, Intent intent) {
+    super(context, privacy);
+
+    setSmallIcon(R.drawable.icon_notification);
+    setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                                              R.drawable.ic_action_warning_red));
+    setContentTitle(context.getString(R.string.MessageNotifier_message_delivery_failed));
+    setContentText(context.getString(R.string.MessageNotifier_failed_to_deliver_message));
+    setTicker(context.getString(R.string.MessageNotifier_error_delivering_message));
+    setContentIntent(PendingIntent.getActivity(context, 0, intent, 0));
+    setAutoCancel(true);
+    setAlarms(null, RecipientPreferenceDatabase.VibrateState.DEFAULT);
+  }
+
+
+
+}

src/org/smssecure/smssecure/notifications/MultipleRecipientNotificationBuilder.java
@@ -1,0 +1,84 @@
+package org.smssecure.smssecure.notifications;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import org.smssecure.smssecure.ConversationListActivity;
+import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.preferences.NotificationPrivacyPreference;
+import org.smssecure.smssecure.recipients.Recipient;
+import org.smssecure.smssecure.util.Util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MultipleRecipientNotificationBuilder extends AbstractNotificationBuilder {
+
+  private final List<CharSequence> messageBodies = new LinkedList<>();
+
+  public MultipleRecipientNotificationBuilder(Context context, NotificationPrivacyPreference privacy) {
+    super(context, privacy);
+
+    setColor(context.getResources().getColor(R.color.textsecure_primary));
+    setSmallIcon(R.drawable.icon_notification);
+    setContentTitle(context.getString(R.string.app_name));
+    setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, ConversationListActivity.class), 0));
+    setCategory(NotificationCompat.CATEGORY_MESSAGE);
+    setPriority(NotificationCompat.PRIORITY_HIGH);
+    setDeleteIntent(PendingIntent.getBroadcast(context, 0, new Intent(MessageNotifier.DeleteReceiver.DELETE_REMINDER_ACTION), 0));
+  }
+
+  public void setMessageCount(int messageCount, int threadCount) {
+    setSubText(context.getString(R.string.MessageNotifier_d_new_messages_in_d_conversations,
+                                 messageCount, threadCount));
+    setContentInfo(String.valueOf(messageCount));
+    setNumber(messageCount);
+  }
+
+  public void setMostRecentSender(Recipient recipient) {
+    if (privacy.isDisplayContact()) {
+      setContentText(context.getString(R.string.MessageNotifier_most_recent_from_s,
+                                       recipient.toShortString()));
+    }
+  }
+
+  public void addActions(PendingIntent markAsReadIntent) {
+    NotificationCompat.Action markAllAsReadAction = new NotificationCompat.Action(R.drawable.check,
+                                            context.getString(R.string.MessageNotifier_mark_all_as_read),
+                                            markAsReadIntent);
+    addAction(markAllAsReadAction);
+    extend(new NotificationCompat.WearableExtender().addAction(markAllAsReadAction));
+  }
+
+  public void addMessageBody(@NonNull Recipient sender, @Nullable CharSequence body) {
+    if (privacy.isDisplayMessage()) {
+      messageBodies.add(getStyledMessage(sender, body));
+    } else if (privacy.isDisplayContact()) {
+      messageBodies.add(Util.getBoldedString(sender.toShortString()));
+    }
+
+    if (privacy.isDisplayContact() && sender.getContactUri() != null) {
+      addPerson(sender.getContactUri().toString());
+    }
+  }
+
+  @Override
+  public Notification build() {
+    if (privacy.isDisplayMessage() || privacy.isDisplayContact()) {
+      NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle();
+
+      for (CharSequence body : messageBodies) {
+        style.addLine(body);
+      }
+
+      setStyle(style);
+    }
+
+    return super.build();
+  }
+}

src/org/smssecure/smssecure/preferences/NotificationPrivacyPreference.java
@@ -1,0 +1,19 @@
+package org.smssecure.smssecure.preferences;
+
+public class NotificationPrivacyPreference {
+
+  private final String preference;
+
+  public NotificationPrivacyPreference(String preference) {
+    this.preference = preference;
+  }
+
+  public boolean isDisplayContact() {
+    return "all".equals(preference) || "contact".equals(preference);
+  }
+
+  public boolean isDisplayMessage() {
+    return "all".equals(preference);
+  }
+
+}

src/org/smssecure/smssecure/preferences/NotificationsPreferenceFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -14,13 +15,18 @@ import android.text.TextUtils;
 
 import org.smssecure.smssecure.ApplicationPreferencesActivity;
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
 
 public class NotificationsPreferenceFragment extends ListSummaryPreferenceFragment {
 
+  private MasterSecret masterSecret;
+
   @Override
   public void onCreate(Bundle paramBundle) {
     super.onCreate(paramBundle);
+    masterSecret = getArguments().getParcelable("master_secret");
     addPreferencesFromResource(R.xml.preferences_notifications);
 
     this.findPreference(SMSSecurePreferences.LED_COLOR_PREF)
@@ -31,10 +37,13 @@ public class NotificationsPreferenceFragment extends ListSummaryPreferenceFragme
         .setOnPreferenceChangeListener(new RingtoneSummaryListener());
     this.findPreference(SMSSecurePreferences.REPEAT_ALERTS_PREF)
         .setOnPreferenceChangeListener(new ListSummaryListener());
+    this.findPreference(SMSSecurePreferences.NOTIFICATION_PRIVACY_PREF)
+        .setOnPreferenceChangeListener(new NotificationPrivacyListener());
 
     initializeListSummary((ListPreference) findPreference(SMSSecurePreferences.LED_COLOR_PREF));
     initializeListSummary((ListPreference) findPreference(SMSSecurePreferences.LED_BLINK_PREF));
     initializeListSummary((ListPreference) findPreference(SMSSecurePreferences.REPEAT_ALERTS_PREF));
+    initializeListSummary((ListPreference) findPreference(SMSSecurePreferences.NOTIFICATION_PRIVACY_PREF));
     initializeRingtoneSummary((RingtonePreference) findPreference(SMSSecurePreferences.RINGTONE_PREF));
   }
 
@@ -75,5 +84,21 @@ public class NotificationsPreferenceFragment extends ListSummaryPreferenceFragme
     final int offCapsResId  = R.string.ApplicationPreferencesActivity_Off;
 
     return context.getString(SMSSecurePreferences.isNotificationsEnabled(context) ? onCapsResId : offCapsResId);
+  }
+
+  private class NotificationPrivacyListener extends ListSummaryListener {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+      new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void... params) {
+          MessageNotifier.updateNotification(getActivity(), masterSecret);
+          return null;
+        }
+      }.execute();
+
+      return super.onPreferenceChange(preference, value);
+    }
+
   }
 }