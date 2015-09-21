SamSprung/Signal-Android

Media download controls
Closes #3991
// FREEBIE

src/org/thoughtcrime/securesms/ApplicationContext.java
@@ -29,6 +29,7 @@ import org.thoughtcrime.securesms.dependencies.TextSecureCommunicationModule;
 import org.thoughtcrime.securesms.jobs.GcmRefreshJob;
 import org.thoughtcrime.securesms.jobs.persistence.EncryptingJobSerializer;
 import org.thoughtcrime.securesms.jobs.requirements.MasterSecretRequirementProvider;
+import org.thoughtcrime.securesms.jobs.requirements.MediaNetworkRequirementProvider;
 import org.thoughtcrime.securesms.jobs.requirements.ServiceRequirementProvider;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.whispersystems.jobqueue.JobManager;
@@ -49,8 +50,10 @@ import dagger.ObjectGraph;
  */
 public class ApplicationContext extends Application implements DependencyInjector {
 
-  private JobManager jobManager;
+  private JobManager  jobManager;
   private ObjectGraph objectGraph;
+
+  private MediaNetworkRequirementProvider mediaNetworkRequirementProvider = new MediaNetworkRequirementProvider();
 
   public static ApplicationContext getInstance(Context context) {
     return (ApplicationContext)context.getApplicationContext();
@@ -103,9 +106,14 @@ public class ApplicationContext extends Application implements DependencyInjecto
                                 .withJobSerializer(new EncryptingJobSerializer())
                                 .withRequirementProviders(new MasterSecretRequirementProvider(this),
                                                           new ServiceRequirementProvider(this),
-                                                          new NetworkRequirementProvider(this))
+                                                          new NetworkRequirementProvider(this),
+                                                          mediaNetworkRequirementProvider)
                                 .withConsumerThreads(5)
                                 .build();
+  }
+
+  public void notifyMediaControlEvent() {
+    mediaNetworkRequirementProvider.notifyMediaControlEvent();
   }
 
   private void initializeDependencyInjection() {

src/org/thoughtcrime/securesms/ConversationItem.java
@@ -47,6 +47,7 @@ import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.MmsDatabase;
 import org.thoughtcrime.securesms.database.MmsSmsDatabase;
+import org.thoughtcrime.securesms.database.PartDatabase;
 import org.thoughtcrime.securesms.database.SmsDatabase;
 import org.thoughtcrime.securesms.database.documents.IdentityKeyMismatch;
 import org.thoughtcrime.securesms.database.model.MediaMmsMessageRecord;
@@ -156,6 +157,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     if (mediaThumbnail != null) {
       mediaThumbnail.setThumbnailClickListener(new ThumbnailClickListener());
       mediaThumbnail.setOnLongClickListener(new MultiSelectLongClickListener());
+      mediaThumbnail.setDownloadClickListener(new ThumbnailDownloadClickListener());
     }
   }
 
@@ -275,7 +277,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
       mediaThumbnail.setImageResource(masterSecret, messageRecord.getId(),
                                       messageRecord.getDateReceived(),
                                       ((MediaMmsMessageRecord)messageRecord).getSlideDeckFuture());
-      mediaThumbnail.setShowProgress(!messageRecord.isFailed() && (!messageRecord.isOutgoing() || messageRecord.isPending()));
+      mediaThumbnail.hideControls(messageRecord.isFailed() || (messageRecord.isOutgoing() && !messageRecord.isPending()));
       bodyText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     } else {
       mediaThumbnail.setVisibility(View.GONE);
@@ -409,6 +411,11 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     }
   }
 
+  private class ThumbnailDownloadClickListener implements ThumbnailView.ThumbnailClickListener {
+    @Override public void onClick(View v, Slide slide) {
+      DatabaseFactory.getPartDatabase(context).setTransferState(messageRecord.getId(), slide.getPart().getPartId(), PartDatabase.TRANSFER_PROGRESS_STARTED);
+    }
+  }
   private class ThumbnailClickListener implements ThumbnailView.ThumbnailClickListener {
     private void fireIntent(Slide slide) {
       Log.w(TAG, "Clicked: " + slide.getUri() + " , " + slide.getContentType());

src/org/thoughtcrime/securesms/jobs/AttachmentDownloadJob.java
@@ -12,6 +12,7 @@ import org.thoughtcrime.securesms.database.PartDatabase;
 import org.thoughtcrime.securesms.database.PartDatabase.PartId;
 import org.thoughtcrime.securesms.dependencies.InjectableType;
 import org.thoughtcrime.securesms.jobs.requirements.MasterSecretRequirement;
+import org.thoughtcrime.securesms.jobs.requirements.MediaNetworkRequirement;
 import org.thoughtcrime.securesms.notifications.MessageNotifier;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.jobqueue.JobParameters;
@@ -26,7 +27,6 @@ import org.whispersystems.textsecure.api.push.exceptions.PushNetworkException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -35,50 +35,49 @@ import ws.com.google.android.mms.MmsException;
 import ws.com.google.android.mms.pdu.PduPart;
 
 public class AttachmentDownloadJob extends MasterSecretJob implements InjectableType {
-
-  private static final String TAG = AttachmentDownloadJob.class.getSimpleName();
+  private static final long   serialVersionUID = 1L;
+  private static final String TAG              = AttachmentDownloadJob.class.getSimpleName();
 
   @Inject transient TextSecureMessageReceiver messageReceiver;
 
   private final long messageId;
+  private final long partRowId;
+  private final long partUniqueId;
 
-  public AttachmentDownloadJob(Context context, long messageId) {
+  public AttachmentDownloadJob(Context context, long messageId, PartId partId) {
     super(context, JobParameters.newBuilder()
+                                .withGroupId(AttachmentDownloadJob.class.getCanonicalName())
                                 .withRequirement(new MasterSecretRequirement(context))
                                 .withRequirement(new NetworkRequirement(context))
+                                .withRequirement(new MediaNetworkRequirement(context, messageId, partId))
                                 .withPersistence()
                                 .create());
 
-    this.messageId = messageId;
+    this.messageId    = messageId;
+    this.partRowId    = partId.getRowId();
+    this.partUniqueId = partId.getUniqueId();
   }
 
   @Override
-  public void onAdded() {}
+  public void onAdded() {
+  }
 
   @Override
   public void onRun(MasterSecret masterSecret) throws IOException {
-    PartDatabase database = DatabaseFactory.getPartDatabase(context);
+    final PartId  partId = new PartId(partRowId, partUniqueId);
+    final PduPart part   = DatabaseFactory.getPartDatabase(context).getPart(partId);
 
-    Log.w(TAG, "Downloading push parts for: " + messageId);
+    Log.w(TAG, "Downloading push part " + partId);
 
-    List<PduPart> parts = database.getParts(messageId);
-
-    for (PduPart part : parts) {
-      retrievePart(masterSecret, part, messageId);
-      Log.w(TAG, "Got part: " + part.getPartId());
-    }
-
+    retrievePart(masterSecret, part, messageId);
     MessageNotifier.updateNotification(context, masterSecret);
   }
 
   @Override
   public void onCanceled() {
-    PartDatabase  database = DatabaseFactory.getPartDatabase(context);
-    List<PduPart> parts    = database.getParts(messageId);
-
-    for (PduPart part : parts) {
-      markFailed(messageId, part, part.getPartId());
-    }
+    final PartId  partId = new PartId(partRowId, partUniqueId);
+    final PduPart part   = DatabaseFactory.getPartDatabase(context).getPart(partId);
+    markFailed(messageId, part, part.getPartId());
   }
 
   @Override

src/org/thoughtcrime/securesms/jobs/requirements/MediaNetworkRequirementProvider.java
@@ -1,0 +1,18 @@
+package org.thoughtcrime.securesms.jobs.requirements;
+
+import org.whispersystems.jobqueue.requirements.RequirementListener;
+import org.whispersystems.jobqueue.requirements.RequirementProvider;
+
+public class MediaNetworkRequirementProvider implements RequirementProvider {
+
+  private RequirementListener listener;
+
+  public void notifyMediaControlEvent() {
+    if (listener != null) listener.onRequirementStatusChanged();
+  }
+
+  @Override
+  public void setListener(RequirementListener listener) {
+    this.listener = listener;
+  }
+}

src/org/thoughtcrime/securesms/mms/IncomingMediaMessage.java
@@ -1,13 +1,11 @@
 package org.thoughtcrime.securesms.mms;
 
 import android.text.TextUtils;
+import android.util.Log;
 
-import org.thoughtcrime.securesms.crypto.AsymmetricMasterCipher;
-import org.thoughtcrime.securesms.crypto.MasterCipher;
-import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.crypto.MasterSecretUnion;
 import org.thoughtcrime.securesms.crypto.MediaKey;
-import org.thoughtcrime.securesms.util.Base64;
+import org.thoughtcrime.securesms.database.PartDatabase;
 import org.thoughtcrime.securesms.util.GroupUtil;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
@@ -83,7 +81,7 @@ public class IncomingMediaMessage {
             media.setName(Util.toIsoBytes(relay.get()));
           }
 
-          media.setInProgress(true);
+          media.setTransferProgress(PartDatabase.TRANSFER_PROGRESS_AUTO_PENDING);
 
           this.body.addPart(media);
         }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/3991
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/80ce83ef9dcbcf5c846bc2b06fb8d43a0e2813aa

src/org/smssecure/smssecure/ApplicationContext.java
@@ -24,6 +24,7 @@ import org.smssecure.smssecure.dependencies.AxolotlStorageModule;
 import org.smssecure.smssecure.dependencies.InjectableType;
 import org.smssecure.smssecure.jobs.persistence.EncryptingJobSerializer;
 import org.smssecure.smssecure.jobs.requirements.MasterSecretRequirementProvider;
+import org.smssecure.smssecure.jobs.requirements.MediaNetworkRequirementProvider;
 import org.smssecure.smssecure.jobs.requirements.ServiceRequirementProvider;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
 import org.whispersystems.jobqueue.JobManager;
@@ -46,8 +47,10 @@ import dagger.ObjectGraph;
  */
 public class ApplicationContext extends Application implements DependencyInjector {
 
-  private JobManager jobManager;
+  private JobManager  jobManager;
   private ObjectGraph objectGraph;
+
+  private MediaNetworkRequirementProvider mediaNetworkRequirementProvider = new MediaNetworkRequirementProvider();
 
   public static ApplicationContext getInstance(Context context) {
     return (ApplicationContext)context.getApplicationContext();
@@ -87,9 +90,14 @@ public class ApplicationContext extends Application implements DependencyInjecto
                                 .withJobSerializer(new EncryptingJobSerializer())
                                 .withRequirementProviders(new MasterSecretRequirementProvider(this),
                                                           new ServiceRequirementProvider(this),
-                                                          new NetworkRequirementProvider(this))
+                                                          new NetworkRequirementProvider(this),
+                                                          mediaNetworkRequirementProvider)
                                 .withConsumerThreads(5)
                                 .build();
+  }
+
+  public void notifyMediaControlEvent() {
+    mediaNetworkRequirementProvider.notifyMediaControlEvent();
   }
 
   private void initializeDependencyInjection() {

src/org/smssecure/smssecure/ConversationItem.java
@@ -48,6 +48,7 @@ import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.database.MmsDatabase;
 import org.smssecure.smssecure.database.MmsSmsDatabase;
+import org.smssecure.smssecure.database.PartDatabase;
 import org.smssecure.smssecure.database.SmsDatabase;
 import org.smssecure.smssecure.database.model.MediaMmsMessageRecord;
 import org.smssecure.smssecure.database.model.MessageRecord;
@@ -159,6 +160,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     if (mediaThumbnail != null) {
       mediaThumbnail.setThumbnailClickListener(new ThumbnailClickListener());
       mediaThumbnail.setOnLongClickListener(new MultiSelectLongClickListener());
+      mediaThumbnail.setDownloadClickListener(new ThumbnailDownloadClickListener());
     }
   }
 
@@ -277,7 +279,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
       mediaThumbnail.setImageResource(masterSecret, messageRecord.getId(),
                                       messageRecord.getDateReceived(),
                                       ((MediaMmsMessageRecord)messageRecord).getSlideDeckFuture());
-      mediaThumbnail.setShowProgress(!messageRecord.isFailed() && (!messageRecord.isOutgoing() || messageRecord.isPending()));
+      mediaThumbnail.hideControls(messageRecord.isFailed() || (messageRecord.isOutgoing() && !messageRecord.isPending()));
       bodyText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     } else {
       mediaThumbnail.setVisibility(View.GONE);
@@ -445,6 +447,11 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     }
   }
 
+  private class ThumbnailDownloadClickListener implements ThumbnailView.ThumbnailClickListener {
+    @Override public void onClick(View v, Slide slide) {
+      DatabaseFactory.getPartDatabase(context).setTransferState(messageRecord.getId(), slide.getPart().getPartId(), PartDatabase.TRANSFER_PROGRESS_STARTED);
+    }
+  }
   private class ThumbnailClickListener implements ThumbnailView.ThumbnailClickListener {
     private void fireIntent(Slide slide) {
       Log.w(TAG, "Clicked: " + slide.getUri() + " , " + slide.getContentType());

src/org/smssecure/smssecure/jobs/AttachmentDownloadJob.java
@@ -11,6 +11,7 @@ import org.smssecure.smssecure.database.PartDatabase;
 import org.smssecure.smssecure.database.PartDatabase.PartId;
 import org.smssecure.smssecure.dependencies.InjectableType;
 import org.smssecure.smssecure.jobs.requirements.MasterSecretRequirement;
+import org.smssecure.smssecure.jobs.requirements.MediaNetworkRequirement;
 import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.util.Base64;
 import org.smssecure.smssecure.util.Util;
@@ -26,7 +27,6 @@ import org.whispersystems.textsecure.api.push.exceptions.PushNetworkException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -35,50 +35,49 @@ import ws.com.google.android.mms.MmsException;
 import ws.com.google.android.mms.pdu.PduPart;
 
 public class AttachmentDownloadJob extends MasterSecretJob implements InjectableType {
-
-  private static final String TAG = AttachmentDownloadJob.class.getSimpleName();
+  private static final long   serialVersionUID = 1L;
+  private static final String TAG              = AttachmentDownloadJob.class.getSimpleName();
 
   @Inject transient TextSecureMessageReceiver messageReceiver;
 
   private final long messageId;
+  private final long partRowId;
+  private final long partUniqueId;
 
-  public AttachmentDownloadJob(Context context, long messageId) {
+  public AttachmentDownloadJob(Context context, long messageId, PartId partId) {
     super(context, JobParameters.newBuilder()
+                                .withGroupId(AttachmentDownloadJob.class.getCanonicalName())
                                 .withRequirement(new MasterSecretRequirement(context))
                                 .withRequirement(new NetworkRequirement(context))
+                                .withRequirement(new MediaNetworkRequirement(context, messageId, partId))
                                 .withPersistence()
                                 .create());
 
-    this.messageId = messageId;
+    this.messageId    = messageId;
+    this.partRowId    = partId.getRowId();
+    this.partUniqueId = partId.getUniqueId();
   }
 
   @Override
-  public void onAdded() {}
+  public void onAdded() {
+  }
 
   @Override
   public void onRun(MasterSecret masterSecret) throws IOException {
-    PartDatabase database = DatabaseFactory.getPartDatabase(context);
+    final PartId  partId = new PartId(partRowId, partUniqueId);
+    final PduPart part   = DatabaseFactory.getPartDatabase(context).getPart(partId);
 
-    Log.w(TAG, "Downloading push parts for: " + messageId);
+    Log.w(TAG, "Downloading push part " + partId);
 
-    List<PduPart> parts = database.getParts(messageId);
-
-    for (PduPart part : parts) {
-      retrievePart(masterSecret, part, messageId);
-      Log.w(TAG, "Got part: " + part.getPartId());
-    }
-
+    retrievePart(masterSecret, part, messageId);
     MessageNotifier.updateNotification(context, masterSecret);
   }
 
   @Override
   public void onCanceled() {
-    PartDatabase  database = DatabaseFactory.getPartDatabase(context);
-    List<PduPart> parts    = database.getParts(messageId);
-
-    for (PduPart part : parts) {
-      markFailed(messageId, part, part.getPartId());
-    }
+    final PartId  partId = new PartId(partRowId, partUniqueId);
+    final PduPart part   = DatabaseFactory.getPartDatabase(context).getPart(partId);
+    markFailed(messageId, part, part.getPartId());
   }
 
   @Override

src/org/smssecure/smssecure/jobs/requirements/MediaNetworkRequirementProvider.java
@@ -1,0 +1,18 @@
+package org.smssecure.smssecure.jobs.requirements;
+
+import org.whispersystems.jobqueue.requirements.RequirementListener;
+import org.whispersystems.jobqueue.requirements.RequirementProvider;
+
+public class MediaNetworkRequirementProvider implements RequirementProvider {
+
+  private RequirementListener listener;
+
+  public void notifyMediaControlEvent() {
+    if (listener != null) listener.onRequirementStatusChanged();
+  }
+
+  @Override
+  public void setListener(RequirementListener listener) {
+    this.listener = listener;
+  }
+}

src/org/smssecure/smssecure/mms/IncomingMediaMessage.java
@@ -1,10 +1,12 @@
 package org.smssecure.smssecure.mms;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import org.smssecure.smssecure.crypto.MasterCipher;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.util.Base64;
+import org.smssecure.smssecure.database.PartDatabase;
 import org.smssecure.smssecure.util.GroupUtil;
 import org.smssecure.smssecure.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
@@ -87,7 +89,7 @@ public class IncomingMediaMessage {
             media.setName(Util.toIsoBytes(relay.get()));
           }
 
-          media.setInProgress(true);
+          media.setTransferProgress(PartDatabase.TRANSFER_PROGRESS_AUTO_PENDING);
 
           this.body.addPart(media);
         }