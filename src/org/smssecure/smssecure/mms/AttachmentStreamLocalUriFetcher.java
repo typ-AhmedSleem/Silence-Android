SamSprung/Signal-Android

use glide for encoding and resizing outgoing media
Closes #3915
// FREEBIE

src/org/thoughtcrime/securesms/mms/AttachmentStreamLocalUriFetcher.java
@@ -1,0 +1,52 @@
+package org.thoughtcrime.securesms.mms;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.data.StreamLocalUriFetcher;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.whispersystems.textsecure.api.crypto.AttachmentCipherInputStream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AttachmentStreamLocalUriFetcher implements DataFetcher<InputStream> {
+  private static final String TAG = AttachmentStreamLocalUriFetcher.class.getSimpleName();
+  private File        attachment;
+  private byte[]      key;
+  private InputStream is;
+
+  public AttachmentStreamLocalUriFetcher(File attachment, byte[] key) {
+    this.attachment = attachment;
+    this.key        = key;
+  }
+
+  @Override public InputStream loadData(Priority priority) throws Exception {
+    is = new AttachmentCipherInputStream(attachment, key);
+    return is;
+  }
+
+  @Override public void cleanup() {
+    try {
+      if (is != null) is.close();
+      is = null;
+    } catch (IOException ioe) {
+      Log.w(TAG, "ioe");
+    }
+  }
+
+  @Override public String getId() {
+    return AttachmentStreamLocalUriFetcher.class.getCanonicalName() + "::" + attachment.getAbsolutePath();
+  }
+
+  @Override public void cancel() {
+
+  }
+}

src/org/thoughtcrime/securesms/mms/AttachmentStreamUriLoader.java
@@ -1,0 +1,63 @@
+package org.thoughtcrime.securesms.mms;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.model.GenericLoaderFactory;
+import com.bumptech.glide.load.model.ModelLoader;
+import com.bumptech.glide.load.model.ModelLoaderFactory;
+import com.bumptech.glide.load.model.stream.StreamModelLoader;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.mms.AttachmentStreamUriLoader.AttachmentModel;
+import org.thoughtcrime.securesms.mms.DecryptableStreamUriLoader.DecryptableUri;
+import org.thoughtcrime.securesms.util.SaveAttachmentTask.Attachment;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * A {@link ModelLoader} for translating uri models into {@link InputStream} data. Capable of handling 'http',
+ * 'https', 'android.resource', 'content', and 'file' schemes. Unsupported schemes will throw an exception in
+ * {@link #getResourceFetcher(Uri, int, int)}.
+ */
+public class AttachmentStreamUriLoader implements StreamModelLoader<AttachmentModel> {
+  private final Context context;
+
+  /**
+   * THe default factory for {@link com.bumptech.glide.load.model.stream.StreamUriLoader}s.
+   */
+  public static class Factory implements ModelLoaderFactory<AttachmentModel, InputStream> {
+
+    @Override
+    public StreamModelLoader<AttachmentModel> build(Context context, GenericLoaderFactory factories) {
+      return new AttachmentStreamUriLoader(context);
+    }
+
+    @Override
+    public void teardown() {
+      // Do nothing.
+    }
+  }
+
+  public AttachmentStreamUriLoader(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public DataFetcher<InputStream> getResourceFetcher(AttachmentModel model, int width, int height) {
+    return new AttachmentStreamLocalUriFetcher(model.attachment, model.key);
+  }
+
+  public static class AttachmentModel {
+    public File   attachment;
+    public byte[] key;
+
+    public AttachmentModel(File attachment, byte[] key) {
+      this.attachment = attachment;
+      this.key        = key;
+    }
+  }
+}
+

src/org/thoughtcrime/securesms/mms/ContactPhotoLocalUriFetcher.java
@@ -1,0 +1,32 @@
+package org.thoughtcrime.securesms.mms;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.provider.ContactsContract;
+
+import com.bumptech.glide.load.data.StreamLocalUriFetcher;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class ContactPhotoLocalUriFetcher extends StreamLocalUriFetcher {
+  private static final String TAG = ContactPhotoLocalUriFetcher.class.getSimpleName();
+
+  public ContactPhotoLocalUriFetcher(Context context, Uri uri) {
+    super(context, uri);
+  }
+
+  @Override
+  protected InputStream loadResource(Uri uri, ContentResolver contentResolver)
+      throws FileNotFoundException
+  {
+    if (VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH) {
+      return ContactsContract.Contacts.openContactPhotoInputStream(contentResolver, uri, true);
+    } else {
+      return ContactsContract.Contacts.openContactPhotoInputStream(contentResolver, uri);
+    }
+  }
+}

src/org/thoughtcrime/securesms/mms/ContactPhotoUriLoader.java
@@ -1,0 +1,51 @@
+package org.thoughtcrime.securesms.mms;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.model.GenericLoaderFactory;
+import com.bumptech.glide.load.model.ModelLoaderFactory;
+import com.bumptech.glide.load.model.stream.StreamModelLoader;
+
+import org.thoughtcrime.securesms.mms.ContactPhotoUriLoader.ContactPhotoUri;
+
+import java.io.InputStream;
+
+public class ContactPhotoUriLoader implements StreamModelLoader<ContactPhotoUri> {
+  private final Context context;
+
+  /**
+   * THe default factory for {@link com.bumptech.glide.load.model.stream.StreamUriLoader}s.
+   */
+  public static class Factory implements ModelLoaderFactory<ContactPhotoUri, InputStream> {
+
+    @Override
+    public StreamModelLoader<ContactPhotoUri> build(Context context, GenericLoaderFactory factories) {
+      return new ContactPhotoUriLoader(context);
+    }
+
+    @Override
+    public void teardown() {
+      // Do nothing.
+    }
+  }
+
+  public ContactPhotoUriLoader(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public DataFetcher<InputStream> getResourceFetcher(ContactPhotoUri model, int width, int height) {
+    return new ContactPhotoLocalUriFetcher(context, model.uri);
+  }
+
+  public static class ContactPhotoUri {
+    public Uri uri;
+
+    public ContactPhotoUri(Uri uri) {
+      this.uri = uri;
+    }
+  }
+}
+

src/org/thoughtcrime/securesms/mms/MediaConstraints.java
@@ -1,20 +1,22 @@
 package org.thoughtcrime.securesms.mms;
 
 import android.content.Context;
+import android.graphics.Bitmap.CompressFormat;
 import android.net.Uri;
 import android.util.Log;
 import android.util.Pair;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.gifdecoder.GifDecoder;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.mms.DecryptableStreamUriLoader.DecryptableUri;
 import org.thoughtcrime.securesms.util.BitmapDecodingException;
 import org.thoughtcrime.securesms.util.BitmapUtil;
 import org.thoughtcrime.securesms.util.MediaUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 import ws.com.google.android.mms.pdu.PduPart;
 
@@ -64,15 +66,10 @@ public abstract class MediaConstraints {
     if (!canResize(part) || part.getDataUri() == null) {
       throw new UnsupportedOperationException("Cannot resize this content type");
     }
-
     try {
-      return BitmapUtil.createScaledBytes(context, masterSecret, part.getDataUri(),
-                                          getImageMaxWidth(context),
-                                          getImageMaxHeight(context),
-                                          getImageMaxSize());
-    } catch (BitmapDecodingException bde) {
-      throw new IOException(bde);
+      return BitmapUtil.createScaledBytes(context, new DecryptableUri(masterSecret, part.getDataUri()), this);
+    } catch (ExecutionException ee) {
+      throw new IOException(ee);
     }
   }
-
 }

src/org/thoughtcrime/securesms/mms/RoundedCorners.java
@@ -10,11 +10,12 @@ import android.graphics.RectF;
 import android.graphics.Shader.TileMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 import com.bumptech.glide.load.resource.bitmap.TransformationUtils;
+
+import org.thoughtcrime.securesms.util.ResUtil;
 
 public class RoundedCorners extends BitmapTransformation {
   private final boolean crop;
@@ -26,6 +27,10 @@ public class RoundedCorners extends BitmapTransformation {
     this.crop      = crop;
     this.radius    = radius;
     this.colorHint = colorHint;
+  }
+
+  public RoundedCorners(@NonNull Context context, int radius) {
+    this(context, true, radius, ResUtil.getColor(context, android.R.attr.windowBackground));
   }
 
   @Override protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth,

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/3915
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/0c9d9e8dcf6bb42f0377

src/org/smssecure/smssecure/mms/AttachmentStreamLocalUriFetcher.java
@@ -1,0 +1,52 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.data.StreamLocalUriFetcher;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.whispersystems.textsecure.api.crypto.AttachmentCipherInputStream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AttachmentStreamLocalUriFetcher implements DataFetcher<InputStream> {
+  private static final String TAG = AttachmentStreamLocalUriFetcher.class.getSimpleName();
+  private File        attachment;
+  private byte[]      key;
+  private InputStream is;
+
+  public AttachmentStreamLocalUriFetcher(File attachment, byte[] key) {
+    this.attachment = attachment;
+    this.key        = key;
+  }
+
+  @Override public InputStream loadData(Priority priority) throws Exception {
+    is = new AttachmentCipherInputStream(attachment, key);
+    return is;
+  }
+
+  @Override public void cleanup() {
+    try {
+      if (is != null) is.close();
+      is = null;
+    } catch (IOException ioe) {
+      Log.w(TAG, "ioe");
+    }
+  }
+
+  @Override public String getId() {
+    return AttachmentStreamLocalUriFetcher.class.getCanonicalName() + "::" + attachment.getAbsolutePath();
+  }
+
+  @Override public void cancel() {
+
+  }
+}

src/org/smssecure/smssecure/mms/AttachmentStreamUriLoader.java
@@ -1,0 +1,63 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.model.GenericLoaderFactory;
+import com.bumptech.glide.load.model.ModelLoader;
+import com.bumptech.glide.load.model.ModelLoaderFactory;
+import com.bumptech.glide.load.model.stream.StreamModelLoader;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.mms.AttachmentStreamUriLoader.AttachmentModel;
+import org.smssecure.smssecure.mms.DecryptableStreamUriLoader.DecryptableUri;
+import org.smssecure.smssecure.util.SaveAttachmentTask.Attachment;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * A {@link ModelLoader} for translating uri models into {@link InputStream} data. Capable of handling 'http',
+ * 'https', 'android.resource', 'content', and 'file' schemes. Unsupported schemes will throw an exception in
+ * {@link #getResourceFetcher(Uri, int, int)}.
+ */
+public class AttachmentStreamUriLoader implements StreamModelLoader<AttachmentModel> {
+  private final Context context;
+
+  /**
+   * THe default factory for {@link com.bumptech.glide.load.model.stream.StreamUriLoader}s.
+   */
+  public static class Factory implements ModelLoaderFactory<AttachmentModel, InputStream> {
+
+    @Override
+    public StreamModelLoader<AttachmentModel> build(Context context, GenericLoaderFactory factories) {
+      return new AttachmentStreamUriLoader(context);
+    }
+
+    @Override
+    public void teardown() {
+      // Do nothing.
+    }
+  }
+
+  public AttachmentStreamUriLoader(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public DataFetcher<InputStream> getResourceFetcher(AttachmentModel model, int width, int height) {
+    return new AttachmentStreamLocalUriFetcher(model.attachment, model.key);
+  }
+
+  public static class AttachmentModel {
+    public File   attachment;
+    public byte[] key;
+
+    public AttachmentModel(File attachment, byte[] key) {
+      this.attachment = attachment;
+      this.key        = key;
+    }
+  }
+}
+

src/org/smssecure/smssecure/mms/ContactPhotoLocalUriFetcher.java
@@ -1,0 +1,32 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.provider.ContactsContract;
+
+import com.bumptech.glide.load.data.StreamLocalUriFetcher;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class ContactPhotoLocalUriFetcher extends StreamLocalUriFetcher {
+  private static final String TAG = ContactPhotoLocalUriFetcher.class.getSimpleName();
+
+  public ContactPhotoLocalUriFetcher(Context context, Uri uri) {
+    super(context, uri);
+  }
+
+  @Override
+  protected InputStream loadResource(Uri uri, ContentResolver contentResolver)
+      throws FileNotFoundException
+  {
+    if (VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH) {
+      return ContactsContract.Contacts.openContactPhotoInputStream(contentResolver, uri, true);
+    } else {
+      return ContactsContract.Contacts.openContactPhotoInputStream(contentResolver, uri);
+    }
+  }
+}

src/org/smssecure/smssecure/mms/ContactPhotoUriLoader.java
@@ -1,0 +1,51 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.model.GenericLoaderFactory;
+import com.bumptech.glide.load.model.ModelLoaderFactory;
+import com.bumptech.glide.load.model.stream.StreamModelLoader;
+
+import org.smssecure.smssecure.mms.ContactPhotoUriLoader.ContactPhotoUri;
+
+import java.io.InputStream;
+
+public class ContactPhotoUriLoader implements StreamModelLoader<ContactPhotoUri> {
+  private final Context context;
+
+  /**
+   * THe default factory for {@link com.bumptech.glide.load.model.stream.StreamUriLoader}s.
+   */
+  public static class Factory implements ModelLoaderFactory<ContactPhotoUri, InputStream> {
+
+    @Override
+    public StreamModelLoader<ContactPhotoUri> build(Context context, GenericLoaderFactory factories) {
+      return new ContactPhotoUriLoader(context);
+    }
+
+    @Override
+    public void teardown() {
+      // Do nothing.
+    }
+  }
+
+  public ContactPhotoUriLoader(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public DataFetcher<InputStream> getResourceFetcher(ContactPhotoUri model, int width, int height) {
+    return new ContactPhotoLocalUriFetcher(context, model.uri);
+  }
+
+  public static class ContactPhotoUri {
+    public Uri uri;
+
+    public ContactPhotoUri(Uri uri) {
+      this.uri = uri;
+    }
+  }
+}
+

src/org/smssecure/smssecure/mms/MediaConstraints.java
@@ -1,20 +1,22 @@
 package org.smssecure.smssecure.mms;
 
 import android.content.Context;
+import android.graphics.Bitmap.CompressFormat;
 import android.net.Uri;
 import android.util.Log;
 import android.util.Pair;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.gifdecoder.GifDecoder;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.mms.DecryptableStreamUriLoader.DecryptableUri;
 import org.smssecure.smssecure.util.BitmapDecodingException;
 import org.smssecure.smssecure.util.BitmapUtil;
 import org.smssecure.smssecure.util.MediaUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 import ws.com.google.android.mms.pdu.PduPart;
 
@@ -64,15 +66,10 @@ public abstract class MediaConstraints {
     if (!canResize(part) || part.getDataUri() == null) {
       throw new UnsupportedOperationException("Cannot resize this content type");
     }
-
     try {
-      return BitmapUtil.createScaledBytes(context, masterSecret, part.getDataUri(),
-                                          getImageMaxWidth(context),
-                                          getImageMaxHeight(context),
-                                          getImageMaxSize());
-    } catch (BitmapDecodingException bde) {
-      throw new IOException(bde);
+      return BitmapUtil.createScaledBytes(context, new DecryptableUri(masterSecret, part.getDataUri()), this);
+    } catch (ExecutionException ee) {
+      throw new IOException(ee);
     }
   }
-
 }

src/org/smssecure/smssecure/mms/RoundedCorners.java
@@ -10,11 +10,12 @@ import android.graphics.RectF;
 import android.graphics.Shader.TileMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 import com.bumptech.glide.load.resource.bitmap.TransformationUtils;
+
+import org.smssecure.smssecure.util.ResUtil;
 
 public class RoundedCorners extends BitmapTransformation {
   private final boolean crop;
@@ -26,6 +27,10 @@ public class RoundedCorners extends BitmapTransformation {
     this.crop      = crop;
     this.radius    = radius;
     this.colorHint = colorHint;
+  }
+
+  public RoundedCorners(@NonNull Context context, int radius) {
+    this(context, true, radius, ResUtil.getColor(context, android.R.attr.windowBackground));
   }
 
   @Override protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth,