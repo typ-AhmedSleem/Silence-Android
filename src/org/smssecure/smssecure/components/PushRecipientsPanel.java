SamSprung/Signal-Android

prevent blocking recipient resolution on UI thread
fixes #4058
Closes #4059
// FREEBIE

src/org/thoughtcrime/securesms/components/PushRecipientsPanel.java
@@ -36,6 +36,7 @@ import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.RecipientFormattingException;
 import org.thoughtcrime.securesms.recipients.Recipients;
+import org.thoughtcrime.securesms.recipients.Recipients.RecipientsModifiedListener;
 
 import java.util.HashSet;
 import java.util.Iterator;
@@ -49,7 +50,7 @@ import java.util.Set;
  *
  * @author Moxie Marlinspike
  */
-public class PushRecipientsPanel extends RelativeLayout {
+public class PushRecipientsPanel extends RelativeLayout implements RecipientsModifiedListener {
   private final String                         TAG = PushRecipientsPanel.class.getSimpleName();
   private       RecipientsPanelChangedListener panelChangeListener;
 
@@ -90,7 +91,7 @@ public class PushRecipientsPanel extends RelativeLayout {
 
   public Recipients getRecipients() throws RecipientFormattingException {
     String rawText = recipientsText.getText().toString();
-    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, false);
+    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, true);
 
     if (recipients.isEmpty())
       throw new RecipientFormattingException("Recipient List Is Empty!");
@@ -129,6 +130,7 @@ public class PushRecipientsPanel extends RelativeLayout {
     } catch (RecipientFormattingException e) {
       recipients = RecipientFactory.getRecipientsFor(getContext(), new LinkedList<Recipient>(), true);
     }
+    recipients.addListener(this);
 
     recipientsText.setAdapter(new RecipientsAdapter(this.getContext()));
     recipientsText.populate(recipients);
@@ -147,6 +149,10 @@ public class PushRecipientsPanel extends RelativeLayout {
         recipientsText.setText("");
       }
     });
+  }
+
+  @Override public void onModified(Recipients recipients) {
+    recipientsText.populate(recipients);
   }
 
   private class FocusChangedListener implements View.OnFocusChangeListener {

src/org/thoughtcrime/securesms/components/SingleRecipientPanel.java
@@ -17,7 +17,6 @@
 package org.thoughtcrime.securesms.components;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -33,6 +32,7 @@ import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.RecipientFormattingException;
 import org.thoughtcrime.securesms.recipients.Recipients;
+import org.thoughtcrime.securesms.recipients.Recipients.RecipientsModifiedListener;
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -44,7 +44,7 @@ import java.util.List;
  *
  * @author Moxie Marlinspike
  */
-public class SingleRecipientPanel extends RelativeLayout {
+public class SingleRecipientPanel extends RelativeLayout implements RecipientsModifiedListener {
   private final String                         TAG = SingleRecipientPanel.class.getSimpleName();
   private       RecipientsPanelChangedListener panelChangeListener;
 
@@ -94,7 +94,7 @@ public class SingleRecipientPanel extends RelativeLayout {
 
   public Recipients getRecipients() throws RecipientFormattingException {
     String rawText = recipientsText.getText().toString();
-    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, false);
+    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, true);
 
     if (recipients.isEmpty())
       throw new RecipientFormattingException("Recipient List Is Empty!");
@@ -132,6 +132,7 @@ public class SingleRecipientPanel extends RelativeLayout {
     } catch (RecipientFormattingException e) {
       recipients = RecipientFactory.getRecipientsFor(getContext(), new LinkedList<Recipient>(), true);
     }
+    recipients.addListener(this);
 
     recipientsText.setAdapter(new RecipientsAdapter(this.getContext()));
     recipientsText.populate(recipients);
@@ -150,6 +151,10 @@ public class SingleRecipientPanel extends RelativeLayout {
         recipientsText.setText("");
       }
     });
+  }
+
+  @Override public void onModified(Recipients recipients) {
+    recipientsText.populate(recipients);
   }
 
   private class FocusChangedListener implements OnFocusChangeListener {

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/4058
Closes https://github.com/WhisperSystems/TextSecure/pull/4059
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/8933e03c025d3c8b9e143eae9c8133500ee1a427

src/org/smssecure/smssecure/components/PushRecipientsPanel.java
@@ -36,6 +36,7 @@ import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.RecipientFormattingException;
 import org.smssecure.smssecure.recipients.Recipients;
+import org.smssecure.smssecure.recipients.Recipients.RecipientsModifiedListener;
 
 import java.util.HashSet;
 import java.util.Iterator;
@@ -49,7 +50,7 @@ import java.util.Set;
  *
  * @author Moxie Marlinspike
  */
-public class PushRecipientsPanel extends RelativeLayout {
+public class PushRecipientsPanel extends RelativeLayout implements RecipientsModifiedListener {
   private final String                         TAG = PushRecipientsPanel.class.getSimpleName();
   private       RecipientsPanelChangedListener panelChangeListener;
 
@@ -90,7 +91,7 @@ public class PushRecipientsPanel extends RelativeLayout {
 
   public Recipients getRecipients() throws RecipientFormattingException {
     String rawText = recipientsText.getText().toString();
-    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, false);
+    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, true);
 
     if (recipients.isEmpty())
       throw new RecipientFormattingException("Recipient List Is Empty!");
@@ -129,6 +130,7 @@ public class PushRecipientsPanel extends RelativeLayout {
     } catch (RecipientFormattingException e) {
       recipients = RecipientFactory.getRecipientsFor(getContext(), new LinkedList<Recipient>(), true);
     }
+    recipients.addListener(this);
 
     recipientsText.setAdapter(new RecipientsAdapter(this.getContext()));
     recipientsText.populate(recipients);
@@ -147,6 +149,10 @@ public class PushRecipientsPanel extends RelativeLayout {
         recipientsText.setText("");
       }
     });
+  }
+
+  @Override public void onModified(Recipients recipients) {
+    recipientsText.populate(recipients);
   }
 
   private class FocusChangedListener implements View.OnFocusChangeListener {

src/org/smssecure/smssecure/components/SingleRecipientPanel.java
@@ -17,7 +17,6 @@
 package org.smssecure.smssecure.components;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -33,6 +32,7 @@ import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.RecipientFormattingException;
 import org.smssecure.smssecure.recipients.Recipients;
+import org.smssecure.smssecure.recipients.Recipients.RecipientsModifiedListener;
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -44,7 +44,7 @@ import java.util.List;
  *
  * @author Moxie Marlinspike
  */
-public class SingleRecipientPanel extends RelativeLayout {
+public class SingleRecipientPanel extends RelativeLayout implements RecipientsModifiedListener {
   private final String                         TAG = SingleRecipientPanel.class.getSimpleName();
   private       RecipientsPanelChangedListener panelChangeListener;
 
@@ -94,7 +94,7 @@ public class SingleRecipientPanel extends RelativeLayout {
 
   public Recipients getRecipients() throws RecipientFormattingException {
     String rawText = recipientsText.getText().toString();
-    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, false);
+    Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText, true);
 
     if (recipients.isEmpty())
       throw new RecipientFormattingException("Recipient List Is Empty!");
@@ -132,6 +132,7 @@ public class SingleRecipientPanel extends RelativeLayout {
     } catch (RecipientFormattingException e) {
       recipients = RecipientFactory.getRecipientsFor(getContext(), new LinkedList<Recipient>(), true);
     }
+    recipients.addListener(this);
 
     recipientsText.setAdapter(new RecipientsAdapter(this.getContext()));
     recipientsText.populate(recipients);
@@ -150,6 +151,10 @@ public class SingleRecipientPanel extends RelativeLayout {
         recipientsText.setText("");
       }
     });
+  }
+
+  @Override public void onModified(Recipients recipients) {
+    recipientsText.populate(recipients);
   }
 
   private class FocusChangedListener implements OnFocusChangeListener {