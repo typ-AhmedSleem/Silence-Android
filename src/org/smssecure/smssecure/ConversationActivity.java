SamSprung/Signal-Android

Large attachment support
Closes #4019
// FREEBIE

src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -35,6 +35,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.WindowCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -85,6 +86,7 @@ import org.thoughtcrime.securesms.database.GroupDatabase;
 import org.thoughtcrime.securesms.database.MmsSmsColumns.Types;
 import org.thoughtcrime.securesms.database.ThreadDatabase;
 import org.thoughtcrime.securesms.mms.AttachmentManager;
+import org.thoughtcrime.securesms.mms.AttachmentManager.MediaType;
 import org.thoughtcrime.securesms.mms.AttachmentTypeSelectorAdapter;
 import org.thoughtcrime.securesms.mms.MediaConstraints;
 import org.thoughtcrime.securesms.mms.MediaTooLargeException;
@@ -113,6 +115,7 @@ import org.thoughtcrime.securesms.util.DirectoryHelper;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicTheme;
 import org.thoughtcrime.securesms.util.GroupUtil;
+import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
 import org.thoughtcrime.securesms.util.concurrent.ListenableFuture;
@@ -288,13 +291,16 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     switch (reqCode) {
     case PICK_IMAGE:
-      addAttachmentImage(masterSecret, data.getData());
+      setMedia(data.getData(),
+               MediaUtil.isGif(MediaUtil.getMimeType(this, data.getData())) ? MediaType.GIF
+                   : MediaType.IMAGE,
+               false);
       break;
     case PICK_VIDEO:
-      addAttachmentVideo(data.getData());
+      setMedia(data.getData(), MediaType.VIDEO, false);
       break;
     case PICK_AUDIO:
-      addAttachmentAudio(data.getData());
+      setMedia(data.getData(), MediaType.AUDIO, false);
       break;
     case PICK_CONTACT_INFO:
       addAttachmentContactInfo(data.getData());
@@ -308,7 +314,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       break;
     case TAKE_PHOTO:
       if (attachmentManager.getCaptureUri() != null) {
-        addAttachmentImage(masterSecret, attachmentManager.getCaptureUri());
+        setMedia(attachmentManager.getCaptureUri(), MediaType.IMAGE, true);
       }
       break;
     }
@@ -671,9 +677,10 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     Uri    draftVideo = getIntent().getParcelableExtra(DRAFT_VIDEO_EXTRA);
 
     if (draftText != null)  composeText.setText(draftText);
-    if (draftImage != null) addAttachmentImage(masterSecret, draftImage);
-    if (draftAudio != null) addAttachmentAudio(draftAudio);
-    if (draftVideo != null) addAttachmentVideo(draftVideo);
+
+    if      (draftImage != null) setMedia(draftImage, MediaType.IMAGE, false);
+    else if (draftAudio != null) setMedia(draftAudio, MediaType.AUDIO, false);
+    else if (draftVideo != null) setMedia(draftVideo, MediaType.VIDEO, false);
 
     if (draftText == null && draftImage == null && draftAudio == null && draftVideo == null) {
       initializeDraftFromDatabase();
@@ -707,11 +714,11 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
           if (draft.getType().equals(Draft.TEXT)) {
             composeText.setText(draft.getValue());
           } else if (draft.getType().equals(Draft.IMAGE)) {
-            addAttachmentImage(masterSecret, Uri.parse(draft.getValue()));
+            setMedia(Uri.parse(draft.getValue()), MediaType.IMAGE, false);
           } else if (draft.getType().equals(Draft.AUDIO)) {
-            addAttachmentAudio(Uri.parse(draft.getValue()));
+            setMedia(Uri.parse(draft.getValue()), MediaType.AUDIO, false);
           } else if (draft.getType().equals(Draft.VIDEO)) {
-            addAttachmentVideo(Uri.parse(draft.getValue()));
+            setMedia(Uri.parse(draft.getValue()), MediaType.VIDEO, false);
           }
         }
 
@@ -917,55 +924,8 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }
   }
 
-  private void addAttachmentImage(MasterSecret masterSecret, Uri imageUri) {
-    try {
-      attachmentManager.setImage(masterSecret, imageUri);
-    } catch (IOException | BitmapDecodingException e) {
-      Log.w(TAG, e);
-      attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_there_was_an_error_setting_your_attachment,
-                     Toast.LENGTH_LONG).show();
-    } catch (MediaTooLargeException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, getString(R.string.ConversationActivity_the_gif_you_selected_was_too_big),
-                     Toast.LENGTH_LONG).show();
-      Log.w(TAG, e);
-    }
-  }
-
-  private void addAttachmentVideo(Uri videoUri) {
-    try {
-      attachmentManager.setVideo(videoUri);
-    } catch (IOException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_there_was_an_error_setting_your_attachment,
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    } catch (MediaTooLargeException e) {
-      attachmentManager.clear();
-
-      Toast.makeText(this, getString(R.string.ConversationActivity_sorry_the_selected_video_exceeds_message_size_restrictions,
-                                     (MmsMediaConstraints.MAX_MESSAGE_SIZE/1024)),
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    }
-  }
-
-  private void addAttachmentAudio(Uri audioUri) {
-    try {
-      attachmentManager.setAudio(audioUri);
-    } catch (IOException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_there_was_an_error_setting_your_attachment,
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    } catch (MediaTooLargeException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, getString(R.string.ConversationActivity_sorry_the_selected_audio_exceeds_message_size_restrictions,
-                                     (MmsMediaConstraints.MAX_MESSAGE_SIZE/1024)),
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    }
+  private void setMedia(Uri uri, MediaType mediaType, boolean isCapture) {
+    attachmentManager.setMedia(masterSecret, uri, mediaType, getCurrentMediaConstraints(), isCapture);
   }
 
   private void addAttachmentContactInfo(Uri contactUri) {
@@ -1132,6 +1092,12 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     return rawText;
   }
 
+  private MediaConstraints getCurrentMediaConstraints() {
+    return sendButton.getSelectedTransport().getType() == Type.TEXTSECURE
+           ? MediaConstraints.PUSH_CONSTRAINTS
+           : MediaConstraints.MMS_CONSTRAINTS;
+  }
+
   private void markThreadAsRead() {
     new AsyncTask<Long, Void, Void>() {
       @Override
@@ -1198,8 +1164,24 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     final Context context = getApplicationContext();
     SlideDeck slideDeck;
 
-    if (attachmentManager.isAttachmentPresent()) slideDeck = new SlideDeck(attachmentManager.getSlideDeck());
-    else                                         slideDeck = new SlideDeck();
+    if (attachmentManager.isAttachmentPresent()) {
+      Slide            mediaSlide  = attachmentManager.getSlideDeck().getThumbnailSlide();
+      MediaConstraints constraints = getCurrentMediaConstraints();
+
+      if (mediaSlide != null &&
+          !constraints.isSatisfied(this, masterSecret, mediaSlide.getPart()) &&
+          !constraints.canResize(mediaSlide.getPart()))
+      {
+        Toast.makeText(context,
+                       R.string.ConversationActivity_attachment_exceeds_size_limits,
+                       Toast.LENGTH_SHORT).show();
+        return;
+      }
+
+      slideDeck = new SlideDeck(attachmentManager.getSlideDeck());
+    } else {
+      slideDeck = new SlideDeck();
+    }
 
     OutgoingMediaMessage outgoingMessage = new OutgoingMediaMessage(this, recipients, slideDeck,
                                                                     getMessage(), distributionType);
@@ -1272,8 +1254,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
   @Override
   public void onImageCapture(@NonNull final byte[] imageBytes) {
-    attachmentManager.setCaptureUri(CaptureProvider.getInstance(this).create(masterSecret, recipients, imageBytes));
-    addAttachmentImage(masterSecret, attachmentManager.getCaptureUri());
+    setMedia(CaptureProvider.getInstance(this).create(masterSecret, recipients, imageBytes), MediaType.IMAGE, true);
     quickAttachmentDrawer.hide(false);
   }
 
@@ -1397,4 +1378,5 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     initializeSecurity();
     updateToggleButtonState();
   }
+
 }

src/org/thoughtcrime/securesms/mms/Slide.java
@@ -21,8 +21,10 @@ import android.content.res.Resources.Theme;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.Util;
 
 import java.io.IOException;
@@ -32,18 +34,12 @@ import ws.com.google.android.mms.pdu.PduPart;
 
 public abstract class Slide {
 
-  protected final PduPart      part;
-  protected final Context      context;
-  protected       MasterSecret masterSecret;
+  protected final PduPart part;
+  protected final Context context;
 
   public Slide(Context context, @NonNull PduPart part) {
     this.part    = part;
     this.context = context;
-  }
-
-  public Slide(Context context, @NonNull MasterSecret masterSecret, @NonNull PduPart part) {
-    this(context, part);
-    this.masterSecret = masterSecret;
   }
 
   public String getContentType() {
@@ -90,18 +86,24 @@ public abstract class Slide {
     return !getPart().getPartId().isValid();
   }
 
-  protected static void assertMediaSize(Context context, Uri uri, long max)
-      throws MediaTooLargeException, IOException
-  {
-    InputStream in = context.getContentResolver().openInputStream(uri);
-    long   size    = 0;
-    byte[] buffer  = new byte[512];
-    int read;
 
-    while ((read = in.read(buffer)) != -1) {
-      size += read;
-      if (size > max) throw new MediaTooLargeException("Media exceeds maximum message size.");
-    }
+  protected static PduPart constructPartFromUri(@NonNull  Context      context,
+                                                @NonNull  Uri          uri,
+                                                @NonNull  String       defaultMime,
+                                                          long         dataSize)
+      throws IOException
+  {
+    final PduPart part            = new PduPart();
+    final String  mimeType        = MediaUtil.getMimeType(context, uri);
+    final String  derivedMimeType = mimeType != null ? mimeType : defaultMime;
+
+    part.setDataSize(dataSize);
+    part.setDataUri(uri);
+    part.setContentType(derivedMimeType.getBytes());
+    part.setContentId((System.currentTimeMillis()+"").getBytes());
+    part.setName((MediaUtil.getDiscreteMimeType(derivedMimeType) + System.currentTimeMillis()).getBytes());
+
+    return part;
   }
 
   @Override
@@ -125,7 +127,4 @@ public abstract class Slide {
     return Util.hashCode(getContentType(), hasAudio(), hasImage(),
                          hasVideo(), isDraft(), getUri(), getThumbnailUri(), getTransferProgress());
   }
-
-
-
 }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4019
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/551274f167db2905714b0b76abc97394a2e221f6

src/org/smssecure/smssecure/ConversationActivity.java
@@ -35,6 +35,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.WindowCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -83,8 +84,9 @@ import org.smssecure.smssecure.database.GroupDatabase;
 import org.smssecure.smssecure.database.MmsSmsColumns.Types;
 import org.smssecure.smssecure.database.ThreadDatabase;
 import org.smssecure.smssecure.mms.AttachmentManager;
+import org.smssecure.smssecure.mms.AttachmentManager.MediaType;
 import org.smssecure.smssecure.mms.AttachmentTypeSelectorAdapter;
-import org.smssecure.smssecure.mms.MediaTooLargeException;
+import org.smssecure.smssecure.mms.MediaConstraints;
 import org.smssecure.smssecure.mms.MmsMediaConstraints;
 import org.smssecure.smssecure.mms.OutgoingMediaMessage;
 import org.smssecure.smssecure.mms.OutgoingSecureMediaMessage;
@@ -108,6 +110,7 @@ import org.smssecure.smssecure.util.Dialogs;
 import org.smssecure.smssecure.util.DynamicLanguage;
 import org.smssecure.smssecure.util.DynamicTheme;
 import org.smssecure.smssecure.util.GroupUtil;
+import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
 import org.smssecure.smssecure.util.TelephonyUtil;
 import org.smssecure.smssecure.util.Util;
@@ -275,20 +278,23 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     switch (reqCode) {
     case PICK_IMAGE:
-      addAttachmentImage(masterSecret, data.getData());
+      setMedia(data.getData(),
+               MediaUtil.isGif(MediaUtil.getMimeType(this, data.getData())) ? MediaType.GIF
+                   : MediaType.IMAGE,
+               false);
       break;
     case PICK_VIDEO:
-      addAttachmentVideo(data.getData());
+      setMedia(data.getData(), MediaType.VIDEO, false);
       break;
     case PICK_AUDIO:
-      addAttachmentAudio(data.getData());
+      setMedia(data.getData(), MediaType.AUDIO, false);
       break;
     case PICK_CONTACT_INFO:
       addAttachmentContactInfo(data.getData());
       break;
     case TAKE_PHOTO:
       if (attachmentManager.getCaptureUri() != null) {
-        addAttachmentImage(masterSecret, attachmentManager.getCaptureUri());
+        setMedia(attachmentManager.getCaptureUri(), MediaType.IMAGE, true);
       }
       break;
     }
@@ -631,9 +637,10 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     Uri    draftVideo = getIntent().getParcelableExtra(DRAFT_VIDEO_EXTRA);
 
     if (draftText != null)  composeText.setText(draftText);
-    if (draftImage != null) addAttachmentImage(masterSecret, draftImage);
-    if (draftAudio != null) addAttachmentAudio(draftAudio);
-    if (draftVideo != null) addAttachmentVideo(draftVideo);
+
+    if      (draftImage != null) setMedia(draftImage, MediaType.IMAGE, false);
+    else if (draftAudio != null) setMedia(draftAudio, MediaType.AUDIO, false);
+    else if (draftVideo != null) setMedia(draftVideo, MediaType.VIDEO, false);
 
     if (draftText == null && draftImage == null && draftAudio == null && draftVideo == null) {
       initializeDraftFromDatabase();
@@ -667,11 +674,11 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
           if (draft.getType().equals(Draft.TEXT)) {
             composeText.setText(draft.getValue());
           } else if (draft.getType().equals(Draft.IMAGE)) {
-            addAttachmentImage(masterSecret, Uri.parse(draft.getValue()));
+            setMedia(Uri.parse(draft.getValue()), MediaType.IMAGE, false);
           } else if (draft.getType().equals(Draft.AUDIO)) {
-            addAttachmentAudio(Uri.parse(draft.getValue()));
+            setMedia(Uri.parse(draft.getValue()), MediaType.AUDIO, false);
           } else if (draft.getType().equals(Draft.VIDEO)) {
-            addAttachmentVideo(Uri.parse(draft.getValue()));
+            setMedia(Uri.parse(draft.getValue()), MediaType.VIDEO, false);
           }
         }
 
@@ -880,55 +887,8 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }
   }
 
-  private void addAttachmentImage(MasterSecret masterSecret, Uri imageUri) {
-    try {
-      attachmentManager.setImage(masterSecret, imageUri);
-    } catch (IOException | BitmapDecodingException e) {
-      Log.w(TAG, e);
-      attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_there_was_an_error_setting_your_attachment,
-                     Toast.LENGTH_LONG).show();
-    } catch (MediaTooLargeException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, getString(R.string.ConversationActivity_the_gif_you_selected_was_too_big),
-                     Toast.LENGTH_LONG).show();
-      Log.w(TAG, e);
-    }
-  }
-
-  private void addAttachmentVideo(Uri videoUri) {
-    try {
-      attachmentManager.setVideo(videoUri);
-    } catch (IOException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_there_was_an_error_setting_your_attachment,
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    } catch (MediaTooLargeException e) {
-      attachmentManager.clear();
-
-      Toast.makeText(this, getString(R.string.ConversationActivity_sorry_the_selected_video_exceeds_message_size_restrictions,
-                                     (MmsMediaConstraints.MAX_MESSAGE_SIZE/1024)),
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    }
-  }
-
-  private void addAttachmentAudio(Uri audioUri) {
-    try {
-      attachmentManager.setAudio(audioUri);
-    } catch (IOException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_there_was_an_error_setting_your_attachment,
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    } catch (MediaTooLargeException e) {
-      attachmentManager.clear();
-      Toast.makeText(this, getString(R.string.ConversationActivity_sorry_the_selected_audio_exceeds_message_size_restrictions,
-                                     (MmsMediaConstraints.MAX_MESSAGE_SIZE/1024)),
-                     Toast.LENGTH_LONG).show();
-      Log.w("ComposeMessageActivity", e);
-    }
+  private void setMedia(Uri uri, MediaType mediaType, boolean isCapture) {
+    attachmentManager.setMedia(masterSecret, uri, mediaType, getCurrentMediaConstraints(), isCapture);
   }
 
   private void addAttachmentContactInfo(Uri contactUri) {
@@ -1103,6 +1063,10 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     return rawText;
   }
 
+  private MediaConstraints getCurrentMediaConstraints() {
+    return MediaConstraints.MMS_CONSTRAINTS;
+  }
+
   private void markThreadAsRead() {
     new AsyncTask<Long, Void, Void>() {
       @Override
@@ -1169,8 +1133,24 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     final Context context = getApplicationContext();
     SlideDeck slideDeck;
 
-    if (attachmentManager.isAttachmentPresent()) slideDeck = new SlideDeck(attachmentManager.getSlideDeck());
-    else                                         slideDeck = new SlideDeck();
+    if (attachmentManager.isAttachmentPresent()) {
+      Slide            mediaSlide  = attachmentManager.getSlideDeck().getThumbnailSlide();
+      MediaConstraints constraints = getCurrentMediaConstraints();
+
+      if (mediaSlide != null &&
+          !constraints.isSatisfied(this, masterSecret, mediaSlide.getPart()) &&
+          !constraints.canResize(mediaSlide.getPart()))
+      {
+        Toast.makeText(context,
+                       R.string.ConversationActivity_attachment_exceeds_size_limits,
+                       Toast.LENGTH_SHORT).show();
+        return;
+      }
+
+      slideDeck = new SlideDeck(attachmentManager.getSlideDeck());
+    } else {
+      slideDeck = new SlideDeck();
+    }
 
     OutgoingMediaMessage outgoingMessage = new OutgoingMediaMessage(this, recipients, slideDeck,
                                                                     getMessage(), distributionType);
@@ -1335,4 +1315,5 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     initializeSecurity();
     updateToggleButtonState();
   }
+
 }

src/org/smssecure/smssecure/mms/Slide.java
@@ -21,8 +21,10 @@ import android.content.res.Resources.Theme;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.Util;
 
 import java.io.IOException;
@@ -32,18 +34,12 @@ import ws.com.google.android.mms.pdu.PduPart;
 
 public abstract class Slide {
 
-  protected final PduPart      part;
-  protected final Context      context;
-  protected       MasterSecret masterSecret;
+  protected final PduPart part;
+  protected final Context context;
 
   public Slide(Context context, @NonNull PduPart part) {
     this.part    = part;
     this.context = context;
-  }
-
-  public Slide(Context context, @NonNull MasterSecret masterSecret, @NonNull PduPart part) {
-    this(context, part);
-    this.masterSecret = masterSecret;
   }
 
   public String getContentType() {
@@ -90,18 +86,24 @@ public abstract class Slide {
     return !getPart().getPartId().isValid();
   }
 
-  protected static void assertMediaSize(Context context, Uri uri, long max)
-      throws MediaTooLargeException, IOException
-  {
-    InputStream in = context.getContentResolver().openInputStream(uri);
-    long   size    = 0;
-    byte[] buffer  = new byte[512];
-    int read;
 
-    while ((read = in.read(buffer)) != -1) {
-      size += read;
-      if (size > max) throw new MediaTooLargeException("Media exceeds maximum message size.");
-    }
+  protected static PduPart constructPartFromUri(@NonNull  Context      context,
+                                                @NonNull  Uri          uri,
+                                                @NonNull  String       defaultMime,
+                                                          long         dataSize)
+      throws IOException
+  {
+    final PduPart part            = new PduPart();
+    final String  mimeType        = MediaUtil.getMimeType(context, uri);
+    final String  derivedMimeType = mimeType != null ? mimeType : defaultMime;
+
+    part.setDataSize(dataSize);
+    part.setDataUri(uri);
+    part.setContentType(derivedMimeType.getBytes());
+    part.setContentId((System.currentTimeMillis()+"").getBytes());
+    part.setName((MediaUtil.getDiscreteMimeType(derivedMimeType) + System.currentTimeMillis()).getBytes());
+
+    return part;
   }
 
   @Override
@@ -125,7 +127,4 @@ public abstract class Slide {
     return Util.hashCode(getContentType(), hasAudio(), hasImage(),
                          hasVideo(), isDraft(), getUri(), getThumbnailUri(), getTransferProgress());
   }
-
-
-
 }