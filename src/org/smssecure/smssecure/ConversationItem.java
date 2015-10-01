SamSprung/Signal-Android

Listen for recipient changes in conversations and group updates.
Closes #4079
// FREEBIE

src/org/thoughtcrime/securesms/ConversationItem.java
@@ -60,6 +60,7 @@ import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.util.DateUtils;
+import org.thoughtcrime.securesms.util.Util;
 
 import java.util.List;
 import java.util.Locale;
@@ -73,7 +74,7 @@ import java.util.Set;
  *
  */
 
-public class ConversationItem extends LinearLayout implements Recipient.RecipientModifiedListener {
+public class ConversationItem extends LinearLayout implements Recipient.RecipientModifiedListener, Unbindable {
   private final static String TAG = ConversationItem.class.getSimpleName();
 
   private MessageRecord messageRecord;
@@ -181,16 +182,13 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
 
     setSelectionBackgroundDrawables(messageRecord);
     setBodyText(messageRecord);
-
-    if (hasConversationBubble(messageRecord)) {
-      setBubbleState(messageRecord, recipient);
-      setStatusIcons(messageRecord);
-      setContactPhoto(recipient);
-      setGroupMessageStatus(messageRecord, recipient);
-      setEvents(messageRecord);
-      setMinimumWidth();
-      setMediaAttributes(messageRecord);
-    }
+    setBubbleState(messageRecord, recipient);
+    setStatusIcons(messageRecord);
+    setContactPhoto(recipient);
+    setGroupMessageStatus(messageRecord, recipient);
+    setEvents(messageRecord);
+    setMinimumWidth();
+    setMediaAttributes(messageRecord);
   }
 
   private void initializeAttributes() {
@@ -205,6 +203,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     attrs.recycle();
   }
 
+  @Override
   public void unbind() {
     if (recipient != null) {
       recipient.removeListener(this);
@@ -234,10 +233,6 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     } else {
       setBackgroundDrawable(normalBackground);
     }
-  }
-
-  private boolean hasConversationBubble(MessageRecord messageRecord) {
-    return !messageRecord.isGroupAction();
   }
 
   private boolean isCaptionlessMms(MessageRecord messageRecord) {
@@ -403,12 +398,15 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
   }
 
   @Override
-  public void onModified(Recipient recipient) {
-    if (hasConversationBubble(messageRecord)) {
-      setBubbleState(messageRecord, recipient);
-      setContactPhoto(recipient);
-      setGroupMessageStatus(messageRecord, recipient);
-    }
+  public void onModified(final Recipient recipient) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        setBubbleState(messageRecord, recipient);
+        setContactPhoto(recipient);
+        setGroupMessageStatus(messageRecord, recipient);
+      }
+    });
   }
 
   private class ThumbnailDownloadClickListener implements ThumbnailView.ThumbnailClickListener {
@@ -416,6 +414,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
       DatabaseFactory.getPartDatabase(context).setTransferState(messageRecord.getId(), slide.getPart().getPartId(), PartDatabase.TRANSFER_PROGRESS_STARTED);
     }
   }
+
   private class ThumbnailClickListener implements ThumbnailView.ThumbnailClickListener {
     private void fireIntent(Slide slide) {
       Log.w(TAG, "Clicked: " + slide.getUri() + " , " + slide.getContentType());

src/org/thoughtcrime/securesms/ConversationUpdateItem.java
@@ -1,0 +1,98 @@
+package org.thoughtcrime.securesms;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.thoughtcrime.securesms.database.model.MessageRecord;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.recipients.Recipients;
+import org.thoughtcrime.securesms.util.GroupUtil;
+import org.thoughtcrime.securesms.util.Util;
+
+public class ConversationUpdateItem extends LinearLayout
+    implements Recipients.RecipientsModifiedListener, Recipient.RecipientModifiedListener, Unbindable, View.OnClickListener
+{
+  private static final String TAG = ConversationUpdateItem.class.getSimpleName();
+
+  private ImageView     icon;
+  private TextView      body;
+  private Recipient     sender;
+  private MessageRecord messageRecord;
+
+  public ConversationUpdateItem(Context context) {
+    super(context);
+  }
+
+  public ConversationUpdateItem(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  public void onFinishInflate() {
+    super.onFinishInflate();
+
+    this.icon = (ImageView)findViewById(R.id.conversation_update_icon);
+    this.body = (TextView)findViewById(R.id.conversation_update_body);
+
+    setOnClickListener(this);
+  }
+
+  public void set(MessageRecord messageRecord) {
+    this.messageRecord = messageRecord;
+    this.sender        = messageRecord.getIndividualRecipient();
+
+    this.sender.addListener(this);
+
+    if (messageRecord.isGroupAction()) {
+      icon.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_group_grey600_24dp));
+
+      if (messageRecord.isGroupQuit() && messageRecord.isOutgoing()) {
+        body.setText(R.string.MessageRecord_left_group);
+      } else if (messageRecord.isGroupQuit()) {
+        body.setText(getContext().getString(R.string.ConversationItem_group_action_left, sender.toShortString()));
+      } else {
+        GroupUtil.GroupDescription description = GroupUtil.getDescription(getContext(), messageRecord.getBody().getBody());
+        description.addListener(this);
+        body.setText(description.toString());
+      }
+    }
+  }
+
+  @Override
+  public void onModified(Recipients recipients) {
+    onModified(recipients.getPrimaryRecipient());
+  }
+
+  @Override
+  public void onModified(Recipient recipient) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        set(messageRecord);
+      }
+    });
+  }
+
+  @Override
+  public void onClick(View v) {
+    if (messageRecord.isIdentityUpdate()) {
+      Intent intent = new Intent(getContext(), RecipientPreferenceActivity.class);
+      intent.putExtra(RecipientPreferenceActivity.RECIPIENTS_EXTRA,
+                      new long[] {messageRecord.getIndividualRecipient().getRecipientId()});
+
+      getContext().startActivity(intent);
+    }
+  }
+
+  @Override
+  public void unbind() {
+    if (sender != null) {
+      sender.removeListener(this);
+    }
+  }
+}

src/org/thoughtcrime/securesms/Unbindable.java
@@ -1,0 +1,5 @@
+package org.thoughtcrime.securesms;
+
+public interface Unbindable {
+  public void unbind();
+}

src/org/thoughtcrime/securesms/util/GroupUtil.java
@@ -1,19 +1,22 @@
 package org.thoughtcrime.securesms.util;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.google.protobuf.InvalidProtocolBufferException;
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.recipients.RecipientFactory;
+import org.thoughtcrime.securesms.recipients.Recipients;
 
 import java.io.IOException;
-import java.util.List;
 
-import org.thoughtcrime.securesms.R;
 import static org.whispersystems.textsecure.internal.push.TextSecureProtos.GroupContext;
 
 public class GroupUtil {
 
   private static final String ENCODED_GROUP_PREFIX = "__textsecure_group__!";
+  private static final String TAG                  = GroupUtil.class.getSimpleName();
 
   public static String getEncodedId(byte[] groupId) {
     return ENCODED_GROUP_PREFIX + Hex.toStringCondensed(groupId);
@@ -31,19 +34,47 @@ public class GroupUtil {
     return groupId.startsWith(ENCODED_GROUP_PREFIX);
   }
 
-  public static String getDescription(Context context, String encodedGroup) {
+  public static @NonNull GroupDescription getDescription(@NonNull Context context, @Nullable String encodedGroup) {
     if (encodedGroup == null) {
-      return context.getString(R.string.GroupUtil_group_updated);
+      return new GroupDescription(context, null);
     }
 
     try {
-      StringBuilder description  = new StringBuilder();
       GroupContext  groupContext = GroupContext.parseFrom(Base64.decode(encodedGroup));
-      List<String>  members      = groupContext.getMembersList();
-      String        title        = groupContext.getName();
+      return new GroupDescription(context, groupContext);
+    } catch (IOException e) {
+      Log.w(TAG, e);
+      return new GroupDescription(context, null);
+    }
+  }
 
-      if (!members.isEmpty()) {
-        description.append(context.getString(R.string.GroupUtil_joined_the_group, Util.join(members, ", ")));
+  public static class GroupDescription {
+
+    @NonNull  private final Context         context;
+    @Nullable private final GroupContext    groupContext;
+    @Nullable private final Recipients      members;
+
+    public GroupDescription(@NonNull Context context, @Nullable GroupContext groupContext) {
+      this.context      = context.getApplicationContext();
+      this.groupContext = groupContext;
+
+      if (groupContext == null || groupContext.getMembersList().isEmpty()) {
+        this.members = null;
+      } else {
+        this.members = RecipientFactory.getRecipientsFromString(context, Util.join(groupContext.getMembersList(), ", "), true);
+      }
+    }
+
+    public String toString() {
+      if (groupContext == null) {
+        return context.getString(R.string.GroupUtil_group_updated);
+      }
+
+      StringBuilder description = new StringBuilder();
+      String        title       = groupContext.getName();
+
+      if (members != null) {
+        description.append(context.getString(R.string.GroupUtil_joined_the_group, members.toShortString()));
       }
 
       if (title != null && !title.trim().isEmpty()) {
@@ -56,12 +87,12 @@ public class GroupUtil {
       } else {
         return context.getString(R.string.GroupUtil_group_updated);
       }
-    } catch (InvalidProtocolBufferException e) {
-      Log.w("GroupUtil", e);
-      return context.getString(R.string.GroupUtil_group_updated);
-    } catch (IOException e) {
-      Log.w("GroupUtil", e);
-      return context.getString(R.string.GroupUtil_group_updated);
+    }
+
+    public void addListener(Recipients.RecipientsModifiedListener listener) {
+      if (this.members != null) {
+        this.members.addListener(listener);
+      }
     }
   }
 }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4079
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/0794380ca8f28503c08b252dd38c2b354b10e8dc

src/org/smssecure/smssecure/ConversationItem.java
@@ -65,6 +65,7 @@ import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.DateUtils;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
 import org.smssecure.smssecure.util.TelephonyUtil;
+import org.smssecure.smssecure.util.Util;
 
 import java.util.Locale;
 import java.util.Set;
@@ -77,7 +78,7 @@ import java.util.Set;
  *
  */
 
-public class ConversationItem extends LinearLayout implements Recipient.RecipientModifiedListener {
+public class ConversationItem extends LinearLayout implements Recipient.RecipientModifiedListener, Unbindable {
   private final static String TAG = ConversationItem.class.getSimpleName();
 
   private MessageRecord messageRecord;
@@ -183,16 +184,13 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
 
     setSelectionBackgroundDrawables(messageRecord);
     setBodyText(messageRecord);
-
-    if (hasConversationBubble(messageRecord)) {
-      setBubbleState(messageRecord, recipient);
-      setStatusIcons(messageRecord);
-      setContactPhoto(recipient);
-      setGroupMessageStatus(messageRecord, recipient);
-      setEvents(messageRecord);
-      setMinimumWidth();
-      setMediaAttributes(messageRecord);
-    }
+    setBubbleState(messageRecord, recipient);
+    setStatusIcons(messageRecord);
+    setContactPhoto(recipient);
+    setGroupMessageStatus(messageRecord, recipient);
+    setEvents(messageRecord);
+    setMinimumWidth();
+    setMediaAttributes(messageRecord);
   }
 
   private void initializeAttributes() {
@@ -207,6 +205,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     attrs.recycle();
   }
 
+  @Override
   public void unbind() {
     if (recipient != null) {
       recipient.removeListener(this);
@@ -236,10 +235,6 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
     } else {
       setBackgroundDrawable(normalBackground);
     }
-  }
-
-  private boolean hasConversationBubble(MessageRecord messageRecord) {
-    return !messageRecord.isGroupAction();
   }
 
   private boolean isCaptionlessMms(MessageRecord messageRecord) {
@@ -439,12 +434,15 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
   }
 
   @Override
-  public void onModified(Recipient recipient) {
-    if (hasConversationBubble(messageRecord)) {
-      setBubbleState(messageRecord, recipient);
-      setContactPhoto(recipient);
-      setGroupMessageStatus(messageRecord, recipient);
-    }
+  public void onModified(final Recipient recipient) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        setBubbleState(messageRecord, recipient);
+        setContactPhoto(recipient);
+        setGroupMessageStatus(messageRecord, recipient);
+      }
+    });
   }
 
   private class ThumbnailDownloadClickListener implements ThumbnailView.ThumbnailClickListener {
@@ -452,6 +450,7 @@ public class ConversationItem extends LinearLayout implements Recipient.Recipien
       DatabaseFactory.getPartDatabase(context).setTransferState(messageRecord.getId(), slide.getPart().getPartId(), PartDatabase.TRANSFER_PROGRESS_STARTED);
     }
   }
+
   private class ThumbnailClickListener implements ThumbnailView.ThumbnailClickListener {
     private void fireIntent(Slide slide) {
       Log.w(TAG, "Clicked: " + slide.getUri() + " , " + slide.getContentType());

src/org/smssecure/smssecure/ConversationUpdateItem.java
@@ -1,0 +1,98 @@
+package org.smssecure.smssecure;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.smssecure.smssecure.database.model.MessageRecord;
+import org.smssecure.smssecure.recipients.Recipient;
+import org.smssecure.smssecure.recipients.Recipients;
+import org.smssecure.smssecure.util.GroupUtil;
+import org.smssecure.smssecure.util.Util;
+
+public class ConversationUpdateItem extends LinearLayout
+    implements Recipients.RecipientsModifiedListener, Recipient.RecipientModifiedListener, Unbindable, View.OnClickListener
+{
+  private static final String TAG = ConversationUpdateItem.class.getSimpleName();
+
+  private ImageView     icon;
+  private TextView      body;
+  private Recipient     sender;
+  private MessageRecord messageRecord;
+
+  public ConversationUpdateItem(Context context) {
+    super(context);
+  }
+
+  public ConversationUpdateItem(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  public void onFinishInflate() {
+    super.onFinishInflate();
+
+    this.icon = (ImageView)findViewById(R.id.conversation_update_icon);
+    this.body = (TextView)findViewById(R.id.conversation_update_body);
+
+    setOnClickListener(this);
+  }
+
+  public void set(MessageRecord messageRecord) {
+    this.messageRecord = messageRecord;
+    this.sender        = messageRecord.getIndividualRecipient();
+
+    this.sender.addListener(this);
+
+    if (messageRecord.isGroupAction()) {
+      icon.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_group_grey600_24dp));
+
+      if (messageRecord.isGroupQuit() && messageRecord.isOutgoing()) {
+        body.setText(R.string.MessageRecord_left_group);
+      } else if (messageRecord.isGroupQuit()) {
+        body.setText(getContext().getString(R.string.ConversationItem_group_action_left, sender.toShortString()));
+      } else {
+        GroupUtil.GroupDescription description = GroupUtil.getDescription(getContext(), messageRecord.getBody().getBody());
+        description.addListener(this);
+        body.setText(description.toString());
+      }
+    }
+  }
+
+  @Override
+  public void onModified(Recipients recipients) {
+    onModified(recipients.getPrimaryRecipient());
+  }
+
+  @Override
+  public void onModified(Recipient recipient) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        set(messageRecord);
+      }
+    });
+  }
+
+  @Override
+  public void onClick(View v) {
+    if (messageRecord.isIdentityUpdate()) {
+      Intent intent = new Intent(getContext(), RecipientPreferenceActivity.class);
+      intent.putExtra(RecipientPreferenceActivity.RECIPIENTS_EXTRA,
+                      new long[] {messageRecord.getIndividualRecipient().getRecipientId()});
+
+      getContext().startActivity(intent);
+    }
+  }
+
+  @Override
+  public void unbind() {
+    if (sender != null) {
+      sender.removeListener(this);
+    }
+  }
+}

src/org/smssecure/smssecure/Unbindable.java
@@ -1,0 +1,5 @@
+package org.smssecure.smssecure;
+
+public interface Unbindable {
+  public void unbind();
+}

src/org/smssecure/smssecure/util/GroupUtil.java
@@ -1,19 +1,22 @@
 package org.smssecure.smssecure.util;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.google.protobuf.InvalidProtocolBufferException;
+import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.recipients.RecipientFactory;
+import org.smssecure.smssecure.recipients.Recipients;
 
 import java.io.IOException;
-import java.util.List;
 
-import org.smssecure.smssecure.R;
 import static org.whispersystems.textsecure.internal.push.TextSecureProtos.GroupContext;
 
 public class GroupUtil {
 
   private static final String ENCODED_GROUP_PREFIX = "__textsecure_group__!";
+  private static final String TAG                  = GroupUtil.class.getSimpleName();
 
   public static String getEncodedId(byte[] groupId) {
     return ENCODED_GROUP_PREFIX + Hex.toStringCondensed(groupId);
@@ -31,18 +34,46 @@ public class GroupUtil {
     return groupId.startsWith(ENCODED_GROUP_PREFIX);
   }
 
-  public static String getDescription(Context context, String encodedGroup) {
+  public static @NonNull GroupDescription getDescription(@NonNull Context context, @NonNull String encodedGroup) {
     if (encodedGroup == null) {
-      return "";
+      new GroupDescription(context, null);
     }
 
     try {
-      StringBuilder description  = new StringBuilder();
       GroupContext  groupContext = GroupContext.parseFrom(Base64.decode(encodedGroup));
-      List<String>  members      = groupContext.getMembersList();
-      String        title        = groupContext.getName();
+      return new GroupDescription(context, groupContext);
+    } catch (IOException e) {
+      Log.w(TAG, e);
+      return new GroupDescription(context, null);
+    }
+  }
 
-      if (!members.isEmpty()) {
+  public static class GroupDescription {
+
+    @NonNull  private final Context         context;
+    @Nullable private final GroupContext    groupContext;
+    @Nullable private final Recipients      members;
+
+    public GroupDescription(@NonNull Context context, @Nullable GroupContext groupContext) {
+      this.context      = context.getApplicationContext();
+      this.groupContext = groupContext;
+
+      if (groupContext == null || groupContext.getMembersList().isEmpty()) {
+        this.members = null;
+      } else {
+        this.members = RecipientFactory.getRecipientsFromString(context, Util.join(groupContext.getMembersList(), ", "), true);
+      }
+    }
+
+    public String toString() {
+      if (groupContext == null) {
+        return "";
+      }
+
+      StringBuilder description = new StringBuilder();
+      String        title       = groupContext.getName();
+
+      if (members != null) {
         description.append("");
       }
 
@@ -56,12 +87,12 @@ public class GroupUtil {
       } else {
         return "";
       }
-    } catch (InvalidProtocolBufferException e) {
-      Log.w("GroupUtil", e);
-      return "";
-    } catch (IOException e) {
-      Log.w("GroupUtil", e);
-      return "";
+    }
+
+    public void addListener(Recipients.RecipientsModifiedListener listener) {
+      if (this.members != null) {
+        this.members.addListener(listener);
+      }
     }
   }
 }