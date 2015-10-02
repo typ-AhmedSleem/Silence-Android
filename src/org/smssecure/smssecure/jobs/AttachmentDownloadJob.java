SamSprung/Signal-Android

be safer when processing parts in AttachmentDownloadJob
Closes #4166
// FREEBIE

src/org/thoughtcrime/securesms/jobs/AttachmentDownloadJob.java
@@ -15,6 +15,7 @@ import org.thoughtcrime.securesms.jobs.requirements.MasterSecretRequirement;
 import org.thoughtcrime.securesms.jobs.requirements.MediaNetworkRequirement;
 import org.thoughtcrime.securesms.notifications.MessageNotifier;
 import org.thoughtcrime.securesms.util.Util;
+import org.thoughtcrime.securesms.util.VisibleForTesting;
 import org.whispersystems.jobqueue.JobParameters;
 import org.whispersystems.jobqueue.requirements.NetworkRequirement;
 import org.whispersystems.libaxolotl.InvalidMessageException;
@@ -122,10 +123,16 @@ public class AttachmentDownloadJob extends MasterSecretJob implements Injectable
     }
   }
 
-  private TextSecureAttachmentPointer createAttachmentPointer(MasterSecret masterSecret, PduPart part)
+  @VisibleForTesting
+  TextSecureAttachmentPointer createAttachmentPointer(MasterSecret masterSecret, PduPart part)
       throws InvalidPartException
   {
-    if (part.getContentLocation() == null) throw new InvalidPartException("null content location");
+    if (part.getContentLocation() == null || part.getContentLocation().length == 0) {
+      throw new InvalidPartException("empty content id");
+    }
+    if (part.getContentDisposition() == null || part.getContentDisposition().length == 0) {
+      throw new InvalidPartException("empty encrypted key");
+    }
 
     try {
       AsymmetricMasterSecret asymmetricMasterSecret = MasterSecretUtil.getAsymmetricMasterSecret(context, masterSecret);
@@ -164,7 +171,7 @@ public class AttachmentDownloadJob extends MasterSecretJob implements Injectable
     }
   }
 
-  private static class InvalidPartException extends Exception {
+  @VisibleForTesting static class InvalidPartException extends Exception {
     public InvalidPartException(String s) {super(s);}
     public InvalidPartException(Exception e) {super(e);}
   }

test/unitTest/java/org/thoughtcrime/securesms/crypto/MasterCipherTest.java
@@ -1,0 +1,24 @@
+package org.thoughtcrime.securesms.crypto;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.thoughtcrime.securesms.BaseUnitTest;
+import org.whispersystems.libaxolotl.InvalidMessageException;
+
+@PowerMockIgnore("javax.crypto.*")
+public class MasterCipherTest extends BaseUnitTest {
+  private MasterCipher masterCipher;
+
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    masterCipher = new MasterCipher(masterSecret);
+  }
+
+  @Test(expected = InvalidMessageException.class)
+  public void testEncryptBytesWithZeroBody() throws Exception {
+    masterCipher.decryptBytes(new byte[]{});
+  }
+}

test/unitTest/java/org/thoughtcrime/securesms/jobs/AttachmentDownloadJobTest.java
@@ -1,0 +1,39 @@
+package org.thoughtcrime.securesms.jobs;
+
+import android.content.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.thoughtcrime.securesms.BaseUnitTest;
+import org.thoughtcrime.securesms.database.PartDatabase.PartId;
+import org.thoughtcrime.securesms.jobs.AttachmentDownloadJob.InvalidPartException;
+import org.thoughtcrime.securesms.util.Util;
+
+import ws.com.google.android.mms.pdu.PduPart;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+public class AttachmentDownloadJobTest extends BaseUnitTest {
+  private AttachmentDownloadJob job;
+
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    job = new AttachmentDownloadJob(mock(Context.class), 1L, new PartId(1L, 1L));
+  }
+
+  @Test(expected = InvalidPartException.class)
+  public void testCreateAttachmentPointerInvalidId() throws Exception {
+    PduPart part = new PduPart();
+    part.setContentDisposition(Util.toIsoBytes("a long and acceptable valid key like we all want"));
+    job.createAttachmentPointer(masterSecret, part);
+  }
+
+  @Test(expected = InvalidPartException.class)
+  public void testCreateAttachmentPointerInvalidKey() throws Exception {
+    PduPart part = new PduPart();
+    part.setContentDisposition(Util.toIsoBytes("1"));
+    job.createAttachmentPointer(masterSecret, part);
+  }
+}

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4166
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/cdf982a356081329ca03b88de9d24669cbd68083

src/org/smssecure/smssecure/jobs/AttachmentDownloadJob.java
@@ -15,6 +15,7 @@ import org.smssecure.smssecure.jobs.requirements.MediaNetworkRequirement;
 import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.util.Base64;
 import org.smssecure.smssecure.util.Util;
+import org.smssecure.smssecure.util.VisibleForTesting;
 import org.whispersystems.jobqueue.JobParameters;
 import org.whispersystems.jobqueue.requirements.NetworkRequirement;
 import org.whispersystems.libaxolotl.InvalidMessageException;
@@ -122,10 +123,16 @@ public class AttachmentDownloadJob extends MasterSecretJob implements Injectable
     }
   }
 
-  private TextSecureAttachmentPointer createAttachmentPointer(MasterSecret masterSecret, PduPart part)
+  @VisibleForTesting
+  TextSecureAttachmentPointer createAttachmentPointer(MasterSecret masterSecret, PduPart part)
       throws InvalidPartException
   {
-    if (part.getContentLocation() == null) throw new InvalidPartException("null content location");
+    if (part.getContentLocation() == null || part.getContentLocation().length == 0) {
+      throw new InvalidPartException("empty content id");
+    }
+    if (part.getContentDisposition() == null || part.getContentDisposition().length == 0) {
+      throw new InvalidPartException("empty encrypted key");
+    }
 
     try {
       MasterCipher masterCipher = new MasterCipher(masterSecret);
@@ -164,7 +171,7 @@ public class AttachmentDownloadJob extends MasterSecretJob implements Injectable
     }
   }
 
-  private static class InvalidPartException extends Exception {
+  @VisibleForTesting static class InvalidPartException extends Exception {
     public InvalidPartException(String s) {super(s);}
     public InvalidPartException(Exception e) {super(e);}
   }

test/unitTest/java/org/smssecure/smssecure/crypto/MasterCipherTest.java
@@ -1,0 +1,24 @@
+package org.smssecure.smssecure.crypto;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.smssecure.smssecure.BaseUnitTest;
+import org.whispersystems.libaxolotl.InvalidMessageException;
+
+@PowerMockIgnore("javax.crypto.*")
+public class MasterCipherTest extends BaseUnitTest {
+  private MasterCipher masterCipher;
+
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    masterCipher = new MasterCipher(masterSecret);
+  }
+
+  @Test(expected = InvalidMessageException.class)
+  public void testEncryptBytesWithZeroBody() throws Exception {
+    masterCipher.decryptBytes(new byte[]{});
+  }
+}

test/unitTest/java/org/smssecure/smssecure/jobs/AttachmentDownloadJobTest.java
@@ -1,0 +1,39 @@
+package org.smssecure.smssecure.jobs;
+
+import android.content.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.smssecure.smssecure.BaseUnitTest;
+import org.smssecure.smssecure.database.PartDatabase.PartId;
+import org.smssecure.smssecure.jobs.AttachmentDownloadJob.InvalidPartException;
+import org.smssecure.smssecure.util.Util;
+
+import ws.com.google.android.mms.pdu.PduPart;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+public class AttachmentDownloadJobTest extends BaseUnitTest {
+  private AttachmentDownloadJob job;
+
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    job = new AttachmentDownloadJob(mock(Context.class), 1L, new PartId(1L, 1L));
+  }
+
+  @Test(expected = InvalidPartException.class)
+  public void testCreateAttachmentPointerInvalidId() throws Exception {
+    PduPart part = new PduPart();
+    part.setContentDisposition(Util.toIsoBytes("a long and acceptable valid key like we all want"));
+    job.createAttachmentPointer(masterSecret, part);
+  }
+
+  @Test(expected = InvalidPartException.class)
+  public void testCreateAttachmentPointerInvalidKey() throws Exception {
+    PduPart part = new PduPart();
+    part.setContentDisposition(Util.toIsoBytes("1"));
+    job.createAttachmentPointer(masterSecret, part);
+  }
+}