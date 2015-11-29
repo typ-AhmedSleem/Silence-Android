SamSprung/Signal-Android

Join attachments instead of running an asynchronous query.
No more SlideDeck futures, just SlideDecks.

// FREEBIE

src/org/thoughtcrime/securesms/database/model/MediaMmsMessageRecord.java
@@ -17,23 +17,18 @@
 package org.thoughtcrime.securesms.database.model;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.text.SpannableString;
-import android.util.Log;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.database.MmsDatabase;
-import org.thoughtcrime.securesms.database.documents.NetworkFailure;
 import org.thoughtcrime.securesms.database.documents.IdentityKeyMismatch;
-import org.thoughtcrime.securesms.mms.MediaNotFoundException;
-import org.thoughtcrime.securesms.mms.Slide;
+import org.thoughtcrime.securesms.database.documents.NetworkFailure;
 import org.thoughtcrime.securesms.mms.SlideDeck;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.Recipients;
-import org.thoughtcrime.securesms.util.FutureTaskListener;
-import org.thoughtcrime.securesms.util.ListenableFutureTask;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Represents the message record model for MMS messages that contain
@@ -48,13 +43,13 @@ public class MediaMmsMessageRecord extends MessageRecord {
 
   private final Context context;
   private final int partCount;
-  private final ListenableFutureTask<SlideDeck> slideDeckFutureTask;
+  private final @NonNull SlideDeck slideDeck;
 
   public MediaMmsMessageRecord(Context context, long id, Recipients recipients,
                                Recipient individualRecipient, int recipientDeviceId,
                                long dateSent, long dateReceived, int deliveredCount,
                                long threadId, Body body,
-                               ListenableFutureTask<SlideDeck> slideDeck,
+                               @NonNull SlideDeck slideDeck,
                                int partCount, long mailbox,
                                List<IdentityKeyMismatch> mismatches,
                                List<NetworkFailure> failures)
@@ -63,51 +58,17 @@ public class MediaMmsMessageRecord extends MessageRecord {
           dateSent, dateReceived, threadId, DELIVERY_STATUS_NONE, deliveredCount, mailbox,
           mismatches, failures);
 
-    this.context             = context.getApplicationContext();
-    this.partCount           = partCount;
-    this.slideDeckFutureTask = slideDeck;
+    this.context   = context.getApplicationContext();
+    this.partCount = partCount;
+    this.slideDeck = slideDeck;
   }
 
-  public ListenableFutureTask<SlideDeck> getSlideDeckFuture() {
-    return slideDeckFutureTask;
-  }
-
-  private SlideDeck getSlideDeckSync() {
-    try {
-      return slideDeckFutureTask.get();
-    } catch (InterruptedException e) {
-      Log.w(TAG, e);
-      return null;
-    } catch (ExecutionException e) {
-      Log.w(TAG, e);
-      return null;
-    }
+  public @NonNull SlideDeck getSlideDeck() {
+    return slideDeck;
   }
 
   public boolean containsMediaSlide() {
-    SlideDeck deck = getSlideDeckSync();
-    return deck != null && deck.containsMediaSlide();
-  }
-
-
-  public void fetchMediaSlide(final FutureTaskListener<Slide> listener) {
-    slideDeckFutureTask.addListener(new FutureTaskListener<SlideDeck>() {
-      @Override
-      public void onSuccess(SlideDeck deck) {
-        for (Slide slide : deck.getSlides()) {
-          if (slide.hasImage() || slide.hasVideo() || slide.hasAudio()) {
-            listener.onSuccess(slide);
-            return;
-          }
-        }
-        listener.onFailure(new MediaNotFoundException("no media slide found"));
-      }
-
-      @Override
-      public void onFailure(Throwable error) {
-        listener.onFailure(error);
-      }
-    });
+    return slideDeck.containsMediaSlide();
   }
 
   public int getPartCount() {

typ-AhmedSleem/Silence-Android

Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/d2f44f6584e779a1215cfff9e0b45fcd353eb4d7

src/org/smssecure/smssecure/database/model/MediaMmsMessageRecord.java
@@ -17,23 +17,18 @@
 package org.smssecure.smssecure.database.model;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.text.SpannableString;
-import android.util.Log;
 
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.database.MmsDatabase;
-import org.smssecure.smssecure.database.documents.NetworkFailure;
 import org.smssecure.smssecure.database.documents.IdentityKeyMismatch;
-import org.smssecure.smssecure.mms.MediaNotFoundException;
-import org.smssecure.smssecure.mms.Slide;
+import org.smssecure.smssecure.database.documents.NetworkFailure;
 import org.smssecure.smssecure.mms.SlideDeck;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.Recipients;
-import org.smssecure.smssecure.util.FutureTaskListener;
-import org.smssecure.smssecure.util.ListenableFutureTask;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Represents the message record model for MMS messages that contain
@@ -48,13 +43,13 @@ public class MediaMmsMessageRecord extends MessageRecord {
 
   private final Context context;
   private final int partCount;
-  private final ListenableFutureTask<SlideDeck> slideDeckFutureTask;
+  private final @NonNull SlideDeck slideDeck;
 
   public MediaMmsMessageRecord(Context context, long id, Recipients recipients,
                                Recipient individualRecipient, int recipientDeviceId,
                                long dateSent, long dateReceived, long dateDeliveryReceived,
                                long threadId, Body body,
-                               ListenableFutureTask<SlideDeck> slideDeck,
+                               @NonNull SlideDeck slideDeck,
                                int partCount, long mailbox,
                                List<IdentityKeyMismatch> mismatches,
                                List<NetworkFailure> failures)
@@ -63,51 +58,17 @@ public class MediaMmsMessageRecord extends MessageRecord {
           dateSent, dateReceived, threadId, DELIVERY_STATUS_NONE, dateDeliveryReceived, mailbox,
           mismatches, failures);
 
-    this.context             = context.getApplicationContext();
-    this.partCount           = partCount;
-    this.slideDeckFutureTask = slideDeck;
+    this.context   = context.getApplicationContext();
+    this.partCount = partCount;
+    this.slideDeck = slideDeck;
   }
 
-  public ListenableFutureTask<SlideDeck> getSlideDeckFuture() {
-    return slideDeckFutureTask;
-  }
-
-  private SlideDeck getSlideDeckSync() {
-    try {
-      return slideDeckFutureTask.get();
-    } catch (InterruptedException e) {
-      Log.w(TAG, e);
-      return null;
-    } catch (ExecutionException e) {
-      Log.w(TAG, e);
-      return null;
-    }
+  public @NonNull SlideDeck getSlideDeck() {
+    return slideDeck;
   }
 
   public boolean containsMediaSlide() {
-    SlideDeck deck = getSlideDeckSync();
-    return deck != null && deck.containsMediaSlide();
-  }
-
-
-  public void fetchMediaSlide(final FutureTaskListener<Slide> listener) {
-    slideDeckFutureTask.addListener(new FutureTaskListener<SlideDeck>() {
-      @Override
-      public void onSuccess(SlideDeck deck) {
-        for (Slide slide : deck.getSlides()) {
-          if (slide.hasImage() || slide.hasVideo() || slide.hasAudio()) {
-            listener.onSuccess(slide);
-            return;
-          }
-        }
-        listener.onFailure(new MediaNotFoundException("no media slide found"));
-      }
-
-      @Override
-      public void onFailure(Throwable error) {
-        listener.onFailure(error);
-      }
-    });
+    return slideDeck.containsMediaSlide();
   }
 
   public int getPartCount() {