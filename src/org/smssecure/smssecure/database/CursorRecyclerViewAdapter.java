SamSprung/Signal-Android

only load partial conversation by default
Closes #4252
Fixes #3911
// FREEBIE

src/org/thoughtcrime/securesms/database/CursorRecyclerViewAdapter.java
@@ -20,17 +20,34 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.DataSetObserver;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.thoughtcrime.securesms.util.VisibleForTesting;
 
 /**
  * RecyclerView.Adapter that manages a Cursor, comparable to the CursorAdapter usable in ListView/GridView.
  */
-public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
-  private final Context         context;
+public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+  private final Context context;
   private final DataSetObserver observer = new AdapterDataSetObserver();
 
-  private Cursor  cursor;
-  private boolean valid;
+  @VisibleForTesting final static int HEADER_TYPE = Integer.MIN_VALUE;
+  @VisibleForTesting final static int FOOTER_TYPE = Integer.MIN_VALUE + 1;
+
+  private           Cursor  cursor;
+  private           boolean valid;
+  private @Nullable View    header;
+  private @Nullable View    footer;
+
+  private static class HeaderFooterViewHolder extends RecyclerView.ViewHolder {
+    public HeaderFooterViewHolder(View itemView) {
+      super(itemView);
+    }
+  }
 
   protected CursorRecyclerViewAdapter(Context context, Cursor cursor) {
     this.context = context;
@@ -39,16 +56,30 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
       valid = true;
       cursor.registerDataSetObserver(observer);
     }
-
-    setHasStableIds(false);
   }
 
-  public Context getContext() {
+  protected @NonNull Context getContext() {
     return context;
   }
 
-  public Cursor getCursor() {
+  public @Nullable Cursor getCursor() {
     return cursor;
+  }
+
+  public void setHeaderView(@Nullable View header) {
+    this.header = header;
+  }
+
+  public void setFooterView(@Nullable View footer) {
+    this.footer = footer;
+  }
+
+  public boolean hasHeaderView() {
+    return header != null;
+  }
+
+  public boolean hasFooterView() {
+    return footer != null;
   }
 
   public void changeCursor(Cursor cursor) {
@@ -78,29 +109,50 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
     return oldCursor;
   }
 
-
   @Override
   public int getItemCount() {
-    return isActiveCursor() ? cursor.getCount() : 0;
+    if (!isActiveCursor()) return 0;
+
+    return cursor.getCount()
+           + (hasHeaderView() ? 1 : 0)
+           + (hasFooterView() ? 1 : 0);
   }
 
+  @SuppressWarnings("unchecked")
   @Override
-  public long getItemId(int position) {
-    return isActiveCursor() && cursor.moveToPosition(position)
-           ? cursor.getLong(cursor.getColumnIndexOrThrow("_id"))
-           : 0;
+  public final void onViewRecycled(ViewHolder holder) {
+    if (!(holder instanceof HeaderFooterViewHolder)) {
+      onItemViewRecycled((VH)holder);
+    }
   }
 
-  public abstract void onBindViewHolder(VH viewHolder, @NonNull Cursor cursor);
+  public void onItemViewRecycled(VH holder) {}
 
+  @Override public final ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    switch (viewType) {
+    case HEADER_TYPE: return new HeaderFooterViewHolder(header);
+    case FOOTER_TYPE: return new HeaderFooterViewHolder(footer);
+    default:          return onCreateItemViewHolder(parent, viewType);
+    }
+  }
+
+  public abstract VH onCreateItemViewHolder(ViewGroup parent, int viewType);
+
+  @SuppressWarnings("unchecked")
   @Override
-  public void onBindViewHolder(VH viewHolder, int position) {
-    moveToPositionOrThrow(position);
-    onBindViewHolder(viewHolder, cursor);
+  public final void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+    if (!isHeaderPosition(position) && !isFooterPosition(position)) {
+      moveToPositionOrThrow(getCursorPosition(position));
+      onBindItemViewHolder((VH)viewHolder, cursor);
+    }
   }
+
+  public abstract void onBindItemViewHolder(VH viewHolder, @NonNull Cursor cursor);
 
   @Override public int getItemViewType(int position) {
-    moveToPositionOrThrow(position);
+    if (isHeaderPosition(position)) return HEADER_TYPE;
+    if (isFooterPosition(position)) return FOOTER_TYPE;
+    moveToPositionOrThrow(getCursorPosition(position));
     return getItemViewType(cursor);
   }
 
@@ -123,6 +175,18 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
 
   private boolean isActiveCursor() {
     return valid && cursor != null;
+  }
+
+  private boolean isFooterPosition(int position) {
+    return hasFooterView() && position == getItemCount() - 1;
+  }
+
+  private boolean isHeaderPosition(int position) {
+    return hasHeaderView() && position == 0;
+  }
+
+  private int getCursorPosition(int position) {
+    return hasHeaderView() ? position - 1 : position;
   }
 
   private class AdapterDataSetObserver extends DataSetObserver {

test/unitTest/java/org/thoughtcrime/securesms/database/CursorRecyclerViewAdapterTest.java
@@ -1,0 +1,78 @@
+package org.thoughtcrime.securesms.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CursorRecyclerViewAdapterTest {
+  private CursorRecyclerViewAdapter adapter;
+  private Context                   context;
+  private Cursor                    cursor;
+
+  @Before
+  public void setUp() {
+    context = mock(Context.class);
+    cursor  = mock(Cursor.class);
+    when(cursor.getCount()).thenReturn(100);
+    when(cursor.moveToPosition(anyInt())).thenReturn(true);
+
+    adapter = new CursorRecyclerViewAdapter(context, cursor) {
+      @Override
+      public ViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
+        return null;
+      }
+
+      @Override
+      public void onBindItemViewHolder(ViewHolder viewHolder, @NonNull Cursor cursor) {
+      }
+    };
+  }
+
+  @Test
+  public void testSanityCount() throws Exception {
+    assertEquals(adapter.getItemCount(), 100);
+  }
+
+  @Test
+  public void testHeaderCount() throws Exception {
+    adapter.setHeaderView(new View(context));
+    assertEquals(adapter.getItemCount(), 101);
+
+    assertEquals(adapter.getItemViewType(0), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(1), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(100), CursorRecyclerViewAdapter.HEADER_TYPE);
+  }
+
+  @Test
+  public void testFooterCount() throws Exception {
+    adapter.setFooterView(new View(context));
+    assertEquals(adapter.getItemCount(), 101);
+    assertEquals(adapter.getItemViewType(100), CursorRecyclerViewAdapter.FOOTER_TYPE);
+    assertNotEquals(adapter.getItemViewType(0), CursorRecyclerViewAdapter.FOOTER_TYPE);
+    assertNotEquals(adapter.getItemViewType(99), CursorRecyclerViewAdapter.FOOTER_TYPE);
+  }
+
+  @Test
+  public void testHeaderFooterCount() throws Exception {
+    adapter.setHeaderView(new View(context));
+    adapter.setFooterView(new View(context));
+    assertEquals(adapter.getItemCount(), 102);
+    assertEquals(adapter.getItemViewType(101), CursorRecyclerViewAdapter.FOOTER_TYPE);
+    assertEquals(adapter.getItemViewType(0), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(1), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(100), CursorRecyclerViewAdapter.FOOTER_TYPE);
+  }
+}
+

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4252
Fixes https://github.com/WhisperSystems/TextSecure/issues/3911
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/ae97495c47afed6613a350b24661e999503f3e06

src/org/smssecure/smssecure/database/CursorRecyclerViewAdapter.java
@@ -20,17 +20,34 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.DataSetObserver;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.smssecure.smssecure.util.VisibleForTesting;
 
 /**
  * RecyclerView.Adapter that manages a Cursor, comparable to the CursorAdapter usable in ListView/GridView.
  */
-public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
-  private final Context         context;
+public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+  private final Context context;
   private final DataSetObserver observer = new AdapterDataSetObserver();
 
-  private Cursor  cursor;
-  private boolean valid;
+  @VisibleForTesting final static int HEADER_TYPE = Integer.MIN_VALUE;
+  @VisibleForTesting final static int FOOTER_TYPE = Integer.MIN_VALUE + 1;
+
+  private           Cursor  cursor;
+  private           boolean valid;
+  private @Nullable View    header;
+  private @Nullable View    footer;
+
+  private static class HeaderFooterViewHolder extends RecyclerView.ViewHolder {
+    public HeaderFooterViewHolder(View itemView) {
+      super(itemView);
+    }
+  }
 
   protected CursorRecyclerViewAdapter(Context context, Cursor cursor) {
     this.context = context;
@@ -39,16 +56,30 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
       valid = true;
       cursor.registerDataSetObserver(observer);
     }
-
-    setHasStableIds(false);
   }
 
-  public Context getContext() {
+  protected @NonNull Context getContext() {
     return context;
   }
 
-  public Cursor getCursor() {
+  public @Nullable Cursor getCursor() {
     return cursor;
+  }
+
+  public void setHeaderView(@Nullable View header) {
+    this.header = header;
+  }
+
+  public void setFooterView(@Nullable View footer) {
+    this.footer = footer;
+  }
+
+  public boolean hasHeaderView() {
+    return header != null;
+  }
+
+  public boolean hasFooterView() {
+    return footer != null;
   }
 
   public void changeCursor(Cursor cursor) {
@@ -78,29 +109,50 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
     return oldCursor;
   }
 
-
   @Override
   public int getItemCount() {
-    return isActiveCursor() ? cursor.getCount() : 0;
+    if (!isActiveCursor()) return 0;
+
+    return cursor.getCount()
+           + (hasHeaderView() ? 1 : 0)
+           + (hasFooterView() ? 1 : 0);
   }
 
+  @SuppressWarnings("unchecked")
   @Override
-  public long getItemId(int position) {
-    return isActiveCursor() && cursor.moveToPosition(position)
-           ? cursor.getLong(cursor.getColumnIndexOrThrow("_id"))
-           : 0;
+  public final void onViewRecycled(ViewHolder holder) {
+    if (!(holder instanceof HeaderFooterViewHolder)) {
+      onItemViewRecycled((VH)holder);
+    }
   }
 
-  public abstract void onBindViewHolder(VH viewHolder, @NonNull Cursor cursor);
+  public void onItemViewRecycled(VH holder) {}
 
+  @Override public final ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    switch (viewType) {
+    case HEADER_TYPE: return new HeaderFooterViewHolder(header);
+    case FOOTER_TYPE: return new HeaderFooterViewHolder(footer);
+    default:          return onCreateItemViewHolder(parent, viewType);
+    }
+  }
+
+  public abstract VH onCreateItemViewHolder(ViewGroup parent, int viewType);
+
+  @SuppressWarnings("unchecked")
   @Override
-  public void onBindViewHolder(VH viewHolder, int position) {
-    moveToPositionOrThrow(position);
-    onBindViewHolder(viewHolder, cursor);
+  public final void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+    if (!isHeaderPosition(position) && !isFooterPosition(position)) {
+      moveToPositionOrThrow(getCursorPosition(position));
+      onBindItemViewHolder((VH)viewHolder, cursor);
+    }
   }
+
+  public abstract void onBindItemViewHolder(VH viewHolder, @NonNull Cursor cursor);
 
   @Override public int getItemViewType(int position) {
-    moveToPositionOrThrow(position);
+    if (isHeaderPosition(position)) return HEADER_TYPE;
+    if (isFooterPosition(position)) return FOOTER_TYPE;
+    moveToPositionOrThrow(getCursorPosition(position));
     return getItemViewType(cursor);
   }
 
@@ -123,6 +175,18 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
 
   private boolean isActiveCursor() {
     return valid && cursor != null;
+  }
+
+  private boolean isFooterPosition(int position) {
+    return hasFooterView() && position == getItemCount() - 1;
+  }
+
+  private boolean isHeaderPosition(int position) {
+    return hasHeaderView() && position == 0;
+  }
+
+  private int getCursorPosition(int position) {
+    return hasHeaderView() ? position - 1 : position;
   }
 
   private class AdapterDataSetObserver extends DataSetObserver {

test/unitTest/java/org/smssecure/smssecure/database/CursorRecyclerViewAdapterTest.java
@@ -1,0 +1,78 @@
+package org.smssecure.smssecure.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CursorRecyclerViewAdapterTest {
+  private CursorRecyclerViewAdapter adapter;
+  private Context                   context;
+  private Cursor                    cursor;
+
+  @Before
+  public void setUp() {
+    context = mock(Context.class);
+    cursor  = mock(Cursor.class);
+    when(cursor.getCount()).thenReturn(100);
+    when(cursor.moveToPosition(anyInt())).thenReturn(true);
+
+    adapter = new CursorRecyclerViewAdapter(context, cursor) {
+      @Override
+      public ViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
+        return null;
+      }
+
+      @Override
+      public void onBindItemViewHolder(ViewHolder viewHolder, @NonNull Cursor cursor) {
+      }
+    };
+  }
+
+  @Test
+  public void testSanityCount() throws Exception {
+    assertEquals(adapter.getItemCount(), 100);
+  }
+
+  @Test
+  public void testHeaderCount() throws Exception {
+    adapter.setHeaderView(new View(context));
+    assertEquals(adapter.getItemCount(), 101);
+
+    assertEquals(adapter.getItemViewType(0), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(1), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(100), CursorRecyclerViewAdapter.HEADER_TYPE);
+  }
+
+  @Test
+  public void testFooterCount() throws Exception {
+    adapter.setFooterView(new View(context));
+    assertEquals(adapter.getItemCount(), 101);
+    assertEquals(adapter.getItemViewType(100), CursorRecyclerViewAdapter.FOOTER_TYPE);
+    assertNotEquals(adapter.getItemViewType(0), CursorRecyclerViewAdapter.FOOTER_TYPE);
+    assertNotEquals(adapter.getItemViewType(99), CursorRecyclerViewAdapter.FOOTER_TYPE);
+  }
+
+  @Test
+  public void testHeaderFooterCount() throws Exception {
+    adapter.setHeaderView(new View(context));
+    adapter.setFooterView(new View(context));
+    assertEquals(adapter.getItemCount(), 102);
+    assertEquals(adapter.getItemViewType(101), CursorRecyclerViewAdapter.FOOTER_TYPE);
+    assertEquals(adapter.getItemViewType(0), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(1), CursorRecyclerViewAdapter.HEADER_TYPE);
+    assertNotEquals(adapter.getItemViewType(100), CursorRecyclerViewAdapter.FOOTER_TYPE);
+  }
+}
+