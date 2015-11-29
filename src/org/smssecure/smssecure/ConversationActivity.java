SamSprung/Signal-Android

Stop audio when leaving conversation.
Fixes #4290
// FREEBIE

src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -61,6 +61,7 @@ import com.google.protobuf.ByteString;
 import org.thoughtcrime.redphone.RedPhone;
 import org.thoughtcrime.redphone.RedPhoneService;
 import org.thoughtcrime.securesms.TransportOptions.OnTransportChangedListener;
+import org.thoughtcrime.securesms.audio.AudioSlidePlayer;
 import org.thoughtcrime.securesms.color.MaterialColor;
 import org.thoughtcrime.securesms.components.AnimatingToggle;
 import org.thoughtcrime.securesms.components.ComposeText;
@@ -280,6 +281,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     MessageNotifier.setVisibleThread(-1L);
     if (isFinishing()) overridePendingTransition(R.anim.fade_scale_in, R.anim.slide_to_right);
     quickAttachmentDrawer.onPause();
+    AudioSlidePlayer.stopAll();
   }
 
   @Override

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/4290
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/d70f0f1216065790f463cc73a791a71de1a5ff1c

src/org/smssecure/smssecure/ConversationActivity.java
@@ -59,6 +59,7 @@ import com.afollestad.materialdialogs.AlertDialogWrapper;
 import com.google.protobuf.ByteString;
 
 import org.smssecure.smssecure.TransportOptions.OnTransportChangedListener;
+import org.smssecure.smssecure.audio.AudioSlidePlayer;
 import org.smssecure.smssecure.color.MaterialColor;
 import org.smssecure.smssecure.components.AnimatingToggle;
 import org.smssecure.smssecure.components.ComposeText;
@@ -251,6 +252,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     super.onPause();
     MessageNotifier.setVisibleThread(-1L);
     if (isFinishing()) overridePendingTransition(R.anim.fade_scale_in, R.anim.slide_to_right);
+    AudioSlidePlayer.stopAll();
   }
 
   @Override public void onConfigurationChanged(Configuration newConfig) {