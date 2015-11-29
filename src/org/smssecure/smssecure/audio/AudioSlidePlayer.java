signalapp/Signal-Android

Support for an audio view to allow in-app playback of audio.
// FREEBIE

src/org/thoughtcrime/securesms/audio/AudioSlidePlayer.java
@@ -1,0 +1,237 @@
+package org.thoughtcrime.securesms.audio;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.util.Pair;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.mms.AudioSlide;
+import org.thoughtcrime.securesms.util.Util;
+import org.whispersystems.libaxolotl.util.guava.Optional;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+public class AudioSlidePlayer {
+
+  private static final String TAG = AudioSlidePlayer.class.getSimpleName();
+
+  private static @NonNull Optional<AudioSlidePlayer> playing = Optional.absent();
+
+  private final @NonNull Context      context;
+  private final @NonNull MasterSecret masterSecret;
+  private final @NonNull AudioSlide   slide;
+  private final @NonNull Handler      progressEventHandler;
+
+  private @NonNull  WeakReference<Listener> listener;
+  private @Nullable MediaPlayer             mediaPlayer;
+  private @Nullable AudioAttachmentServer   audioAttachmentServer;
+
+  public synchronized static AudioSlidePlayer createFor(@NonNull Context context,
+                                                        @NonNull MasterSecret masterSecret,
+                                                        @NonNull AudioSlide slide,
+                                                        @NonNull Listener listener)
+  {
+    if (playing.isPresent() && playing.get().getAudioSlide().equals(slide)) {
+      playing.get().setListener(listener);
+      return playing.get();
+    } else {
+      return new AudioSlidePlayer(context, masterSecret, slide, listener);
+    }
+  }
+
+  private AudioSlidePlayer(@NonNull Context context,
+                           @NonNull MasterSecret masterSecret,
+                           @NonNull AudioSlide slide,
+                           @NonNull Listener listener)
+  {
+    this.context              = context;
+    this.masterSecret         = masterSecret;
+    this.slide                = slide;
+    this.listener             = new WeakReference<>(listener);
+    this.progressEventHandler = new ProgressEventHandler(this);
+  }
+
+  public void play(final double progress) throws IOException {
+    if (this.mediaPlayer != null) return;
+
+    this.mediaPlayer           = new MediaPlayer();
+    this.audioAttachmentServer = new AudioAttachmentServer(context, masterSecret, slide.asAttachment());
+
+    audioAttachmentServer.start();
+
+    mediaPlayer.setDataSource(context, audioAttachmentServer.getUri());
+    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+      @Override
+      public void onPrepared(MediaPlayer mp) {
+        Log.w(TAG, "onPrepared");
+        if (progress > 0) {
+          mediaPlayer.seekTo((int)(mediaPlayer.getDuration() * progress));
+        }
+
+        mediaPlayer.start();
+
+        notifyOnStart();
+        setPlaying(AudioSlidePlayer.this);
+        progressEventHandler.sendEmptyMessage(0);
+      }
+    });
+
+    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+      @Override
+      public void onCompletion(MediaPlayer mp) {
+        Log.w(TAG, "onComplete");
+        mediaPlayer = null;
+        audioAttachmentServer.stop();
+        audioAttachmentServer = null;
+
+        notifyOnStop();
+        progressEventHandler.removeMessages(0);
+      }
+    });
+
+    mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+      @Override
+      public boolean onError(MediaPlayer mp, int what, int extra) {
+        Log.w(TAG, "MediaPlayer Error: " + what + " , " + extra);
+        notifyOnStop();
+        return true;
+      }
+    });
+
+    mediaPlayer.prepareAsync();
+  }
+
+  public void stop() {
+    Log.w(TAG, "Stop called!");
+    shutdown();
+  }
+
+  public void setListener(@NonNull Listener listener) {
+    this.listener = new WeakReference<>(listener);
+
+    if (this.mediaPlayer != null && this.mediaPlayer.isPlaying()) {
+      notifyOnStart();
+    }
+  }
+
+  public @NonNull AudioSlide getAudioSlide() {
+    return slide;
+  }
+
+  private void shutdown() {
+    removePlaying(this);
+
+    if (this.mediaPlayer != null) {
+      this.mediaPlayer.stop();
+    }
+
+    if (this.audioAttachmentServer != null) {
+      this.audioAttachmentServer.stop();
+    }
+
+    this.mediaPlayer           = null;
+    this.audioAttachmentServer = null;
+  }
+
+  private Pair<Double, Integer> getProgress() {
+    if (mediaPlayer == null || mediaPlayer.getCurrentPosition() <= 0 || mediaPlayer.getDuration() <= 0) {
+      return new Pair<>(0D, 0);
+    } else {
+      return new Pair<>((double) mediaPlayer.getCurrentPosition() / (double) mediaPlayer.getDuration(),
+                        mediaPlayer.getCurrentPosition());
+    }
+  }
+
+  private void notifyOnStart() {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        getListener().onStart();
+      }
+    });
+  }
+
+  private void notifyOnStop() {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        getListener().onStop();
+      }
+    });
+  }
+
+  private void notifyOnProgress(final double progress, final long millis) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        getListener().onProgress(progress, millis);
+      }
+    });
+  }
+
+  private @NonNull Listener getListener() {
+    Listener listener = this.listener.get();
+
+    if (listener != null) return listener;
+    else                  return new Listener() {
+      @Override
+      public void onStart() {}
+      @Override
+      public void onStop() {}
+      @Override
+      public void onProgress(double progress, long millis) {}
+    };
+  }
+
+  private synchronized static void setPlaying(@NonNull AudioSlidePlayer player) {
+    if (playing.isPresent() && playing.get() != player) {
+      playing.get().notifyOnStop();
+      playing.get().stop();
+    }
+
+    playing = Optional.of(player);
+  }
+
+  private synchronized static void removePlaying(@NonNull AudioSlidePlayer player) {
+    if (playing.isPresent() && playing.get() == player) {
+      playing = Optional.absent();
+    }
+  }
+
+  public interface Listener {
+    public void onStart();
+    public void onStop();
+    public void onProgress(double progress, long millis);
+  }
+
+  private static class ProgressEventHandler extends Handler {
+
+    private final WeakReference<AudioSlidePlayer> playerReference;
+
+    private ProgressEventHandler(@NonNull AudioSlidePlayer player) {
+      this.playerReference = new WeakReference<>(player);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+      AudioSlidePlayer player = playerReference.get();
+
+      if (player == null || player.mediaPlayer == null || !player.mediaPlayer.isPlaying()) {
+        return;
+      }
+
+      Pair<Double, Integer> progress = player.getProgress();
+      player.notifyOnProgress(progress.first, progress.second);
+      sendEmptyMessageDelayed(0, 50);
+    }
+  }
+
+}

src/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -34,6 +34,8 @@ import android.view.animation.Animation;
 import android.widget.Toast;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.components.AudioView;
+import org.thoughtcrime.securesms.components.RemovableMediaView;
 import org.thoughtcrime.securesms.components.ThumbnailView;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.providers.CaptureProvider;
@@ -43,24 +45,29 @@ import org.thoughtcrime.securesms.util.MediaUtil;
 import java.io.IOException;
 
 public class AttachmentManager {
+
   private final static String TAG = AttachmentManager.class.getSimpleName();
 
-  private final Context            context;
-  private final View               attachmentView;
-  private final ThumbnailView      thumbnail;
-  private final SlideDeck          slideDeck;
-  private final AttachmentListener attachmentListener;
+  private final @NonNull Context            context;
+  private final @NonNull View               attachmentView;
+  private final @NonNull RemovableMediaView removableMediaView;
+  private final @NonNull ThumbnailView      thumbnail;
+  private final @NonNull AudioView          audioView;
+  private final @NonNull SlideDeck          slideDeck;
+  private final @NonNull AttachmentListener attachmentListener;
 
   private Uri captureUri;
 
-  public AttachmentManager(Activity view, AttachmentListener listener) {
+  public AttachmentManager(@NonNull Activity view, @NonNull AttachmentListener listener) {
     this.attachmentView     = view.findViewById(R.id.attachment_editor);
     this.thumbnail          = (ThumbnailView) view.findViewById(R.id.attachment_thumbnail);
+    this.audioView          = (AudioView) view.findViewById(R.id.attachment_audio);
+    this.removableMediaView = (RemovableMediaView) view.findViewById(R.id.removable_media_view);
     this.slideDeck          = new SlideDeck();
     this.context            = view;
     this.attachmentListener = listener;
 
-    thumbnail.setRemoveClickListener(new RemoveButtonListener());
+    removableMediaView.setRemoveClickListener(new RemoveButtonListener());
   }
 
   public void clear() {
@@ -81,6 +88,7 @@ public class AttachmentManager {
     });
 
     attachmentView.startAnimation(animation);
+    audioView.cleanup();
   }
 
   public void cleanup() {
@@ -135,7 +143,15 @@ public class AttachmentManager {
         } else {
           slideDeck.addSlide(slide);
           attachmentView.setVisibility(View.VISIBLE);
-          thumbnail.setImageResource(masterSecret, slide, false, true);
+
+          if (slide.hasAudio()) {
+            audioView.setAudio(masterSecret, (AudioSlide)slide, false);
+            removableMediaView.display(audioView);
+          } else {
+            thumbnail.setImageResource(masterSecret, slide, false);
+            removableMediaView.display(thumbnail);
+          }
+
           attachmentListener.onAttachmentChanged();
         }
       }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4270
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/15c6f18750a78474fe5ed2084070a2a60693a353

src/org/smssecure/smssecure/audio/AudioSlidePlayer.java
@@ -1,0 +1,237 @@
+package org.smssecure.smssecure.audio;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.util.Pair;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.mms.AudioSlide;
+import org.smssecure.smssecure.util.Util;
+import org.whispersystems.libaxolotl.util.guava.Optional;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+public class AudioSlidePlayer {
+
+  private static final String TAG = AudioSlidePlayer.class.getSimpleName();
+
+  private static @NonNull Optional<AudioSlidePlayer> playing = Optional.absent();
+
+  private final @NonNull Context      context;
+  private final @NonNull MasterSecret masterSecret;
+  private final @NonNull AudioSlide   slide;
+  private final @NonNull Handler      progressEventHandler;
+
+  private @NonNull  WeakReference<Listener> listener;
+  private @Nullable MediaPlayer             mediaPlayer;
+  private @Nullable AudioAttachmentServer   audioAttachmentServer;
+
+  public synchronized static AudioSlidePlayer createFor(@NonNull Context context,
+                                                        @NonNull MasterSecret masterSecret,
+                                                        @NonNull AudioSlide slide,
+                                                        @NonNull Listener listener)
+  {
+    if (playing.isPresent() && playing.get().getAudioSlide().equals(slide)) {
+      playing.get().setListener(listener);
+      return playing.get();
+    } else {
+      return new AudioSlidePlayer(context, masterSecret, slide, listener);
+    }
+  }
+
+  private AudioSlidePlayer(@NonNull Context context,
+                           @NonNull MasterSecret masterSecret,
+                           @NonNull AudioSlide slide,
+                           @NonNull Listener listener)
+  {
+    this.context              = context;
+    this.masterSecret         = masterSecret;
+    this.slide                = slide;
+    this.listener             = new WeakReference<>(listener);
+    this.progressEventHandler = new ProgressEventHandler(this);
+  }
+
+  public void play(final double progress) throws IOException {
+    if (this.mediaPlayer != null) return;
+
+    this.mediaPlayer           = new MediaPlayer();
+    this.audioAttachmentServer = new AudioAttachmentServer(context, masterSecret, slide.asAttachment());
+
+    audioAttachmentServer.start();
+
+    mediaPlayer.setDataSource(context, audioAttachmentServer.getUri());
+    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+      @Override
+      public void onPrepared(MediaPlayer mp) {
+        Log.w(TAG, "onPrepared");
+        if (progress > 0) {
+          mediaPlayer.seekTo((int)(mediaPlayer.getDuration() * progress));
+        }
+
+        mediaPlayer.start();
+
+        notifyOnStart();
+        setPlaying(AudioSlidePlayer.this);
+        progressEventHandler.sendEmptyMessage(0);
+      }
+    });
+
+    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+      @Override
+      public void onCompletion(MediaPlayer mp) {
+        Log.w(TAG, "onComplete");
+        mediaPlayer = null;
+        audioAttachmentServer.stop();
+        audioAttachmentServer = null;
+
+        notifyOnStop();
+        progressEventHandler.removeMessages(0);
+      }
+    });
+
+    mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+      @Override
+      public boolean onError(MediaPlayer mp, int what, int extra) {
+        Log.w(TAG, "MediaPlayer Error: " + what + " , " + extra);
+        notifyOnStop();
+        return true;
+      }
+    });
+
+    mediaPlayer.prepareAsync();
+  }
+
+  public void stop() {
+    Log.w(TAG, "Stop called!");
+    shutdown();
+  }
+
+  public void setListener(@NonNull Listener listener) {
+    this.listener = new WeakReference<>(listener);
+
+    if (this.mediaPlayer != null && this.mediaPlayer.isPlaying()) {
+      notifyOnStart();
+    }
+  }
+
+  public @NonNull AudioSlide getAudioSlide() {
+    return slide;
+  }
+
+  private void shutdown() {
+    removePlaying(this);
+
+    if (this.mediaPlayer != null) {
+      this.mediaPlayer.stop();
+    }
+
+    if (this.audioAttachmentServer != null) {
+      this.audioAttachmentServer.stop();
+    }
+
+    this.mediaPlayer           = null;
+    this.audioAttachmentServer = null;
+  }
+
+  private Pair<Double, Integer> getProgress() {
+    if (mediaPlayer == null || mediaPlayer.getCurrentPosition() <= 0 || mediaPlayer.getDuration() <= 0) {
+      return new Pair<>(0D, 0);
+    } else {
+      return new Pair<>((double) mediaPlayer.getCurrentPosition() / (double) mediaPlayer.getDuration(),
+                        mediaPlayer.getCurrentPosition());
+    }
+  }
+
+  private void notifyOnStart() {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        getListener().onStart();
+      }
+    });
+  }
+
+  private void notifyOnStop() {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        getListener().onStop();
+      }
+    });
+  }
+
+  private void notifyOnProgress(final double progress, final long millis) {
+    Util.runOnMain(new Runnable() {
+      @Override
+      public void run() {
+        getListener().onProgress(progress, millis);
+      }
+    });
+  }
+
+  private @NonNull Listener getListener() {
+    Listener listener = this.listener.get();
+
+    if (listener != null) return listener;
+    else                  return new Listener() {
+      @Override
+      public void onStart() {}
+      @Override
+      public void onStop() {}
+      @Override
+      public void onProgress(double progress, long millis) {}
+    };
+  }
+
+  private synchronized static void setPlaying(@NonNull AudioSlidePlayer player) {
+    if (playing.isPresent() && playing.get() != player) {
+      playing.get().notifyOnStop();
+      playing.get().stop();
+    }
+
+    playing = Optional.of(player);
+  }
+
+  private synchronized static void removePlaying(@NonNull AudioSlidePlayer player) {
+    if (playing.isPresent() && playing.get() == player) {
+      playing = Optional.absent();
+    }
+  }
+
+  public interface Listener {
+    public void onStart();
+    public void onStop();
+    public void onProgress(double progress, long millis);
+  }
+
+  private static class ProgressEventHandler extends Handler {
+
+    private final WeakReference<AudioSlidePlayer> playerReference;
+
+    private ProgressEventHandler(@NonNull AudioSlidePlayer player) {
+      this.playerReference = new WeakReference<>(player);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+      AudioSlidePlayer player = playerReference.get();
+
+      if (player == null || player.mediaPlayer == null || !player.mediaPlayer.isPlaying()) {
+        return;
+      }
+
+      Pair<Double, Integer> progress = player.getProgress();
+      player.notifyOnProgress(progress.first, progress.second);
+      sendEmptyMessageDelayed(0, 50);
+    }
+  }
+
+}

src/org/smssecure/smssecure/mms/AttachmentManager.java
@@ -36,6 +36,8 @@ import android.widget.Toast;
 import junit.framework.Assert;
 
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.components.AudioView;
+import org.smssecure.smssecure.components.RemovableMediaView;
 import org.smssecure.smssecure.components.ThumbnailView;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.providers.CaptureProvider;
@@ -45,24 +47,29 @@ import org.smssecure.smssecure.util.MediaUtil;
 import java.io.IOException;
 
 public class AttachmentManager {
+
   private final static String TAG = AttachmentManager.class.getSimpleName();
 
-  private final Context            context;
-  private final View               attachmentView;
-  private final ThumbnailView      thumbnail;
-  private final SlideDeck          slideDeck;
-  private final AttachmentListener attachmentListener;
+  private final @NonNull Context            context;
+  private final @NonNull View               attachmentView;
+  private final @NonNull RemovableMediaView removableMediaView;
+  private final @NonNull ThumbnailView      thumbnail;
+  private final @NonNull AudioView          audioView;
+  private final @NonNull SlideDeck          slideDeck;
+  private final @NonNull AttachmentListener attachmentListener;
 
   private Uri captureUri;
 
-  public AttachmentManager(Activity view, AttachmentListener listener) {
+  public AttachmentManager(@NonNull Activity view, @NonNull AttachmentListener listener) {
     this.attachmentView     = view.findViewById(R.id.attachment_editor);
     this.thumbnail          = (ThumbnailView) view.findViewById(R.id.attachment_thumbnail);
+    this.audioView          = (AudioView) view.findViewById(R.id.attachment_audio);
+    this.removableMediaView = (RemovableMediaView) view.findViewById(R.id.removable_media_view);
     this.slideDeck          = new SlideDeck();
     this.context            = view;
     this.attachmentListener = listener;
 
-    thumbnail.setRemoveClickListener(new RemoveButtonListener());
+    removableMediaView.setRemoveClickListener(new RemoveButtonListener());
   }
 
   public void clear() {
@@ -80,6 +87,7 @@ public class AttachmentManager {
     });
 
     attachmentView.startAnimation(animation);
+    audioView.cleanup();
   }
 
   public void cleanup() {
@@ -131,7 +139,15 @@ public class AttachmentManager {
         } else {
           slideDeck.addSlide(slide);
           attachmentView.setVisibility(View.VISIBLE);
-          thumbnail.setImageResource(masterSecret, slide, false, true);
+
+          if (slide.hasAudio()) {
+            audioView.setAudio(masterSecret, (AudioSlide)slide, false);
+            removableMediaView.display(audioView);
+          } else {
+            thumbnail.setImageResource(masterSecret, slide, false);
+            removableMediaView.display(thumbnail);
+          }
+
           attachmentListener.onAttachmentChanged();
         }
       }