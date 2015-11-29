SamSprung/Signal-Android

Refactor "parts" to contain MMS/PDU madness to MMS code paths.
Closes #4248
// FREEBIE

src/org/thoughtcrime/securesms/attachments/UriAttachment.java
@@ -1,0 +1,42 @@
+package org.thoughtcrime.securesms.attachments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.util.MediaUtil;
+import org.whispersystems.libaxolotl.util.guava.Optional;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UriAttachment extends Attachment {
+
+  private final Uri dataUri;
+  private final Uri thumbnailUri;
+
+  public UriAttachment(Uri uri, String contentType, int transferState, long size) {
+    this(uri, uri, contentType, transferState, size);
+  }
+
+  public UriAttachment(Uri dataUri, Uri thumbnailUri,
+                       String contentType, int transferState, long size)
+  {
+    super(contentType, transferState, size, null, null, null);
+    this.dataUri      = dataUri;
+    this.thumbnailUri = thumbnailUri;
+  }
+
+  @Override
+  @NonNull
+  public Uri getDataUri() {
+    return dataUri;
+  }
+
+  @Override
+  @NonNull
+  public Uri getThumbnailUri() {
+    return thumbnailUri;
+  }
+}

src/org/thoughtcrime/securesms/mms/AudioSlide.java
@@ -21,8 +21,10 @@ import android.content.res.Resources.Theme;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.attachments.Attachment;
 import org.thoughtcrime.securesms.util.ResUtil;
 
 import java.io.IOException;
@@ -33,11 +35,22 @@ import ws.com.google.android.mms.pdu.PduPart;
 public class AudioSlide extends Slide {
 
   public AudioSlide(Context context, Uri uri, long dataSize) throws IOException {
-    super(context, constructPartFromUri(context, uri, ContentType.AUDIO_UNSPECIFIED, dataSize));
+    super(context, constructAttachmentFromUri(context, uri, ContentType.AUDIO_UNSPECIFIED, dataSize));
   }
 
-  public AudioSlide(Context context, PduPart part) {
-    super(context, part);
+  public AudioSlide(Context context, Attachment attachment) {
+    super(context, attachment);
+  }
+
+  @Override
+  @Nullable
+  public Uri getThumbnailUri() {
+    return null;
+  }
+
+  @Override
+  public boolean hasPlaceholder() {
+    return true;
   }
 
   @Override
@@ -50,7 +63,9 @@ public class AudioSlide extends Slide {
     return true;
   }
 
-  @NonNull @Override public String getContentDescription() {
+  @NonNull
+  @Override
+  public String getContentDescription() {
     return context.getString(R.string.Slide_audio);
   }
 

src/org/thoughtcrime/securesms/mms/ImageSlide.java
@@ -23,32 +23,22 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.attachments.Attachment;
 
 import java.io.IOException;
 
 import ws.com.google.android.mms.ContentType;
-import ws.com.google.android.mms.pdu.PduPart;
 
 public class ImageSlide extends Slide {
+
   private static final String TAG = ImageSlide.class.getSimpleName();
 
-  public ImageSlide(Context context, PduPart part) {
-    super(context, part);
+  public ImageSlide(@NonNull Context context, @NonNull Attachment attachment) {
+    super(context, attachment);
   }
 
   public ImageSlide(Context context, Uri uri, long size) throws IOException {
-    super(context, constructPartFromUri(context, uri, ContentType.IMAGE_JPEG, size));
-  }
-
-  @Override
-  public Uri getThumbnailUri() {
-    if (getPart().getDataUri() != null) {
-      return isDraft()
-             ? getPart().getDataUri()
-             : PartAuthority.getThumbnailUri(getPart().getPartId());
-    }
-
-    return null;
+    super(context, constructAttachmentFromUri(context, uri, ContentType.IMAGE_JPEG, size));
   }
 
   @Override

src/org/thoughtcrime/securesms/mms/IncomingMediaMessage.java
@@ -1,38 +1,42 @@
 package org.thoughtcrime.securesms.mms;
 
-import android.text.TextUtils;
-import android.util.Log;
-
+import org.thoughtcrime.securesms.attachments.Attachment;
+import org.thoughtcrime.securesms.attachments.PointerAttachment;
 import org.thoughtcrime.securesms.crypto.MasterSecretUnion;
-import org.thoughtcrime.securesms.crypto.MediaKey;
-import org.thoughtcrime.securesms.database.PartDatabase;
+import org.thoughtcrime.securesms.database.MmsAddresses;
 import org.thoughtcrime.securesms.util.GroupUtil;
-import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
 import org.whispersystems.textsecure.api.messages.TextSecureAttachment;
 import org.whispersystems.textsecure.api.messages.TextSecureGroup;
 
+import java.util.LinkedList;
 import java.util.List;
-
-import ws.com.google.android.mms.pdu.CharacterSets;
-import ws.com.google.android.mms.pdu.EncodedStringValue;
-import ws.com.google.android.mms.pdu.PduBody;
-import ws.com.google.android.mms.pdu.PduHeaders;
-import ws.com.google.android.mms.pdu.PduPart;
-import ws.com.google.android.mms.pdu.RetrieveConf;
 
 public class IncomingMediaMessage {
 
-  private final PduHeaders headers;
-  private final PduBody    body;
-  private final String     groupId;
-  private final boolean    push;
+  private final String  from;
+  private final String  body;
+  private final String  groupId;
+  private final boolean push;
+  private final long    sentTimeMillis;
 
-  public IncomingMediaMessage(RetrieveConf retrieved) {
-    this.headers = retrieved.getPduHeaders();
-    this.body    = retrieved.getBody();
-    this.groupId = null;
-    this.push    = false;
+  private final List<String>     to          = new LinkedList<>();
+  private final List<String>     cc          = new LinkedList<>();
+  private final List<Attachment> attachments = new LinkedList<>();
+
+  public IncomingMediaMessage(String from, List<String> to, List<String> cc,
+                              String body, long sentTimeMillis,
+                              List<Attachment> attachments)
+  {
+    this.from           = from;
+    this.sentTimeMillis = sentTimeMillis;
+    this.body           = body;
+    this.groupId        = null;
+    this.push           = false;
+
+    this.to.addAll(to);
+    this.cc.addAll(cc);
+    this.attachments.addAll(attachments);
   }
 
   public IncomingMediaMessage(MasterSecretUnion masterSecret,
@@ -44,57 +48,28 @@ public class IncomingMediaMessage {
                               Optional<TextSecureGroup> group,
                               Optional<List<TextSecureAttachment>> attachments)
   {
-    this.headers = new PduHeaders();
-    this.body    = new PduBody();
-    this.push    = true;
+    this.push           = true;
+    this.from           = from;
+    this.sentTimeMillis = sentTimeMillis;
+    this.body           = body.orNull();
 
-    if (group.isPresent()) {
-      this.groupId = GroupUtil.getEncodedId(group.get().getGroupId());
-    } else {
-      this.groupId = null;
-    }
+    if (group.isPresent()) this.groupId = GroupUtil.getEncodedId(group.get().getGroupId());
+    else                   this.groupId = null;
 
-    this.headers.setEncodedStringValue(new EncodedStringValue(from), PduHeaders.FROM);
-    this.headers.appendEncodedStringValue(new EncodedStringValue(to), PduHeaders.TO);
-    this.headers.setLongInteger(sentTimeMillis / 1000, PduHeaders.DATE);
-
-
-    if (body.isPresent() && !TextUtils.isEmpty(body.get())) {
-      PduPart text = new PduPart();
-      text.setData(Util.toUtf8Bytes(body.get()));
-      text.setContentType(Util.toIsoBytes("text/plain"));
-      text.setCharset(CharacterSets.UTF_8);
-      this.body.addPart(text);
-    }
-
-    if (attachments.isPresent()) {
-      for (TextSecureAttachment attachment : attachments.get()) {
-        if (attachment.isPointer()) {
-          PduPart media        = new PduPart();
-          String  encryptedKey = MediaKey.getEncrypted(masterSecret, attachment.asPointer().getKey());
-
-          media.setContentType(Util.toIsoBytes(attachment.getContentType()));
-          media.setContentLocation(Util.toIsoBytes(String.valueOf(attachment.asPointer().getId())));
-          media.setContentDisposition(Util.toIsoBytes(encryptedKey));
-
-          if (relay.isPresent()) {
-            media.setName(Util.toIsoBytes(relay.get()));
-          }
-
-          media.setTransferProgress(PartDatabase.TRANSFER_PROGRESS_AUTO_PENDING);
-
-          this.body.addPart(media);
-        }
-      }
-    }
+    this.to.add(to);
+    this.attachments.addAll(PointerAttachment.forPointers(masterSecret, attachments));
   }
 
-  public PduHeaders getPduHeaders() {
-    return headers;
-  }
-
-  public PduBody getBody() {
+  public String getBody() {
     return body;
+  }
+
+  public MmsAddresses getAddresses() {
+    return new MmsAddresses(from, to, cc, new LinkedList<String>());
+  }
+
+  public List<Attachment> getAttachments() {
+    return attachments;
   }
 
   public String getGroupId() {
@@ -105,10 +80,11 @@ public class IncomingMediaMessage {
     return push;
   }
 
+  public long getSentTimeMillis() {
+    return sentTimeMillis;
+  }
+
   public boolean isGroupMessage() {
-    return groupId != null                                           ||
-        !Util.isEmpty(headers.getEncodedStringValues(PduHeaders.CC)) ||
-        (headers.getEncodedStringValues(PduHeaders.TO) != null &&
-         headers.getEncodedStringValues(PduHeaders.TO).length > 1);
+    return groupId != null || to.size() > 1 || cc.size() > 0;
   }
 }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4248
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/09e52834a6cc080a2234dbf01e713e12222b467c

src/org/smssecure/smssecure/attachments/UriAttachment.java
@@ -1,0 +1,42 @@
+package org.smssecure.smssecure.attachments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.MediaUtil;
+import org.whispersystems.libaxolotl.util.guava.Optional;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UriAttachment extends Attachment {
+
+  private final Uri dataUri;
+  private final Uri thumbnailUri;
+
+  public UriAttachment(Uri uri, String contentType, int transferState, long size) {
+    this(uri, uri, contentType, transferState, size);
+  }
+
+  public UriAttachment(Uri dataUri, Uri thumbnailUri,
+                       String contentType, int transferState, long size)
+  {
+    super(contentType, transferState, size, null, null, null);
+    this.dataUri      = dataUri;
+    this.thumbnailUri = thumbnailUri;
+  }
+
+  @Override
+  @NonNull
+  public Uri getDataUri() {
+    return dataUri;
+  }
+
+  @Override
+  @NonNull
+  public Uri getThumbnailUri() {
+    return thumbnailUri;
+  }
+}

src/org/smssecure/smssecure/mms/AudioSlide.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,8 +21,10 @@ import android.content.res.Resources.Theme;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.attachments.Attachment;
 import org.smssecure.smssecure.util.ResUtil;
 
 import java.io.IOException;
@@ -33,11 +35,22 @@ import ws.com.google.android.mms.pdu.PduPart;
 public class AudioSlide extends Slide {
 
   public AudioSlide(Context context, Uri uri, long dataSize) throws IOException {
-    super(context, constructPartFromUri(context, uri, ContentType.AUDIO_UNSPECIFIED, dataSize));
+    super(context, constructAttachmentFromUri(context, uri, ContentType.AUDIO_UNSPECIFIED, dataSize));
   }
 
-  public AudioSlide(Context context, PduPart part) {
-    super(context, part);
+  public AudioSlide(Context context, Attachment attachment) {
+    super(context, attachment);
+  }
+
+  @Override
+  @Nullable
+  public Uri getThumbnailUri() {
+    return null;
+  }
+
+  @Override
+  public boolean hasPlaceholder() {
+    return true;
   }
 
   @Override
@@ -50,7 +63,9 @@ public class AudioSlide extends Slide {
     return true;
   }
 
-  @NonNull @Override public String getContentDescription() {
+  @NonNull
+  @Override
+  public String getContentDescription() {
     return context.getString(R.string.Slide_audio);
   }
 

src/org/smssecure/smssecure/mms/ImageSlide.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,33 +22,23 @@ import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 
+import org.smssecure.smssecure.attachments.Attachment;
 import org.smssecure.smssecure.R;
 
 import java.io.IOException;
 
 import ws.com.google.android.mms.ContentType;
-import ws.com.google.android.mms.pdu.PduPart;
 
 public class ImageSlide extends Slide {
+
   private static final String TAG = ImageSlide.class.getSimpleName();
 
-  public ImageSlide(Context context, PduPart part) {
-    super(context, part);
+  public ImageSlide(@NonNull Context context, @NonNull Attachment attachment) {
+    super(context, attachment);
   }
 
   public ImageSlide(Context context, Uri uri, long size) throws IOException {
-    super(context, constructPartFromUri(context, uri, ContentType.IMAGE_JPEG, size));
-  }
-
-  @Override
-  public Uri getThumbnailUri() {
-    if (getPart().getDataUri() != null) {
-      return isDraft()
-             ? getPart().getDataUri()
-             : PartAuthority.getThumbnailUri(getPart().getPartId());
-    }
-
-    return null;
+    super(context, constructAttachmentFromUri(context, uri, ContentType.IMAGE_JPEG, size));
   }
 
   @Override

src/org/smssecure/smssecure/mms/IncomingMediaMessage.java
@@ -1,78 +1,53 @@
 package org.smssecure.smssecure.mms;
 
-import android.text.TextUtils;
-import android.util.Log;
-
+import org.smssecure.smssecure.attachments.Attachment;
 import org.smssecure.smssecure.crypto.MasterCipher;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.util.Base64;
-import org.smssecure.smssecure.database.PartDatabase;
+import org.smssecure.smssecure.database.MmsAddresses;
 import org.smssecure.smssecure.util.GroupUtil;
-import org.smssecure.smssecure.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
 
+import java.util.LinkedList;
 import java.util.List;
-
-import ws.com.google.android.mms.pdu.CharacterSets;
-import ws.com.google.android.mms.pdu.EncodedStringValue;
-import ws.com.google.android.mms.pdu.PduBody;
-import ws.com.google.android.mms.pdu.PduHeaders;
-import ws.com.google.android.mms.pdu.PduPart;
-import ws.com.google.android.mms.pdu.RetrieveConf;
 
 public class IncomingMediaMessage {
 
-  private final PduHeaders headers;
-  private final PduBody    body;
-  private final String     groupId;
-  private final boolean    push;
+  private final String  from;
+  private final String  body;
+  private final String  groupId;
+  private final boolean push;
+  private final long    sentTimeMillis;
 
-  public IncomingMediaMessage(RetrieveConf retrieved) {
-    this.headers = retrieved.getPduHeaders();
-    this.body    = retrieved.getBody();
-    this.groupId = null;
-    this.push    = false;
-  }
+  private final List<String>     to          = new LinkedList<>();
+  private final List<String>     cc          = new LinkedList<>();
+  private final List<Attachment> attachments = new LinkedList<>();
 
-  public IncomingMediaMessage(RetrieveConf retrieved, PduHeaders originalHeaders) {
-    this.headers = originalHeaders;
-    this.body    = retrieved.getBody();
-    this.groupId = null;
-    this.push    = false;
-  }
-
-  public IncomingMediaMessage(MasterSecret masterSecret,
-                              String from,
-                              String to,
-                              long sentTimeMillis,
-                              Optional<String> relay,
-                              Optional<String> body)
+  public IncomingMediaMessage(String from, List<String> to, List<String> cc,
+                              String body, long sentTimeMillis,
+                              List<Attachment> attachments)
   {
-    this.headers = new PduHeaders();
-    this.body    = new PduBody();
-    this.push    = true;
-    this.groupId = null;
+    this.from           = from;
+    this.sentTimeMillis = sentTimeMillis;
+    this.body           = body;
+    this.groupId        = null;
+    this.push           = false;
 
-    this.headers.setEncodedStringValue(new EncodedStringValue(from), PduHeaders.FROM);
-    this.headers.appendEncodedStringValue(new EncodedStringValue(to), PduHeaders.TO);
-    this.headers.setLongInteger(sentTimeMillis / 1000, PduHeaders.DATE);
-
-
-    if (body.isPresent() && !TextUtils.isEmpty(body.get())) {
-      PduPart text = new PduPart();
-      text.setData(Util.toUtf8Bytes(body.get()));
-      text.setContentType(Util.toIsoBytes("text/plain"));
-      text.setCharset(CharacterSets.UTF_8);
-      this.body.addPart(text);
-    }
+    this.to.addAll(to);
+    this.cc.addAll(cc);
+    this.attachments.addAll(attachments);
   }
 
-  public PduHeaders getPduHeaders() {
-    return headers;
-  }
-
-  public PduBody getBody() {
+  public String getBody() {
     return body;
+  }
+
+  public MmsAddresses getAddresses() {
+    return new MmsAddresses(from, to, cc, new LinkedList<String>());
+  }
+
+  public List<Attachment> getAttachments() {
+    return attachments;
   }
 
   public String getGroupId() {
@@ -83,10 +58,11 @@ public class IncomingMediaMessage {
     return push;
   }
 
+  public long getSentTimeMillis() {
+    return sentTimeMillis;
+  }
+
   public boolean isGroupMessage() {
-    return groupId != null                                           ||
-        !Util.isEmpty(headers.getEncodedStringValues(PduHeaders.CC)) ||
-        (headers.getEncodedStringValues(PduHeaders.TO) != null &&
-         headers.getEncodedStringValues(PduHeaders.TO).length > 1);
+    return groupId != null || to.size() > 1 || cc.size() > 0;
   }
 }