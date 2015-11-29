SamSprung/Signal-Android

Display thumbnail previews for images in conversation list.
Closes #4262
// FREEBIE

src/org/thoughtcrime/securesms/ConversationListItem.java
@@ -25,12 +25,16 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Handler;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.thoughtcrime.securesms.components.AvatarImageView;
 import org.thoughtcrime.securesms.components.FromTextView;
+import org.thoughtcrime.securesms.components.ThumbnailView;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.model.ThreadRecord;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.DateUtils;
@@ -64,6 +68,7 @@ public class ConversationListItem extends RelativeLayout
   private TextView        dateView;
   private boolean         read;
   private AvatarImageView contactPhotoImage;
+  private ThumbnailView   thumbnailView;
 
   private final @DrawableRes int readBackground;
   private final @DrawableRes int unreadBackround;
@@ -88,9 +93,12 @@ public class ConversationListItem extends RelativeLayout
     this.fromView          = (FromTextView)    findViewById(R.id.from);
     this.dateView          = (TextView)        findViewById(R.id.date);
     this.contactPhotoImage = (AvatarImageView) findViewById(R.id.contact_photo_image);
+    this.thumbnailView     = (ThumbnailView)   findViewById(R.id.thumbnail);
   }
 
-  public void set(ThreadRecord thread, Locale locale, Set<Long> selectedThreads, boolean batchMode) {
+  public void set(@NonNull MasterSecret masterSecret, @NonNull ThreadRecord thread,
+                  @NonNull Locale locale, @NonNull Set<Long> selectedThreads, boolean batchMode)
+  {
     this.selectedThreads  = selectedThreads;
     this.recipients       = thread.getRecipients();
     this.threadId         = thread.getThreadId();
@@ -109,6 +117,7 @@ public class ConversationListItem extends RelativeLayout
       dateView.setTypeface(read ? LIGHT_TYPEFACE : BOLD_TYPEFACE);
     }
 
+    setThumbnailSnippet(masterSecret, thread);
     setBatchState(batchMode);
     setBackground(thread);
     setRippleColor(recipients);
@@ -134,6 +143,23 @@ public class ConversationListItem extends RelativeLayout
 
   public int getDistributionType() {
     return distributionType;
+  }
+
+  private void setThumbnailSnippet(MasterSecret masterSecret, ThreadRecord thread) {
+    if (thread.getSnippetUri() != null) {
+      this.thumbnailView.setVisibility(View.VISIBLE);
+      this.thumbnailView.setImageResource(masterSecret, thread.getSnippetUri());
+
+      LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
+      subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.thumbnail);
+      this.subjectView.setLayoutParams(subjectParams);
+    } else {
+      this.thumbnailView.setVisibility(View.GONE);
+
+      LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
+      subjectParams.addRule(RelativeLayout.LEFT_OF, 0);
+      this.subjectView.setLayoutParams(subjectParams);
+    }
   }
 
   private void setBackground(ThreadRecord thread) {

src/org/thoughtcrime/securesms/database/ThreadDatabase.java
@@ -22,6 +22,7 @@ import android.database.Cursor;
 import android.database.MergeCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
@@ -29,8 +30,11 @@ import android.util.Log;
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.crypto.MasterCipher;
 import org.thoughtcrime.securesms.database.model.DisplayRecord;
+import org.thoughtcrime.securesms.database.model.MediaMmsMessageRecord;
 import org.thoughtcrime.securesms.database.model.MessageRecord;
 import org.thoughtcrime.securesms.database.model.ThreadRecord;
+import org.thoughtcrime.securesms.mms.Slide;
+import org.thoughtcrime.securesms.mms.SlideDeck;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.Recipients;
@@ -45,6 +49,8 @@ import java.util.Set;
 
 public class ThreadDatabase extends Database {
 
+  private static final String TAG = ThreadDatabase.class.getSimpleName();
+
           static final String TABLE_NAME      = "thread";
   public  static final String ID              = "_id";
   public  static final String DATE            = "date";
@@ -55,14 +61,14 @@ public class ThreadDatabase extends Database {
   public  static final String READ            = "read";
   private static final String TYPE            = "type";
   private static final String ERROR           = "error";
-  private static final String HAS_ATTACHMENT  = "has_attachment";
   public  static final String SNIPPET_TYPE    = "snippet_type";
+  private static final String SNIPPET_URI     = "snippet_uri";
 
   public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY, "                             +
     DATE + " INTEGER DEFAULT 0, " + MESSAGE_COUNT + " INTEGER DEFAULT 0, "                         +
     RECIPIENT_IDS + " TEXT, " + SNIPPET + " TEXT, " + SNIPPET_CHARSET + " INTEGER DEFAULT 0, "     +
     READ + " INTEGER DEFAULT 1, " + TYPE + " INTEGER DEFAULT 0, " + ERROR + " INTEGER DEFAULT 0, " +
-    SNIPPET_TYPE + " INTEGER DEFAULT 0);";
+    SNIPPET_TYPE + " INTEGER DEFAULT 0, " + SNIPPET_URI + " TEXT DEFAULT NULL);";
 
   public static final String[] CREATE_INDEXS = {
     "CREATE INDEX IF NOT EXISTS thread_recipient_ids_index ON " + TABLE_NAME + " (" + RECIPIENT_IDS + ");",
@@ -73,7 +79,7 @@ public class ThreadDatabase extends Database {
   }
 
   private long[] getRecipientIds(Recipients recipients) {
-    Set<Long>       recipientSet  = new HashSet<Long>();
+    Set<Long>       recipientSet  = new HashSet<>();
     List<Recipient> recipientList = recipients.getRecipientsList();
 
     for (Recipient recipient : recipientList) {
@@ -118,12 +124,13 @@ public class ThreadDatabase extends Database {
     return db.insert(TABLE_NAME, null, contentValues);
   }
 
-  private void updateThread(long threadId, long count, String body, long date, long type)
+  private void updateThread(long threadId, long count, String body, @Nullable Uri attachment, long date, long type)
   {
     ContentValues contentValues = new ContentValues(4);
     contentValues.put(DATE, date - date % 1000);
     contentValues.put(MESSAGE_COUNT, count);
     contentValues.put(SNIPPET, body);
+    contentValues.put(SNIPPET_URI, attachment == null ? null : attachment.toString());
     contentValues.put(SNIPPET_TYPE, type);
 
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
@@ -131,12 +138,13 @@ public class ThreadDatabase extends Database {
     notifyConversationListListeners();
   }
 
-  public void updateSnippet(long threadId, String snippet, long date, long type) {
+  public void updateSnippet(long threadId, String snippet, @Nullable Uri attachment, long date, long type) {
     ContentValues contentValues = new ContentValues(3);
 
     contentValues.put(DATE, date - date % 1000);
     contentValues.put(SNIPPET, snippet);
     contentValues.put(SNIPPET_TYPE, type);
+    contentValues.put(SNIPPET_URI, attachment == null ? null : attachment.toString());
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
     db.update(TABLE_NAME, contentValues, ID + " = ?", new String[] {threadId + ""});
     notifyConversationListListeners();
@@ -144,7 +152,7 @@ public class ThreadDatabase extends Database {
 
   private void deleteThread(long threadId) {
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
-    db.delete(TABLE_NAME, ID_WHERE, new String[] {threadId+""});
+    db.delete(TABLE_NAME, ID_WHERE, new String[] {threadId + ""});
     notifyConversationListListeners();
   }
 
@@ -247,7 +255,7 @@ public class ThreadDatabase extends Database {
     contentValues.put(READ, 0);
 
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
-    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId+""});
+    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId + ""});
     notifyConversationListListeners();
   }
 
@@ -256,7 +264,7 @@ public class ThreadDatabase extends Database {
     contentValues.put(TYPE, distributionType);
 
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
-    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId+""});
+    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId + ""});
     notifyConversationListListeners();
   }
 
@@ -413,7 +421,7 @@ public class ThreadDatabase extends Database {
         if (record.isPush()) timestamp = record.getDateSent();
         else                 timestamp = record.getDateReceived();
 
-        updateThread(threadId, count, record.getBody().getBody(), timestamp, record.getType());
+        updateThread(threadId, count, record.getBody().getBody(), getAttachmentUriFor(record), timestamp, record.getType());
         notifyConversationListListeners();
         return false;
       } else {
@@ -425,6 +433,15 @@ public class ThreadDatabase extends Database {
       if (reader != null)
         reader.close();
     }
+  }
+
+  private @Nullable Uri getAttachmentUriFor(MessageRecord record) {
+    if (!record.isMms() || record.isMmsNotification()) return null;
+
+    SlideDeck slideDeck = ((MediaMmsMessageRecord)record).getSlideDeck();
+    Slide     thumbnail = slideDeck.getThumbnailSlide();
+
+    return thumbnail != null ? thumbnail.getThumbnailUri() : null;
   }
 
   public static interface ProgressListener {
@@ -459,9 +476,9 @@ public class ThreadDatabase extends Database {
     }
 
     public ThreadRecord getCurrent() {
-      long threadId         = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.ID));
-      String recipientId    = cursor.getString(cursor.getColumnIndexOrThrow(ThreadDatabase.RECIPIENT_IDS));
-      Recipients recipients = RecipientFactory.getRecipientsForIds(context, recipientId, true);
+      long       threadId    = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.ID));
+      String     recipientId = cursor.getString(cursor.getColumnIndexOrThrow(ThreadDatabase.RECIPIENT_IDS));
+      Recipients recipients  = RecipientFactory.getRecipientsForIds(context, recipientId, true);
 
       DisplayRecord.Body body = getPlaintextBody(cursor);
       long date               = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.DATE));
@@ -469,8 +486,9 @@ public class ThreadDatabase extends Database {
       long read               = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.READ));
       long type               = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.SNIPPET_TYPE));
       int distributionType    = cursor.getInt(cursor.getColumnIndexOrThrow(ThreadDatabase.TYPE));
+      Uri snippetUri          = getSnippetUri(cursor);
 
-      return new ThreadRecord(context, body, recipients, date, count,
+      return new ThreadRecord(context, body, snippetUri, recipients, date, count,
                               read == 1, threadId, type, distributionType);
     }
 
@@ -489,6 +507,19 @@ public class ThreadDatabase extends Database {
       } catch (InvalidMessageException e) {
         Log.w("ThreadDatabase", e);
         return new DisplayRecord.Body(context.getString(R.string.ThreadDatabase_error_decrypting_message), true);
+      }
+    }
+
+    private @Nullable Uri getSnippetUri(Cursor cursor) {
+      if (cursor.isNull(cursor.getColumnIndexOrThrow(ThreadDatabase.SNIPPET_URI))) {
+        return null;
+      }
+
+      try {
+        return Uri.parse(cursor.getString(cursor.getColumnIndexOrThrow(ThreadDatabase.SNIPPET_URI)));
+      } catch (IllegalArgumentException e) {
+        Log.w(TAG, e);
+        return null;
       }
     }
 

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4262
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/5111fe2e95b705ce25e041f1691665817c2f764a

src/org/smssecure/smssecure/ConversationListItem.java
@@ -25,12 +25,16 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Handler;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.smssecure.smssecure.components.AvatarImageView;
 import org.smssecure.smssecure.components.FromTextView;
+import org.smssecure.smssecure.components.ThumbnailView;
+import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.model.ThreadRecord;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.DateUtils;
@@ -64,6 +68,7 @@ public class ConversationListItem extends RelativeLayout
   private TextView        dateView;
   private boolean         read;
   private AvatarImageView contactPhotoImage;
+  private ThumbnailView   thumbnailView;
 
   private final @DrawableRes int readBackground;
   private final @DrawableRes int unreadBackround;
@@ -88,9 +93,12 @@ public class ConversationListItem extends RelativeLayout
     this.fromView          = (FromTextView)    findViewById(R.id.from);
     this.dateView          = (TextView)        findViewById(R.id.date);
     this.contactPhotoImage = (AvatarImageView) findViewById(R.id.contact_photo_image);
+    this.thumbnailView     = (ThumbnailView)   findViewById(R.id.thumbnail);
   }
 
-  public void set(ThreadRecord thread, Locale locale, Set<Long> selectedThreads, boolean batchMode) {
+  public void set(@NonNull MasterSecret masterSecret, @NonNull ThreadRecord thread,
+                  @NonNull Locale locale, @NonNull Set<Long> selectedThreads, boolean batchMode)
+  {
     this.selectedThreads  = selectedThreads;
     this.recipients       = thread.getRecipients();
     this.threadId         = thread.getThreadId();
@@ -109,6 +117,7 @@ public class ConversationListItem extends RelativeLayout
       dateView.setTypeface(read ? LIGHT_TYPEFACE : BOLD_TYPEFACE);
     }
 
+    setThumbnailSnippet(masterSecret, thread);
     setBatchState(batchMode);
     setBackground(thread);
     setRippleColor(recipients);
@@ -134,6 +143,23 @@ public class ConversationListItem extends RelativeLayout
 
   public int getDistributionType() {
     return distributionType;
+  }
+
+  private void setThumbnailSnippet(MasterSecret masterSecret, ThreadRecord thread) {
+    if (thread.getSnippetUri() != null) {
+      this.thumbnailView.setVisibility(View.VISIBLE);
+      this.thumbnailView.setImageResource(masterSecret, thread.getSnippetUri());
+
+      LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
+      subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.thumbnail);
+      this.subjectView.setLayoutParams(subjectParams);
+    } else {
+      this.thumbnailView.setVisibility(View.GONE);
+
+      LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
+      subjectParams.addRule(RelativeLayout.LEFT_OF, 0);
+      this.subjectView.setLayoutParams(subjectParams);
+    }
   }
 
   private void setBackground(ThreadRecord thread) {

src/org/smssecure/smssecure/database/ThreadDatabase.java
@@ -22,6 +22,7 @@ import android.database.Cursor;
 import android.database.MergeCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
@@ -29,8 +30,11 @@ import android.util.Log;
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.crypto.MasterCipher;
 import org.smssecure.smssecure.database.model.DisplayRecord;
+import org.smssecure.smssecure.database.model.MediaMmsMessageRecord;
 import org.smssecure.smssecure.database.model.MessageRecord;
 import org.smssecure.smssecure.database.model.ThreadRecord;
+import org.smssecure.smssecure.mms.Slide;
+import org.smssecure.smssecure.mms.SlideDeck;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.Recipients;
@@ -46,6 +50,8 @@ import java.util.Set;
 
 public class ThreadDatabase extends Database {
 
+  private static final String TAG = ThreadDatabase.class.getSimpleName();
+
           static final String TABLE_NAME      = "thread";
   public  static final String ID              = "_id";
   public  static final String DATE            = "date";
@@ -56,14 +62,14 @@ public class ThreadDatabase extends Database {
   public  static final String READ            = "read";
   private static final String TYPE            = "type";
   private static final String ERROR           = "error";
-  private static final String HAS_ATTACHMENT  = "has_attachment";
   public  static final String SNIPPET_TYPE    = "snippet_type";
+  private static final String SNIPPET_URI     = "snippet_uri";
 
   public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY, "                             +
     DATE + " INTEGER DEFAULT 0, " + MESSAGE_COUNT + " INTEGER DEFAULT 0, "                         +
     RECIPIENT_IDS + " TEXT, " + SNIPPET + " TEXT, " + SNIPPET_CHARSET + " INTEGER DEFAULT 0, "     +
     READ + " INTEGER DEFAULT 1, " + TYPE + " INTEGER DEFAULT 0, " + ERROR + " INTEGER DEFAULT 0, " +
-    SNIPPET_TYPE + " INTEGER DEFAULT 0);";
+    SNIPPET_TYPE + " INTEGER DEFAULT 0, " + SNIPPET_URI + " TEXT DEFAULT NULL);";
 
   public static final String[] CREATE_INDEXS = {
     "CREATE INDEX IF NOT EXISTS thread_recipient_ids_index ON " + TABLE_NAME + " (" + RECIPIENT_IDS + ");",
@@ -74,7 +80,7 @@ public class ThreadDatabase extends Database {
   }
 
   private long[] getRecipientIds(Recipients recipients) {
-    Set<Long>       recipientSet  = new HashSet<Long>();
+    Set<Long>       recipientSet  = new HashSet<>();
     List<Recipient> recipientList = recipients.getRecipientsList();
 
     for (Recipient recipient : recipientList) {
@@ -119,12 +125,13 @@ public class ThreadDatabase extends Database {
     return db.insert(TABLE_NAME, null, contentValues);
   }
 
-  private void updateThread(long threadId, long count, String body, long date, long type)
+  private void updateThread(long threadId, long count, String body, @Nullable Uri attachment, long date, long type)
   {
     ContentValues contentValues = new ContentValues(4);
     contentValues.put(DATE, date - date % 1000);
     contentValues.put(MESSAGE_COUNT, count);
     contentValues.put(SNIPPET, body);
+    contentValues.put(SNIPPET_URI, attachment == null ? null : attachment.toString());
     contentValues.put(SNIPPET_TYPE, type);
 
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
@@ -132,12 +139,13 @@ public class ThreadDatabase extends Database {
     notifyConversationListListeners();
   }
 
-  public void updateSnippet(long threadId, String snippet, long date, long type) {
+  public void updateSnippet(long threadId, String snippet, @Nullable Uri attachment, long date, long type) {
     ContentValues contentValues = new ContentValues(3);
 
     contentValues.put(DATE, date - date % 1000);
     contentValues.put(SNIPPET, snippet);
     contentValues.put(SNIPPET_TYPE, type);
+    contentValues.put(SNIPPET_URI, attachment == null ? null : attachment.toString());
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
     db.update(TABLE_NAME, contentValues, ID + " = ?", new String[] {threadId + ""});
     notifyConversationListListeners();
@@ -145,7 +153,7 @@ public class ThreadDatabase extends Database {
 
   private void deleteThread(long threadId) {
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
-    db.delete(TABLE_NAME, ID_WHERE, new String[] {threadId+""});
+    db.delete(TABLE_NAME, ID_WHERE, new String[] {threadId + ""});
     notifyConversationListListeners();
   }
 
@@ -248,7 +256,7 @@ public class ThreadDatabase extends Database {
     contentValues.put(READ, 0);
 
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
-    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId+""});
+    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId + ""});
     notifyConversationListListeners();
   }
 
@@ -257,7 +265,7 @@ public class ThreadDatabase extends Database {
     contentValues.put(TYPE, distributionType);
 
     SQLiteDatabase db = databaseHelper.getWritableDatabase();
-    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId+""});
+    db.update(TABLE_NAME, contentValues, ID_WHERE, new String[] {threadId + ""});
     notifyConversationListListeners();
   }
 
@@ -414,7 +422,7 @@ public class ThreadDatabase extends Database {
         if (SMSSecurePreferences.showSentTime(context)) timestamp = record.getDateSent();
         else                                            timestamp = record.getDateReceived();
 
-        updateThread(threadId, count, record.getBody().getBody(), timestamp, record.getType());
+        updateThread(threadId, count, record.getBody().getBody(), getAttachmentUriFor(record), timestamp, record.getType());
         notifyConversationListListeners();
         return false;
       } else {
@@ -426,6 +434,15 @@ public class ThreadDatabase extends Database {
       if (reader != null)
         reader.close();
     }
+  }
+
+  private @Nullable Uri getAttachmentUriFor(MessageRecord record) {
+    if (!record.isMms() || record.isMmsNotification()) return null;
+
+    SlideDeck slideDeck = ((MediaMmsMessageRecord)record).getSlideDeck();
+    Slide     thumbnail = slideDeck.getThumbnailSlide();
+
+    return thumbnail != null ? thumbnail.getThumbnailUri() : null;
   }
 
   public static interface ProgressListener {
@@ -460,9 +477,9 @@ public class ThreadDatabase extends Database {
     }
 
     public ThreadRecord getCurrent() {
-      long threadId         = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.ID));
-      String recipientId    = cursor.getString(cursor.getColumnIndexOrThrow(ThreadDatabase.RECIPIENT_IDS));
-      Recipients recipients = RecipientFactory.getRecipientsForIds(context, recipientId, true);
+      long       threadId    = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.ID));
+      String     recipientId = cursor.getString(cursor.getColumnIndexOrThrow(ThreadDatabase.RECIPIENT_IDS));
+      Recipients recipients  = RecipientFactory.getRecipientsForIds(context, recipientId, true);
 
       DisplayRecord.Body body = getPlaintextBody(cursor);
       long date               = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.DATE));
@@ -470,8 +487,9 @@ public class ThreadDatabase extends Database {
       long read               = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.READ));
       long type               = cursor.getLong(cursor.getColumnIndexOrThrow(ThreadDatabase.SNIPPET_TYPE));
       int distributionType    = cursor.getInt(cursor.getColumnIndexOrThrow(ThreadDatabase.TYPE));
+      Uri snippetUri          = getSnippetUri(cursor);
 
-      return new ThreadRecord(context, body, recipients, date, count,
+      return new ThreadRecord(context, body, snippetUri, recipients, date, count,
                               read == 1, threadId, type, distributionType);
     }
 
@@ -490,6 +508,19 @@ public class ThreadDatabase extends Database {
       } catch (InvalidMessageException e) {
         Log.w("ThreadDatabase", e);
         return new DisplayRecord.Body(context.getString(R.string.EncryptingSmsDatabase_error_decrypting_message), true);
+      }
+    }
+
+    private @Nullable Uri getSnippetUri(Cursor cursor) {
+      if (cursor.isNull(cursor.getColumnIndexOrThrow(ThreadDatabase.SNIPPET_URI))) {
+        return null;
+      }
+
+      try {
+        return Uri.parse(cursor.getString(cursor.getColumnIndexOrThrow(ThreadDatabase.SNIPPET_URI)));
+      } catch (IllegalArgumentException e) {
+        Log.w(TAG, e);
+        return null;
       }
     }
 