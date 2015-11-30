SamSprung/Signal-Android

unjankify incoming media sharing
Closes #4374
Fixes #3989
// FREEBIE

src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -16,7 +16,6 @@
  */
 package org.thoughtcrime.securesms;
 
-import android.annotation.TargetApi;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -98,7 +97,7 @@ import org.thoughtcrime.securesms.mms.OutgoingMediaMessage;
 import org.thoughtcrime.securesms.mms.OutgoingSecureMediaMessage;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.notifications.MessageNotifier;
-import org.thoughtcrime.securesms.providers.CaptureProvider;
+import org.thoughtcrime.securesms.providers.PersistentBlobProvider;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.RecipientFormattingException;
@@ -201,7 +200,6 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private DynamicTheme    dynamicTheme    = new DynamicTheme();
   private DynamicLanguage dynamicLanguage = new DynamicLanguage();
 
-  @TargetApi(Build.VERSION_CODES.KITKAT)
   @Override
   protected void onPreCreate() {
     dynamicTheme.onCreate(this);
@@ -311,13 +309,13 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     switch (reqCode) {
     case PICK_IMAGE:
       boolean isGif = MediaUtil.isGif(MediaUtil.getMimeType(this, data.getData()));
-      setMedia(data.getData(), isGif ? MediaType.GIF : MediaType.IMAGE, false);
+      setMedia(data.getData(), isGif ? MediaType.GIF : MediaType.IMAGE);
       break;
     case PICK_VIDEO:
-      setMedia(data.getData(), MediaType.VIDEO, false);
+      setMedia(data.getData(), MediaType.VIDEO);
       break;
     case PICK_AUDIO:
-      setMedia(data.getData(), MediaType.AUDIO, false);
+      setMedia(data.getData(), MediaType.AUDIO);
       break;
     case PICK_CONTACT_INFO:
       addAttachmentContactInfo(data.getData());
@@ -331,7 +329,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       break;
     case TAKE_PHOTO:
       if (attachmentManager.getCaptureUri() != null) {
-        setMedia(attachmentManager.getCaptureUri(), MediaType.IMAGE, true);
+        setMedia(attachmentManager.getCaptureUri(), MediaType.IMAGE);
       }
       break;
     }
@@ -717,9 +715,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     if (draftText != null)  composeText.setText(draftText);
 
-    if      (draftImage != null) setMedia(draftImage, MediaType.IMAGE, false);
-    else if (draftAudio != null) setMedia(draftAudio, MediaType.AUDIO, false);
-    else if (draftVideo != null) setMedia(draftVideo, MediaType.VIDEO, false);
+    if      (draftImage != null) setMedia(draftImage, MediaType.IMAGE);
+    else if (draftAudio != null) setMedia(draftAudio, MediaType.AUDIO);
+    else if (draftVideo != null) setMedia(draftVideo, MediaType.VIDEO);
 
     if (draftText == null && draftImage == null && draftAudio == null && draftVideo == null) {
       initializeDraftFromDatabase();
@@ -753,11 +751,11 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
           if (draft.getType().equals(Draft.TEXT)) {
             composeText.setText(draft.getValue());
           } else if (draft.getType().equals(Draft.IMAGE)) {
-            setMedia(Uri.parse(draft.getValue()), MediaType.IMAGE, false);
+            setMedia(Uri.parse(draft.getValue()), MediaType.IMAGE);
           } else if (draft.getType().equals(Draft.AUDIO)) {
-            setMedia(Uri.parse(draft.getValue()), MediaType.AUDIO, false);
+            setMedia(Uri.parse(draft.getValue()), MediaType.AUDIO);
           } else if (draft.getType().equals(Draft.VIDEO)) {
-            setMedia(Uri.parse(draft.getValue()), MediaType.VIDEO, false);
+            setMedia(Uri.parse(draft.getValue()), MediaType.VIDEO);
           }
         }
 
@@ -1012,8 +1010,8 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }
   }
 
-  private void setMedia(Uri uri, MediaType mediaType, boolean isCapture) {
-    attachmentManager.setMedia(masterSecret, uri, mediaType, getCurrentMediaConstraints(), isCapture);
+  private void setMedia(Uri uri, MediaType mediaType) {
+    attachmentManager.setMedia(masterSecret, uri, mediaType, getCurrentMediaConstraints());
   }
 
   private void addAttachmentContactInfo(Uri contactUri) {
@@ -1053,7 +1051,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       drafts.add(new Draft(Draft.TEXT, composeText.getText().toString()));
     }
 
-    for (Slide slide : attachmentManager.getSlideDeck().getSlides()) {
+    for (Slide slide : attachmentManager.buildSlideDeck().getSlides()) {
       if      (slide.hasAudio()) drafts.add(new Draft(Draft.AUDIO, slide.getUri().toString()));
       else if (slide.hasVideo()) drafts.add(new Draft(Draft.VIDEO, slide.getUri().toString()));
       else if (slide.hasImage()) drafts.add(new Draft(Draft.IMAGE, slide.getUri().toString()));
@@ -1263,7 +1261,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   {
     final Context context                = getApplicationContext();
     OutgoingMediaMessage outgoingMessage = new OutgoingMediaMessage(recipients,
-                                                                    attachmentManager.getSlideDeck(),
+                                                                    attachmentManager.buildSlideDeck(),
                                                                     getMessage(),
                                                                     System.currentTimeMillis(),
                                                                     distributionType);
@@ -1336,7 +1334,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
   @Override
   public void onImageCapture(@NonNull final byte[] imageBytes) {
-    setMedia(CaptureProvider.getInstance(this).create(masterSecret, recipients, imageBytes), MediaType.IMAGE, true);
+    setMedia(PersistentBlobProvider.getInstance(this).create(masterSecret, recipients, imageBytes), MediaType.IMAGE);
     quickAttachmentDrawer.hide(false);
   }
 

src/org/thoughtcrime/securesms/mms/PartAuthority.java
@@ -9,7 +9,7 @@ import android.support.annotation.NonNull;
 import org.thoughtcrime.securesms.attachments.AttachmentId;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
-import org.thoughtcrime.securesms.providers.CaptureProvider;
+import org.thoughtcrime.securesms.providers.PersistentBlobProvider;
 import org.thoughtcrime.securesms.providers.PartProvider;
 import org.thoughtcrime.securesms.providers.SingleUseBlobProvider;
 
@@ -25,7 +25,7 @@ public class PartAuthority {
 
   private static final int PART_ROW       = 1;
   private static final int THUMB_ROW      = 2;
-  private static final int CAPTURE_ROW    = 3;
+  private static final int PERSISTENT_ROW = 3;
   private static final int SINGLE_USE_ROW = 4;
 
   private static final UriMatcher uriMatcher;
@@ -34,7 +34,7 @@ public class PartAuthority {
     uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     uriMatcher.addURI("org.thoughtcrime.securesms", "part/*/#", PART_ROW);
     uriMatcher.addURI("org.thoughtcrime.securesms", "thumb/*/#", THUMB_ROW);
-    uriMatcher.addURI(CaptureProvider.AUTHORITY, CaptureProvider.EXPECTED_PATH, CAPTURE_ROW);
+    uriMatcher.addURI(PersistentBlobProvider.AUTHORITY, PersistentBlobProvider.EXPECTED_PATH, PERSISTENT_ROW);
     uriMatcher.addURI(SingleUseBlobProvider.AUTHORITY, SingleUseBlobProvider.PATH, SINGLE_USE_ROW);
   }
 
@@ -50,8 +50,8 @@ public class PartAuthority {
       case THUMB_ROW:
         partUri = new PartUriParser(uri);
         return DatabaseFactory.getAttachmentDatabase(context).getThumbnailStream(masterSecret, partUri.getPartId());
-      case CAPTURE_ROW:
-        return CaptureProvider.getInstance(context).getStream(masterSecret, ContentUris.parseId(uri));
+      case PERSISTENT_ROW:
+        return PersistentBlobProvider.getInstance(context).getStream(masterSecret, ContentUris.parseId(uri));
       case SINGLE_USE_ROW:
         return SingleUseBlobProvider.getInstance().getStream(ContentUris.parseId(uri));
       default:

src/org/thoughtcrime/securesms/providers/PersistentBlobProvider.java
@@ -6,7 +6,6 @@ import android.content.UriMatcher;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.support.v4.util.SparseArrayCompat;
 import android.util.Log;
 
 import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
@@ -21,25 +20,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CaptureProvider {
-  private static final String TAG           = CaptureProvider.class.getSimpleName();
-  private static final String URI_STRING    = "content://org.thoughtcrime.securesms/capture";
-  public  static final Uri    CONTENT_URI   = Uri.parse(URI_STRING);
-  public  static final String AUTHORITY     = "org.thoughtcrime.securesms";
-  public  static final String EXPECTED_PATH = "capture/*/#";
-  private static final int    MATCH         = 1;
-  public static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH) {{
+public class PersistentBlobProvider {
+  private static final String     TAG           = PersistentBlobProvider.class.getSimpleName();
+  private static final String     URI_STRING    = "content://org.thoughtcrime.securesms/capture";
+  public  static final Uri        CONTENT_URI   = Uri.parse(URI_STRING);
+  public  static final String     AUTHORITY     = "org.thoughtcrime.securesms";
+  public  static final String     EXPECTED_PATH = "capture/*/#";
+  private static final int        MATCH         = 1;
+  private static final UriMatcher MATCHER       = new UriMatcher(UriMatcher.NO_MATCH) {{
     addURI(AUTHORITY, EXPECTED_PATH, MATCH);
   }};
 
-  private static volatile CaptureProvider instance;
+  private static volatile PersistentBlobProvider instance;
 
-  public static CaptureProvider getInstance(Context context) {
+  public static PersistentBlobProvider getInstance(Context context) {
     if (instance == null) {
-      synchronized (CaptureProvider.class) {
+      synchronized (PersistentBlobProvider.class) {
         if (instance == null) {
-          instance = new CaptureProvider(context);
+          instance = new PersistentBlobProvider(context);
         }
       }
     }
@@ -47,9 +48,9 @@ public class CaptureProvider {
   }
 
   private final Context context;
-  private final SparseArrayCompat<byte[]> cache = new SparseArrayCompat<>();
+  private final Map<Long, byte[]> cache = new HashMap<>();
 
-  private CaptureProvider(Context context) {
+  private PersistentBlobProvider(Context context) {
     this.context = context.getApplicationContext();
   }
 
@@ -57,19 +58,31 @@ public class CaptureProvider {
                     @NonNull Recipients recipients,
                     @NonNull byte[] imageBytes)
   {
-    final int id = generateId(recipients);
+    final long id = generateId(recipients);
     cache.put(id, imageBytes);
-    persistToDisk(masterSecret, id, imageBytes);
+    return create(masterSecret, new ByteArrayInputStream(imageBytes), id);
+  }
+
+  public Uri create(@NonNull MasterSecret masterSecret,
+                    @NonNull InputStream input)
+  {
+    return create(masterSecret, input, System.currentTimeMillis());
+  }
+
+  private Uri create(MasterSecret masterSecret, InputStream input, long id) {
+    persistToDisk(masterSecret, id, input);
     final Uri uniqueUri = Uri.withAppendedPath(CONTENT_URI, String.valueOf(System.currentTimeMillis()));
     return ContentUris.withAppendedId(uniqueUri, id);
   }
 
-  private void persistToDisk(final MasterSecret masterSecret, final int id, final byte[] imageBytes) {
+  private void persistToDisk(final MasterSecret masterSecret, final long id,
+                             final InputStream input)
+  {
     new AsyncTask<Void, Void, Void>() {
       @Override protected Void doInBackground(Void... params) {
         try {
           final OutputStream output = new EncryptingPartOutputStream(getFile(id), masterSecret);
-          Util.copy(new ByteArrayInputStream(imageBytes), output);
+          Util.copy(input, output);
         } catch (IOException e) {
           Log.w(TAG, e);
         }
@@ -83,23 +96,21 @@ public class CaptureProvider {
   }
 
   public Uri createForExternal(@NonNull Recipients recipients) throws IOException {
-    final File externalDir = context.getExternalFilesDir(null);
-    if (externalDir == null) throw new IOException("no external files directory");
-    return Uri.fromFile(new File(externalDir, String.valueOf(generateId(recipients)) + ".jpg"))
+    return Uri.fromFile(new File(getExternalDir(context), String.valueOf(generateId(recipients)) + ".jpg"))
               .buildUpon()
               .appendQueryParameter("unique", String.valueOf(System.currentTimeMillis()))
               .build();
   }
 
   public boolean delete(@NonNull Uri uri) {
-    switch (uriMatcher.match(uri)) {
+    switch (MATCHER.match(uri)) {
     case MATCH: return getFile(ContentUris.parseId(uri)).delete();
     default:    return new File(uri.getPath()).delete();
     }
   }
 
   public @NonNull InputStream getStream(MasterSecret masterSecret, long id) throws IOException {
-    final byte[] cached = cache.get((int)id);
+    final byte[] cached = cache.get(id);
     return cached != null ? new ByteArrayInputStream(cached)
                           : new DecryptingPartInputStream(getFile(id), masterSecret);
   }
@@ -110,5 +121,19 @@ public class CaptureProvider {
 
   private File getFile(long id) {
     return new File(context.getDir("captures", Context.MODE_PRIVATE), id + ".jpg");
+  }
+
+  private static @NonNull File getExternalDir(Context context) throws IOException {
+    final File externalDir = context.getExternalFilesDir(null);
+    if (externalDir == null) throw new IOException("no external files directory");
+    return externalDir;
+  }
+
+  public static boolean isAuthority(@NonNull Context context, @NonNull Uri uri) {
+    try {
+      return MATCHER.match(uri) == MATCH || uri.getPath().startsWith(getExternalDir(context).getAbsolutePath());
+    } catch (IOException ioe) {
+      return false;
+    }
   }
 }

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4374
Fixes https://github.com/WhisperSystems/TextSecure/issues/3989
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/59f2446a2ba6a3158be893d5dce7deae7e113cd8

src/org/smssecure/smssecure/ConversationActivity.java
@@ -16,7 +16,6 @@
  */
 package org.smssecure.smssecure;
 
-import android.annotation.TargetApi;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -94,6 +93,7 @@ import org.smssecure.smssecure.mms.OutgoingSecureMediaMessage;
 import org.smssecure.smssecure.mms.Slide;
 import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.protocol.AutoInitiate;
+import org.smssecure.smssecure.providers.PersistentBlobProvider;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.RecipientFormattingException;
@@ -183,7 +183,6 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private DynamicTheme    dynamicTheme    = new DynamicTheme();
   private DynamicLanguage dynamicLanguage = new DynamicLanguage();
 
-  @TargetApi(Build.VERSION_CODES.KITKAT)
   @Override
   protected void onPreCreate() {
     dynamicTheme.onCreate(this);
@@ -281,20 +280,20 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     switch (reqCode) {
     case PICK_IMAGE:
       boolean isGif = MediaUtil.isGif(MediaUtil.getMimeType(this, data.getData()));
-      setMedia(data.getData(), isGif ? MediaType.GIF : MediaType.IMAGE, false);
+      setMedia(data.getData(), isGif ? MediaType.GIF : MediaType.IMAGE);
       break;
     case PICK_VIDEO:
-      setMedia(data.getData(), MediaType.VIDEO, false);
+      setMedia(data.getData(), MediaType.VIDEO);
       break;
     case PICK_AUDIO:
-      setMedia(data.getData(), MediaType.AUDIO, false);
+      setMedia(data.getData(), MediaType.AUDIO);
       break;
     case PICK_CONTACT_INFO:
       addAttachmentContactInfo(data.getData());
       break;
     case TAKE_PHOTO:
       if (attachmentManager.getCaptureUri() != null) {
-        setMedia(attachmentManager.getCaptureUri(), MediaType.IMAGE, true);
+        setMedia(attachmentManager.getCaptureUri(), MediaType.IMAGE);
       }
       break;
     }
@@ -638,9 +637,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     if (draftText != null)  composeText.setText(draftText);
 
-    if      (draftImage != null) setMedia(draftImage, MediaType.IMAGE, false);
-    else if (draftAudio != null) setMedia(draftAudio, MediaType.AUDIO, false);
-    else if (draftVideo != null) setMedia(draftVideo, MediaType.VIDEO, false);
+    if      (draftImage != null) setMedia(draftImage, MediaType.IMAGE);
+    else if (draftAudio != null) setMedia(draftAudio, MediaType.AUDIO);
+    else if (draftVideo != null) setMedia(draftVideo, MediaType.VIDEO);
 
     if (draftText == null && draftImage == null && draftAudio == null && draftVideo == null) {
       initializeDraftFromDatabase();
@@ -674,11 +673,11 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
           if (draft.getType().equals(Draft.TEXT)) {
             composeText.setText(draft.getValue());
           } else if (draft.getType().equals(Draft.IMAGE)) {
-            setMedia(Uri.parse(draft.getValue()), MediaType.IMAGE, false);
+            setMedia(Uri.parse(draft.getValue()), MediaType.IMAGE);
           } else if (draft.getType().equals(Draft.AUDIO)) {
-            setMedia(Uri.parse(draft.getValue()), MediaType.AUDIO, false);
+            setMedia(Uri.parse(draft.getValue()), MediaType.AUDIO);
           } else if (draft.getType().equals(Draft.VIDEO)) {
-            setMedia(Uri.parse(draft.getValue()), MediaType.VIDEO, false);
+            setMedia(Uri.parse(draft.getValue()), MediaType.VIDEO);
           }
         }
 
@@ -890,8 +889,8 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     }
   }
 
-  private void setMedia(Uri uri, MediaType mediaType, boolean isCapture) {
-    attachmentManager.setMedia(masterSecret, uri, mediaType, getCurrentMediaConstraints(), isCapture);
+  private void setMedia(Uri uri, MediaType mediaType) {
+    attachmentManager.setMedia(masterSecret, uri, mediaType, getCurrentMediaConstraints());
   }
 
   private void addAttachmentContactInfo(Uri contactUri) {
@@ -931,7 +930,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       drafts.add(new Draft(Draft.TEXT, composeText.getText().toString()));
     }
 
-    for (Slide slide : attachmentManager.getSlideDeck().getSlides()) {
+    for (Slide slide : attachmentManager.buildSlideDeck().getSlides()) {
       if      (slide.hasAudio()) drafts.add(new Draft(Draft.AUDIO, slide.getUri().toString()));
       else if (slide.hasVideo()) drafts.add(new Draft(Draft.VIDEO, slide.getUri().toString()));
       else if (slide.hasImage()) drafts.add(new Draft(Draft.IMAGE, slide.getUri().toString()));
@@ -1137,7 +1136,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   {
     final Context context                = getApplicationContext();
     OutgoingMediaMessage outgoingMessage = new OutgoingMediaMessage(recipients,
-                                                                    attachmentManager.getSlideDeck(),
+                                                                    attachmentManager.buildSlideDeck(),
                                                                     getMessage(),
                                                                     System.currentTimeMillis(),
                                                                     distributionType);

src/org/smssecure/smssecure/mms/PartAuthority.java
@@ -9,7 +9,7 @@ import android.support.annotation.NonNull;
 import org.smssecure.smssecure.attachments.AttachmentId;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.DatabaseFactory;
-import org.smssecure.smssecure.providers.CaptureProvider;
+import org.smssecure.smssecure.providers.PersistentBlobProvider;
 import org.smssecure.smssecure.providers.PartProvider;
 import org.smssecure.smssecure.providers.SingleUseBlobProvider;
 
@@ -25,7 +25,7 @@ public class PartAuthority {
 
   private static final int PART_ROW       = 1;
   private static final int THUMB_ROW      = 2;
-  private static final int CAPTURE_ROW    = 3;
+  private static final int PERSISTENT_ROW = 3;
   private static final int SINGLE_USE_ROW = 4;
 
   private static final UriMatcher uriMatcher;
@@ -34,7 +34,7 @@ public class PartAuthority {
     uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     uriMatcher.addURI("org.smssecure.smssecure", "part/*/#", PART_ROW);
     uriMatcher.addURI("org.smssecure.smssecure", "thumb/*/#", THUMB_ROW);
-    uriMatcher.addURI(CaptureProvider.AUTHORITY, CaptureProvider.EXPECTED_PATH, CAPTURE_ROW);
+    uriMatcher.addURI(PersistentBlobProvider.AUTHORITY, PersistentBlobProvider.EXPECTED_PATH, PERSISTENT_ROW);
     uriMatcher.addURI(SingleUseBlobProvider.AUTHORITY, SingleUseBlobProvider.PATH, SINGLE_USE_ROW);
   }
 
@@ -50,8 +50,8 @@ public class PartAuthority {
       case THUMB_ROW:
         partUri = new PartUriParser(uri);
         return DatabaseFactory.getAttachmentDatabase(context).getThumbnailStream(masterSecret, partUri.getPartId());
-      case CAPTURE_ROW:
-        return CaptureProvider.getInstance(context).getStream(masterSecret, ContentUris.parseId(uri));
+      case PERSISTENT_ROW:
+        return PersistentBlobProvider.getInstance(context).getStream(masterSecret, ContentUris.parseId(uri));
       case SINGLE_USE_ROW:
         return SingleUseBlobProvider.getInstance().getStream(ContentUris.parseId(uri));
       default:

src/org/smssecure/smssecure/providers/PersistentBlobProvider.java
@@ -6,7 +6,6 @@ import android.content.UriMatcher;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.support.v4.util.SparseArrayCompat;
 import android.util.Log;
 
 import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
@@ -21,25 +20,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CaptureProvider {
-  private static final String TAG           = CaptureProvider.class.getSimpleName();
-  private static final String URI_STRING    = "content://org.smssecure.smssecure/capture";
-  public  static final Uri    CONTENT_URI   = Uri.parse(URI_STRING);
-  public  static final String AUTHORITY     = "org.smssecure.smssecure";
-  public  static final String EXPECTED_PATH = "capture/*/#";
-  private static final int    MATCH         = 1;
-  public static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH) {{
+public class PersistentBlobProvider {
+  private static final String     TAG           = PersistentBlobProvider.class.getSimpleName();
+  private static final String     URI_STRING    = "content://org.smssecure.smssecure/capture";
+  public  static final Uri        CONTENT_URI   = Uri.parse(URI_STRING);
+  public  static final String     AUTHORITY     = "org.smssecure.smssecure";
+  public  static final String     EXPECTED_PATH = "capture/*/#";
+  private static final int        MATCH         = 1;
+  private static final UriMatcher MATCHER       = new UriMatcher(UriMatcher.NO_MATCH) {{
     addURI(AUTHORITY, EXPECTED_PATH, MATCH);
   }};
 
-  private static volatile CaptureProvider instance;
+  private static volatile PersistentBlobProvider instance;
 
-  public static CaptureProvider getInstance(Context context) {
+  public static PersistentBlobProvider getInstance(Context context) {
     if (instance == null) {
-      synchronized (CaptureProvider.class) {
+      synchronized (PersistentBlobProvider.class) {
         if (instance == null) {
-          instance = new CaptureProvider(context);
+          instance = new PersistentBlobProvider(context);
         }
       }
     }
@@ -47,9 +48,9 @@ public class CaptureProvider {
   }
 
   private final Context context;
-  private final SparseArrayCompat<byte[]> cache = new SparseArrayCompat<>();
+  private final Map<Long, byte[]> cache = new HashMap<>();
 
-  private CaptureProvider(Context context) {
+  private PersistentBlobProvider(Context context) {
     this.context = context.getApplicationContext();
   }
 
@@ -57,19 +58,31 @@ public class CaptureProvider {
                     @NonNull Recipients recipients,
                     @NonNull byte[] imageBytes)
   {
-    final int id = generateId(recipients);
+    final long id = generateId(recipients);
     cache.put(id, imageBytes);
-    persistToDisk(masterSecret, id, imageBytes);
+    return create(masterSecret, new ByteArrayInputStream(imageBytes), id);
+  }
+
+  public Uri create(@NonNull MasterSecret masterSecret,
+                    @NonNull InputStream input)
+  {
+    return create(masterSecret, input, System.currentTimeMillis());
+  }
+
+  private Uri create(MasterSecret masterSecret, InputStream input, long id) {
+    persistToDisk(masterSecret, id, input);
     final Uri uniqueUri = Uri.withAppendedPath(CONTENT_URI, String.valueOf(System.currentTimeMillis()));
     return ContentUris.withAppendedId(uniqueUri, id);
   }
 
-  private void persistToDisk(final MasterSecret masterSecret, final int id, final byte[] imageBytes) {
+  private void persistToDisk(final MasterSecret masterSecret, final long id,
+                             final InputStream input)
+  {
     new AsyncTask<Void, Void, Void>() {
       @Override protected Void doInBackground(Void... params) {
         try {
           final OutputStream output = new EncryptingPartOutputStream(getFile(id), masterSecret);
-          Util.copy(new ByteArrayInputStream(imageBytes), output);
+          Util.copy(input, output);
         } catch (IOException e) {
           Log.w(TAG, e);
         }
@@ -83,23 +96,21 @@ public class CaptureProvider {
   }
 
   public Uri createForExternal(@NonNull Recipients recipients) throws IOException {
-    final File externalDir = context.getExternalFilesDir(null);
-    if (externalDir == null) throw new IOException("no external files directory");
-    return Uri.fromFile(new File(externalDir, String.valueOf(generateId(recipients)) + ".jpg"))
+    return Uri.fromFile(new File(getExternalDir(context), String.valueOf(generateId(recipients)) + ".jpg"))
               .buildUpon()
               .appendQueryParameter("unique", String.valueOf(System.currentTimeMillis()))
               .build();
   }
 
   public boolean delete(@NonNull Uri uri) {
-    switch (uriMatcher.match(uri)) {
+    switch (MATCHER.match(uri)) {
     case MATCH: return getFile(ContentUris.parseId(uri)).delete();
     default:    return new File(uri.getPath()).delete();
     }
   }
 
   public @NonNull InputStream getStream(MasterSecret masterSecret, long id) throws IOException {
-    final byte[] cached = cache.get((int)id);
+    final byte[] cached = cache.get(id);
     return cached != null ? new ByteArrayInputStream(cached)
                           : new DecryptingPartInputStream(getFile(id), masterSecret);
   }
@@ -110,5 +121,19 @@ public class CaptureProvider {
 
   private File getFile(long id) {
     return new File(context.getDir("captures", Context.MODE_PRIVATE), id + ".jpg");
+  }
+
+  private static @NonNull File getExternalDir(Context context) throws IOException {
+    final File externalDir = context.getExternalFilesDir(null);
+    if (externalDir == null) throw new IOException("no external files directory");
+    return externalDir;
+  }
+
+  public static boolean isAuthority(@NonNull Context context, @NonNull Uri uri) {
+    try {
+      return MATCHER.match(uri) == MATCH || uri.getPath().startsWith(getExternalDir(context).getAbsolutePath());
+    } catch (IOException ioe) {
+      return false;
+    }
   }
 }