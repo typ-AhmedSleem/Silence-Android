SamSprung/Signal-Android

Only prompt for importing system SMS db when we're the SMS app
// FREEBIE

src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -58,6 +58,7 @@ import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.loaders.ConversationListLoader;
 import org.thoughtcrime.securesms.notifications.MessageNotifier;
 import org.thoughtcrime.securesms.recipients.Recipients;
+import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Locale;
@@ -144,7 +145,7 @@ public class ConversationListFragment extends Fragment
           return Optional.of(new ExpiredBuildReminder(context));
         } else if (DefaultSmsReminder.isEligible(context)) {
           return Optional.of(new DefaultSmsReminder(context));
-        } else if (SystemSmsImportReminder.isEligible(context)) {
+        } else if (Util.isDefaultSmsProvider(context) && SystemSmsImportReminder.isEligible(context)) {
           return Optional.of((new SystemSmsImportReminder(context, masterSecret)));
         } else if (PushRegistrationReminder.isEligible(context)) {
           return Optional.of((new PushRegistrationReminder(context, masterSecret)));

typ-AhmedSleem/Silence-Android

Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/bd280ec8954c5fba7b43da9396dba3cbb0638ff8

src/org/smssecure/smssecure/ConversationListFragment.java
@@ -59,6 +59,7 @@ import org.smssecure.smssecure.database.loaders.ConversationListLoader;
 import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.util.Locale;
@@ -138,7 +139,7 @@ public class ConversationListFragment extends Fragment
         final Context context = params[0];
          if (DefaultSmsReminder.isEligible(context)) {
           return Optional.of(new DefaultSmsReminder(context));
-        } else if (SystemSmsImportReminder.isEligible(context)) {
+        } else if (Util.isDefaultSmsProvider(context) && SystemSmsImportReminder.isEligible(context)) {
           return Optional.of((new SystemSmsImportReminder(context, masterSecret)));
         } else if (StoreRatingReminder.isEligible(context)) {
           return Optional.of((new StoreRatingReminder(context)));