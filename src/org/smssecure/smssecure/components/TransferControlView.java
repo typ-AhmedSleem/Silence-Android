bbodenmiller/Signal-Android

avoid older android 9patch transparency bugs
// FREEBIE

src/org/thoughtcrime/securesms/components/TransferControlView.java
@@ -53,7 +53,7 @@ public class TransferControlView extends FrameLayout {
     inflate(context, R.layout.transfer_controls_view, this);
 
     final Drawable background = ContextCompat.getDrawable(context, R.drawable.transfer_controls_background);
-    if (VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH) {
+    if (VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
       background.setColorFilter(0x66ffffff, Mode.MULTIPLY);
     }
     setLongClickable(false);

typ-AhmedSleem/Silence-Android

Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/dd0a952c833617985e0449df8ae05656ae638abf

src/org/smssecure/smssecure/components/TransferControlView.java
@@ -53,7 +53,7 @@ public class TransferControlView extends FrameLayout {
     inflate(context, R.layout.transfer_controls_view, this);
 
     final Drawable background = ContextCompat.getDrawable(context, R.drawable.transfer_controls_background);
-    if (VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH) {
+    if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
       background.setColorFilter(0x66ffffff, Mode.MULTIPLY);
     }
     setLongClickable(false);