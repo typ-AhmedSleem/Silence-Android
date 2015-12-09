SamSprung/Signal-Android

fix external gif sharing
fixes #4539
Closes #4556
// FREEBIE

src/org/thoughtcrime/securesms/ShareActivity.java
@@ -36,6 +36,7 @@ import org.thoughtcrime.securesms.providers.PersistentBlobProvider;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicTheme;
+import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.ViewUtil;
 
 import java.io.IOException;
@@ -188,7 +189,9 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
     final String type        = streamExtra != null ? getMimeType(streamExtra) : getIntent().getType();
 
     if (resolvedExtra != null) {
-      if (ContentType.isImageType(type)) {
+      if (MediaUtil.isGif(type)) {
+        intent.putExtra(ConversationActivity.DRAFT_GIF_EXTRA, resolvedExtra);
+      } else if (ContentType.isImageType(type)) {
         intent.putExtra(ConversationActivity.DRAFT_IMAGE_EXTRA, resolvedExtra);
       } else if (ContentType.isAudioType(type)) {
         intent.putExtra(ConversationActivity.DRAFT_AUDIO_EXTRA, resolvedExtra);

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/4539
Closes https://github.com/WhisperSystems/TextSecure/pull/4556
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/ae379cd3ddc6ccc7161214e9a58ffff3dc2bec1c

src/org/smssecure/smssecure/ShareActivity.java
@@ -36,6 +36,7 @@ import org.smssecure.smssecure.providers.PersistentBlobProvider;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.DynamicLanguage;
 import org.smssecure.smssecure.util.DynamicTheme;
+import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.ViewUtil;
 
 import java.io.IOException;
@@ -188,7 +189,9 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
     final String type        = streamExtra != null ? getMimeType(streamExtra) : getIntent().getType();
 
     if (resolvedExtra != null) {
-      if (ContentType.isImageType(type)) {
+      if (MediaUtil.isGif(type)) {
+        intent.putExtra(ConversationActivity.DRAFT_GIF_EXTRA, resolvedExtra);
+      } else if (ContentType.isImageType(type)) {
         intent.putExtra(ConversationActivity.DRAFT_IMAGE_EXTRA, resolvedExtra);
       } else if (ContentType.isAudioType(type)) {
         intent.putExtra(ConversationActivity.DRAFT_AUDIO_EXTRA, resolvedExtra);