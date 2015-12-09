SamSprung/Signal-Android

clear drafts immediately upon sending or new intent
fixes #3843
closes #3850
Closes #4571

// FREEBIE

src/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -29,8 +29,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Toast;
 
 import org.thoughtcrime.securesms.R;
@@ -42,9 +40,11 @@ import org.thoughtcrime.securesms.providers.PersistentBlobProvider;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.ViewUtil;
+import org.thoughtcrime.securesms.util.concurrent.ListenableFuture.Listener;
 import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class AttachmentManager {
 
@@ -72,25 +72,18 @@ public class AttachmentManager {
   }
 
   public void clear() {
-    AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
-    animation.setDuration(200);
-    animation.setAnimationListener(new Animation.AnimationListener() {
+    ViewUtil.fadeOut(attachmentView, 200).addListener(new Listener<Boolean>() {
       @Override
-      public void onAnimationStart(Animation animation) {}
-
-      @Override
-      public void onAnimationRepeat(Animation animation) {}
-
-      @Override
-      public void onAnimationEnd(Animation animation) {
-        slide = Optional.absent();
+      public void onSuccess(Boolean result) {
         thumbnail.clear();
         attachmentView.setVisibility(View.GONE);
         attachmentListener.onAttachmentChanged();
       }
-    });
 
-    attachmentView.startAnimation(animation);
+      @Override
+      public void onFailure(ExecutionException e) {}
+    });
+    slide = Optional.absent();
     audioView.cleanup();
   }
 

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/3843
Closes https://github.com/WhisperSystems/TextSecure/pull/3850
Closes https://github.com/WhisperSystems/TextSecure/pull/4571
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/bcd08956bd8d041fd80df72e1b964d38aaa1b555

src/org/smssecure/smssecure/mms/AttachmentManager.java
@@ -29,8 +29,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Toast;
 
 import junit.framework.Assert;
@@ -44,9 +42,11 @@ import org.smssecure.smssecure.providers.PersistentBlobProvider;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.ViewUtil;
+import org.smssecure.smssecure.util.concurrent.ListenableFuture.Listener;
 import org.whispersystems.libaxolotl.util.guava.Optional;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class AttachmentManager {
 
@@ -74,24 +74,18 @@ public class AttachmentManager {
   }
 
   public void clear() {
-    AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
-    animation.setDuration(200);
-    animation.setAnimationListener(new Animation.AnimationListener() {
+    ViewUtil.fadeOut(attachmentView, 200).addListener(new Listener<Boolean>() {
       @Override
-      public void onAnimationStart(Animation animation) {}
-
-      @Override
-      public void onAnimationRepeat(Animation animation) {}
-
-      @Override public void onAnimationEnd(Animation animation) {
-        slide = Optional.absent();
+      public void onSuccess(Boolean result) {
         thumbnail.clear();
         attachmentView.setVisibility(View.GONE);
         attachmentListener.onAttachmentChanged();
       }
-    });
 
-    attachmentView.startAnimation(animation);
+      @Override
+      public void onFailure(ExecutionException e) {}
+    });
+    slide = Optional.absent();
     audioView.cleanup();
   }
 