SamSprung/Signal-Android

enable media forwarding
fixes #1362
closes #4589
// FREEBIE

src/org/thoughtcrime/securesms/ShareActivity.java
@@ -32,17 +32,15 @@ import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.providers.PersistentBlobProvider;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicTheme;
-import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.ViewUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-import ws.com.google.android.mms.ContentType;
 
 /**
  * An activity to quickly share content with contacts
@@ -110,35 +108,18 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
   private void initializeMedia() {
     final Context context = this;
     isPassingAlongMedia = false;
-    fragmentContainer.setVisibility(View.GONE);
-    progressWheel.setVisibility(View.VISIBLE);
-    new AsyncTask<Uri, Void, Uri>() {
-      @Override
-      protected Uri doInBackground(Uri... uris) {
-        try {
-          if (uris.length != 1 || uris[0] == null) {
-            return null;
-          }
 
-          InputStream input = context.getContentResolver().openInputStream(uris[0]);
-          if (input == null) {
-            return null;
-          }
-
-          return PersistentBlobProvider.getInstance(context).create(masterSecret, input);
-        } catch (IOException ioe) {
-          Log.w(TAG, ioe);
-          return null;
-        }
-      }
-
-      @Override
-      protected void onPostExecute(Uri uri) {
-        resolvedExtra = uri;
-        ViewUtil.fadeIn(fragmentContainer, 300);
-        ViewUtil.fadeOut(progressWheel, 300);
-      }
-    }.execute(getIntent().<Uri>getParcelableExtra(Intent.EXTRA_STREAM));
+    Uri streamExtra = getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
+    if (streamExtra != null && PartAuthority.isLocalUri(streamExtra)) {
+      isPassingAlongMedia = true;
+      resolvedExtra       = streamExtra;
+      fragmentContainer.setVisibility(View.VISIBLE);
+      progressWheel.setVisibility(View.GONE);
+    } else {
+      fragmentContainer.setVisibility(View.GONE);
+      progressWheel.setVisibility(View.VISIBLE);
+      new ResolveMediaTask(context).execute(streamExtra);
+    }
   }
 
   @Override
@@ -187,19 +168,8 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
     final String textExtra   = getIntent().getStringExtra(Intent.EXTRA_TEXT);
     final Uri    streamExtra = getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
     final String type        = streamExtra != null ? getMimeType(streamExtra) : getIntent().getType();
-
-    if (resolvedExtra != null) {
-      if (MediaUtil.isGif(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_GIF_EXTRA, resolvedExtra);
-      } else if (ContentType.isImageType(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_IMAGE_EXTRA, resolvedExtra);
-      } else if (ContentType.isAudioType(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_AUDIO_EXTRA, resolvedExtra);
-      } else if (ContentType.isVideoType(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_VIDEO_EXTRA, resolvedExtra);
-      }
-    }
-    intent.putExtra(ConversationActivity.DRAFT_TEXT_EXTRA, textExtra);
+    intent.putExtra(ConversationActivity.TEXT_EXTRA, textExtra);
+    if (resolvedExtra != null) intent.setDataAndType(resolvedExtra, type);
 
     return intent;
   }
@@ -212,6 +182,40 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
       type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
     }
 
-    return type;
+    return type == null ? getIntent().getType() : type;
+  }
+
+  private class ResolveMediaTask extends AsyncTask<Uri, Void, Uri> {
+    private final Context context;
+
+    public ResolveMediaTask(Context context) {
+      this.context = context;
+    }
+
+    @Override
+    protected Uri doInBackground(Uri... uris) {
+      try {
+        if (uris.length != 1 || uris[0] == null) {
+          return null;
+        }
+
+        InputStream input = context.getContentResolver().openInputStream(uris[0]);
+        if (input == null) {
+          return null;
+        }
+
+        return PersistentBlobProvider.getInstance(context).create(masterSecret, input);
+      } catch (IOException ioe) {
+        Log.w(TAG, ioe);
+        return null;
+      }
+    }
+
+    @Override
+    protected void onPostExecute(Uri uri) {
+      resolvedExtra = uri;
+      ViewUtil.fadeIn(fragmentContainer, 300);
+      ViewUtil.fadeOut(progressWheel, 300);
+    }
   }
 }

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/1362
Closes https://github.com/WhisperSystems/TextSecure/pull/4589
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/02c37e815cecb41a2378a8134130afd1f51bb5d1

src/org/smssecure/smssecure/ShareActivity.java
@@ -32,17 +32,15 @@ import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.mms.PartAuthority;
 import org.smssecure.smssecure.providers.PersistentBlobProvider;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.DynamicLanguage;
 import org.smssecure.smssecure.util.DynamicTheme;
-import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.ViewUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-import ws.com.google.android.mms.ContentType;
 
 /**
  * An activity to quickly share content with contacts
@@ -110,35 +108,18 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
   private void initializeMedia() {
     final Context context = this;
     isPassingAlongMedia = false;
-    fragmentContainer.setVisibility(View.GONE);
-    progressWheel.setVisibility(View.VISIBLE);
-    new AsyncTask<Uri, Void, Uri>() {
-      @Override
-      protected Uri doInBackground(Uri... uris) {
-        try {
-          if (uris.length != 1 || uris[0] == null) {
-            return null;
-          }
 
-          InputStream input = context.getContentResolver().openInputStream(uris[0]);
-          if (input == null) {
-            return null;
-          }
-
-          return PersistentBlobProvider.getInstance(context).create(masterSecret, input);
-        } catch (IOException ioe) {
-          Log.w(TAG, ioe);
-          return null;
-        }
-      }
-
-      @Override
-      protected void onPostExecute(Uri uri) {
-        resolvedExtra = uri;
-        ViewUtil.fadeIn(fragmentContainer, 300);
-        ViewUtil.fadeOut(progressWheel, 300);
-      }
-    }.execute(getIntent().<Uri>getParcelableExtra(Intent.EXTRA_STREAM));
+    Uri streamExtra = getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
+    if (streamExtra != null && PartAuthority.isLocalUri(streamExtra)) {
+      isPassingAlongMedia = true;
+      resolvedExtra       = streamExtra;
+      fragmentContainer.setVisibility(View.VISIBLE);
+      progressWheel.setVisibility(View.GONE);
+    } else {
+      fragmentContainer.setVisibility(View.GONE);
+      progressWheel.setVisibility(View.VISIBLE);
+      new ResolveMediaTask(context).execute(streamExtra);
+    }
   }
 
   @Override
@@ -187,19 +168,8 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
     final String textExtra   = getIntent().getStringExtra(Intent.EXTRA_TEXT);
     final Uri    streamExtra = getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
     final String type        = streamExtra != null ? getMimeType(streamExtra) : getIntent().getType();
-
-    if (resolvedExtra != null) {
-      if (MediaUtil.isGif(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_GIF_EXTRA, resolvedExtra);
-      } else if (ContentType.isImageType(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_IMAGE_EXTRA, resolvedExtra);
-      } else if (ContentType.isAudioType(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_AUDIO_EXTRA, resolvedExtra);
-      } else if (ContentType.isVideoType(type)) {
-        intent.putExtra(ConversationActivity.DRAFT_VIDEO_EXTRA, resolvedExtra);
-      }
-    }
-    intent.putExtra(ConversationActivity.DRAFT_TEXT_EXTRA, textExtra);
+    intent.putExtra(ConversationActivity.TEXT_EXTRA, textExtra);
+    if (resolvedExtra != null) intent.setDataAndType(resolvedExtra, type);
 
     return intent;
   }
@@ -212,6 +182,40 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity
       type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
     }
 
-    return type;
+    return type == null ? getIntent().getType() : type;
+  }
+
+  private class ResolveMediaTask extends AsyncTask<Uri, Void, Uri> {
+    private final Context context;
+
+    public ResolveMediaTask(Context context) {
+      this.context = context;
+    }
+
+    @Override
+    protected Uri doInBackground(Uri... uris) {
+      try {
+        if (uris.length != 1 || uris[0] == null) {
+          return null;
+        }
+
+        InputStream input = context.getContentResolver().openInputStream(uris[0]);
+        if (input == null) {
+          return null;
+        }
+
+        return PersistentBlobProvider.getInstance(context).create(masterSecret, input);
+      } catch (IOException ioe) {
+        Log.w(TAG, ioe);
+        return null;
+      }
+    }
+
+    @Override
+    protected void onPostExecute(Uri uri) {
+      resolvedExtra = uri;
+      ViewUtil.fadeIn(fragmentContainer, 300);
+      ViewUtil.fadeOut(progressWheel, 300);
+    }
   }
 }