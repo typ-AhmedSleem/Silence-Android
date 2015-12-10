SamSprung/Signal-Android

Update attachment content type when updating attachment data
Fixes #4687
Closes #4688

src/org/thoughtcrime/securesms/database/AttachmentDatabase.java
@@ -36,6 +36,7 @@ import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
 import org.thoughtcrime.securesms.crypto.EncryptingPartOutputStream;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.crypto.MasterSecretUnion;
+import org.thoughtcrime.securesms.mms.MediaStream;
 import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.MediaUtil.ThumbnailData;
@@ -289,7 +290,7 @@ public class AttachmentDatabase extends Database {
 
   public @NonNull Attachment updateAttachmentData(@NonNull MasterSecret masterSecret,
                                                   @NonNull Attachment attachment,
-                                                  @NonNull InputStream inputStream)
+                                                  @NonNull MediaStream mediaStream)
       throws MmsException
   {
     SQLiteDatabase     database           = databaseHelper.getWritableDatabase();
@@ -300,19 +301,21 @@ public class AttachmentDatabase extends Database {
       throw new MmsException("No attachment data found!");
     }
 
-    long dataSize = setAttachmentData(masterSecret, dataFile, inputStream);
+    long dataSize = setAttachmentData(masterSecret, dataFile, mediaStream.getStream());
 
     ContentValues contentValues = new ContentValues();
     contentValues.put(SIZE, dataSize);
+    contentValues.put(CONTENT_TYPE, mediaStream.getMimeType());
 
     database.update(TABLE_NAME, contentValues, PART_ID_WHERE, databaseAttachment.getAttachmentId().toStrings());
 
     return new DatabaseAttachment(databaseAttachment.getAttachmentId(),
                                   databaseAttachment.getMmsId(),
                                   databaseAttachment.hasData(),
-                                  databaseAttachment.getContentType(),
+                                  mediaStream.getMimeType(),
                                   databaseAttachment.getTransferState(),
-                                  dataSize, databaseAttachment.getLocation(),
+                                  dataSize,
+                                  databaseAttachment.getLocation(),
                                   databaseAttachment.getKey(),
                                   databaseAttachment.getRelay());
   }

src/org/thoughtcrime/securesms/jobs/SendJob.java
@@ -10,7 +10,9 @@ import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.AttachmentDatabase;
 import org.thoughtcrime.securesms.mms.MediaConstraints;
+import org.thoughtcrime.securesms.mms.MediaStream;
 import org.thoughtcrime.securesms.transport.UndeliverableMessageException;
+import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.jobqueue.JobParameters;
 
@@ -19,6 +21,7 @@ import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import ws.com.google.android.mms.ContentType;
 import ws.com.google.android.mms.MmsException;
 
 public abstract class SendJob extends MasterSecretJob {
@@ -63,7 +66,7 @@ public abstract class SendJob extends MasterSecretJob {
         if (constraints.isSatisfied(context, masterSecret, attachment)) {
           results.add(attachment);
         } else if (constraints.canResize(attachment)) {
-          InputStream resized = constraints.getResizedMedia(context, masterSecret, attachment);
+          MediaStream resized = constraints.getResizedMedia(context, masterSecret, attachment);
           results.add(attachmentDatabase.updateAttachmentData(masterSecret, attachment, resized));
         } else {
           throw new UndeliverableMessageException("Size constraints could not be met!");

src/org/thoughtcrime/securesms/mms/MediaStream.java
@@ -1,0 +1,37 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms.mms;
+
+import java.io.InputStream;
+
+public class MediaStream {
+  private final InputStream stream;
+  private final String      mimeType;
+
+  public MediaStream(InputStream stream, String mimeType) {
+    this.stream   = stream;
+    this.mimeType = mimeType;
+  }
+
+  public InputStream getStream() {
+    return stream;
+  }
+
+  public String getMimeType() {
+    return mimeType;
+  }
+}

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/4687
Closes https://github.com/WhisperSystems/TextSecure/pull/4688
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/8269047c88c06815381438b98a842db0ab3c997c

src/org/smssecure/smssecure/database/AttachmentDatabase.java
@@ -35,6 +35,7 @@ import org.smssecure.smssecure.attachments.DatabaseAttachment;
 import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
 import org.smssecure.smssecure.crypto.EncryptingPartOutputStream;
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.mms.MediaStream;
 import org.smssecure.smssecure.mms.PartAuthority;
 import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.MediaUtil.ThumbnailData;
@@ -288,7 +289,7 @@ public class AttachmentDatabase extends Database {
 
   public @NonNull Attachment updateAttachmentData(@NonNull MasterSecret masterSecret,
                                                   @NonNull Attachment attachment,
-                                                  @NonNull InputStream inputStream)
+                                                  @NonNull MediaStream mediaStream)
       throws MmsException
   {
     SQLiteDatabase     database           = databaseHelper.getWritableDatabase();
@@ -299,19 +300,21 @@ public class AttachmentDatabase extends Database {
       throw new MmsException("No attachment data found!");
     }
 
-    long dataSize = setAttachmentData(masterSecret, dataFile, inputStream);
+    long dataSize = setAttachmentData(masterSecret, dataFile, mediaStream.getStream());
 
     ContentValues contentValues = new ContentValues();
     contentValues.put(SIZE, dataSize);
+    contentValues.put(CONTENT_TYPE, mediaStream.getMimeType());
 
     database.update(TABLE_NAME, contentValues, PART_ID_WHERE, databaseAttachment.getAttachmentId().toStrings());
 
     return new DatabaseAttachment(databaseAttachment.getAttachmentId(),
                                   databaseAttachment.getMmsId(),
                                   databaseAttachment.hasData(),
-                                  databaseAttachment.getContentType(),
+                                  mediaStream.getMimeType(),
                                   databaseAttachment.getTransferState(),
-                                  dataSize, databaseAttachment.getLocation(),
+                                  dataSize,
+                                  databaseAttachment.getLocation(),
                                   databaseAttachment.getKey(),
                                   databaseAttachment.getRelay());
   }

src/org/smssecure/smssecure/jobs/SendJob.java
@@ -10,7 +10,9 @@ import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.AttachmentDatabase;
 import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.mms.MediaConstraints;
+import org.smssecure.smssecure.mms.MediaStream;
 import org.smssecure.smssecure.transport.UndeliverableMessageException;
+import org.smssecure.smssecure.util.MediaUtil;
 import org.smssecure.smssecure.util.Util;
 import org.whispersystems.jobqueue.JobParameters;
 
@@ -19,6 +21,7 @@ import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import ws.com.google.android.mms.ContentType;
 import ws.com.google.android.mms.MmsException;
 
 public abstract class SendJob extends MasterSecretJob {
@@ -57,7 +60,7 @@ public abstract class SendJob extends MasterSecretJob {
         if (constraints.isSatisfied(context, masterSecret, attachment)) {
           results.add(attachment);
         } else if (constraints.canResize(attachment)) {
-          InputStream resized = constraints.getResizedMedia(context, masterSecret, attachment);
+          MediaStream resized = constraints.getResizedMedia(context, masterSecret, attachment);
           results.add(attachmentDatabase.updateAttachmentData(masterSecret, attachment, resized));
         } else {
           throw new UndeliverableMessageException("Size constraints could not be met!");

src/org/smssecure/smssecure/mms/MediaStream.java
@@ -1,0 +1,37 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.smssecure.smssecure.mms;
+
+import java.io.InputStream;
+
+public class MediaStream {
+  private final InputStream stream;
+  private final String      mimeType;
+
+  public MediaStream(InputStream stream, String mimeType) {
+    this.stream   = stream;
+    this.mimeType = mimeType;
+  }
+
+  public InputStream getStream() {
+    return stream;
+  }
+
+  public String getMimeType() {
+    return mimeType;
+  }
+}