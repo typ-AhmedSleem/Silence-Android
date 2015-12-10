SamSprung/Signal-Android

Switch from 'Contents Hidden' to 'New message'
Fixes #4004
Closes #4566

src/org/thoughtcrime/securesms/notifications/SingleRecipientNotificationBuilder.java
@@ -65,7 +65,7 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
                             .asDrawable(context, recipient.getColor()
                                                           .toConversationColor(context)));
     } else {
-      setContentTitle(context.getString(R.string.SingleRecipientNotificationBuilder_new_signal_message));
+      setContentTitle(context.getString(R.string.SingleRecipientNotificationBuilder_signal));
       setLargeIcon(Recipient.getUnknownRecipient()
                             .getContactPhoto()
                             .asDrawable(context, Recipient.getUnknownRecipient()
@@ -84,7 +84,7 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
       setContentText(message);
       this.slideDeck = slideDeck;
     } else {
-      setContentText(context.getString(R.string.SingleRecipientNotificationBuilder_contents_hidden));
+      setContentText(context.getString(R.string.SingleRecipientNotificationBuilder_new_message));
     }
   }
 
@@ -131,9 +131,9 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
     if (privacy.isDisplayMessage()) {
       setTicker(getStyledMessage(recipient, message));
     } else if (privacy.isDisplayContact()) {
-      setTicker(getStyledMessage(recipient, context.getString(R.string.SingleRecipientNotificationBuilder_new_signal_message)));
+      setTicker(getStyledMessage(recipient, context.getString(R.string.SingleRecipientNotificationBuilder_new_message)));
     } else {
-      setTicker(context.getString(R.string.SingleRecipientNotificationBuilder_new_signal_message));
+      setTicker(context.getString(R.string.SingleRecipientNotificationBuilder_new_message));
     }
   }
 

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/4004
Closes https://github.com/WhisperSystems/TextSecure/pull/4566
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/796decdb0fb6a5dae038752159b9c8a755f58515

src/org/smssecure/smssecure/notifications/SingleRecipientNotificationBuilder.java
@@ -65,7 +65,7 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
                             .asDrawable(context, recipient.getColor()
                                                           .toConversationColor(context)));
     } else {
-      setContentTitle(context.getString(R.string.SingleRecipientNotificationBuilder_new_smssecure_message));
+      setContentTitle(context.getString(R.string.SingleRecipientNotificationBuilder_smssecure));
       setLargeIcon(Recipient.getUnknownRecipient()
                             .getContactPhoto()
                             .asDrawable(context, Recipient.getUnknownRecipient()
@@ -84,7 +84,7 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
       setContentText(message);
       this.slideDeck = slideDeck;
     } else {
-      setContentText(context.getString(R.string.SingleRecipientNotificationBuilder_contents_hidden));
+      setContentText(context.getString(R.string.SingleRecipientNotificationBuilder_new_message));
     }
   }
 
@@ -131,9 +131,9 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
     if (privacy.isDisplayMessage()) {
       setTicker(getStyledMessage(recipient, message));
     } else if (privacy.isDisplayContact()) {
-      setTicker(getStyledMessage(recipient, context.getString(R.string.SingleRecipientNotificationBuilder_new_smssecure_message)));
+      setTicker(getStyledMessage(recipient, context.getString(R.string.SingleRecipientNotificationBuilder_new_message)));
     } else {
-      setTicker(context.getString(R.string.SingleRecipientNotificationBuilder_new_smssecure_message));
+      setTicker(context.getString(R.string.SingleRecipientNotificationBuilder_new_message));
     }
   }
 