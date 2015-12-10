SamSprung/Signal-Android

Support for archive actions
Fixes #1141
Closes #4698
// FREEBIE

src/org/thoughtcrime/securesms/BindableConversationListItem.java
@@ -1,0 +1,15 @@
+package org.thoughtcrime.securesms;
+
+import android.support.annotation.NonNull;
+
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.database.model.ThreadRecord;
+
+import java.util.Locale;
+import java.util.Set;
+
+public interface BindableConversationListItem extends Unbindable {
+
+  public void bind(@NonNull MasterSecret masterSecret, @NonNull ThreadRecord thread,
+                   @NonNull Locale locale, @NonNull Set<Long> selectedThreads, boolean batchMode);
+}

src/org/thoughtcrime/securesms/database/loaders/ConversationListLoader.java
@@ -2,30 +2,64 @@ package org.thoughtcrime.securesms.database.loaders;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.MergeCursor;
 
 import org.thoughtcrime.securesms.contacts.ContactAccessor;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
+import org.thoughtcrime.securesms.database.ThreadDatabase;
 import org.thoughtcrime.securesms.util.AbstractCursorLoader;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ConversationListLoader extends AbstractCursorLoader {
 
   private final String filter;
+  private final boolean archived;
 
-  public ConversationListLoader(Context context, String filter) {
+  public ConversationListLoader(Context context, String filter, boolean archived) {
     super(context);
-    this.filter = filter;
+    this.filter   = filter;
+    this.archived = archived;
   }
 
   @Override
   public Cursor getCursor() {
-    if (filter != null && filter.trim().length() != 0) {
-      List<String> numbers = ContactAccessor.getInstance().getNumbersForThreadSearchFilter(context, filter);
+    if      (filter != null && filter.trim().length() != 0) return getFilteredConversationList(filter);
+    else if (!archived)                                     return getUnarchivedConversationList();
+    else                                                    return getArchivedConversationList();
+  }
 
-      return DatabaseFactory.getThreadDatabase(context).getFilteredConversationList(numbers);
-    } else {
-      return DatabaseFactory.getThreadDatabase(context).getConversationList();
+  private Cursor getUnarchivedConversationList() {
+    List<Cursor> cursorList = new LinkedList<>();
+    cursorList.add(DatabaseFactory.getThreadDatabase(context).getConversationList());
+
+    int archivedCount = DatabaseFactory.getThreadDatabase(context)
+                                       .getArchivedConversationListCount();
+
+    if (archivedCount > 0) {
+      MatrixCursor switchToArchiveCursor = new MatrixCursor(new String[] {
+          ThreadDatabase.ID, ThreadDatabase.DATE, ThreadDatabase.MESSAGE_COUNT,
+          ThreadDatabase.RECIPIENT_IDS, ThreadDatabase.SNIPPET, ThreadDatabase.READ,
+          ThreadDatabase.TYPE, ThreadDatabase.SNIPPET_TYPE, ThreadDatabase.SNIPPET_URI,
+          ThreadDatabase.ARCHIVED}, 1);
+
+      switchToArchiveCursor.addRow(new Object[] {-1L, System.currentTimeMillis(), archivedCount,
+                                                 "-1", null, 1, ThreadDatabase.DistributionTypes.ARCHIVE, 0, null, 0});
+
+      cursorList.add(switchToArchiveCursor);
     }
+
+    return new MergeCursor(cursorList.toArray(new Cursor[0]));
+  }
+
+  private Cursor getArchivedConversationList() {
+    return DatabaseFactory.getThreadDatabase(context).getArchivedConversationList();
+  }
+
+  private Cursor getFilteredConversationList(String filter) {
+    List<String> numbers = ContactAccessor.getInstance().getNumbersForThreadSearchFilter(context, filter);
+    return DatabaseFactory.getThreadDatabase(context).getFilteredConversationList(numbers);
   }
 }

src/org/thoughtcrime/securesms/util/task/ProgressDialogAsyncTask.java
@@ -1,4 +1,4 @@
-package org.thoughtcrime.securesms.util;
+package org.thoughtcrime.securesms.util.task;
 
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import java.lang.ref.WeakReference;
 
 public abstract class ProgressDialogAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+
   private final WeakReference<Context> contextReference;
   private       ProgressDialog         progress;
   private final String                 title;

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/1141
Closes https://github.com/WhisperSystems/TextSecure/pull/4698
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/2ab0029d4929e020414ab56e5afdfddc3a695726

src/org/smssecure/smssecure/BindableConversationListItem.java
@@ -1,0 +1,15 @@
+package org.smssecure.smssecure;
+
+import android.support.annotation.NonNull;
+
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.database.model.ThreadRecord;
+
+import java.util.Locale;
+import java.util.Set;
+
+public interface BindableConversationListItem extends Unbindable {
+
+  public void bind(@NonNull MasterSecret masterSecret, @NonNull ThreadRecord thread,
+                   @NonNull Locale locale, @NonNull Set<Long> selectedThreads, boolean batchMode);
+}

src/org/smssecure/smssecure/database/loaders/ConversationListLoader.java
@@ -2,30 +2,64 @@ package org.smssecure.smssecure.database.loaders;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.MergeCursor;
 
 import org.smssecure.smssecure.contacts.ContactAccessor;
 import org.smssecure.smssecure.database.DatabaseFactory;
+import org.smssecure.smssecure.database.ThreadDatabase;
 import org.smssecure.smssecure.util.AbstractCursorLoader;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ConversationListLoader extends AbstractCursorLoader {
 
   private final String filter;
+  private final boolean archived;
 
-  public ConversationListLoader(Context context, String filter) {
+  public ConversationListLoader(Context context, String filter, boolean archived) {
     super(context);
-    this.filter = filter;
+    this.filter   = filter;
+    this.archived = archived;
   }
 
   @Override
   public Cursor getCursor() {
-    if (filter != null && filter.trim().length() != 0) {
-      List<String> numbers = ContactAccessor.getInstance().getNumbersForThreadSearchFilter(context, filter);
+    if      (filter != null && filter.trim().length() != 0) return getFilteredConversationList(filter);
+    else if (!archived)                                     return getUnarchivedConversationList();
+    else                                                    return getArchivedConversationList();
+  }
 
-      return DatabaseFactory.getThreadDatabase(context).getFilteredConversationList(numbers);
-    } else {
-      return DatabaseFactory.getThreadDatabase(context).getConversationList();
+  private Cursor getUnarchivedConversationList() {
+    List<Cursor> cursorList = new LinkedList<>();
+    cursorList.add(DatabaseFactory.getThreadDatabase(context).getConversationList());
+
+    int archivedCount = DatabaseFactory.getThreadDatabase(context)
+                                       .getArchivedConversationListCount();
+
+    if (archivedCount > 0) {
+      MatrixCursor switchToArchiveCursor = new MatrixCursor(new String[] {
+          ThreadDatabase.ID, ThreadDatabase.DATE, ThreadDatabase.MESSAGE_COUNT,
+          ThreadDatabase.RECIPIENT_IDS, ThreadDatabase.SNIPPET, ThreadDatabase.READ,
+          ThreadDatabase.TYPE, ThreadDatabase.SNIPPET_TYPE, ThreadDatabase.SNIPPET_URI,
+          ThreadDatabase.ARCHIVED}, 1);
+
+      switchToArchiveCursor.addRow(new Object[] {-1L, System.currentTimeMillis(), archivedCount,
+                                                 "-1", null, 1, ThreadDatabase.DistributionTypes.ARCHIVE, 0, null, 0});
+
+      cursorList.add(switchToArchiveCursor);
     }
+
+    return new MergeCursor(cursorList.toArray(new Cursor[0]));
+  }
+
+  private Cursor getArchivedConversationList() {
+    return DatabaseFactory.getThreadDatabase(context).getArchivedConversationList();
+  }
+
+  private Cursor getFilteredConversationList(String filter) {
+    List<String> numbers = ContactAccessor.getInstance().getNumbersForThreadSearchFilter(context, filter);
+    return DatabaseFactory.getThreadDatabase(context).getFilteredConversationList(numbers);
   }
 }

src/org/smssecure/smssecure/util/task/ProgressDialogAsyncTask.java
@@ -1,4 +1,4 @@
-package org.smssecure.smssecure.util;
+package org.smssecure.smssecure.util.task;
 
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import java.lang.ref.WeakReference;
 
 public abstract class ProgressDialogAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+
   private final WeakReference<Context> contextReference;
   private       ProgressDialog         progress;
   private final String                 title;
@@ -14,7 +15,7 @@ public abstract class ProgressDialogAsyncTask<Params, Progress, Result> extends 
 
   public ProgressDialogAsyncTask(Context context, String title, String message) {
     super();
-    this.contextReference = new WeakReference<Context>(context);
+    this.contextReference = new WeakReference<>(context);
     this.title            = title;
     this.message          = message;
   }