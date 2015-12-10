SamSprung/Signal-Android

Add delivery status icons to the conversation list
Closes #4710

src/org/thoughtcrime/securesms/ConversationListItem.java
@@ -27,12 +27,13 @@ import android.os.Handler;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.thoughtcrime.securesms.components.AvatarImageView;
+import org.thoughtcrime.securesms.components.DeliveryStatusView;
+import org.thoughtcrime.securesms.components.AlertView;
 import org.thoughtcrime.securesms.components.FromTextView;
 import org.thoughtcrime.securesms.components.ThumbnailView;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
@@ -63,13 +64,16 @@ public class ConversationListItem extends RelativeLayout
   private final static Typeface BOLD_TYPEFACE  = Typeface.create("sans-serif", Typeface.BOLD);
   private final static Typeface LIGHT_TYPEFACE = Typeface.create("sans-serif-light", Typeface.NORMAL);
 
-  private Set<Long>       selectedThreads;
-  private Recipients      recipients;
-  private long            threadId;
-  private TextView        subjectView;
-  private FromTextView    fromView;
-  private TextView        dateView;
-  private TextView        archivedView;
+  private Set<Long>          selectedThreads;
+  private Recipients         recipients;
+  private long               threadId;
+  private TextView           subjectView;
+  private FromTextView       fromView;
+  private TextView           dateView;
+  private TextView           archivedView;
+  private DeliveryStatusView deliveryStatusIndicator;
+  private AlertView          alertView;
+
   private boolean         read;
   private AvatarImageView contactPhotoImage;
   private ThumbnailView   thumbnailView;
@@ -93,12 +97,14 @@ public class ConversationListItem extends RelativeLayout
   @Override
   protected void onFinishInflate() {
     super.onFinishInflate();
-    this.subjectView       = (TextView)        findViewById(R.id.subject);
-    this.fromView          = (FromTextView)    findViewById(R.id.from);
-    this.dateView          = (TextView)        findViewById(R.id.date);
-    this.contactPhotoImage = (AvatarImageView) findViewById(R.id.contact_photo_image);
-    this.thumbnailView     = (ThumbnailView)   findViewById(R.id.thumbnail);
-    this.archivedView      = ViewUtil.findById(this, R.id.archived);
+    this.subjectView             = (TextView)           findViewById(R.id.subject);
+    this.fromView                = (FromTextView)       findViewById(R.id.from);
+    this.dateView                = (TextView)           findViewById(R.id.date);
+    this.deliveryStatusIndicator = (DeliveryStatusView) findViewById(R.id.delivery_status);
+    this.alertView               = (AlertView)          findViewById(R.id.indicators_parent);
+    this.contactPhotoImage       = (AvatarImageView)    findViewById(R.id.contact_photo_image);
+    this.thumbnailView           = (ThumbnailView)      findViewById(R.id.thumbnail);
+    this.archivedView            = ViewUtil.findById(this, R.id.archived);
     thumbnailView.setClickable(false);
   }
 
@@ -129,6 +135,7 @@ public class ConversationListItem extends RelativeLayout
       this.archivedView.setVisibility(View.GONE);
     }
 
+    setStatusIcons(thread);
     setThumbnailSnippet(masterSecret, thread);
     setBatchState(batchMode);
     setBackground(thread);
@@ -165,13 +172,32 @@ public class ConversationListItem extends RelativeLayout
       LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
       subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.thumbnail);
       this.subjectView.setLayoutParams(subjectParams);
-      this.post(new ThumbnailPositioner(thumbnailView, archivedView, dateView));
+      this.post(new ThumbnailPositioner(thumbnailView, archivedView, deliveryStatusIndicator, dateView));
     } else {
       this.thumbnailView.setVisibility(View.GONE);
 
       LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
-      subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.archived);
+      subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.delivery_status);
       this.subjectView.setLayoutParams(subjectParams);
+    }
+  }
+
+  private void setStatusIcons(ThreadRecord thread) {
+    if (!thread.isOutgoing()) {
+      deliveryStatusIndicator.setNone();
+      alertView.setNone();
+    } else if (thread.isFailed()) {
+      deliveryStatusIndicator.setNone();
+      alertView.setFailed();
+    } else if (thread.isPendingInsecureSmsFallback()) {
+      deliveryStatusIndicator.setNone();
+      alertView.setPendingApproval();
+    } else {
+      alertView.setNone();
+
+      if      (thread.isPending())   deliveryStatusIndicator.setPending();
+      else if (thread.isDelivered()) deliveryStatusIndicator.setDelivered();
+      else                           deliveryStatusIndicator.setSent();
     }
   }
 
@@ -204,20 +230,24 @@ public class ConversationListItem extends RelativeLayout
 
     private final View thumbnailView;
     private final View archivedView;
+    private final View deliveryStatusView;
     private final View dateView;
 
-    public ThumbnailPositioner(View thumbnailView, View archivedView, View dateView) {
-      this.thumbnailView = thumbnailView;
-      this.archivedView  = archivedView;
-      this.dateView      = dateView;
+    public ThumbnailPositioner(View thumbnailView, View archivedView, View deliveryStatusView, View dateView) {
+      this.thumbnailView      = thumbnailView;
+      this.archivedView       = archivedView;
+      this.deliveryStatusView = deliveryStatusView;
+      this.dateView           = dateView;
     }
 
     @Override
     public void run() {
       LayoutParams thumbnailParams = (RelativeLayout.LayoutParams)thumbnailView.getLayoutParams();
 
-      if (archivedView.getVisibility() == View.VISIBLE && archivedView.getWidth() > dateView.getWidth()) {
-        thumbnailParams.addRule(RelativeLayout.LEFT_OF, R.id.archived);
+      if (archivedView.getVisibility() == View.VISIBLE &&
+          (archivedView.getWidth() + deliveryStatusView.getWidth()) > dateView.getWidth())
+      {
+        thumbnailParams.addRule(RelativeLayout.LEFT_OF, R.id.delivery_status);
       } else {
         thumbnailParams.addRule(RelativeLayout.LEFT_OF, R.id.date);
       }

src/org/thoughtcrime/securesms/database/model/MediaMmsMessageRecord.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.text.SpannableString;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.database.SmsDatabase.Status;
 import org.thoughtcrime.securesms.database.MmsDatabase;
 import org.thoughtcrime.securesms.database.documents.IdentityKeyMismatch;
 import org.thoughtcrime.securesms.database.documents.NetworkFailure;
@@ -47,16 +48,15 @@ public class MediaMmsMessageRecord extends MessageRecord {
 
   public MediaMmsMessageRecord(Context context, long id, Recipients recipients,
                                Recipient individualRecipient, int recipientDeviceId,
-                               long dateSent, long dateReceived, int deliveredCount,
+                               long dateSent, long dateReceived, int receiptCount,
                                long threadId, Body body,
                                @NonNull SlideDeck slideDeck,
                                int partCount, long mailbox,
                                List<IdentityKeyMismatch> mismatches,
                                List<NetworkFailure> failures)
   {
-    super(context, id, body, recipients, individualRecipient, recipientDeviceId,
-          dateSent, dateReceived, threadId, DELIVERY_STATUS_NONE, deliveredCount, mailbox,
-          mismatches, failures);
+    super(context, id, body, recipients, individualRecipient, recipientDeviceId, dateSent,
+          dateReceived, threadId, Status.STATUS_NONE, receiptCount, mailbox, mismatches, failures);
 
     this.context   = context.getApplicationContext();
     this.partCount = partCount;

src/org/thoughtcrime/securesms/database/model/NotificationMmsMessageRecord.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.text.SpannableString;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.database.SmsDatabase.Status;
 import org.thoughtcrime.securesms.database.MmsDatabase;
 import org.thoughtcrime.securesms.database.documents.NetworkFailure;
 import org.thoughtcrime.securesms.database.documents.IdentityKeyMismatch;
@@ -51,7 +52,7 @@ public class NotificationMmsMessageRecord extends MessageRecord {
                                       long expiry, int status, byte[] transactionId, long mailbox)
   {
     super(context, id, new Body("", true), recipients, individualRecipient, recipientDeviceId,
-          dateSent, dateReceived, threadId, DELIVERY_STATUS_NONE, receiptCount, mailbox,
+          dateSent, dateReceived, threadId, Status.STATUS_NONE, receiptCount, mailbox,
           new LinkedList<IdentityKeyMismatch>(), new LinkedList<NetworkFailure>());
 
     this.contentLocation = contentLocation;

src/org/thoughtcrime/securesms/database/model/SmsMessageRecord.java
@@ -50,7 +50,7 @@ public class SmsMessageRecord extends MessageRecord {
                           int status, List<IdentityKeyMismatch> mismatches)
   {
     super(context, id, body, recipients, individualRecipient, recipientDeviceId,
-          dateSent, dateReceived, threadId, getGenericDeliveryStatus(status), receiptCount, type,
+          dateSent, dateReceived, threadId, status, receiptCount, type,
           mismatches, new LinkedList<NetworkFailure>());
   }
 
@@ -103,17 +103,5 @@ public class SmsMessageRecord extends MessageRecord {
   @Override
   public boolean isMmsNotification() {
     return false;
-  }
-
-  private static int getGenericDeliveryStatus(int status) {
-    if (status == SmsDatabase.Status.STATUS_NONE) {
-      return MessageRecord.DELIVERY_STATUS_NONE;
-    } else if (status >= SmsDatabase.Status.STATUS_FAILED) {
-      return MessageRecord.DELIVERY_STATUS_FAILED;
-    } else if (status >= SmsDatabase.Status.STATUS_PENDING) {
-      return MessageRecord.DELIVERY_STATUS_PENDING;
-    } else {
-      return MessageRecord.DELIVERY_STATUS_RECEIVED;
-    }
   }
 }

src/org/thoughtcrime/securesms/database/model/ThreadRecord.java
@@ -28,6 +28,7 @@ import android.text.style.StyleSpan;
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.database.MmsSmsColumns;
 import org.thoughtcrime.securesms.database.SmsDatabase;
+import org.thoughtcrime.securesms.database.ThreadDatabase;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.GroupUtil;
 
@@ -48,9 +49,10 @@ public class ThreadRecord extends DisplayRecord {
 
   public ThreadRecord(@NonNull Context context, @NonNull Body body, @Nullable Uri snippetUri,
                       @NonNull Recipients recipients, long date, long count, boolean read,
-                      long threadId, long snippetType, int distributionType, boolean archived)
+                      long threadId, int receiptCount, int status, long snippetType,
+                      int distributionType, boolean archived)
   {
-    super(context, body, recipients, date, date, threadId, snippetType);
+    super(context, body, recipients, date, date, threadId, status, receiptCount, snippetType);
     this.context          = context.getApplicationContext();
     this.snippetUri       = snippetUri;
     this.count            = count;

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4710
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/7c95adc7e61e8157a686caa19ac8b6c8cf593eae

src/org/smssecure/smssecure/ConversationListItem.java
@@ -27,12 +27,13 @@ import android.os.Handler;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.smssecure.smssecure.components.AvatarImageView;
+import org.smssecure.smssecure.components.DeliveryStatusView;
+import org.smssecure.smssecure.components.AlertView;
 import org.smssecure.smssecure.components.FromTextView;
 import org.smssecure.smssecure.components.ThumbnailView;
 import org.smssecure.smssecure.crypto.MasterSecret;
@@ -63,13 +64,16 @@ public class ConversationListItem extends RelativeLayout
   private final static Typeface BOLD_TYPEFACE  = Typeface.create("sans-serif", Typeface.BOLD);
   private final static Typeface LIGHT_TYPEFACE = Typeface.create("sans-serif-light", Typeface.NORMAL);
 
-  private Set<Long>       selectedThreads;
-  private Recipients      recipients;
-  private long            threadId;
-  private TextView        subjectView;
-  private FromTextView    fromView;
-  private TextView        dateView;
-  private TextView        archivedView;
+  private Set<Long>          selectedThreads;
+  private Recipients         recipients;
+  private long               threadId;
+  private TextView           subjectView;
+  private FromTextView       fromView;
+  private TextView           dateView;
+  private TextView           archivedView;
+  private DeliveryStatusView deliveryStatusIndicator;
+  private AlertView          alertView;
+
   private boolean         read;
   private AvatarImageView contactPhotoImage;
   private ThumbnailView   thumbnailView;
@@ -93,12 +97,14 @@ public class ConversationListItem extends RelativeLayout
   @Override
   protected void onFinishInflate() {
     super.onFinishInflate();
-    this.subjectView       = (TextView)        findViewById(R.id.subject);
-    this.fromView          = (FromTextView)    findViewById(R.id.from);
-    this.dateView          = (TextView)        findViewById(R.id.date);
-    this.contactPhotoImage = (AvatarImageView) findViewById(R.id.contact_photo_image);
-    this.thumbnailView     = (ThumbnailView)   findViewById(R.id.thumbnail);
-    this.archivedView      = ViewUtil.findById(this, R.id.archived);
+    this.subjectView             = (TextView)           findViewById(R.id.subject);
+    this.fromView                = (FromTextView)       findViewById(R.id.from);
+    this.dateView                = (TextView)           findViewById(R.id.date);
+    this.deliveryStatusIndicator = (DeliveryStatusView) findViewById(R.id.delivery_status);
+    this.alertView               = (AlertView)          findViewById(R.id.indicators_parent);
+    this.contactPhotoImage       = (AvatarImageView)    findViewById(R.id.contact_photo_image);
+    this.thumbnailView           = (ThumbnailView)      findViewById(R.id.thumbnail);
+    this.archivedView            = ViewUtil.findById(this, R.id.archived);
     thumbnailView.setClickable(false);
   }
 
@@ -129,6 +135,7 @@ public class ConversationListItem extends RelativeLayout
       this.archivedView.setVisibility(View.GONE);
     }
 
+    setStatusIcons(thread);
     setThumbnailSnippet(masterSecret, thread);
     setBatchState(batchMode);
     setBackground(thread);
@@ -165,13 +172,32 @@ public class ConversationListItem extends RelativeLayout
       LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
       subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.thumbnail);
       this.subjectView.setLayoutParams(subjectParams);
-      this.post(new ThumbnailPositioner(thumbnailView, archivedView, dateView));
+      this.post(new ThumbnailPositioner(thumbnailView, archivedView, deliveryStatusIndicator, dateView));
     } else {
       this.thumbnailView.setVisibility(View.GONE);
 
       LayoutParams subjectParams = (RelativeLayout.LayoutParams)this.subjectView.getLayoutParams();
-      subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.archived);
+      subjectParams.addRule(RelativeLayout.LEFT_OF, R.id.delivery_status);
       this.subjectView.setLayoutParams(subjectParams);
+    }
+  }
+
+  private void setStatusIcons(ThreadRecord thread) {
+    if (!thread.isOutgoing()) {
+      deliveryStatusIndicator.setNone();
+      alertView.setNone();
+    } else if (thread.isFailed()) {
+      deliveryStatusIndicator.setNone();
+      alertView.setFailed();
+    } else if (thread.isPendingInsecureSmsFallback()) {
+      deliveryStatusIndicator.setNone();
+      alertView.setPendingApproval();
+    } else {
+      alertView.setNone();
+
+      if      (thread.isPending())   deliveryStatusIndicator.setPending();
+      else if (thread.isDelivered()) deliveryStatusIndicator.setDelivered();
+      else                           deliveryStatusIndicator.setSent();
     }
   }
 
@@ -204,20 +230,24 @@ public class ConversationListItem extends RelativeLayout
 
     private final View thumbnailView;
     private final View archivedView;
+    private final View deliveryStatusView;
     private final View dateView;
 
-    public ThumbnailPositioner(View thumbnailView, View archivedView, View dateView) {
-      this.thumbnailView = thumbnailView;
-      this.archivedView  = archivedView;
-      this.dateView      = dateView;
+    public ThumbnailPositioner(View thumbnailView, View archivedView, View deliveryStatusView, View dateView) {
+      this.thumbnailView      = thumbnailView;
+      this.archivedView       = archivedView;
+      this.deliveryStatusView = deliveryStatusView;
+      this.dateView           = dateView;
     }
 
     @Override
     public void run() {
       LayoutParams thumbnailParams = (RelativeLayout.LayoutParams)thumbnailView.getLayoutParams();
 
-      if (archivedView.getVisibility() == View.VISIBLE && archivedView.getWidth() > dateView.getWidth()) {
-        thumbnailParams.addRule(RelativeLayout.LEFT_OF, R.id.archived);
+      if (archivedView.getVisibility() == View.VISIBLE &&
+          (archivedView.getWidth() + deliveryStatusView.getWidth()) > dateView.getWidth())
+      {
+        thumbnailParams.addRule(RelativeLayout.LEFT_OF, R.id.delivery_status);
       } else {
         thumbnailParams.addRule(RelativeLayout.LEFT_OF, R.id.date);
       }

src/org/smssecure/smssecure/database/model/MediaMmsMessageRecord.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.text.SpannableString;
 
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.database.SmsDatabase.Status;
 import org.smssecure.smssecure.database.MmsDatabase;
 import org.smssecure.smssecure.database.documents.IdentityKeyMismatch;
 import org.smssecure.smssecure.database.documents.NetworkFailure;
@@ -54,9 +55,8 @@ public class MediaMmsMessageRecord extends MessageRecord {
                                List<IdentityKeyMismatch> mismatches,
                                List<NetworkFailure> failures)
   {
-    super(context, id, body, recipients, individualRecipient, recipientDeviceId,
-          dateSent, dateReceived, threadId, DELIVERY_STATUS_NONE, dateDeliveryReceived, mailbox,
-          mismatches, failures);
+    super(context, id, body, recipients, individualRecipient, recipientDeviceId, dateSent,
+          dateReceived, threadId, Status.STATUS_NONE, dateDeliveryReceived, mailbox, mismatches, failures);
 
     this.context   = context.getApplicationContext();
     this.partCount = partCount;

src/org/smssecure/smssecure/database/model/NotificationMmsMessageRecord.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.text.SpannableString;
 
 import org.smssecure.smssecure.R;
+import org.smssecure.smssecure.database.SmsDatabase.Status;
 import org.smssecure.smssecure.database.MmsDatabase;
 import org.smssecure.smssecure.database.documents.NetworkFailure;
 import org.smssecure.smssecure.database.documents.IdentityKeyMismatch;
@@ -51,7 +52,7 @@ public class NotificationMmsMessageRecord extends MessageRecord {
                                       long expiry, int status, byte[] transactionId, long mailbox)
   {
     super(context, id, new Body("", true), recipients, individualRecipient, recipientDeviceId,
-          dateSent, dateReceived, threadId, DELIVERY_STATUS_NONE, dateDeliveryReceived, mailbox,
+          dateSent, dateReceived, threadId, Status.STATUS_NONE, dateDeliveryReceived, mailbox,
           new LinkedList<IdentityKeyMismatch>(), new LinkedList<NetworkFailure>());
 
     this.contentLocation = contentLocation;

src/org/smssecure/smssecure/database/model/SmsMessageRecord.java
@@ -50,7 +50,7 @@ public class SmsMessageRecord extends MessageRecord {
                           int status, List<IdentityKeyMismatch> mismatches)
   {
     super(context, id, body, recipients, individualRecipient, recipientDeviceId,
-          dateSent, dateReceived, threadId, getGenericDeliveryStatus(status), dateDeliveryReceived, type,
+          dateSent, dateReceived, threadId, status, dateDeliveryReceived, type,
           mismatches, new LinkedList<NetworkFailure>());
   }
 
@@ -107,17 +107,5 @@ public class SmsMessageRecord extends MessageRecord {
   @Override
   public boolean isMmsNotification() {
     return false;
-  }
-
-  private static int getGenericDeliveryStatus(int status) {
-    if (status == SmsDatabase.Status.STATUS_NONE) {
-      return MessageRecord.DELIVERY_STATUS_NONE;
-    } else if (status >= SmsDatabase.Status.STATUS_FAILED) {
-      return MessageRecord.DELIVERY_STATUS_FAILED;
-    } else if (status >= SmsDatabase.Status.STATUS_PENDING) {
-      return MessageRecord.DELIVERY_STATUS_PENDING;
-    } else {
-      return MessageRecord.DELIVERY_STATUS_RECEIVED;
-    }
   }
 }

src/org/smssecure/smssecure/database/model/ThreadRecord.java
@@ -28,6 +28,7 @@ import android.text.style.StyleSpan;
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.database.MmsSmsColumns;
 import org.smssecure.smssecure.database.SmsDatabase;
+import org.smssecure.smssecure.database.ThreadDatabase;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.GroupUtil;
 
@@ -48,9 +49,9 @@ public class ThreadRecord extends DisplayRecord {
 
   public ThreadRecord(@NonNull Context context, @NonNull Body body, @Nullable Uri snippetUri,
                       @NonNull Recipients recipients, long date, long count, boolean read,
-                      long threadId, long snippetType, int distributionType, boolean archived)
+                      long threadId, int status, long snippetType, int distributionType, boolean archived)
   {
-    super(context, body, recipients, date, date, date, threadId, snippetType);
+    super(context, body, recipients, date, date, date, threadId, status, snippetType);
     this.context          = context.getApplicationContext();
     this.snippetUri       = snippetUri;
     this.count            = count;