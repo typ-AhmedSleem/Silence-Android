SamSprung/Signal-Android

Don't notify of failure when viewing message details
If message delivery fails and the currently visible activity is the
MessageDetailsActivity of the failed message, treat the failure
notification as an in-thread notification.

Closes #3947

src/org/thoughtcrime/securesms/MessageDetailsActivity.java
@@ -42,6 +42,7 @@ import org.thoughtcrime.securesms.database.MmsSmsDatabase;
 import org.thoughtcrime.securesms.database.SmsDatabase;
 import org.thoughtcrime.securesms.database.loaders.MessageDetailsLoader;
 import org.thoughtcrime.securesms.database.model.MessageRecord;
+import org.thoughtcrime.securesms.notifications.MessageNotifier;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.Recipients;
@@ -67,11 +68,13 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
 
   public final static String MASTER_SECRET_EXTRA  = "master_secret";
   public final static String MESSAGE_ID_EXTRA     = "message_id";
+  public final static String THREAD_ID_EXTRA      = "thread_id";
   public final static String IS_PUSH_GROUP_EXTRA  = "is_push_group";
   public final static String TYPE_EXTRA           = "type";
   public final static String RECIPIENTS_IDS_EXTRA = "recipients_ids";
 
   private MasterSecret     masterSecret;
+  private long             threadId;
   private boolean          isPushGroup;
   private ConversationItem conversationItem;
   private ViewGroup        itemParent;
@@ -109,6 +112,14 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     dynamicTheme.onResume(this);
     dynamicLanguage.onResume(this);
     getSupportActionBar().setTitle(R.string.AndroidManifest__message_details);
+
+    MessageNotifier.setVisibleThread(threadId);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    MessageNotifier.setVisibleThread(-1L);
   }
 
   private void initializeActionBar() {
@@ -143,6 +154,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     View header = inflater.inflate(R.layout.message_details_header, recipientsList, false);
 
     masterSecret      = getIntent().getParcelableExtra(MASTER_SECRET_EXTRA);
+    threadId          = getIntent().getLongExtra(THREAD_ID_EXTRA, -1);
     isPushGroup       = getIntent().getBooleanExtra(IS_PUSH_GROUP_EXTRA, false);
     itemParent        = (ViewGroup) header.findViewById(R.id.item_container);
     recipientsList    = (ListView ) findViewById(R.id.recipients_list);

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/3947
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/57cb68849e6d096517f4a8daab78f388755c98b4

src/org/smssecure/smssecure/MessageDetailsActivity.java
@@ -42,6 +42,7 @@ import org.smssecure.smssecure.database.MmsSmsDatabase;
 import org.smssecure.smssecure.database.SmsDatabase;
 import org.smssecure.smssecure.database.loaders.MessageDetailsLoader;
 import org.smssecure.smssecure.database.model.MessageRecord;
+import org.smssecure.smssecure.notifications.MessageNotifier;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.Recipients;
@@ -68,10 +69,12 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
 
   public final static String MASTER_SECRET_EXTRA  = "master_secret";
   public final static String MESSAGE_ID_EXTRA     = "message_id";
+  public final static String THREAD_ID_EXTRA      = "thread_id";
   public final static String TYPE_EXTRA           = "type";
   public final static String RECIPIENTS_IDS_EXTRA = "recipients_ids";
 
   private MasterSecret     masterSecret;
+  private long             threadId;
   private ConversationItem conversationItem;
   private ViewGroup        itemParent;
   private View             metadataContainer;
@@ -108,6 +111,14 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     dynamicTheme.onResume(this);
     dynamicLanguage.onResume(this);
     getSupportActionBar().setTitle(R.string.AndroidManifest__message_details);
+
+    MessageNotifier.setVisibleThread(threadId);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    MessageNotifier.setVisibleThread(-1L);
   }
 
   private void initializeActionBar() {
@@ -142,6 +153,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     View header = inflater.inflate(R.layout.message_details_header, recipientsList, false);
 
     masterSecret      = getIntent().getParcelableExtra(MASTER_SECRET_EXTRA);
+    threadId          = getIntent().getLongExtra(THREAD_ID_EXTRA, -1);
     itemParent        = (ViewGroup) header.findViewById(R.id.item_container);
     recipientsList    = (ListView ) findViewById(R.id.recipients_list);
     metadataContainer =             header.findViewById(R.id.metadata_container);