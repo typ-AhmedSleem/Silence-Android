52c72ea322773752251da4c039ba99778350dd27
Bastien Le Querrec <bastienlq@smssecure.org>
2015-12-18 13:37:56 +0100

    (re)add a menu to delete a conversation while viewing it

--- src/org/smssecure/smssecure/ConversationActivity.java
+++ src/org/smssecure/smssecure/ConversationActivity.java
@@ -353,6 +353,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     super.onOptionsItemSelected(item);
     switch (item.getItemId()) {
     case R.id.menu_call:                      handleDial(getRecipients().getPrimaryRecipient()); return true;
+    case R.id.menu_delete_conversation:       handleDeleteConversation();                        return true;
     case R.id.menu_add_attachment:            handleAddAttachment();                             return true;
     case R.id.menu_view_media:                handleViewMedia();                                 return true;
     case R.id.menu_add_to_contacts:           handleAddToContacts();                             return true;
@@ -586,6 +587,28 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
   private void handleDisplayGroupRecipients() {
     new GroupMembersDialog(this, getRecipients()).display();
+  }
+
+  private void handleDeleteConversation() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle(R.string.ConversationActivity_delete_thread_question);
+    builder.setIconAttribute(R.attr.dialog_alert_icon);
+    builder.setCancelable(true);
+    builder.setMessage(R.string.ConversationActivity_this_will_permanently_delete_all_messages_in_this_conversation);
+    builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        if (threadId > 0) {
+          DatabaseFactory.getThreadDatabase(ConversationActivity.this).deleteConversation(threadId);
+        }
+        composeText.getText().clear();
+        threadId = -1;
+        finish();
+      }
+    });
+
+    builder.setNegativeButton(android.R.string.cancel, null);
+    builder.show();
   }
 
   private void handleAddToContacts() {
