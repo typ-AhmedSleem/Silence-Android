b6caf7a27cc5e631f040290dcacc15165f942483
Bastien Le Querrec <bastienlq@smssecure.org>
2016-02-06 14:09:21 +0100

    find-missing-strings.sh returns 1 if a string is missing

--- scripts/find-missing-strings.sh
+++ scripts/find-missing-strings.sh
@@ -5,12 +5,20 @@ for file in $(find src/ -type f); do
     output=$(cat $file | grep -o "R.string.[A-Za-z_]*" | sed 's/R.string.//g')$'\n'$output
 done
 
+error=0
 for string in $output; do
     result=$(grep $string res/values/strings.xml)
     if [[ $result == "" ]]; then
         is_a_comment=$(grep -r 'src/' -e "^\/\/.*R.string.$string.*")
         if [[ $is_a_comment == "" ]]; then
             echo "$string is missing!"
+            error=1
         fi
     fi
 done
+
+if [[ error -eq 0 ]]; then
+    exit 0
+else
+    exit 1
+fi
