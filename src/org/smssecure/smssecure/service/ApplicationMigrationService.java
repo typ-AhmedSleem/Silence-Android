SamSprung/Signal-Android

Make system database import notification translatable
Also replace "TextSecure" with "Signal" in the notification text.

Closes #5256
// FREEBIE

src/org/thoughtcrime/securesms/service/ApplicationMigrationService.java
@@ -188,8 +188,8 @@ public class ApplicationMigrationService extends Service
     public void onReceive(Context context, Intent intent) {
       NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
       builder.setSmallIcon(R.drawable.icon_notification);
-      builder.setContentTitle("Import Complete");
-      builder.setContentText("TextSecure system database import is complete.");
+      builder.setContentTitle(context.getString(R.string.ApplicationMigrationService_import_complete));
+      builder.setContentText(context.getString(R.string.ApplicationMigrationService_Signal_system_database_import_is_complete));
       builder.setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, ConversationListActivity.class), 0));
       builder.setWhen(System.currentTimeMillis());
       builder.setDefaults(Notification.DEFAULT_VIBRATE);

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/5256
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/25d7931358658764b99881d8a507ebd806dc53ef

src/org/smssecure/smssecure/service/ApplicationMigrationService.java
@@ -188,8 +188,8 @@ public class ApplicationMigrationService extends Service
     public void onReceive(Context context, Intent intent) {
       NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
       builder.setSmallIcon(R.drawable.icon_notification);
-      builder.setContentTitle("Import Complete");
-      builder.setContentText("SMSSecure system database import is complete.");
+      builder.setContentTitle(context.getString(R.string.ApplicationMigrationService_import_complete));
+      builder.setContentText(context.getString(R.string.ApplicationMigrationService_SMSSecure_system_database_import_is_complete));
       builder.setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, ConversationListActivity.class), 0));
       builder.setWhen(System.currentTimeMillis());
       builder.setDefaults(Notification.DEFAULT_VIBRATE);