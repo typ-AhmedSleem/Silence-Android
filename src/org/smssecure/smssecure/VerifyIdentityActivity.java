signalapp/Signal-Android

Fix identity key formatting regression
Fixes #5376
// FREEBIE

src/org/thoughtcrime/securesms/VerifyIdentityActivity.java
@@ -29,6 +29,7 @@ import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.crypto.storage.TextSecureSessionStore;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
+import org.thoughtcrime.securesms.util.Hex;
 import org.whispersystems.libsignal.SignalProtocolAddress;
 import org.whispersystems.libsignal.IdentityKey;
 import org.whispersystems.libsignal.state.SessionRecord;
@@ -75,14 +76,14 @@ public class VerifyIdentityActivity extends KeyScanningActivity {
       return;
     }
 
-    localIdentityFingerprint.setText(IdentityKeyUtil.getIdentityKey(this).getFingerprint());
+    localIdentityFingerprint.setText(Hex.toString(IdentityKeyUtil.getIdentityKey(this).serialize()));
 
     IdentityKey identityKey = getRemoteIdentityKey(masterSecret, recipient);
 
     if (identityKey == null) {
       remoteIdentityFingerprint.setText(R.string.VerifyIdentityActivity_recipient_has_no_identity_key);
     } else {
-      remoteIdentityFingerprint.setText(identityKey.getFingerprint());
+      remoteIdentityFingerprint.setText(Hex.toString(identityKey.serialize()));
     }
   }
 

src/org/thoughtcrime/securesms/ViewIdentityActivity.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.widget.TextView;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.util.Hex;
 import org.whispersystems.libsignal.IdentityKey;
 import org.thoughtcrime.securesms.crypto.IdentityKeyParcelable;
 
@@ -54,7 +55,7 @@ public class ViewIdentityActivity extends KeyScanningActivity {
     if (identityKey == null) {
       identityFingerprint.setText(R.string.ViewIdentityActivity_you_do_not_have_an_identity_key);
     } else {
-      identityFingerprint.setText(identityKey.getFingerprint());
+      identityFingerprint.setText(Hex.toString(identityKey.serialize()));
     }
   }
 

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/5376
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/2f64b84838f56818775eba401c26be5908140f7b

src/org/smssecure/smssecure/VerifyIdentityActivity.java
@@ -29,6 +29,7 @@ import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.crypto.storage.SMSSecureSessionStore;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.RecipientFactory;
+import org.smssecure.smssecure.util.Hex;
 import org.whispersystems.libaxolotl.AxolotlAddress;
 import org.whispersystems.libaxolotl.IdentityKey;
 import org.whispersystems.libaxolotl.state.SessionRecord;
@@ -74,14 +75,14 @@ public class VerifyIdentityActivity extends KeyScanningActivity {
       return;
     }
 
-    localIdentityFingerprint.setText(IdentityKeyUtil.getIdentityKey(this).getFingerprint());
+    localIdentityFingerprint.setText(Hex.toString(IdentityKeyUtil.getIdentityKey(this).serialize()));
 
     IdentityKey identityKey = getRemoteIdentityKey(masterSecret, recipient);
 
     if (identityKey == null) {
       remoteIdentityFingerprint.setText(R.string.VerifyIdentityActivity_recipient_has_no_identity_key);
     } else {
-      remoteIdentityFingerprint.setText(identityKey.getFingerprint());
+      remoteIdentityFingerprint.setText(Hex.toString(identityKey.serialize()));
     }
   }
 

src/org/smssecure/smssecure/ViewIdentityActivity.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.widget.TextView;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.Hex;
 import org.whispersystems.libaxolotl.IdentityKey;
 import org.smssecure.smssecure.crypto.IdentityKeyParcelable;
 
@@ -54,7 +55,7 @@ public class ViewIdentityActivity extends KeyScanningActivity {
     if (identityKey == null) {
       identityFingerprint.setText(R.string.VerifyIdentityActivity_you_do_not_have_an_identity_key);
     } else {
-      identityFingerprint.setText(identityKey.getFingerprint());
+      identityFingerprint.setText(Hex.toString(identityKey.serialize()));
     }
   }
 