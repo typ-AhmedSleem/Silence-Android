SamSprung/Signal-Android

Extract compose subhint for SIM name to string resource
Fixes #5304
Closes #5305
// FREEBIE

src/org/thoughtcrime/securesms/components/ComposeText.java
@@ -11,6 +11,7 @@ import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
 import android.view.inputmethod.EditorInfo;
 
+import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.TransportOption;
 import org.thoughtcrime.securesms.components.emoji.EmojiEditText;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
@@ -104,6 +105,9 @@ public class ComposeText extends EmojiEditText {
 
     setInputType(inputType);
     setImeOptions(imeOptions);
-    setHint(transport.getComposeHint(), transport.getSimName().isPresent() ? "From " + transport.getSimName().get() : null);
+    setHint(transport.getComposeHint(),
+            transport.getSimName().isPresent()
+                ? getContext().getString(R.string.conversation_activity__from_sim_name, transport.getSimName().get())
+                : null);
   }
 }

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/5304
Closes https://github.com/WhisperSystems/TextSecure/pull/5305
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/ba960a82e217a4ce31bc8e7118cf6700c07d9a2f

src/org/smssecure/smssecure/components/ComposeText.java
@@ -14,6 +14,7 @@ import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 import android.view.inputmethod.EditorInfo;
 
+import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.TransportOption;
 import org.smssecure.smssecure.components.emoji.EmojiEditText;
 import org.smssecure.smssecure.util.SMSSecurePreferences;
@@ -113,6 +114,9 @@ public class ComposeText extends EmojiEditText {
 
     setInputType(inputType);
     setImeOptions(imeOptions);
-    setHint(transport.getComposeHint(), transport.getSimName().isPresent() ? "From " + transport.getSimName().get() : null);
+    setHint(transport.getComposeHint(),
+            transport.getSimName().isPresent()
+                ? getContext().getString(R.string.conversation_activity__from_sim_name, transport.getSimName().get())
+                : null);
   }
 }