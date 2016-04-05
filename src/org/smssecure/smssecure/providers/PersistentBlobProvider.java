SamSprung/Signal-Android

Add MIME type to PersistentBlobProvider
Fixes #4536
Closes #4689

src/org/thoughtcrime/securesms/providers/PersistentBlobProvider.java
@@ -1,16 +1,18 @@
 package org.thoughtcrime.securesms.providers;
 
+import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
 import org.thoughtcrime.securesms.crypto.EncryptingPartOutputStream;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
-import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.Util;
 
 import java.io.ByteArrayInputStream;
@@ -18,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,12 +30,14 @@ public class PersistentBlobProvider {
 
   private static final String TAG = PersistentBlobProvider.class.getSimpleName();
 
-  private static final String     URI_STRING    = "content://org.thoughtcrime.securesms/capture";
-  public  static final Uri        CONTENT_URI   = Uri.parse(URI_STRING);
-  public  static final String     AUTHORITY     = "org.thoughtcrime.securesms";
-  public  static final String     EXPECTED_PATH = "capture/*/#";
-  private static final int        MATCH         = 1;
-  private static final UriMatcher MATCHER       = new UriMatcher(UriMatcher.NO_MATCH) {{
+  private static final String     URI_STRING            = "content://org.thoughtcrime.securesms/capture";
+  public  static final Uri        CONTENT_URI           = Uri.parse(URI_STRING);
+  public  static final String     AUTHORITY             = "org.thoughtcrime.securesms";
+  public  static final String     EXPECTED_PATH         = "capture/*/*/#";
+  private static final int        MIMETYPE_PATH_SEGMENT = 1;
+  private static final String     BLOB_EXTENSION        = "blob";
+  private static final int        MATCH                 = 1;
+  private static final UriMatcher MATCHER               = new UriMatcher(UriMatcher.NO_MATCH) {{
     addURI(AUTHORITY, EXPECTED_PATH, MATCH);
   }};
 
@@ -51,7 +54,8 @@ public class PersistentBlobProvider {
     return instance;
   }
 
-  private final Context context;
+  private final Context           context;
+  @SuppressLint("UseSparseArrays")
   private final Map<Long, byte[]> cache    = Collections.synchronizedMap(new HashMap<Long, byte[]>());
   private final ExecutorService   executor = Executors.newCachedThreadPool();
 
@@ -60,22 +64,27 @@ public class PersistentBlobProvider {
   }
 
   public Uri create(@NonNull MasterSecret masterSecret,
-                    @NonNull byte[] imageBytes)
+                    @NonNull byte[] blobBytes,
+                    @NonNull String mimeType)
   {
     final long id = System.currentTimeMillis();
-    cache.put(id, imageBytes);
-    return create(masterSecret, new ByteArrayInputStream(imageBytes), id);
+    cache.put(id, blobBytes);
+    return create(masterSecret, new ByteArrayInputStream(blobBytes), id, mimeType);
   }
 
   public Uri create(@NonNull MasterSecret masterSecret,
-                    @NonNull InputStream input)
+                    @NonNull InputStream input,
+                    @NonNull String mimeType)
   {
-    return create(masterSecret, input, System.currentTimeMillis());
+    return create(masterSecret, input, System.currentTimeMillis(), mimeType);
   }
 
-  private Uri create(MasterSecret masterSecret, InputStream input, long id) {
+  private Uri create(MasterSecret masterSecret, InputStream input, long id, String mimeType) {
     persistToDisk(masterSecret, id, input);
-    final Uri uniqueUri = Uri.withAppendedPath(CONTENT_URI, String.valueOf(System.currentTimeMillis()));
+    final Uri uniqueUri = CONTENT_URI.buildUpon()
+                                     .appendPath(mimeType)
+                                     .appendEncodedPath(String.valueOf(System.currentTimeMillis()))
+                                     .build();
     return ContentUris.withAppendedId(uniqueUri, id);
   }
 
@@ -97,10 +106,9 @@ public class PersistentBlobProvider {
     });
   }
 
-  public Uri createForExternal() throws IOException {
-    return Uri.fromFile(new File(getExternalDir(context), String.valueOf(System.currentTimeMillis()) + ".jpg"))
-              .buildUpon()
-              .build();
+  public Uri createForExternal(@NonNull String mimeType) throws IOException {
+    return Uri.fromFile(new File(getExternalDir(context),
+                        String.valueOf(System.currentTimeMillis()) + "." + getExtensionFromMimeType(mimeType)));
   }
 
   public boolean delete(@NonNull Uri uri) {
@@ -121,7 +129,17 @@ public class PersistentBlobProvider {
   }
 
   private File getFile(long id) {
-    return new File(context.getDir("captures", Context.MODE_PRIVATE), id + ".jpg");
+    return new File(context.getDir("captures", Context.MODE_PRIVATE), id + "." + BLOB_EXTENSION);
+  }
+
+  public static @Nullable String getMimeType(@NonNull Context context, @NonNull Uri persistentBlobUri) {
+    if (!isAuthority(context, persistentBlobUri)) return null;
+    return persistentBlobUri.getPathSegments().get(MIMETYPE_PATH_SEGMENT);
+  }
+
+  private static @NonNull String getExtensionFromMimeType(String mimeType) {
+    final String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType);
+    return extension != null ? extension : BLOB_EXTENSION;
   }
 
   private static @NonNull File getExternalDir(Context context) throws IOException {

src/org/thoughtcrime/securesms/util/MediaUtil.java
@@ -19,6 +19,7 @@ import org.thoughtcrime.securesms.mms.ImageSlide;
 import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.mms.VideoSlide;
+import org.thoughtcrime.securesms.providers.PersistentBlobProvider;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -71,10 +72,14 @@ public class MediaUtil {
   }
 
   public static @Nullable String getMimeType(Context context, Uri uri) {
+    if (PersistentBlobProvider.isAuthority(context, uri)) {
+      return PersistentBlobProvider.getMimeType(context, uri);
+    }
+
     String type = context.getContentResolver().getType(uri);
     if (type == null) {
       final String extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
-      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
     }
     return getCorrectedMimeType(type);
   }

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/4536
Closes https://github.com/WhisperSystems/TextSecure/pull/4689
Upstream commit: https://github.com/WhisperSystems/TextSecure/commit/a2f478570a1e2582c11e7a52c4d2fffa8b543fe1

src/org/smssecure/smssecure/providers/PersistentBlobProvider.java
@@ -1,17 +1,18 @@
 package org.smssecure.smssecure.providers;
 
+import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
 import org.smssecure.smssecure.crypto.EncryptingPartOutputStream;
 import org.smssecure.smssecure.crypto.MasterSecret;
-import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.Util;
 
 import java.io.ByteArrayInputStream;
@@ -19,18 +20,24 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PersistentBlobProvider {
-  private static final String     TAG           = PersistentBlobProvider.class.getSimpleName();
-  private static final String     URI_STRING    = "content://org.smssecure.smssecure/capture";
-  public  static final Uri        CONTENT_URI   = Uri.parse(URI_STRING);
-  public  static final String     AUTHORITY     = "org.smssecure.smssecure";
-  public  static final String     EXPECTED_PATH = "capture/*/#";
-  private static final int        MATCH         = 1;
-  private static final UriMatcher MATCHER       = new UriMatcher(UriMatcher.NO_MATCH) {{
+
+  private static final String TAG = PersistentBlobProvider.class.getSimpleName();
+
+  private static final String     URI_STRING            = "content://org.smssecure.smssecure/capture";
+  public  static final Uri        CONTENT_URI           = Uri.parse(URI_STRING);
+  public  static final String     AUTHORITY             = "org.smssecure.smssecure";
+  public  static final String     EXPECTED_PATH         = "capture/*/*/#";
+  private static final int        MIMETYPE_PATH_SEGMENT = 1;
+  private static final String     BLOB_EXTENSION        = "blob";
+  private static final int        MATCH                 = 1;
+  private static final UriMatcher MATCHER               = new UriMatcher(UriMatcher.NO_MATCH) {{
     addURI(AUTHORITY, EXPECTED_PATH, MATCH);
   }};
 
@@ -47,64 +54,71 @@ public class PersistentBlobProvider {
     return instance;
   }
 
-  private final Context context;
-  private final Map<Long, byte[]> cache = new HashMap<>();
+  private final Context           context;
+  @SuppressLint("UseSparseArrays")
+  private final Map<Long, byte[]> cache    = Collections.synchronizedMap(new HashMap<Long, byte[]>());
+  private final ExecutorService   executor = Executors.newCachedThreadPool();
 
   private PersistentBlobProvider(Context context) {
     this.context = context.getApplicationContext();
   }
 
   public Uri create(@NonNull MasterSecret masterSecret,
-                    @NonNull Recipients recipients,
-                    @NonNull byte[] imageBytes)
+                    @NonNull byte[] blobBytes,
+                    @NonNull String mimeType)
   {
     final long id = System.currentTimeMillis();
-    cache.put(id, imageBytes);
-    return create(masterSecret, new ByteArrayInputStream(imageBytes), id);
+    cache.put(id, blobBytes);
+    return create(masterSecret, new ByteArrayInputStream(blobBytes), id, mimeType);
   }
 
   public Uri create(@NonNull MasterSecret masterSecret,
-                    @NonNull InputStream input)
+                    @NonNull InputStream input,
+                    @NonNull String mimeType)
   {
-    return create(masterSecret, input, System.currentTimeMillis());
+    return create(masterSecret, input, System.currentTimeMillis(), mimeType);
   }
 
-  private Uri create(MasterSecret masterSecret, InputStream input, long id) {
+  private Uri create(MasterSecret masterSecret, InputStream input, long id, String mimeType) {
     persistToDisk(masterSecret, id, input);
-    final Uri uniqueUri = Uri.withAppendedPath(CONTENT_URI, String.valueOf(System.currentTimeMillis()));
+    final Uri uniqueUri = CONTENT_URI.buildUpon()
+                                     .appendPath(mimeType)
+                                     .appendEncodedPath(String.valueOf(System.currentTimeMillis()))
+                                     .build();
     return ContentUris.withAppendedId(uniqueUri, id);
   }
 
-  private void persistToDisk(final MasterSecret masterSecret, final long id,
-                             final InputStream input)
-  {
-    new AsyncTask<Void, Void, Void>() {
-      @Override protected Void doInBackground(Void... params) {
+  private void persistToDisk(final MasterSecret masterSecret, final long id, final InputStream input) {
+    executor.submit(new Runnable() {
+      @Override
+      public void run() {
         try {
-          final OutputStream output = new EncryptingPartOutputStream(getFile(id), masterSecret);
+          OutputStream output = new EncryptingPartOutputStream(getFile(id), masterSecret);
+          Log.w(TAG, "Starting stream copy....");
           Util.copy(input, output);
+          Log.w(TAG, "Stream copy finished...");
         } catch (IOException e) {
           Log.w(TAG, e);
         }
-        return null;
-      }
 
-      @Override protected void onPostExecute(Void aVoid) {
         cache.remove(id);
       }
-    }.execute();
+    });
   }
 
-  public Uri createForExternal() throws IOException {
-    return Uri.fromFile(new File(getExternalDir(context), String.valueOf(System.currentTimeMillis()) + ".jpg"))
-              .buildUpon()
-              .build();
+  public Uri createForExternal(@NonNull String mimeType) throws IOException {
+    return Uri.fromFile(new File(getExternalDir(context),
+                        String.valueOf(System.currentTimeMillis()) + "." + getExtensionFromMimeType(mimeType)));
   }
 
   public boolean delete(@NonNull Uri uri) {
     switch (MATCHER.match(uri)) {
-    case MATCH: return getFile(ContentUris.parseId(uri)).delete();
-    default:    return new File(uri.getPath()).delete();
+    case MATCH:
+      long id = ContentUris.parseId(uri);
+      cache.remove(id);
+      return getFile(ContentUris.parseId(uri)).delete();
+    default:
+      return new File(uri.getPath()).delete();
     }
   }
 
@@ -115,7 +129,17 @@ public class PersistentBlobProvider {
   }
 
   private File getFile(long id) {
-    return new File(context.getDir("captures", Context.MODE_PRIVATE), id + ".jpg");
+    return new File(context.getDir("captures", Context.MODE_PRIVATE), id + "." + BLOB_EXTENSION);
+  }
+
+  public static @Nullable String getMimeType(@NonNull Context context, @NonNull Uri persistentBlobUri) {
+    if (!isAuthority(context, persistentBlobUri)) return null;
+    return persistentBlobUri.getPathSegments().get(MIMETYPE_PATH_SEGMENT);
+  }
+
+  private static @NonNull String getExtensionFromMimeType(String mimeType) {
+    final String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType);
+    return extension != null ? extension : BLOB_EXTENSION;
   }
 
   private static @NonNull File getExternalDir(Context context) throws IOException {
@@ -131,4 +155,5 @@ public class PersistentBlobProvider {
       return false;
     }
   }
+
 }

src/org/smssecure/smssecure/util/MediaUtil.java
@@ -19,6 +19,7 @@ import org.smssecure.smssecure.mms.ImageSlide;
 import org.smssecure.smssecure.mms.PartAuthority;
 import org.smssecure.smssecure.mms.Slide;
 import org.smssecure.smssecure.mms.VideoSlide;
+import org.smssecure.smssecure.providers.PersistentBlobProvider;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -71,10 +72,14 @@ public class MediaUtil {
   }
 
   public static @Nullable String getMimeType(Context context, Uri uri) {
+    if (PersistentBlobProvider.isAuthority(context, uri)) {
+      return PersistentBlobProvider.getMimeType(context, uri);
+    }
+
     String type = context.getContentResolver().getType(uri);
     if (type == null) {
       final String extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
-      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
     }
     return getCorrectedMimeType(type);
   }