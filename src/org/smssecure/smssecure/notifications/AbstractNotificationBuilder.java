SamSprung/Signal-Android

Respect privacy settings on ticker content for Android <5
Fixes #5517
Closes #5592

// FREEBIE

src/org/thoughtcrime/securesms/notifications/AbstractNotificationBuilder.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 
+import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.database.RecipientPreferenceDatabase;
 import org.thoughtcrime.securesms.preferences.NotificationPrivacyPreference;
 import org.thoughtcrime.securesms.recipients.Recipient;
@@ -58,6 +59,16 @@ public abstract class AbstractNotificationBuilder extends NotificationCompat.Bui
       setLights(Color.parseColor(ledColor),
                 Integer.parseInt(blinkPatternArray[0]),
                 Integer.parseInt(blinkPatternArray[1]));
+    }
+  }
+
+  public void setTicker(@NonNull Recipient recipient, @Nullable CharSequence message) {
+    if (privacy.isDisplayMessage()) {
+      setTicker(getStyledMessage(recipient, message));
+    } else if (privacy.isDisplayContact()) {
+      setTicker(getStyledMessage(recipient, context.getString(R.string.AbstractNotificationBuilder_new_message)));
+    } else {
+      setTicker(context.getString(R.string.AbstractNotificationBuilder_new_message));
     }
   }
 

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/TextSecure/issues/5517
Closes https://github.com/WhisperSystems/TextSecure/pull/5592
Upstream commit: https://github.com/WhisperSystems/Signal-Android/commit/a370f086c0dcbd93b4c89abc410fb9f4708e2945

src/org/smssecure/smssecure/notifications/AbstractNotificationBuilder.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 
+import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.database.RecipientPreferenceDatabase;
 import org.smssecure.smssecure.preferences.NotificationPrivacyPreference;
 import org.smssecure.smssecure.recipients.Recipient;
@@ -62,6 +63,16 @@ public abstract class AbstractNotificationBuilder extends NotificationCompat.Bui
       setLights(Color.parseColor(ledColor),
                 Integer.parseInt(blinkPatternArray[0]),
                 Integer.parseInt(blinkPatternArray[1]));
+    }
+  }
+
+  public void setTicker(@NonNull Recipient recipient, @Nullable CharSequence message) {
+    if (privacy.isDisplayMessage()) {
+      setTicker(getStyledMessage(recipient, message));
+    } else if (privacy.isDisplayContact()) {
+      setTicker(getStyledMessage(recipient, context.getString(R.string.AbstractNotificationBuilder_new_message)));
+    } else {
+      setTicker(context.getString(R.string.AbstractNotificationBuilder_new_message));
     }
   }
 