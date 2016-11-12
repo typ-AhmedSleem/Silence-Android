signalapp/Signal-Android

Let LogSubmitActivity adapt to theme
Addresses #2625
// FREEBIE

src/org/thoughtcrime/securesms/LogSubmitActivity.java
@@ -9,16 +9,20 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.thoughtcrime.securesms.util.DynamicTheme;
 import org.whispersystems.libpastelog.SubmitLogFragment;
 
 /**
  * Activity for submitting logcat logs to a pastebin service.
  */
 public class LogSubmitActivity extends BaseActionBarActivity implements SubmitLogFragment.OnLogSubmittedListener {
+
   private static final String TAG = LogSubmitActivity.class.getSimpleName();
+  private DynamicTheme dynamicTheme = new DynamicTheme();
 
   @Override
   protected void onCreate(Bundle icicle) {
+    dynamicTheme.onCreate(this);
     super.onCreate(icicle);
     setContentView(R.layout.log_submit_activity);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -30,6 +34,7 @@ public class LogSubmitActivity extends BaseActionBarActivity implements SubmitLo
 
   @Override
   protected void onResume() {
+    dynamicTheme.onResume(this);
     super.onResume();
   }
 

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/5665
Upstream commit: https://github.com/WhisperSystems/Signal-Android/commit/24c99c322ca54c3587fe4a349086f7589bf70043

src/org/smssecure/smssecure/LogSubmitActivity.java
@@ -6,16 +6,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.smssecure.smssecure.util.DynamicTheme;
 import org.whispersystems.libpastelog.SubmitLogFragment;
 
 /**
  * Activity for submitting logcat logs to a pastebin service.
  */
 public class LogSubmitActivity extends BaseActionBarActivity implements SubmitLogFragment.OnLogSubmittedListener {
+
   private static final String TAG = LogSubmitActivity.class.getSimpleName();
+  private DynamicTheme dynamicTheme = new DynamicTheme();
 
   @Override
   protected void onCreate(Bundle icicle) {
+    dynamicTheme.onCreate(this);
     super.onCreate(icicle);
     setContentView(R.layout.log_submit_activity);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -27,6 +31,7 @@ public class LogSubmitActivity extends BaseActionBarActivity implements SubmitLo
 
   @Override
   protected void onResume() {
+    dynamicTheme.onResume(this);
     super.onResume();
   }
 