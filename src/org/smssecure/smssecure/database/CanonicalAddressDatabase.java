2fff2271e0fe33bf5377a01cc5d1d7bf4d40b061
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-01-12 16:06:14 -0800

    Fix non-thread-safe operations on canonical id cache
    // FREEBIE
    
    Upstream commit: https://github.com/WhisperSystems/Signal-Android/commit/edac0e85c76edb6c6632481611794cf13bb8456c

--- src/org/smssecure/smssecure/database/CanonicalAddressDatabase.java
+++ src/org/smssecure/smssecure/database/CanonicalAddressDatabase.java
@@ -27,20 +27,15 @@ import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.google.i18n.phonenumbers.NumberParseException;
-import com.google.i18n.phonenumbers.PhoneNumberUtil;
-import com.google.i18n.phonenumbers.Phonenumber;
-import com.google.i18n.phonenumbers.ShortNumberInfo;
-
 import org.smssecure.smssecure.util.GroupUtil;
 import org.smssecure.smssecure.util.LRUCache;
 import org.smssecure.smssecure.util.ShortCodeUtil;
 import org.smssecure.smssecure.util.SilencePreferences;
 import org.smssecure.smssecure.util.VisibleForTesting;
-import org.whispersystems.libaxolotl.util.guava.Optional;
 import org.whispersystems.textsecure.api.util.InvalidNumberException;
 import org.whispersystems.textsecure.api.util.PhoneNumberFormatter;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -64,9 +59,9 @@ public class CanonicalAddressDatabase {
   private        DatabaseHelper           databaseHelper;
   private final  Context                  context;
 
-  private final Map<String, Long>        addressCache          = new ConcurrentHashMap<>();
-  private final Map<Long, String>        idCache               = new ConcurrentHashMap<>();
-  private final LRUCache<String, String> formattedAddressCache = new LRUCache<>(100);
+  private final Map<String, Long>   addressCache          = new ConcurrentHashMap<>();
+  private final Map<Long, String>   idCache               = new ConcurrentHashMap<>();
+  private final Map<String, String> formattedAddressCache = Collections.synchronizedMap(new LRUCache<String, String>(100));
 
   public synchronized static CanonicalAddressDatabase getInstance(Context context) {
     if (instance == null)
