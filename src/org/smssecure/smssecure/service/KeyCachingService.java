signalapp/Signal-Android

A slight foray into the modern era
// FREEBIE

src/org/thoughtcrime/securesms/service/KeyCachingService.java
@@ -265,17 +265,16 @@ public class KeyCachingService extends Service {
   }
 
   private void foregroundServiceLegacy() {
-    Notification notification  = new Notification(R.drawable.icon_cached,
-                                                  getString(R.string.KeyCachingService_signal_passphrase_cached),
-                                                  System.currentTimeMillis());
-    notification.setLatestEventInfo(getApplicationContext(),
-                                    getString(R.string.KeyCachingService_passphrase_cached),
-                                    getString(R.string.KeyCachingService_signal_passphrase_cached),
-                                    buildLaunchIntent());
-    notification.tickerText = null;
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+    builder.setSmallIcon(R.drawable.icon_cached);
+    builder.setWhen(System.currentTimeMillis());
+
+    builder.setContentTitle(getString(R.string.KeyCachingService_passphrase_cached));
+    builder.setContentText(getString(R.string.KeyCachingService_signal_passphrase_cached));
+    builder.setContentIntent(buildLaunchIntent());
 
     stopForeground(true);
-    startForeground(SERVICE_RUNNING_ID, notification);
+    startForeground(SERVICE_RUNNING_ID, builder.build());
   }
 
   private void foregroundService() {

typ-AhmedSleem/Silence-Android

Upstream commit: https://github.com/WhisperSystems/Signal-Android/commit/6308e6491a336b12ff2ec7af2c4023c7cf0b10eb

src/org/smssecure/smssecure/service/KeyCachingService.java
@@ -264,17 +264,16 @@ public class KeyCachingService extends Service {
   }
 
   private void foregroundServiceLegacy() {
-    Notification notification  = new Notification(R.drawable.icon_cached,
-                                                  getString(R.string.KeyCachingService_silence_passphrase_cached),
-                                                  System.currentTimeMillis());
-    notification.setLatestEventInfo(getApplicationContext(),
-                                    getString(R.string.KeyCachingService_passphrase_cached),
-                                    getString(R.string.KeyCachingService_silence_passphrase_cached),
-                                    buildLaunchIntent());
-    notification.tickerText = null;
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+    builder.setSmallIcon(R.drawable.icon_cached);
+    builder.setWhen(System.currentTimeMillis());
+
+    builder.setContentTitle(getString(R.string.KeyCachingService_passphrase_cached));
+    builder.setContentText(getString(R.string.KeyCachingService_silence_passphrase_cached));
+    builder.setContentIntent(buildLaunchIntent());
 
     stopForeground(true);
-    startForeground(SERVICE_RUNNING_ID, notification);
+    startForeground(SERVICE_RUNNING_ID, builder.build());
   }
 
   private void foregroundService() {