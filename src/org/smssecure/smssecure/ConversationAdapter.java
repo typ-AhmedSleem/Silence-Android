signalapp/Signal-Android

Support for sticky date headers
Closes #4696
// FREEBIE

src/org/thoughtcrime/securesms/ConversationAdapter.java
@@ -29,6 +29,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.CursorRecyclerViewAdapter;
@@ -39,11 +40,17 @@ import org.thoughtcrime.securesms.database.SmsDatabase;
 import org.thoughtcrime.securesms.database.model.MessageRecord;
 import org.thoughtcrime.securesms.database.model.MmsMessageRecord;
 import org.thoughtcrime.securesms.recipients.Recipients;
+import org.thoughtcrime.securesms.util.DateUtils;
 import org.thoughtcrime.securesms.util.LRUCache;
+import org.thoughtcrime.securesms.util.StickyHeaderDecoration;
+import org.thoughtcrime.securesms.util.Util;
 import org.thoughtcrime.securesms.util.ViewUtil;
+import org.thoughtcrime.securesms.ConversationAdapter.HeaderViewHolder;
 
 import java.lang.ref.SoftReference;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
@@ -59,6 +66,7 @@ import java.util.Set;
  */
 public class ConversationAdapter <V extends View & BindableConversationItem>
     extends CursorRecyclerViewAdapter<ConversationAdapter.ViewHolder>
+  implements StickyHeaderDecoration.StickyHeaderAdapter<HeaderViewHolder>
 {
 
   private static final int MAX_CACHE_SIZE = 40;
@@ -82,6 +90,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
   private final @NonNull  Recipients        recipients;
   private final @NonNull  MmsSmsDatabase    db;
   private final @NonNull  LayoutInflater    inflater;
+  private final @NonNull  Calendar          calendar;
 
   protected static class ViewHolder extends RecyclerView.ViewHolder {
     public <V extends View & BindableConversationItem> ViewHolder(final @NonNull V itemView) {
@@ -93,6 +102,21 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
       return (V)itemView;
     }
   }
+
+
+  protected static class HeaderViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+
+    public HeaderViewHolder(View itemView) {
+      super(itemView);
+      textView = ViewUtil.findById(itemView, R.id.text);
+    }
+
+    public void setText(CharSequence text) {
+      textView.setText(text);
+    }
+  }
+
 
   public interface ItemClickListener {
     void onItemClick(MessageRecord item);
@@ -109,6 +133,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
     this.recipients    = null;
     this.inflater      = null;
     this.db            = null;
+    this.calendar      = null;
   }
 
   public ConversationAdapter(@NonNull Context context,
@@ -125,6 +150,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
     this.recipients    = recipients;
     this.inflater      = LayoutInflater.from(context);
     this.db            = DatabaseFactory.getMmsSmsDatabase(context);
+    this.calendar      = Calendar.getInstance();
 
     setHasStableIds(true);
   }
@@ -137,10 +163,8 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
 
   @Override
   public void onBindItemViewHolder(ViewHolder viewHolder, @NonNull Cursor cursor) {
-    long start = System.currentTimeMillis();
-    long          id            = cursor.getLong(cursor.getColumnIndexOrThrow(SmsDatabase.ID));
-    String        type          = cursor.getString(cursor.getColumnIndexOrThrow(MmsSmsDatabase.TRANSPORT));
-    MessageRecord messageRecord = getMessageRecord(id, cursor, type);
+    long          start         = System.currentTimeMillis();
+    MessageRecord messageRecord = getMessageRecord(cursor);
 
     viewHolder.getView().bind(masterSecret, messageRecord, locale, batchSelected, recipients);
     Log.w(TAG, "Bind time: " + (System.currentTimeMillis() - start));
@@ -191,11 +215,9 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
 
   @Override
   public int getItemViewType(@NonNull Cursor cursor) {
-    long          id            = cursor.getLong(cursor.getColumnIndexOrThrow(MmsSmsColumns.ID));
-    String        type          = cursor.getString(cursor.getColumnIndexOrThrow(MmsSmsDatabase.TRANSPORT));
-    MessageRecord messageRecord = getMessageRecord(id, cursor, type);
+    MessageRecord messageRecord = getMessageRecord(cursor);
 
-    if (messageRecord.isGroupAction() || messageRecord.isCallLog() || messageRecord.isJoined() || 
+    if (messageRecord.isGroupAction() || messageRecord.isCallLog() || messageRecord.isJoined() ||
         messageRecord.isExpirationTimerUpdate() || messageRecord.isEndSession() || messageRecord.isIdentityUpdate()) {
       return MESSAGE_TYPE_UPDATE;
     } else if (hasAudio(messageRecord)) {
@@ -216,7 +238,10 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
     return cursor.getLong(cursor.getColumnIndexOrThrow(MmsSmsColumns.UNIQUE_ROW_ID));
   }
 
-  private MessageRecord getMessageRecord(long messageId, Cursor cursor, String type) {
+  private MessageRecord getMessageRecord(Cursor cursor) {
+    long   messageId = cursor.getLong(cursor.getColumnIndexOrThrow(MmsSmsColumns.ID));
+    String type      = cursor.getString(cursor.getColumnIndexOrThrow(MmsSmsDatabase.TRANSPORT));
+
     final SoftReference<MessageRecord> reference = messageRecordCache.get(type + messageId);
     if (reference != null) {
       final MessageRecord record = reference.get();
@@ -254,4 +279,26 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
   private boolean hasThumbnail(MessageRecord messageRecord) {
     return messageRecord.isMms() && ((MmsMessageRecord)messageRecord).getSlideDeck().getThumbnailSlide() != null;
   }
+
+
+  @Override
+  public long getHeaderId(int position) {
+    Cursor        cursor = getCursorAtPositionOrThrow(position);
+    MessageRecord record = getMessageRecord(cursor);
+
+    calendar.setTime(new Date(record.getDateSent()));
+    return Util.hashCode(calendar.get(Calendar.YEAR), calendar.get(Calendar.DAY_OF_YEAR));
+  }
+
+  @Override
+  public HeaderViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+    return new HeaderViewHolder(LayoutInflater.from(getContext()).inflate(R.layout.conversation_item_header, parent, false));
+  }
+
+  @Override
+  public void onBindHeaderViewHolder(HeaderViewHolder viewHolder, int position) {
+    Cursor cursor = getCursorAtPositionOrThrow(position);
+    viewHolder.setText(DateUtils.getRelativeDate(getContext(), locale, getMessageRecord(cursor).getDateReceived()));
+  }
 }
+

src/org/thoughtcrime/securesms/ConversationFragment.java
@@ -32,7 +32,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ItemAnimator.ItemAnimatorFinishedListener;
 import android.support.v7.widget.RecyclerView.OnScrollListener;
 import android.text.ClipboardManager;
 import android.text.TextUtils;
@@ -60,6 +59,7 @@ import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.sms.MessageSender;
 import org.thoughtcrime.securesms.util.SaveAttachmentTask;
 import org.thoughtcrime.securesms.util.SaveAttachmentTask.Attachment;
+import org.thoughtcrime.securesms.util.StickyHeaderDecoration;
 import org.thoughtcrime.securesms.util.ViewUtil;
 import org.thoughtcrime.securesms.util.task.ProgressDialogAsyncTask;
 
@@ -170,7 +170,10 @@ public class ConversationFragment extends Fragment
 
   private void initializeListAdapter() {
     if (this.recipients != null && this.threadId != -1) {
-      list.setAdapter(new ConversationAdapter(getActivity(), masterSecret, locale, selectionClickListener, null, this.recipients));
+      ConversationAdapter adapter = new ConversationAdapter(getActivity(), masterSecret, locale, selectionClickListener, null, this.recipients);
+      list.setAdapter(adapter);
+      list.addItemDecoration(new StickyHeaderDecoration(adapter, false));
+
       getLoaderManager().restartLoader(0, Bundle.EMPTY, this);
     }
   }

src/org/thoughtcrime/securesms/contacts/ContactSelectionListAdapter.java
@@ -35,7 +35,7 @@ import android.widget.TextView;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.components.RecyclerViewFastScroller.FastScrollAdapter;
-import org.thoughtcrime.securesms.ContactSelectionListFragment.StickyHeaderAdapter;
+import org.thoughtcrime.securesms.util.StickyHeaderDecoration.StickyHeaderAdapter;
 import org.thoughtcrime.securesms.contacts.ContactSelectionListAdapter.HeaderViewHolder;
 import org.thoughtcrime.securesms.contacts.ContactSelectionListAdapter.ViewHolder;
 import org.thoughtcrime.securesms.database.CursorRecyclerViewAdapter;

typ-AhmedSleem/Silence-Android

Closes https://github.com/WhisperSystems/TextSecure/pull/4696
Upstream commit: https://github.com/WhisperSystems/Signal-Android/commit/4c815db076bdad365c785857e690982bfdb63aa3

src/org/smssecure/smssecure/ConversationAdapter.java
@@ -28,6 +28,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.smssecure.smssecure.util.Conversions;
 import org.smssecure.smssecure.crypto.MasterSecret;
@@ -39,12 +40,18 @@ import org.smssecure.smssecure.database.SmsDatabase;
 import org.smssecure.smssecure.database.model.MediaMmsMessageRecord;
 import org.smssecure.smssecure.database.model.MessageRecord;
 import org.smssecure.smssecure.recipients.Recipients;
+import org.smssecure.smssecure.util.DateUtils;
 import org.smssecure.smssecure.util.LRUCache;
+import org.smssecure.smssecure.util.StickyHeaderDecoration;
+import org.smssecure.smssecure.util.Util;
+import org.smssecure.smssecure.ConversationAdapter.HeaderViewHolder;
 
 import java.lang.ref.SoftReference;
+import java.util.Calendar;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
@@ -63,6 +70,7 @@ import org.smssecure.smssecure.util.VisibleForTesting;
  */
 public class ConversationAdapter <V extends View & BindableConversationItem>
     extends CursorRecyclerViewAdapter<ConversationAdapter.ViewHolder>
+  implements StickyHeaderDecoration.StickyHeaderAdapter<HeaderViewHolder>
 {
 
   private static final int MAX_CACHE_SIZE = 40;
@@ -83,6 +91,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
   private final @NonNull  Recipients        recipients;
   private final @NonNull  MmsSmsDatabase    db;
   private final @NonNull  LayoutInflater    inflater;
+  private final @NonNull  Calendar          calendar;
   private final @NonNull  MessageDigest     digest;
 
   protected static class ViewHolder extends RecyclerView.ViewHolder {
@@ -93,6 +102,19 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
     @SuppressWarnings("unchecked")
     public <V extends View & BindableConversationItem> V getView() {
       return (V)itemView;
+    }
+  }
+
+  protected static class HeaderViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+
+    public HeaderViewHolder(View itemView) {
+      super(itemView);
+      textView = ViewUtil.findById(itemView, R.id.text);
+    }
+
+    public void setText(CharSequence text) {
+      textView.setText(text);
     }
   }
 
@@ -112,6 +134,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
       this.recipients    = null;
       this.inflater      = null;
       this.db            = null;
+      this.calendar      = Calendar.getInstance();
       this.digest        = MessageDigest.getInstance("SHA1");
     } catch (NoSuchAlgorithmException nsae) {
       throw new AssertionError("SHA1 isn't supported!");
@@ -133,6 +156,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
       this.recipients    = recipients;
       this.inflater      = LayoutInflater.from(context);
       this.db            = DatabaseFactory.getMmsSmsDatabase(context);
+      this.calendar      = Calendar.getInstance();
       this.digest        = MessageDigest.getInstance("SHA1");
 
       setHasStableIds(true);
@@ -149,9 +173,8 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
 
   @Override
   public void onBindItemViewHolder(ViewHolder viewHolder, @NonNull Cursor cursor) {
-    long          id            = cursor.getLong(cursor.getColumnIndexOrThrow(SmsDatabase.ID));
-    String        type          = cursor.getString(cursor.getColumnIndexOrThrow(MmsSmsDatabase.TRANSPORT));
-    MessageRecord messageRecord = getMessageRecord(id, cursor, type);
+    long          start         = System.currentTimeMillis();
+    MessageRecord messageRecord = getMessageRecord(cursor);
 
     viewHolder.getView().bind(masterSecret, messageRecord, locale, batchSelected, recipients);
   }
@@ -196,9 +219,7 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
 
   @Override
   public int getItemViewType(@NonNull Cursor cursor) {
-    long          id            = cursor.getLong(cursor.getColumnIndexOrThrow(MmsSmsColumns.ID));
-    String        type          = cursor.getString(cursor.getColumnIndexOrThrow(MmsSmsDatabase.TRANSPORT));
-    MessageRecord messageRecord = getMessageRecord(id, cursor, type);
+    MessageRecord messageRecord = getMessageRecord(cursor);
 
     if (messageRecord.isGroupAction() || messageRecord.isEndSession()) {
       return MESSAGE_TYPE_UPDATE;
@@ -219,7 +240,10 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
     return Conversions.byteArrayToLong(bytes);
   }
 
-  private MessageRecord getMessageRecord(long messageId, Cursor cursor, String type) {
+  private MessageRecord getMessageRecord(Cursor cursor) {
+    long   messageId = cursor.getLong(cursor.getColumnIndexOrThrow(MmsSmsColumns.ID));
+    String type      = cursor.getString(cursor.getColumnIndexOrThrow(MmsSmsDatabase.TRANSPORT));
+
     final SoftReference<MessageRecord> reference = messageRecordCache.get(type + messageId);
     if (reference != null) {
       final MessageRecord record = reference.get();
@@ -254,5 +278,25 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
     return messageRecord.isMms() &&
         !messageRecord.isMmsNotification() &&
         ((MediaMmsMessageRecord)messageRecord).getSlideDeck().getAudioSlide() != null;
+  }
+
+  @Override
+  public long getHeaderId(int position) {
+    Cursor        cursor = getCursorAtPositionOrThrow(position);
+    MessageRecord record = getMessageRecord(cursor);
+
+    calendar.setTime(new Date(record.getDateSent()));
+    return Util.hashCode(calendar.get(Calendar.YEAR), calendar.get(Calendar.DAY_OF_YEAR));
+  }
+
+  @Override
+  public HeaderViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+    return new HeaderViewHolder(LayoutInflater.from(getContext()).inflate(R.layout.conversation_item_header, parent, false));
+  }
+
+  @Override
+  public void onBindHeaderViewHolder(HeaderViewHolder viewHolder, int position) {
+    Cursor cursor = getCursorAtPositionOrThrow(position);
+    viewHolder.setText(DateUtils.getRelativeDate(getContext(), locale, getMessageRecord(cursor).getDateReceived()));
   }
 }

src/org/smssecure/smssecure/ConversationFragment.java
@@ -61,6 +61,7 @@ import org.smssecure.smssecure.sms.MessageSender;
 import org.smssecure.smssecure.util.task.ProgressDialogAsyncTask;
 import org.smssecure.smssecure.util.SaveAttachmentTask;
 import org.smssecure.smssecure.util.SaveAttachmentTask.Attachment;
+import org.smssecure.smssecure.util.StickyHeaderDecoration;
 import org.smssecure.smssecure.util.ViewUtil;
 
 import java.util.Collections;
@@ -169,7 +170,10 @@ public class ConversationFragment extends Fragment
 
   private void initializeListAdapter() {
     if (this.recipients != null && this.threadId != -1) {
-      list.setAdapter(new ConversationAdapter(getActivity(), masterSecret, locale, selectionClickListener, null, this.recipients));
+      ConversationAdapter adapter = new ConversationAdapter(getActivity(), masterSecret, locale, selectionClickListener, null, this.recipients);
+      list.setAdapter(adapter);
+      list.addItemDecoration(new StickyHeaderDecoration(adapter, false));
+
       getLoaderManager().restartLoader(0, Bundle.EMPTY, this);
       list.getItemAnimator().setMoveDuration(120);
     }

src/org/smssecure/smssecure/contacts/ContactSelectionListAdapter.java
@@ -36,7 +36,7 @@ import android.widget.TextView;
 
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.components.RecyclerViewFastScroller.FastScrollAdapter;
-import org.smssecure.smssecure.ContactSelectionListFragment.StickyHeaderAdapter;
+import org.smssecure.smssecure.util.StickyHeaderDecoration.StickyHeaderAdapter;
 import org.smssecure.smssecure.contacts.ContactSelectionListAdapter.HeaderViewHolder;
 import org.smssecure.smssecure.contacts.ContactSelectionListAdapter.ViewHolder;
 import org.smssecure.smssecure.database.CursorRecyclerViewAdapter;