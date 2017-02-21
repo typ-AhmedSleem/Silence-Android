67180b40495f610dbcb584fec297473acba704cf
Bastien Le Querrec <bastien@silence.im>
2017-02-21 11:56:07 +0100

    improve multi-SIM support
    * Each SIM card is now bound to a unique key pair.
    * Display number or slot index if display name is the same for multiple
      cards.
    * Use subcription ID of last received message as default SIM, but do not
      change it if user is in conversation.
    * Update app subscription IDs on new phone
    * Start secure session only for SIM which received message, allow user to
      select SIM when starting a secure session
    
    Fixes #467
    Fixes #497

--- src/org/smssecure/smssecure/util/dualsim/SubscriptionManagerCompat.java
+++ src/org/smssecure/smssecure/util/dualsim/SubscriptionManagerCompat.java
@@ -1,11 +1,16 @@
 package org.smssecure.smssecure.util.dualsim;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.telephony.SmsManager;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import org.smssecure.smssecure.util.ServiceUtil;
 
 import org.whispersystems.libsignal.util.guava.Optional;
 
@@ -13,43 +18,108 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class SubscriptionManagerCompat {
+  private static final String TAG = SubscriptionManagerCompat.class.getSimpleName();
 
-  private final Context context;
+  private static SubscriptionManagerCompat instance;
 
-  public SubscriptionManagerCompat(Context context) {
+  private final Context                      context;
+  private       List<String>                 displayNameList;
+  private       List<SubscriptionInfoCompat> compatList;
+
+  public static SubscriptionManagerCompat from(Context context) {
+    Log.w(TAG, "from()");
+
+    if (instance == null) {
+      instance = new SubscriptionManagerCompat(context);
+    }
+    return instance;
+  }
+
+  private SubscriptionManagerCompat(Context context) {
     this.context = context.getApplicationContext();
+    this.displayNameList = new LinkedList<String>();
   }
 
   public Optional<SubscriptionInfoCompat> getActiveSubscriptionInfo(int subscriptionId) {
-    if (Build.VERSION.SDK_INT < 22) {
+    if (getActiveSubscriptionInfoList().size() <= 0) {
       return Optional.absent();
     }
 
     SubscriptionInfo subscriptionInfo = SubscriptionManager.from(context).getActiveSubscriptionInfo(subscriptionId);
 
     if (subscriptionInfo != null) {
-      return Optional.of(new SubscriptionInfoCompat(subscriptionId, subscriptionInfo.getDisplayName()));
+      return Optional.of(new SubscriptionInfoCompat(context,
+                                                    subscriptionId,
+                                                    subscriptionInfo.getDisplayName(),
+                                                    subscriptionInfo.getNumber(),
+                                                    subscriptionInfo.getIccId(),
+                                                    subscriptionInfo.getSimSlotIndex()+1,
+                                                    knowThisDisplayNameTwice(subscriptionInfo.getDisplayName())));
     } else {
       return Optional.absent();
     }
   }
 
+  @TargetApi(22)
+  private void updateDisplayNameList(List<SubscriptionInfo> activeSubscriptions) {
+    displayNameList = new LinkedList<String>();
+
+    if (activeSubscriptions != null) {
+      for (SubscriptionInfo subscriptionInfo : activeSubscriptions) {
+        displayNameList.add(subscriptionInfo.getDisplayName().toString());
+      }
+    }
+  }
+
+  public boolean knowThisDisplayNameTwice(CharSequence displayName) {
+    if (displayName == null) return false;
+
+    boolean found = false;
+
+    for (String potentialDisplayName : displayNameList) {
+      if (found && potentialDisplayName != null && potentialDisplayName.equals(displayName.toString()))
+        return true;
+      if (potentialDisplayName != null && potentialDisplayName.equals(displayName.toString()))
+        found = true;
+    }
+    return false;
+  }
+
   public @NonNull List<SubscriptionInfoCompat> getActiveSubscriptionInfoList() {
+    if (compatList == null) return updateActiveSubscriptionInfoList();
+    return compatList;
+  }
+
+  public @NonNull List<SubscriptionInfoCompat> updateActiveSubscriptionInfoList() {
+    compatList = new LinkedList<>();
+
     if (Build.VERSION.SDK_INT < 22) {
-      return new LinkedList<>();
+      TelephonyManager telephonyManager = ServiceUtil.getTelephonyManager(context);
+      compatList.add(new SubscriptionInfoCompat(context,
+                                                -1,
+                                                telephonyManager.getSimOperatorName(),
+                                                telephonyManager.getLine1Number(),
+                                                telephonyManager.getSimSerialNumber(),
+                                                1,
+                                                false));
+      return compatList;
     }
 
     List<SubscriptionInfo> subscriptionInfos = SubscriptionManager.from(context).getActiveSubscriptionInfoList();
+    updateDisplayNameList(subscriptionInfos);
 
     if (subscriptionInfos == null || subscriptionInfos.isEmpty()) {
-      return new LinkedList<>();
+      return compatList;
     }
 
-    List<SubscriptionInfoCompat> compatList = new LinkedList<>();
-
     for (SubscriptionInfo subscriptionInfo : subscriptionInfos) {
-      compatList.add(new SubscriptionInfoCompat(subscriptionInfo.getSubscriptionId(),
-                                                subscriptionInfo.getDisplayName()));
+      compatList.add(new SubscriptionInfoCompat(context,
+                                                subscriptionInfo.getSubscriptionId(),
+                                                subscriptionInfo.getDisplayName(),
+                                                subscriptionInfo.getNumber(),
+                                                subscriptionInfo.getIccId(),
+                                                subscriptionInfo.getSimSlotIndex()+1,
+                                                knowThisDisplayNameTwice(subscriptionInfo.getDisplayName())));
     }
 
     return compatList;
