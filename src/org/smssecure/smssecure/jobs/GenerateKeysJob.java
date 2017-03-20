dc9d0fd3f78608d293a44c02f0aeeb90dddd183d
Bastien Le Querrec <bastien@silence.im>
2017-03-20 20:46:09 +0100

    fix keys management if app subscription ID is not equal to device's one

--- src/org/smssecure/smssecure/jobs/GenerateKeysJob.java
+++ src/org/smssecure/smssecure/jobs/GenerateKeysJob.java
@@ -5,7 +5,7 @@ import android.util.Log;
 
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.jobs.requirements.MasterSecretRequirement;
-import org.smssecure.smssecure.util.dualsim.DualSimUpgradeUtil;
+import org.smssecure.smssecure.util.dualsim.DualSimUtil;
 import org.smssecure.smssecure.util.dualsim.SubscriptionInfoCompat;
 import org.smssecure.smssecure.util.dualsim.SubscriptionManagerCompat;
 import org.whispersystems.jobqueue.JobParameters;
@@ -13,12 +13,12 @@ import org.whispersystems.jobqueue.JobParameters;
 
 import java.util.List;
 
-public class CheckSimStateJob extends MasterSecretJob {
-  private static final String TAG = CheckSimStateJob.class.getSimpleName();
+public class GenerateKeysJob extends MasterSecretJob {
+  private static final String TAG = GenerateKeysJob.class.getSimpleName();
 
   private List<SubscriptionInfoCompat> activeSubscriptions;
 
-  public CheckSimStateJob(Context context) {
+  public GenerateKeysJob(Context context) {
     super(context, JobParameters.newBuilder()
                                 .withPersistence()
                                 .withRequirement(new MasterSecretRequirement(context))
@@ -33,10 +33,8 @@ public class CheckSimStateJob extends MasterSecretJob {
   @Override
   public void onRun(MasterSecret masterSecret) {
     Log.w(TAG, "onRun()");
-    List<SubscriptionInfoCompat> activeSubscriptions = SubscriptionManagerCompat.from(context).getActiveSubscriptionInfoList();
-    DualSimUpgradeUtil.bindAppSubscriptionId(context, activeSubscriptions);
-    DualSimUpgradeUtil.generateKeysIfDoNotExist(context, masterSecret, activeSubscriptions);
-    DualSimUpgradeUtil.bindSubscriptionId(context, activeSubscriptions);
+    List<SubscriptionInfoCompat> activeSubscriptions = SubscriptionManagerCompat.from(context).updateActiveSubscriptionInfoList();
+    DualSimUtil.generateKeysIfDoNotExist(context, masterSecret, activeSubscriptions);
   }
 
   @Override
