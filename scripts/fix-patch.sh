64ca1e2396e16465f0407556e08b6132b67177ea
Bastien Le Querrec <bastien@silence.im>
2017-03-29 17:03:20 +0200

    update upstream links

--- scripts/fix-patch.sh
+++ scripts/fix-patch.sh
@@ -26,8 +26,8 @@ for file in "$@"; do
            -e 's/queryTextSecureContacts/querySilenceContacts/g' \
            -e 's/@color\/signal/@color\/silence/g' \
            -e 's/@color\/textsecure/@color\/silence/g' \
-           -e 's/^[Ff]ixes #/Fixes https:\/\/github.com\/WhisperSystems\/TextSecure\/issues\//g' \
-           -e 's/^[Cc]loses #/Closes https:\/\/github.com\/WhisperSystems\/TextSecure\/pull\//g' \
+           -e 's/^[Ff]ixes #/Fixes https:\/\/github.com\/WhisperSystems\/Signal-Android\/issues\//g' \
+           -e 's/^[Cc]loses #/Closes https:\/\/github.com\/WhisperSystems\/Signal-Android\/pull\//g' \
            -e 's/^SingleRecipientNotificationBuilder_new_textsecure_message/SingleRecipientNotificationBuilder_new_smssecure_message/g' \
            -e 's/src\/org.smssecure.smssecure.util.SilencePreferences.java/src\/org\/smssecure\/smssecure\/util\/SilencePreferences.java/g' \
            -e "0,/^---/s//\nUpstream commit: https:\/\/github.com\/WhisperSystems\/Signal-Android\/commit\/$commit\n---/" \
