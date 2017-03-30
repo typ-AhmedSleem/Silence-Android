c5cbcb1d5b84dfc0fc3348d9a7f923fa78e96df7
Bastien Le Querrec <bastien@silence.im>
2017-03-30 13:26:51 +0200

    fix crash if no SIM card is inserted

--- src/org/smssecure/smssecure/ConversationActivity.java
+++ src/org/smssecure/smssecure/ConversationActivity.java
@@ -1300,6 +1300,10 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   }
 
   private void sendMessage() {
+    TransportOption transportOption = sendButton.getSelectedTransport();
+
+    if (transportOption == null || transportOption.getType() == Type.DISABLED) return;
+
     try {
       Recipients recipients = getRecipients();
 
