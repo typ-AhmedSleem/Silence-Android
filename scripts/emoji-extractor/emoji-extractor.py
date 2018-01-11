ed6d9145ea5256a37d893189f2acd145f6beb359
Tenma <tenma@nuolezio.org>
2018-01-11 19:19:47 +0100

    Fixed Python 3 compatibility issue for emoji-extractor
    Closes #647

--- scripts/emoji-extractor/emoji-extractor.py
+++ scripts/emoji-extractor/emoji-extractor.py
@@ -2,7 +2,7 @@
 
 import argparse
 import binascii
-import io
+from time import time
 import shutil
 from pathlib import Path
 import xml.etree.ElementTree as ElementTree
@@ -24,44 +24,58 @@ parser.add_argument(
 args = parser.parse_args()
 
 path = Path(args.output)
+
+# Clean working directory
 shutil.rmtree(path, ignore_errors=True)
 path.mkdir()
 
-font_xml = io.StringIO()
-TTFont(args.input).saveXML(font_xml)
-ttx = ElementTree.fromstring(font_xml.getvalue())
+# Load font data
+print("Loading font data...")
+
+t0 = time()
+font = TTFont(args.input)
+font.saveXML('.NotoColorEmoji.ttx')
+ttx = ElementTree.parse('.NotoColorEmoji.ttx').getroot()
+print("Time:", time() - t0)
 
 
-def mass_rename(ligature_set_or_emoji):
+def rename_to_resolved_unicode(glyph_name):
+    """Use TTF cmap (name <-> Unicode code point) mapping to rename an extracted glyph"""
+    glyph_name = str(glyph_name)  # In case it's a path
     cmap = ttx.find('cmap').find('cmap_format_12')
-    code = ''
     for map_element in cmap:
-        if map_element.attrib['name'].lower() == ligature_set_or_emoji:
+        if map_element.attrib['name'].lower() == glyph_name:
             replace_s = '' if len(map_element.attrib['code']) > 4 else '00'
             code = map_element.attrib['code'].replace('0x', replace_s)
-            rename('emoji_{}.png'.format(ligature_set_or_emoji),
+            rename_to('emoji_{}.png'.format(glyph_name),
                    'emoji_u{}.png'.format(code))
+            break  # Accelerate processing
+    else:
+        print('Ignoring ' + glyph_name + '...')
 
 
-def rename(old_name, new_name):
+def rename_to(old_name, new_name):
+    """Rename an extracted glyph"""
     print('Renaming {} to {}'.format(old_name, new_name))
     try:
         (path / old_name).rename(path / new_name)
-    except:
-        print('!! Cannot rename {}'.format(old_name))
+    except FileNotFoundError as ex:
+        print(ex.strerror)
 
 
+# Extract all emojis
 for element in ttx.find('CBDT').find('strikedata'):
     data = element.find('rawimagedata').text.split()
     name = element.attrib['name'].lower().replace('uni', 'u')
     image_path = path / 'emoji_{}.png'.format(name)
     print('Extracting {}'.format(image_path.name))
-    emoji = open(image_path, "wb")
+    emoji = open(str(image_path), "wb")
     for char in data:
         hexChar = binascii.unhexlify(char)
         emoji.write(hexChar)
-    emoji.close
+    emoji.close()
 
+print('*** Fixing names of compound emojis ***')
 for ligature_set_xml in ttx.find('GSUB')\
         .find('LookupList')\
         .find('Lookup')\
@@ -73,17 +87,18 @@ for ligature_set_xml in ttx.find('GSUB')\
                 .replace(',', '_')\
                 .replace('uni', 'u')
             glyph = ligature_xml.attrib['glyph']
-            rename('emoji_{}.png'.format(glyph),
-                   'emoji_{}_{}.png'.format(ligature_set, component))
+            if (path / 'emoji_{}.png'.format(glyph)).exists():
+                rename_to('emoji_{}.png'.format(glyph),
+                    'emoji_{}_{}.png'.format(ligature_set, component))
     else:
-        mass_rename(ligature_set)
+        rename_to_resolved_unicode(ligature_set)
 
+print('*** Fixing remaining names ***')
 emojis = path.glob('*.png')
 for emoji in emojis:
     if not emoji.name.startswith('emoji_u'):
         emoji = emoji.with_name(emoji.name.replace('emoji_', '').replace('.png', ''))
-        print('Fixing {}...'.format(emoji.name))
-        mass_rename(emoji)
+        rename_to_resolved_unicode(emoji.name)
 
 # Some flags aren't correctly sorted
 trans_table = {
@@ -99,8 +114,8 @@ trans_table = {
     'fe4ee': '1f1f0_u1f1f7',
 }
 
+# Fix these flag names
 for old, new in trans_table.items():
-    old_path = path / 'emoji_u{}.png'.format(old)
-    new_path = path / 'emoji_u{}.png'.format(new)
-    print('Renaming incorrect {} to {}'.format(old_path.name, new_path.name))
-    old_path.rename(new_path)
+    old_name = 'emoji_u{}.png'.format(old)
+    new_name = 'emoji_u{}.png'.format(new)
+    rename_to(old_name, new_name)
