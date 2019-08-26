SamSprung/Signal-Android

Use FileProvider for external camera capture Uri on L+
Fixes #7237

src/org/thoughtcrime/securesms/providers/PersistentBlobProvider.java
@@ -5,8 +5,10 @@ import android.content.ContentUris;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
@@ -14,6 +16,7 @@ import org.thoughtcrime.securesms.crypto.DecryptingPartInputStream;
 import org.thoughtcrime.securesms.crypto.EncryptingPartOutputStream;
 import org.thoughtcrime.securesms.crypto.MasterCipher;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.util.FileProviderUtil;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libsignal.InvalidMessageException;
 
@@ -128,8 +131,8 @@ public class PersistentBlobProvider {
   }
 
   public Uri createForExternal(@NonNull String mimeType) throws IOException {
-    return Uri.fromFile(new File(getExternalDir(context),
-                        String.valueOf(System.currentTimeMillis()) + "." + getExtensionFromMimeType(mimeType)));
+    File target = new File(getExternalDir(context), String.valueOf(System.currentTimeMillis()) + "." + getExtensionFromMimeType(mimeType));
+    return FileProviderUtil.getUriFor(context, target);
   }
 
   public boolean delete(@NonNull Uri uri) {

src/org/thoughtcrime/securesms/util/FileProviderUtil.java
@@ -1,0 +1,21 @@
+package org.thoughtcrime.securesms.util;
+
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+public class FileProviderUtil {
+
+  private static final String AUTHORITY = "org.thoughtcrime.securesms.fileprovider";
+
+  public static Uri getUriFor(@NonNull Context context, @NonNull File file) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) return FileProvider.getUriForFile(context, AUTHORITY, file);
+    else                                                       return Uri.fromFile(file);
+  }
+
+}

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/Signal-Android/issues/7237
Upstream commit: https://github.com/WhisperSystems/Signal-Android/commit/4cb2ac7b27e7b170a9102af638b98b86e589bc9d

src/org/smssecure/smssecure/providers/PersistentBlobProvider.java
@@ -5,14 +5,17 @@ import android.content.ContentUris;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import org.smssecure.smssecure.crypto.DecryptingPartInputStream;
 import org.smssecure.smssecure.crypto.EncryptingPartOutputStream;
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.FileProviderUtil;
 import org.smssecure.smssecure.util.Util;
 
 import java.io.ByteArrayInputStream;
@@ -107,8 +110,8 @@ public class PersistentBlobProvider {
   }
 
   public Uri createForExternal(@NonNull String mimeType) throws IOException {
-    return Uri.fromFile(new File(getExternalDir(context),
-                        String.valueOf(System.currentTimeMillis()) + "." + getExtensionFromMimeType(mimeType)));
+    File target = new File(getExternalDir(context), String.valueOf(System.currentTimeMillis()) + "." + getExtensionFromMimeType(mimeType));
+    return FileProviderUtil.getUriFor(context, target);
   }
 
   public boolean delete(@NonNull Uri uri) {

src/org/smssecure/smssecure/util/FileProviderUtil.java
@@ -1,0 +1,21 @@
+package org.smssecure.smssecure.util;
+
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+public class FileProviderUtil {
+
+  private static final String AUTHORITY = "org.smssecure.smssecure.fileprovider";
+
+  public static Uri getUriFor(@NonNull Context context, @NonNull File file) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) return FileProvider.getUriForFile(context, AUTHORITY, file);
+    else                                                       return Uri.fromFile(file);
+  }
+
+}