SamSprung/Signal-Android

Show videos in media overview
Fixes #5941
Closes #6152
// FREEBIE

src/org/thoughtcrime/securesms/MediaAdapter.java
@@ -27,18 +27,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
-import org.thoughtcrime.securesms.ImageMediaAdapter.ViewHolder;
+import org.thoughtcrime.securesms.MediaAdapter.ViewHolder;
 import org.thoughtcrime.securesms.components.ThumbnailView;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.CursorRecyclerViewAdapter;
-import org.thoughtcrime.securesms.database.ImageDatabase.ImageRecord;
+import org.thoughtcrime.securesms.database.MediaDatabase.MediaRecord;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.MediaUtil;
 
-public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
-  private static final String TAG = ImageMediaAdapter.class.getSimpleName();
+public class MediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
+  private static final String TAG = MediaAdapter.class.getSimpleName();
 
   private final MasterSecret masterSecret;
   private final long         threadId;
@@ -52,7 +52,7 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
     }
   }
 
-  public ImageMediaAdapter(Context context, MasterSecret masterSecret, Cursor c, long threadId) {
+  public MediaAdapter(Context context, MasterSecret masterSecret, Cursor c, long threadId) {
     super(context, c);
     this.masterSecret = masterSecret;
     this.threadId     = threadId;
@@ -67,40 +67,41 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
   @Override
   public void onBindItemViewHolder(final ViewHolder viewHolder, final @NonNull Cursor cursor) {
     final ThumbnailView imageView   = viewHolder.imageView;
-    final ImageRecord imageRecord = ImageRecord.from(cursor);
+    final MediaRecord   mediaRecord = MediaRecord.from(cursor);
 
-    Slide slide = MediaUtil.getSlideForAttachment(getContext(), imageRecord.getAttachment());
+    Slide slide = MediaUtil.getSlideForAttachment(getContext(), mediaRecord.getAttachment());
 
     if (slide != null) {
       imageView.setImageResource(masterSecret, slide, false);
     }
 
-    imageView.setOnClickListener(new OnMediaClickListener(imageRecord));
+    imageView.setOnClickListener(new OnMediaClickListener(mediaRecord));
   }
 
   private class OnMediaClickListener implements OnClickListener {
-    private final ImageRecord imageRecord;
+    private final MediaRecord mediaRecord;
 
-    private OnMediaClickListener(ImageRecord imageRecord) {
-      this.imageRecord = imageRecord;
+    private OnMediaClickListener(MediaRecord mediaRecord) {
+      this.mediaRecord = mediaRecord;
     }
 
     @Override
     public void onClick(View v) {
-      if (imageRecord.getAttachment().getDataUri() != null) {
+      if (mediaRecord.getAttachment().getDataUri() != null) {
         Intent intent = new Intent(getContext(), MediaPreviewActivity.class);
-        intent.putExtra(MediaPreviewActivity.DATE_EXTRA, imageRecord.getDate());
+        intent.putExtra(MediaPreviewActivity.DATE_EXTRA, mediaRecord.getDate());
+        intent.putExtra(MediaPreviewActivity.SIZE_EXTRA, mediaRecord.getAttachment().getSize());
         intent.putExtra(MediaPreviewActivity.THREAD_ID_EXTRA, threadId);
 
-        if (!TextUtils.isEmpty(imageRecord.getAddress())) {
+        if (!TextUtils.isEmpty(mediaRecord.getAddress())) {
           Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(),
-                                                                           imageRecord.getAddress(),
+                                                                           mediaRecord.getAddress(),
                                                                            true);
           if (recipients != null && recipients.getPrimaryRecipient() != null) {
             intent.putExtra(MediaPreviewActivity.RECIPIENT_EXTRA, recipients.getPrimaryRecipient().getRecipientId());
           }
         }
-        intent.setDataAndType(imageRecord.getAttachment().getDataUri(), imageRecord.getContentType());
+        intent.setDataAndType(mediaRecord.getAttachment().getDataUri(), mediaRecord.getContentType());
         getContext().startActivity(intent);
       }
     }

src/org/thoughtcrime/securesms/MediaOverviewActivity.java
@@ -40,7 +40,7 @@ import android.widget.TextView;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.database.CursorRecyclerViewAdapter;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
-import org.thoughtcrime.securesms.database.ImageDatabase.ImageRecord;
+import org.thoughtcrime.securesms.database.MediaDatabase.MediaRecord;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.Recipient.RecipientModifiedListener;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
@@ -114,8 +114,8 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
 
   private void initializeActionBar() {
     getSupportActionBar().setTitle(recipient == null
-                                   ? getString(R.string.AndroidManifest__media_overview)
-                                   : getString(R.string.AndroidManifest__media_overview_named, recipient.toShortString()));
+                                   ? getString(R.string.AndroidManifest__all_media)
+                                   : getString(R.string.AndroidManifest__all_media_named, recipient.toShortString()));
   }
 
   @Override
@@ -162,11 +162,11 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
                                                                                      R.string.please_wait) {
           @Override
           protected List<SaveAttachmentTask.Attachment> doInBackground(Void... params) {
-            Cursor cursor                                   = DatabaseFactory.getImageDatabase(c).getImagesForThread(threadId);
+            Cursor cursor                                   = DatabaseFactory.getMediaDatabase(c).getMediaForThread(threadId);
             List<SaveAttachmentTask.Attachment> attachments = new ArrayList<>(cursor.getCount());
 
             while (cursor != null && cursor.moveToNext()) {
-              ImageRecord record = ImageRecord.from(cursor);
+              MediaRecord record = MediaRecord.from(cursor);
               attachments.add(new SaveAttachmentTask.Attachment(record.getAttachment().getDataUri(),
                                                                 record.getContentType(),
                                                                 record.getDate()));
@@ -220,7 +220,7 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
   @Override
   public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
     Log.w(TAG, "onLoadFinished()");
-    gridView.setAdapter(new ImageMediaAdapter(this, masterSecret, cursor, threadId));
+    gridView.setAdapter(new MediaAdapter(this, masterSecret, cursor, threadId));
     noImages.setVisibility(gridView.getAdapter().getItemCount() > 0 ? View.GONE : View.VISIBLE);
     invalidateOptionsMenu();
   }
@@ -240,7 +240,7 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
 
     @Override
     public Cursor getCursor() {
-      return DatabaseFactory.getImageDatabase(getContext()).getImagesForThread(threadId);
+      return DatabaseFactory.getMediaDatabase(getContext()).getMediaForThread(threadId);
     }
   }
 }

typ-AhmedSleem/Silence-Android

Fixes https://github.com/WhisperSystems/Signal-Android/issues/5941
Closes https://github.com/WhisperSystems/Signal-Android/pull/6152
Upstream commit: https://github.com/signalapp/Signal-Android/commit/83e31cac074ff7945128af2ccdebc9c2d3cbeae1

src/org/smssecure/smssecure/MediaAdapter.java
@@ -27,18 +27,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
-import org.smssecure.smssecure.ImageMediaAdapter.ViewHolder;
+import org.smssecure.smssecure.MediaAdapter.ViewHolder;
 import org.smssecure.smssecure.components.ThumbnailView;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.CursorRecyclerViewAdapter;
-import org.smssecure.smssecure.database.ImageDatabase.ImageRecord;
+import org.smssecure.smssecure.database.MediaDatabase.MediaRecord;
 import org.smssecure.smssecure.mms.Slide;
 import org.smssecure.smssecure.recipients.RecipientFactory;
 import org.smssecure.smssecure.recipients.Recipients;
 import org.smssecure.smssecure.util.MediaUtil;
 
-public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
-  private static final String TAG = ImageMediaAdapter.class.getSimpleName();
+public class MediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
+  private static final String TAG = MediaAdapter.class.getSimpleName();
 
   private final MasterSecret masterSecret;
   private final long         threadId;
@@ -52,7 +52,7 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
     }
   }
 
-  public ImageMediaAdapter(Context context, MasterSecret masterSecret, Cursor c, long threadId) {
+  public MediaAdapter(Context context, MasterSecret masterSecret, Cursor c, long threadId) {
     super(context, c);
     this.masterSecret = masterSecret;
     this.threadId     = threadId;
@@ -67,40 +67,41 @@ public class ImageMediaAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
   @Override
   public void onBindItemViewHolder(final ViewHolder viewHolder, final @NonNull Cursor cursor) {
     final ThumbnailView imageView   = viewHolder.imageView;
-    final ImageRecord imageRecord = ImageRecord.from(cursor);
+    final MediaRecord   mediaRecord = MediaRecord.from(cursor);
 
-    Slide slide = MediaUtil.getSlideForAttachment(getContext(), imageRecord.getAttachment());
+    Slide slide = MediaUtil.getSlideForAttachment(getContext(), mediaRecord.getAttachment());
 
     if (slide != null) {
       imageView.setImageResource(masterSecret, slide, false);
     }
 
-    imageView.setOnClickListener(new OnMediaClickListener(imageRecord));
+    imageView.setOnClickListener(new OnMediaClickListener(mediaRecord));
   }
 
   private class OnMediaClickListener implements OnClickListener {
-    private final ImageRecord imageRecord;
+    private final MediaRecord mediaRecord;
 
-    private OnMediaClickListener(ImageRecord imageRecord) {
-      this.imageRecord = imageRecord;
+    private OnMediaClickListener(MediaRecord mediaRecord) {
+      this.mediaRecord = mediaRecord;
     }
 
     @Override
     public void onClick(View v) {
-      if (imageRecord.getAttachment().getDataUri() != null) {
+      if (mediaRecord.getAttachment().getDataUri() != null) {
         Intent intent = new Intent(getContext(), MediaPreviewActivity.class);
-        intent.putExtra(MediaPreviewActivity.DATE_EXTRA, imageRecord.getDate());
+        intent.putExtra(MediaPreviewActivity.DATE_EXTRA, mediaRecord.getDate());
+        intent.putExtra(MediaPreviewActivity.SIZE_EXTRA, mediaRecord.getAttachment().getSize());
         intent.putExtra(MediaPreviewActivity.THREAD_ID_EXTRA, threadId);
 
-        if (!TextUtils.isEmpty(imageRecord.getAddress())) {
+        if (!TextUtils.isEmpty(mediaRecord.getAddress())) {
           Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(),
-                                                                           imageRecord.getAddress(),
+                                                                           mediaRecord.getAddress(),
                                                                            true);
           if (recipients != null && recipients.getPrimaryRecipient() != null) {
             intent.putExtra(MediaPreviewActivity.RECIPIENT_EXTRA, recipients.getPrimaryRecipient().getRecipientId());
           }
         }
-        intent.setDataAndType(imageRecord.getAttachment().getDataUri(), imageRecord.getContentType());
+        intent.setDataAndType(mediaRecord.getAttachment().getDataUri(), mediaRecord.getContentType());
         getContext().startActivity(intent);
       }
     }

src/org/smssecure/smssecure/MediaOverviewActivity.java
@@ -40,7 +40,7 @@ import android.widget.TextView;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.CursorRecyclerViewAdapter;
 import org.smssecure.smssecure.database.DatabaseFactory;
-import org.smssecure.smssecure.database.ImageDatabase.ImageRecord;
+import org.smssecure.smssecure.database.MediaDatabase.MediaRecord;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.Recipient.RecipientModifiedListener;
 import org.smssecure.smssecure.recipients.RecipientFactory;
@@ -114,8 +114,8 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
 
   private void initializeActionBar() {
     getSupportActionBar().setTitle(recipient == null
-                                   ? getString(R.string.AndroidManifest__media_overview)
-                                   : getString(R.string.AndroidManifest__media_overview_named, recipient.toShortString()));
+                                   ? getString(R.string.AndroidManifest__all_media)
+                                   : getString(R.string.AndroidManifest__all_media_named, recipient.toShortString()));
   }
 
   @Override
@@ -162,11 +162,11 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
                                                                                      R.string.please_wait) {
           @Override
           protected List<SaveAttachmentTask.Attachment> doInBackground(Void... params) {
-            Cursor cursor                                   = DatabaseFactory.getImageDatabase(c).getImagesForThread(threadId);
+            Cursor cursor                                   = DatabaseFactory.getMediaDatabase(c).getMediaForThread(threadId);
             List<SaveAttachmentTask.Attachment> attachments = new ArrayList<>(cursor.getCount());
 
             while (cursor != null && cursor.moveToNext()) {
-              ImageRecord record = ImageRecord.from(cursor);
+              MediaRecord record = MediaRecord.from(cursor);
               attachments.add(new SaveAttachmentTask.Attachment(record.getAttachment().getDataUri(),
                                                                 record.getContentType(),
                                                                 record.getDate()));
@@ -220,7 +220,7 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
   @Override
   public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
     Log.w(TAG, "onLoadFinished()");
-    gridView.setAdapter(new ImageMediaAdapter(this, masterSecret, cursor, threadId));
+    gridView.setAdapter(new MediaAdapter(this, masterSecret, cursor, threadId));
     noImages.setVisibility(gridView.getAdapter().getItemCount() > 0 ? View.GONE : View.VISIBLE);
     invalidateOptionsMenu();
   }
@@ -240,7 +240,7 @@ public class MediaOverviewActivity extends PassphraseRequiredActionBarActivity i
 
     @Override
     public Cursor getCursor() {
-      return DatabaseFactory.getImageDatabase(getContext()).getImagesForThread(threadId);
+      return DatabaseFactory.getMediaDatabase(getContext()).getMediaForThread(threadId);
     }
   }
 }