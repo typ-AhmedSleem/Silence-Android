b092720f4866425f5d82ec5a7af2229e655b15a3
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-10-08 18:31:13 +0300

    feat: implemented the new search feature

--- src/main/java/org/smssecure/smssecure/search/SearchResultsAdapter.kt
+++ src/main/java/org/smssecure/smssecure/search/SearchResultsAdapter.kt
@@ -1,4 +1,76 @@
 package org.smssecure.smssecure.search
 
-class SearchResultsAdapter {
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.TextView
+import com.jude.easyrecyclerview.adapter.BaseViewHolder
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.smssecure.smssecure.ConversationListItem
+import org.smssecure.smssecure.R
+import org.smssecure.smssecure.color.MaterialColor
+import org.smssecure.smssecure.components.AvatarImageView
+import org.smssecure.smssecure.contacts.avatars.ContactPhotoFactory
+import org.smssecure.smssecure.crypto.MasterSecret
+import java.util.Locale
+
+class SearchResultsAdapter(
+	context: Context,
+	val secret: MasterSecret,
+	val locale: Locale
+) : RecyclerArrayAdapter<GlobalSearchResult>(context) {
+
+	override fun OnCreateViewHolder(parent: ViewGroup, type: Int): BaseViewHolder<*> {
+		return if (type == 1) ContactResultVH(parent)
+		else ConversationResultVH(parent, secret, locale)
+	}
+
+	override fun getViewType(position: Int): Int {
+		return if (getItem(position) is GlobalSearchResult.ContactSearchResult) 1 else 2
+	}
+
+	private class ContactResultVH(parent: ViewGroup) : BaseViewHolder<GlobalSearchResult.ContactSearchResult>(parent, R.layout.item_contact_result) {
+
+		private val tvName: TextView = itemView.findViewById(R.id.name)
+		private val tvNumber: TextView = itemView.findViewById(R.id.number)
+		private val aivAvatar: AvatarImageView = itemView.findViewById(R.id.contact_photo_image)
+
+		@OptIn(DelicateCoroutinesApi::class)
+		override fun setData(result: GlobalSearchResult.ContactSearchResult) {
+			tvName.text = result.contact.name
+			tvNumber.text = result.contact.number
+
+			GlobalScope.launch(Dispatchers.IO) {
+				val bgColor = MaterialColor.AMBER
+				val contactPhoto = async {
+					ContactPhotoFactory.getContactPhoto(
+						context,
+						result.contact.photoUri,
+						result.contact.name
+					).asDrawable(
+						context,
+						bgColor.toConversationColor(context)
+					)
+				}
+				withContext(Dispatchers.Main) { aivAvatar.setImageDrawable(contactPhoto.await()) }
+			}
+		}
+
+	}
+
+	private class ConversationResultVH(parent: ViewGroup, val secret: MasterSecret, val locale: Locale) : BaseViewHolder<GlobalSearchResult.MessageSearchResult>(parent, R.layout.conversation_list_item_view) {
+
+		private val listItem = itemView as ConversationListItem
+
+		override fun setData(result: GlobalSearchResult.MessageSearchResult) {
+			listItem.bind(secret, result.conversation.record, locale, emptySet(), false)
+		}
+
+	}
+
 }
--- src/main/java/org/smssecure/smssecure/search/SearchStickyHeaderAdapter.kt
+++ src/main/java/org/smssecure/smssecure/search/SearchStickyHeaderAdapter.kt
@@ -1,4 +1,44 @@
 package org.smssecure.smssecure.search
 
-class SearchStickyHeaderAdapter {
+import android.content.Context
+import android.view.ViewGroup
+import com.google.android.material.textview.MaterialTextView
+import com.jude.easyrecyclerview.adapter.BaseViewHolder
+import com.jude.easyrecyclerview.decoration.StickyHeaderDecoration
+import org.smssecure.smssecure.R
+
+class SearchStickyHeaderAdapter(val context: Context, val adapter: SearchResultsAdapter) : StickyHeaderDecoration.IStickyHeaderAdapter<SearchStickyHeaderAdapter.SearchStickyHeaderVH> {
+
+	override fun getHeaderId(position: Int): Long {
+		return if (adapter.getItem(position) is GlobalSearchResult.ContactSearchResult) TYPE_CONTACT
+		else TYPE_THREAD
+	}
+
+	override fun onCreateHeaderViewHolder(parent: ViewGroup): SearchStickyHeaderVH {
+		return SearchStickyHeaderVH(parent)
+	}
+
+	override fun onBindHeaderViewHolder(vh: SearchStickyHeaderVH, position: Int) {
+		vh.setData(adapter.getItem(position))
+	}
+
+	class SearchStickyHeaderVH(parent: ViewGroup) : BaseViewHolder<GlobalSearchResult>(parent, R.layout.sticky_header_search) {
+
+		override fun setData(result: GlobalSearchResult) {
+			(itemView as MaterialTextView).apply {
+				setText(
+					if (result is GlobalSearchResult.ContactSearchResult) R.string.contacts
+					else R.string.conversations
+				)
+			}
+		}
+	}
+
+	companion object {
+
+		private const val TYPE_CONTACT = 1L
+		private const val TYPE_THREAD = 2L
+
+	}
+
 }
