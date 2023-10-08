60c181adea06c9756ccdac7cc0e9463340c8ef98
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-10-08 18:24:39 +0300

    feat: impelemented contacts search

--- src/main/java/org/smssecure/smssecure/search/contacts/ContactRecord.kt
+++ src/main/java/org/smssecure/smssecure/search/contacts/ContactRecord.kt
@@ -1,11 +1,15 @@
 package org.smssecure.smssecure.search.contacts
 
-data class Contact(
+import android.net.Uri
+import java.net.URI
+
+data class ContactRecord(
 	val id: Int,
 	val name: String,
-	val number: String
+	val number: String,
+	val photoUri: Uri?
 ) {
 	override fun toString(): String {
-		return "Contact[($id): $name -> $number]"
+		return "Contact[($id): $name -> $number | $photoUri]"
 	}
 }
--- src/main/java/org/smssecure/smssecure/search/contacts/Contacts.kt
+++ src/main/java/org/smssecure/smssecure/search/contacts/Contacts.kt
@@ -1,4 +1,51 @@
 package org.smssecure.smssecure.search.contacts
 
-class Contacts {
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.provider.ContactsContract.CommonDataKinds.Phone
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.smssecure.smssecure.database.DatabaseFactory
+
+object Contacts {
+
+	@JvmStatic
+	suspend fun queryContacts(context: Context, query: String): Array<ContactRecord> {
+		return withContext(Dispatchers.IO) {
+			val contacts = mutableListOf<ContactRecord>()
+			var cursor: Cursor? = null
+
+			try {
+				cursor = context.contentResolver.query(
+					Uri.withAppendedPath(
+						Phone.CONTENT_FILTER_URI,
+						Uri.encode(query)
+					),
+					arrayOf(Phone.CONTACT_ID, Phone.DISPLAY_NAME, Phone.NUMBER, Phone.PHOTO_URI),
+					null,
+					null,
+					null
+				)
+				Log.i("Contacts", "queryContacts: START =======================")
+				while (cursor != null && cursor.moveToNext()) {
+					contacts.add(
+						ContactRecord(
+							id = cursor.getInt(cursor.getColumnIndexOrThrow(Phone.CONTACT_ID)),
+							name = cursor.getString(cursor.getColumnIndexOrThrow(Phone.DISPLAY_NAME)),
+							number = cursor.getString(cursor.getColumnIndexOrThrow(Phone.NUMBER)),
+							photoUri = Uri.parse(cursor.getString(cursor.getColumnIndexOrThrow(Phone.PHOTO_URI)) ?: "")
+						)
+					)
+					Log.d("Contacts", "\t${contacts.last()}")
+				}
+				Log.i("Contacts", "queryContacts: END =====================")
+			} finally {
+				cursor?.close()
+			}
+			return@withContext contacts.toTypedArray()
+		}
+	}
+
 }
